754a0fe954cfabb095640e449479357598c6e503
Guido Huijsmans <Guido.Huijsmans@iter.org>
2009-08-31 07:27:56 +0000

    adding model302
    git-svn-id: https://scm.gforge.inria.fr/svn/aster/jorek2/trunk@63 d232b540-7924-0410-b3cd-8064bb5aaf68

---
+++ models/model302/initial_conditions.f90
@@ -0,0 +1,181 @@
+subroutine initial_conditions(my_id,node_list,element_list,xpoint2)
+!-----------------------------------------------------------------------
+!
+!-----------------------------------------------------------------------
+use data_structure
+use phys_module
+implicit none
+
+type (type_node_list)    :: node_list
+type (type_element_list) :: element_list
+type (type_surface_list) :: surface_list
+
+integer    :: my_id, i, in, mm, i_elm_axis, i_elm_xpoint
+real*8     :: amplitude, psi, psi_axis, theta, PI
+real*8     :: zn, dn_dpsi, dn_dpsi2, dn_dz, dn_dz2, dn_dpsi_dz, dn_dpsi3, dn_dpsi2_dz, dn_dpsi_dz2
+real*8     :: zT, dT_dpsi, dT_dpsi2, dT_dz, dT_dz2, dT_dpsi_dz, dT_dpsi3, dT_dpsi2_dz, dT_dpsi_dz2
+real*8     :: zFFprime,dFFprime_dpsi,dFFprime_dz, dFFprime_dpsi_dz, dFFprime_dz2, dFFprime_dpsi2
+real*8     :: R_axis, Z_axis, s_axis, t_axis, R, Z, BigR, T0, BigR_s, T0_s
+real*8     :: zjz, dj_dpsi, dj_dR, dj_dZ, dj_dR_dZ, dj_dR_DR, dj_dZ_dZ, dj_dpsi2, dj_dR_dpsi, dj_dZ_dpsi
+real*8     :: psi_n, psi_bnd,psi_xpoint,R_xpoint,Z_xpoint,s_xpoint,t_xpoint
+real*8     :: ps0_s, ps0_t, p_s, p_t, zj0_s, zj0_t,R_s, R_t, ps0_x, ps0_y, Z_s, Z_t, xjac, direction, Btot
+logical    :: xpoint2
+
+if (my_id .eq. 0) then
+  write(*,*) '***************************************'
+  write(*,*) '*      initial conditions  (300)      *'
+  write(*,*) '***************************************'
+endif
+
+PI = 2.d0 *asin(1.d0)
+
+if (my_id .eq. 0) then
+
+  call find_axis(node_list,element_list,psi_axis,R_axis,Z_axis,i_elm_axis,s_axis,t_axis)
+  if (xpoint2) call find_xpoint(node_list,element_list,psi_xpoint,R_xpoint,Z_xpoint,i_elm_xpoint,s_xpoint,t_xpoint)
+
+  psi_bnd = 0.d0
+  if (xpoint2) psi_bnd = psi_xpoint
+
+  do i=1,node_list%n_nodes
+
+    psi = node_list%node(i)%values(1,1,1)
+    R   = node_list%node(i)%x(1,1)
+    Z   = node_list%node(i)%x(1,2)
+
+    call density(    xpoint2, Z, Z_xpoint, psi,psi_axis,psi_bnd,zn,dn_dpsi,dn_dz,dn_dpsi2,dn_dz2,             &
+                                                               dn_dpsi_dz,dn_dpsi3,dn_dpsi_dz2, dn_dpsi2_dz)
+
+    call temperature(xpoint2, Z, Z_xpoint, psi,psi_axis,psi_bnd,zT,dT_dpsi,dT_dz,dT_dpsi2,dT_dz2,             &
+                                                               dT_dpsi_dz,dT_dpsi3,dT_dpsi_dz2, dT_dpsi2_dz)
+
+    call FFprime(    xpoint2, Z, Z_xpoint, psi,psi_axis,psi_bnd,zFFprime,dFFprime_dpsi,dFFprime_dz, &
+                                                               dFFprime_dpsi2,dFFprime_dz2, dFFprime_dpsi_dz)
+
+    node_list%node(i)%values(1,1,5) = zn
+    node_list%node(i)%values(1,2,5) = dn_dpsi  * node_list%node(i)%values(1,2,1) + dn_dz * node_list%node(i)%x(2,2)
+    node_list%node(i)%values(1,3,5) = dn_dpsi  * node_list%node(i)%values(1,3,1) + dn_dz * node_list%node(i)%x(3,2)
+    node_list%node(i)%values(1,4,5) = dn_dpsi  * node_list%node(i)%values(1,4,1) + dn_dz * node_list%node(i)%x(4,2) &
+                                    + dn_dpsi2 * node_list%node(i)%values(1,2,1) * node_list%node(i)%values(1,3,1)  &
+                                    + dn_dz2   * node_list%node(i)%x(2,2)        * node_list%node(i)%x(3,2)
+
+    node_list%node(i)%values(1,1,6) = zT
+    node_list%node(i)%values(1,2,6) = dT_dpsi  * node_list%node(i)%values(1,2,1) + dT_dz * node_list%node(i)%x(2,2)
+    node_list%node(i)%values(1,3,6) = dT_dpsi  * node_list%node(i)%values(1,3,1) + dT_dz * node_list%node(i)%x(3,2)
+    node_list%node(i)%values(1,4,6) = dT_dpsi  * node_list%node(i)%values(1,4,1) + dT_dz * node_list%node(i)%x(4,2) &
+                                    + dT_dpsi2 * node_list%node(i)%values(1,2,1) * node_list%node(i)%values(1,3,1)  &
+                                    + dT_dz2   * node_list%node(i)%x(2,2)        * node_list%node(i)%x(3,2)
+
+    node_list%node(i)%values(1,1,7) = 0.d0        ! parallel velocity
+    node_list%node(i)%values(1,2,7) = 0.d0
+    node_list%node(i)%values(1,3,7) = 0.d0
+    node_list%node(i)%values(1,4,7) = 0.d0
+    
+    node_list%node(i)%deltas = 0.d0
+
+  enddo
+
+endif
+
+!----------------------------------------- flux boundary perturbation (to be completed, see Marina)
+!if (my_id .eq. 0) then
+!  do i=1,node_list%n_nodes
+!    psi = node_list%node(i)%values(1,1,1)
+!    R   = node_list%node(i)%x(1,1)
+!    Z   = node_list%node(i)%x(1,2)
+!    theta = atan2(Z-Z_axis, R-R_axis)
+!    psi_bnd = 0.d0
+!    if (xpoint2) psi_bnd = psi_xpoint
+!    psi_n = (psi - psi_axis)/(psi_bnd - psi_axis)     
+!    if (node_list%node(i)%boundary .ne. 0) then
+!      node_list%node(i)%values(2,1,1) =  0.01 * sin(2.d0*theta)
+!      node_list%node(i)%values(3,1,1) = -0.01 * cos(2.d0*theta) 
+!      node_list%node(i)%values(2,3,1) =  0.01 * cos(2.d0*theta) * 2.d0*PI/float(n_tht)
+!      node_list%node(i)%values(3,3,1) = +0.01 * sin(2.d0*theta) * 2.d0*PI/float(n_tht)
+!    endif
+!  enddo
+!call poisson(my_id,-2,node_list,element_list,1,3,2,xpoint2) 
+!call poisson(my_id,-2,node_list,element_list,1,3,3,xpoint2) 
+!endif    
+    
+!---------------------------- initialise perturbations
+amplitude = 1.d-12
+mm = 2
+
+do in=2,n_tor
+
+  if (my_id .eq. 0) then
+
+    do i=1,node_list%n_nodes
+
+      psi = node_list%node(i)%values(1,1,1)
+      Z   = node_list%node(i)%x(1,2)
+
+      psi_bnd = 0.d0
+      if (xpoint2) psi_bnd = psi_xpoint
+      psi_n = (psi - psi_axis)/(psi_bnd - psi_axis)
+
+      node_list%node(i)%values(in,1,4) = amplitude * psi_n * (1.d0 -psi_n)
+      node_list%node(i)%values(in,2,4) = amplitude * (1. - 2.d0 * psi_n)/(psi_bnd - psi_axis) * node_list%node(i)%values(1,2,1)
+      node_list%node(i)%values(in,3,4) = amplitude * (1. - 2.d0 * psi_n)/(psi_bnd - psi_axis) * node_list%node(i)%values(1,3,1)
+      node_list%node(i)%values(in,4,4) = amplitude * (1. - 2.d0 * psi_n)/(psi_bnd - psi_axis) * node_list%node(i)%values(1,4,1)
+      
+      if (xpoint2 .and. ((psi_n .gt. 1.d0) .or. (Z .lt. Z_xpoint))) then
+        node_list%node(i)%values(in,1:4,4) = 0.d0
+      endif
+
+      node_list%node(i)%deltas = 0.d0
+
+    enddo
+
+  endif
+
+  call poisson(my_id,1,node_list,element_list,4,2,in,xpoint2)   !----------- for Poisson (toroidal) use 1
+
+! call poisson(my_id,2,node_list,element_list,4,2,in,xpoint2) !----------- for cylinder use 2
+
+enddo
+
+!----------------------------------- fill in parallel velocity at boundary (on open field lines)
+do i=1,node_list%n_nodes
+
+  if ((node_list%node(i)%boundary .eq. 1) .or. (node_list%node(i)%boundary .eq. 3)) then
+
+    ps0_s     = node_list%node(i)%values(1,2,1)
+    ps0_t     = node_list%node(i)%values(1,3,1)
+    R_s       = node_list%node(i)%x(2,1)
+    R_t       = node_list%node(i)%x(3,1)
+    Z_s       = node_list%node(i)%x(2,2)
+    Z_t       = node_list%node(i)%x(3,2)
+
+    xjac  =  R_s*Z_t - R_t*Z_s
+    ps0_x = (	Z_t * ps0_s - Z_s * ps0_t ) / xjac
+    ps0_y = ( - R_t * ps0_s + R_s * ps0_t ) / xjac
+
+    direction = + ps0_x / abs(ps0_x)		 ! temporary solution for lower x-point only
+
+    Btot = sqrt(F0**2 + ps0_x**2 + ps0_y**2) / BigR
+
+    do in=1,n_tor
+
+      BigR = node_list%node(i)%x(1,1)
+      T0   = node_list%node(i)%values(in,1,6)
+      node_list%node(i)%values(in,1,n_var) = direction / Btot * sqrt(GAMMA * T0)
+
+      BigR_s = node_list%node(i)%x(2,1)
+      T0_s   = node_list%node(i)%values(in,2,6)
+      node_list%node(i)%values(in,2,n_var) = BigR_s / (BigR*Btot) * sqrt(GAMMA * T0) + 0.5d0 / Btot * sqrt(GAMMA / T0) * T0_s
+      node_list%node(i)%values(in,2,n_var) = direction *  node_list%node(i)%values(in,2,n_var)
+
+      write(*,'(A,8e14.6)') ' Boundary condition (eq): ',BigR,psi_xpoint,node_list%node(i)%values(1,1,1),ps0_x,ps0_y, &
+			    node_list%node(i)%values(in,1,n_var),BigR/F0 * sqrt(GAMMA*T0)
+
+    enddo
+  endif
+enddo
+
+!call add_pellet(node_list,element_list,5.,0.08,0.03,R_geo-0.78,Z_geo)
+!call add_pellet(node_list,element_list,25.,0.08,0.03,R_geo+0.85,Z_geo)
+
+return
+end
---
+++ models/model302/sources.f90
@@ -0,0 +1,22 @@
+subroutine sources(xpoint2,Z,Z_xpoint,psi,psi_axis,psi_bnd,particle_source,heat_source)
+!-----------------------------------------------------------------------
+!
+!-----------------------------------------------------------------------
+use phys_module
+
+implicit none
+
+logical :: xpoint2
+real*8  :: Z, Z_xpoint, psi, psi_axis, psi_bnd, psi_n, sig_n, sig_T
+real*8  :: particle_source, heat_source
+
+sig_n = 0.1
+sig_T = 0.1
+
+psi_n = (psi - psi_axis) / (psi_bnd - psi_axis)
+
+particle_source = particlesource !* (0.5d0 - 0.5d0*tanh((psi_n - 1.d0)/sig_n))
+heat_source     = heatsource     * (0.5d0 - 0.5d0*tanh((psi_n - 1.d0)/sig_T))
+
+return
+end
