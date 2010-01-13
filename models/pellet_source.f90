a13157379546098837b4402889cd62c0288afea8
Guido Huijsmans <Guido.Huijsmans@iter.org>
2010-01-13 15:43:05 +0000

    adding pellet_source
    git-svn-id: https://scm.gforge.inria.fr/svn/aster/jorek2/trunk@90 d232b540-7924-0410-b3cd-8064bb5aaf68

---
+++ models/pellet_source.f90
@@ -0,0 +1,25 @@
+subroutine pellet_source(pellet_amplitude,pellet_R,pellet_Z,pellet_phi,pellet_radius, &
+                         pellet_sig,pellet_length,R,Z,phi,particle_source)
+
+use data_structure
+use phys_module
+implicit none
+
+real*8 :: R, Z, phi, particle_source
+real*8 :: pellet_amplitude, pellet_R, pellet_Z, pellet_phi, pellet_radius, pellet_sig, pellet_length
+real*8 :: radius, atn, atn_phi, PI
+
+PI = 3.14159265358979
+
+if (phi .gt. PI) phi = 2*PI - phi
+
+radius = sqrt((R-pellet_R)**2 + (Z-pellet_Z)**2)
+
+atn     = (0.5d0 - 0.5d0*tanh((radius - pellet_radius)/pellet_sig))
+
+atn_phi = (0.5d0 - 0.5d0*tanh(abs((phi- pellet_phi))/pellet_length))
+
+particle_source = pellet_amplitude * atn * atn_phi
+
+return
+end
