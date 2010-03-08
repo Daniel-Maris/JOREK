63f6dfc8695075cab56136ff64f56476a68a49e1
Guido Huijsmans <Guido.Huijsmans@iter.org>
2010-03-08 10:55:21 +0000

    add flux dependence pellet_source
    git-svn-id: https://scm.gforge.inria.fr/svn/aster/jorek2/trunk@122 d232b540-7924-0410-b3cd-8064bb5aaf68

--- models/pellet_source.f90
+++ models/pellet_source.f90
@@ -1,11 +1,12 @@
-subroutine pellet_source(pellet_amplitude,pellet_R,pellet_Z,pellet_phi,pellet_radius, &
-                         pellet_sig,pellet_length,R,Z,phi,particle_source)
+subroutine pellet_source(pellet_amplitude,pellet_R,pellet_Z,pellet_psi,pellet_phi, &
+                         pellet_radius, pellet_delta_psi, pellet_sig, pellet_length, &
+			 R,Z,psi,phi,particle_source)
 
 implicit none
 
-real*8 :: R, Z, phi, particle_source
+real*8 :: R, Z, psi, phi, particle_source
 real*8 :: pellet_amplitude, pellet_R, pellet_Z, pellet_phi, pellet_radius, pellet_sig, pellet_length
-real*8 :: radius, atn, atn_phi, PI
+real*8 :: pellet_psi, pellet_delta_psi, radius, atn, atn_psi, atn_phi, PI
 
 PI = 3.14159265358979
 
@@ -15,9 +16,11 @@ radius = sqrt((R-pellet_R)**2 + (Z-pellet_Z)**2)
 
 atn     = (0.5d0 - 0.5d0*tanh((radius - pellet_radius)/pellet_sig))
 
+atn_psi = (0.5d0 - 0.5d0*tanh(abs(psi- pellet_psi)/pellet_delta_psi))
+
 atn_phi = (0.5d0 - 0.5d0*tanh((phi- pellet_phi)/pellet_length))
 
-particle_source = pellet_amplitude * atn * atn_phi
+particle_source = pellet_amplitude * atn * atn_phi * atn_psi
 
 return
 end
