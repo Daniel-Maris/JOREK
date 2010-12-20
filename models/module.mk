4156ef1529b14ad87ef0b2ccb9526280f75031d8
Xavier Lacoste <xavier.lacoste@inria.fr>
2010-12-20 17:15:14 +0000

    Mis a jour des makefiles NewMake.mk/module.mk
    git-svn-id: https://scm.gforge.inria.fr/svn/aster/jorek2/trunk@230 d232b540-7924-0410-b3cd-8064bb5aaf68

--- models/module.mk
+++ models/module.mk
@@ -1,21 +1,21 @@
 DIR=models
-JOREK2_MAIN_SRC  := $(JOREK2_MAIN_SRC)		\
-	$(DIR)/current.f90 			\
-	$(DIR)/density.f90 			\
-	$(DIR)/element_matrix_GS.f90 		\
-	$(DIR)/element_matrix_GS_inverse.f90 	\
+JOREK2_MAIN_SRC  := $(JOREK2_MAIN_SRC)			\
+	$(DIR)/current.f90 				\
+	$(DIR)/density.f90 				\
+	$(DIR)/element_matrix_GS.f90 			\
+	$(DIR)/element_matrix_GS_inverse.f90 		\
 	$(DIR)/element_matrix_GS_perturbation.f90 	\
-	$(DIR)/element_matrix_poisson.f90 	\
+	$(DIR)/element_matrix_poisson.f90 		\
 	$(DIR)/element_matrix_poisson_inverse.f90 	\
-	$(DIR)/equilibrium.f90 			\
-	$(DIR)/ffprime.f90 			\
-	$(DIR)/poisson.f90 			\
-	$(DIR)/temperature.f90 			\
-	$(DIR)/temperature_i.f90 		\
-	$(DIR)/temperature_e.f90 		\
-	$(DIR)/mod_phys_module.f90 		\
-	$(DIR)/mod_nodes_elements.f90		\
-	$(DIR)/chgmt_node.f90			\
+	$(DIR)/equilibrium.f90 				\
+	$(DIR)/ffprime.f90 				\
+	$(DIR)/poisson.f90 				\
+	$(DIR)/temperature.f90 				\
+	$(DIR)/temperature_i.f90 			\
+	$(DIR)/temperature_e.f90 			\
+	$(DIR)/mod_phys_module.f90 			\
+	$(DIR)/mod_nodes_elements.f90			\
+	$(DIR)/chgmt_node.f90				\
 	$(DIR)/log_parameters.f90			\
 	$(DIR)/read_num_profiles.f90 			\
 	$(DIR)/pellet_source.f90			\
