2798a7febb67b4356fab91accabc5fed7a440c36
Matthias Hoelzl <mhoelzl@ipp.mpg.de>
2011-11-22 09:35:56 +0000

    Minor correction for numerical input profiles.
    git-svn-id: https://scm.gforge.inria.fr/svn/aster/jorek2/trunk@514 d232b540-7924-0410-b3cd-8064bb5aaf68

--- models/read_num_profiles.f90
+++ models/read_num_profiles.f90
@@ -9,7 +9,7 @@ subroutine read_num_profiles()
   num_rho = ( rho_file /= 'none' )
   if ( num_rho ) then
     call readProf(num_rho_x, num_rho_y0, num_rho_len, rho_file)
-    if ( num_rho_len <= 2 ) then 
+    if ( num_rho_len < 2 ) then 
       write(*,*) '  ERROR: Could not read the numerical profile "'//trim(rho_file)//'".'
       stop
     end if
@@ -20,7 +20,7 @@ subroutine read_num_profiles()
   num_T = ( T_file /= 'none' )
   if ( num_T ) then
     call readProf(num_T_x, num_T_y0, num_T_len, T_file)
-    if ( num_T_len <= 2 ) then 
+    if ( num_T_len < 2 ) then 
       write(*,*) '  ERROR: Could not read the numerical profile "'//trim(T_file)//'".'
       stop
     end if
@@ -31,7 +31,7 @@ subroutine read_num_profiles()
   num_ffprime = ( ffprime_file /= 'none' )
   if ( num_ffprime ) then
     call readProf(num_ffprime_x, num_ffprime_y0, num_ffprime_len, ffprime_file)
-    if ( num_ffprime_len <= 2 ) then 
+    if ( num_ffprime_len < 2 ) then 
       write(*,*) '  ERROR: Could not read the numerical profile "'//trim(ffprime_file)//'".'
       stop
     end if
@@ -41,7 +41,7 @@ subroutine read_num_profiles()
   num_d_perp = ( d_perp_file /= 'none' )
   if ( num_d_perp ) then
     call readProf(num_d_perp_x, num_d_perp_y, num_d_perp_len, d_perp_file)
-    if ( num_d_perp_len <= 2 ) then 
+    if ( num_d_perp_len < 2 ) then 
       write(*,*) '  ERROR: Could not read the numerical profile "'//trim(d_perp_file)//'".'
       stop
     end if
@@ -50,7 +50,7 @@ subroutine read_num_profiles()
   num_zk_perp = ( zk_perp_file /= 'none' )
   if ( num_zk_perp ) then
     call readProf(num_zk_perp_x, num_zk_perp_y, num_zk_perp_len, zk_perp_file)
-    if ( num_zk_perp_len <= 2 ) then 
+    if ( num_zk_perp_len < 2 ) then 
       write(*,*) '  ERROR: Could not read the numerical profile "'//trim(zk_perp_file)//'".'
       stop
     end if
@@ -60,7 +60,7 @@ subroutine read_num_profiles()
     num_zk_e_perp = ( zk_e_perp_file /= 'none' )
     if ( num_zk_e_perp ) then
       call readProf(num_zk_e_perp_x, num_zk_e_perp_y, num_zk_e_perp_len, zk_e_perp_file)
-      if ( num_zk_e_perp_len <= 2 ) then 
+      if ( num_zk_e_perp_len < 2 ) then 
         write(*,*) '  ERROR: Could not read the numerical profile "'//trim(zk_e_perp_file)//'".'
         stop
       end if
@@ -69,7 +69,7 @@ subroutine read_num_profiles()
     num_zk_i_perp = ( zk_i_perp_file /= 'none' )
     if ( num_zk_i_perp ) then
       call readProf(num_zk_i_perp_x, num_zk_i_perp_y, num_zk_i_perp_len, zk_i_perp_file)
-      if ( num_zk_i_perp_len <= 2 ) then 
+      if ( num_zk_i_perp_len < 2 ) then 
         write(*,*) '  ERROR: Could not read the numerical profile "'//trim(zk_i_perp_file)//'".'
         stop
       end if
