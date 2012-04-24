7cd94d336fe1efa27badce2f773c38e8d6f3a897
Stanislas Pamela <stanislas.pamela@ccfe.ac.uk>
2012-04-24 06:35:41 +0000

    Addapted jorek2vtk to model_400
    git-svn-id: https://scm.gforge.inria.fr/svn/aster/jorek2/trunk@608 d232b540-7924-0410-b3cd-8064bb5aaf68

--- diagnostics/jorek2vtk.f90
+++ diagnostics/jorek2vtk.f90
@@ -11,6 +11,7 @@ implicit none
 
 type (type_node_list)   , pointer :: node_list
 type (type_element_list), pointer :: element_list
+type (type_surface_list)          :: flux_list
 
 integer               :: nnoel, nnos, nel, nsub, inode, ielm, n_scalars, n_vectors
 real*4,allocatable    :: xyz (:,:), scalars(:,:), vectors(:,:,:)
@@ -20,15 +21,36 @@ integer               :: i, j, k, m, etype, irst, int, i_var, i_tor, i_tor_old, 
 character             :: buffer*80, lf*1, str1*12, str2*12
 character*12, allocatable :: scalar_names(:), vector_names(:)
 real*8                :: s, t
-real*8                :: P,P_s,P_t,P_st,P_ss,P_tt,R,R_s,R_t,R_st,R_ss,R_tt,Z,Z_s,Z_t,Z_st,Z_ss,Z_tt
-real*8                :: Psi,Ps_s,Ps_t,Ps_st,Ps_ss,Ps_tt, ZJ,ZJ_s,ZJ_t,ZJ_st,ZJ_ss,ZJ_tt, W,W_s,W_t,W_st,W_ss,W_tt
-real*8                :: U,U_s,U_t,U_st,U_ss,U_tt, RHO,RHO_s,RHO_t,RHO_st,RHO_ss,RHO_tt, TT,TT_s,TT_t,TT_st,TT_ss,TT_tt
-real*8                :: u0_x, u0_y, xjac, v_perp, Psi_J, R_p, error, zj_x, zj_y, ps_x, ps_y, TT_x, TT_y, TT_p
-real*8                :: RHO_x, RHO_y, RHO_p, V, V_s, V_t, V_st, V_ss, V_tt, Btot, BigR
-real*8                :: psi_bnd,psi_axis,R_axis,Z_axis,s_axis,t_axis
-real*8                :: psi_xpoint(2),R_xpoint(2),Z_xpoint(2),s_xpoint(2),t_xpoint(2)
-real*8                :: ps0, psi_norm, particle_source, D_prof, ZK_prof, grad_psi, source_pellet, ZKpar_T
-real*8                :: w0_x, w0_y, w0_xx, w0_yy, xjac_x, xjac_y
+real*8                :: P,P_s,P_t,P_st,P_ss,P_tt
+real*8                :: R,R_s,R_t,R_st,R_ss,R_tt
+real*8                :: Z,Z_s,Z_t,Z_st,Z_ss,Z_tt
+real*8                :: Psi,Ps_s,Ps_t,Ps_st,Ps_ss,Ps_tt
+real*8                :: ZJ,ZJ_s,ZJ_t,ZJ_st,ZJ_ss,ZJ_tt
+real*8                :: U,U_s,U_t,U_st,U_ss,U_tt
+real*8                :: W,W_s,W_t,W_st,W_ss,W_tt
+real*8                :: RHO,RHO_s,RHO_t,RHO_st,RHO_ss,RHO_tt
+real*8                :: TT,TT_s,TT_t,TT_st,TT_ss,TT_tt
+real*8                :: Ti,Ti_s,Ti_t,Ti_st,Ti_ss,Ti_tt
+real*8                :: Te,Te_s,Te_t,Te_st,Te_ss,Te_tt
+real*8                :: V, V_s, V_t, V_st, V_ss, V_tt
+real*8                :: ps_x, ps_y
+real*8                :: u0_x, u0_y
+real*8                :: zj_x, zj_y
+real*8                :: w0_x, w0_y, w0_xx, w0_yy
+real*8                :: RHO_x, RHO_y, RHO_p
+real*8                :: TT_x, TT_y, TT_p
+real*8                :: Ti_x, Ti_y, Ti_p
+real*8                :: Te_x, Te_y, Te_p
+real*8                :: psi_axis,      R_axis,      Z_axis,      s_axis,      t_axis
+real*8                :: psi_xpoint(2), R_xpoint(2), Z_xpoint(2), s_xpoint(2), t_xpoint(2)
+real*8                :: ps0, psi_norm, psi_bnd, grad_psi
+real*8                :: xjac, xjac_x, xjac_y, v_perp, Psi_J, R_p, error, Btot, BigR
+real*8                :: particle_source, D_prof, ZK_prof, source_pellet, ZKpar_T
+integer 	      :: i_find, i_elm_find(8)
+real*8  	      :: Router,dRRg1_dr,dRRg1_ds,dRRg1_drs,dRRg1_drr,dRRg1_dss
+real*8  	      :: Zouter,dZZg1_dr,dZZg1_ds,dZZg1_drs,dZZg1_drr,dZZg1_dss
+real*8  	      :: s_find(8), t_find(8)
+real*8                :: Jb, minRad
 integer               :: i_elm_axis, i_elm_xpoint(2), k_tor, ifail, ierr
 logical               :: without_n0_mode
 !====================== --- add the diagnostics Er, Vtheta and [not yet Vneo]
@@ -73,7 +95,7 @@ write(*,*)
 call flush_it(6)
 
 ! --- Number of scalars to write to the VTK output file
-n_scalars = n_var + 10+4
+n_scalars = n_var + 10+5
 
 ! --- Number of vectors to write to the VTK output file
 n_vectors = 0
@@ -86,7 +108,7 @@ scalar_names(1:n_var) = variable_names(1:n_var)
 scalar_names(n_var+1:n_scalars) = (/ &
   'pressure    ', 'E_flux_Kpar ', 'E_flux_kperp', 'E_flux_Vpar ', 'E_flux_Vperp', 'D_flux_Dperp', &
   'D_flux_Vpar ', 'D_flux_Vperp', 'Er          ', 'Vtheta      ', 'Mach_par    ',&
-'Mach_pol    ','Vsound      ','Btot        '/)
+  'Mach_pol    ', 'Vsound      ', 'Btot        ', 'J-bootstrap '/)
 
 !vector_names = (/ 'v_perp  ','v_par   ','V_tot   '/)
 
@@ -122,6 +144,22 @@ if (xpoint) then
   endif
 else
   psi_bnd = 0.d0
+endif
+
+if (jorek_model .eq. 400) then      
+  flux_list%n_psi = 1
+  call tr_allocate(flux_list%psi_values,1,flux_list%n_psi,"flux_list%psi_values",CAT_GRID)
+  flux_list%psi_values(1) = psi_bnd
+  call find_flux_surfaces(xpoint,xcase,node_list,element_list,flux_list)
+  call find_theta_surface(node_list, element_list, flux_list, 1, 0.0, R_axis, Z_axis,i_elm_find,s_find,t_find,i_find)
+  call interp_RZ(node_list,element_list,i_elm_find(1),s_find(1),t_find(1),&
+		 Router,dRRg1_dr,dRRg1_ds,dRRg1_drs,dRRg1_drr,dRRg1_dss,  &
+		 Zouter,dZZg1_dr,dZZg1_ds,dZZg1_drs,dZZg1_drr,dZZg1_dss)
+  call tr_deallocate(flux_list%psi_values,"flux_list%psi_values",CAT_GRID)
+  minRad = Router - R_axis
+  write(*,*)'minor radius : ',minRad
+else
+  minRad = 0.0
 endif
 
 do i=1,element_list%n_elements
@@ -274,6 +312,7 @@ do i=1,element_list%n_elements
 
             u0_x = 0.d0; u0_y = 0.d0; ps_x = 0.d0; ps_y  = 0.d0; zj_x  = 0.d0; zj_y  = 0.d0;
             TT_x = 0.d0; TT_y = 0.d0; TT_p = 0.d0; RHO_x = 0.d0; RHO_y = 0.d0; RHO_p = 0.d0;
+            Ti_x = 0.d0; Ti_y = 0.d0; Ti_p = 0.d0; Te_x  = 0.d0; Te_y  = 0.d0; Te_p  = 0.d0
 
             w0_x = 0.d0; w0_y = 0.d0; w0_xx = 0.d0; w0_yy = 0.d0
 
@@ -297,6 +336,10 @@ do i=1,element_list%n_elements
                else
                   V=0; V_s=0; V_t=0; V_st=0; V_ss=0; V_tt=0
                end if
+               if ( jorek_model .eq. 400 ) then
+                  call interp(node_list,element_list,i,6,i_tor,s,t,Ti,Ti_s,Ti_t,Ti_st,Ti_ss,Ti_tt)
+                  call interp(node_list,element_list,i,8,i_tor,s,t,Te,Te_s,Te_t,Te_st,Te_ss,Te_tt)
+               end if
 
                if ((xjac .gt. 1.d-6)) then      ! avoid the axis
 
@@ -312,6 +355,15 @@ do i=1,element_list%n_elements
                   TT_x  = TT_x   + (   Z_t * TT_s - Z_s * TT_t )   / xjac * HZ(i_tor,i_plane)
                   TT_y  = TT_y   + ( - R_t * TT_s + R_s * TT_t )   / xjac * HZ(i_tor,i_plane)
                   TT_p  = TT_p   + TT * HZ_p(i_tor,1)
+
+                  if ( jorek_model .eq. 400 ) then
+                    Ti_x  = Ti_x   + (   Z_t * Ti_s - Z_s * Ti_t )   / xjac * HZ(i_tor,i_plane)
+                    Ti_y  = Ti_y   + ( - R_t * Ti_s + R_s * Ti_t )   / xjac * HZ(i_tor,i_plane)
+                    Ti_p  = Ti_p   + Ti * HZ_p(i_tor,1)
+                    Te_x  = Te_x   + (   Z_t * Te_s - Z_s * Te_t )   / xjac * HZ(i_tor,i_plane)
+                    Te_y  = Te_y   + ( - R_t * Te_s + R_s * Te_t )   / xjac * HZ(i_tor,i_plane)
+                    Te_p  = Te_p   + Te * HZ_p(i_tor,1)
+                  end if
 
                   RHO_x = RHO_x  + (   Z_t * RHO_s - Z_s * RHO_t ) / xjac * HZ(i_tor,i_plane)
                   RHO_y = RHO_y  + ( - R_t * RHO_s + R_s * RHO_t ) / xjac * HZ(i_tor,i_plane)
@@ -343,6 +395,14 @@ do i=1,element_list%n_elements
                psi_norm = 2.d0 - psi_norm
             endif
 
+            if ( jorek_model .eq. 400 ) then
+              call bootstrap_current_rhs(BigR, minRad, R_axis, psi_axis, psi_bnd,  &
+                                         psi, ps_x, ps_y, rho,  rho_x,  rho_y,     &
+				         Ti,  Ti_x, Ti_y, Te,   Te_x,   Te_y,       Jb)
+            else
+	      Jb = 0.d0
+            endif
+ 
             v_perp = R * sqrt(u0_x*u0_x + u0_y * u0_y)
 
             Btot = sqrt(F0**2 + ps_x**2 + ps_y**2) / BigR
@@ -373,6 +433,8 @@ do i=1,element_list%n_elements
                scalars(inode,n_var+7)  = scalars(inode,5) * scalars(inode,7) * Btot
 
                scalars(inode,n_var+8)  = BigR   * (u0_x * ps_y - u0_y * ps_x) / sqrt(ps_x*ps_x + ps_y*ps_y) * scalars(inode,5)
+
+               scalars(inode,n_var+15) = Jb
 
                !           call pellet_source(pellet_amplitude, pellet_R, pellet_Z, pellet_psi, pellet_phi, &
                !                        pellet_radius, pellet_delta_psi, pellet_sig, pellet_length,   &
