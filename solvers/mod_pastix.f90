6e7e241407f4f5c41a780c6e5b7be56c7e3790d0
Xavier Lacoste <xavier.lacoste@inria.fr>
2012-11-14 15:52:08 +0000

    Use PaStiX API instead of hardcoded values
    git-svn-id: https://scm.gforge.inria.fr/svn/aster/jorek2/trunk@714 d232b540-7924-0410-b3cd-8064bb5aaf68

--- solvers/mod_pastix.f90
+++ solvers/mod_pastix.f90
@@ -1,7 +1,8 @@
 module pastix_module             ! PastiX specific variables
+#include "pastix_fortran.h"
   integer(kind=8)       :: pastix_data
-  integer               :: pastix_iparm(128)
-  real*8                :: pastix_dparm(128)
+  integer               :: pastix_iparm(IPARM_SIZE)
+  real*8                :: pastix_dparm(DPARM_SIZE)
   integer,allocatable   :: pastix_perm_vars(:), pastix_iperm_vars(:)
   integer,allocatable   :: sparskit_work(:)
   integer,allocatable   :: ihwb(:),iwk(:)
@@ -9,16 +10,16 @@ module pastix_module             ! PastiX specific variables
 
   logical               :: use_pastix, pastix_initialised, pastix_analysed, pastix_smp_only, no_zeros_pastix
 
-  integer, parameter    :: pastix_facto    = 2
+  integer, parameter    :: pastix_facto    = API_FACT_LU
   integer, parameter    :: pastix_rhs      = 0
-  integer, parameter    :: pastix_sym      = 1
+  integer, parameter    :: pastix_sym      = API_SYM_NO
   integer               :: pastix_nthrd    = 1
   integer, parameter    :: pastix_iter     = 250
-  integer, parameter    :: pastix_verb     = 1
+  integer, parameter    :: pastix_verb     = API_VERBOSE_NO
   integer, parameter    :: pastix_ricar    = 0
   integer, parameter    :: pastix_iluk     = 3
   integer, parameter    :: pastix_amalg    = 5 
-  integer, parameter    :: pastix_endsolve = 5 ! 5: direct solve, 6: refinement step (not required usually)
+  integer, parameter    :: pastix_endsolve = API_TASK_SOLVE
   real*8,  parameter    :: pastix_epsilon  = 1.d-12
   real*8,  parameter    :: pastix_pivot    = 1.d-64
 end module pastix_module
