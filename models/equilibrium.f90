b23b0a5978e11aeffc33a3efd7594c51af001441
Willem Haverkort <J.W.Haverkort@cwi.nl>
2013-01-04 16:58:44 +0000

    The call to find_limiter removed in revision 743 is required for limiter plasmas. It has been reinserted within an if ( xpoint == .false ) clause.
    git-svn-id: https://scm.gforge.inria.fr/svn/aster/jorek2/trunk@747 d232b540-7924-0410-b3cd-8064bb5aaf68

--- models/equilibrium.f90
+++ models/equilibrium.f90
@@ -98,14 +98,16 @@ do iter = 1, n_iter
     psi_bnd = 99.d0
   endif
 
-!  call find_limiter(my_id,node_list,element_list,bnd_elm_list,psi_lim,R_lim,Z_lim)
-!  if ( (Z_lim .gt. Z_xpoint(1)) .and. (Z_lim .lt. Z_xpoint(2)) ) then
-!    if (psi_lim .lt. psi_bnd) then
-!      psi_bnd = psi_lim
-!      write(*,'(A,3f8.3)') ' LIMITER PLASMA ',psi_lim,R_lim,Z_lim
-!    endif
-!  endif
-    
+  if ( xpoint == .false. ) then
+    call find_limiter(my_id,node_list,element_list,bnd_elm_list,psi_lim,R_lim,Z_lim)
+    if ( (Z_lim .gt. Z_xpoint(1)) .and. (Z_lim .lt. Z_xpoint(2)) ) then
+      if (psi_lim .lt. psi_bnd) then
+        psi_bnd = psi_lim
+        write(*,'(A,3f8.3)') ' LIMITER PLASMA ',psi_lim,R_lim,Z_lim
+      endif
+    endif
+  endif
+
   if(xcase2 .eq. 1) write(*,'(A,3es14.6,i3)') ' PSI_AXIS, PSI_BND : ',psi_axis,psi_bnd,Z_xpoint(1),ifail
   if(xcase2 .eq. 2) write(*,'(A,3es14.6,i3)') ' PSI_AXIS, PSI_BND : ',psi_axis,psi_bnd,Z_xpoint(2),ifail
   
