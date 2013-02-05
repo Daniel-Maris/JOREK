f128171e0424f7bba0bc6c3d8d3d8e173c713282
Jane Pratt <J.L.Pratt@differ.nl>
2013-02-05 10:10:01 +0000

    adding diagnostics enbiggen and jordel to Makefile
    git-svn-id: https://scm.gforge.inria.fr/svn/aster/jorek2/trunk@762 d232b540-7924-0410-b3cd-8064bb5aaf68

--- elements/module.mk
+++ elements/module.mk
@@ -105,3 +105,26 @@ JOREK2_DIAGNO_SRC := $(JOREK2_DIAGNO_SRC)	\
 	$(DIR)/initialise_basis.f90 		\
 	$(DIR)/interp.f90 			\
 	$(DIR)/interp_RZ.f90
+
+JORDEL_SRC := $(JORDEL_SRC)                    \
+	$(DIR)/mod_gauss.f90                    \
+	$(DIR)/mod_basis_at_gaussian.f90        \
+	$(DIR)/hermite_1d.f90                   \
+	$(DIR)/initialise_basis.f90             \
+	$(DIR)/basis_functions.f90              \
+	$(DIR)/basis_functions1.f90             \
+	$(DIR)/basis_functions2.f90             \
+	$(DIR)/interp.f90                       \
+	$(DIR)/interp_RZ.f90
+
+ENBIGGEN_SRC := $(ENBIGGEN_SRC)               \
+	$(DIR)/mod_gauss.f90                    \
+	$(DIR)/mod_basis_at_gaussian.f90        \
+	$(DIR)/hermite_1d.f90                   \
+	$(DIR)/initialise_basis.f90             \
+	$(DIR)/basis_functions.f90              \
+	$(DIR)/basis_functions1.f90             \
+	$(DIR)/basis_functions2.f90             \
+	$(DIR)/interp.f90                       \
+	$(DIR)/interp_RZ.f90
+
