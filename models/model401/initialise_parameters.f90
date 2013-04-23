090ab8d37894d05d69085e738812ead0a33c8c62
Stanislas Pamela <stanislas.pamela@ccfe.ac.uk>
2013-04-23 10:49:15 +0000

    Adding a development model401 for SPAMela. Single fluid with optional routines for a new variable for the parallel thermal flux (although that does not work very well for now...).
    git-svn-id: https://scm.gforge.inria.fr/svn/aster/jorek2/trunk@846 d232b540-7924-0410-b3cd-8064bb5aaf68

---
+++ models/model401/initialise_parameters.f90
@@ -0,0 +1,139 @@
+!> Initialise input parameters and read the input namelist
+subroutine initialise_parameters(my_id, filename)
+
+use tr_module
+use phys_module
+use mumps_module,  only: use_mumps, no_zeros_mumps
+use murge_module,  only: use_murge, use_murge_element
+use pastix_module, only: use_pastix, no_zeros_pastix, pastix_smp_only
+use vacuum,        only: vacuum_preset
+use wsmp_module,   only: use_wsmp
+
+implicit none
+
+! --- Routine parameters
+integer,                      intent(in) :: my_id
+character(len=*),             intent(in) :: filename
+
+! --- Local variables
+integer :: ierr
+
+! --- Namelist with input parameters.
+namelist /in1/  tstep, nstep, tstep_n, nstep_n,                     &
+                eta, visco, visco_par,                              &
+                restart, regrid, bootstrap,                         &
+                n_R, n_Z, n_radial, n_pol, n_tht, n_flux,           &
+                n_open, n_private, n_leg,                           &
+                n_outer, n_inner, n_up_priv, n_up_leg,              &
+                psi_axis_init, XR_r, SIG_r, XR_tht, SIG_tht,        &
+                SIG_closed, SIG_open, SIG_private, SIG_theta,       &
+                SIG_leg_0, SIG_leg_1, dPSI_open, dPSI_private,      &
+                SIG_up_leg_0, SIG_up_leg_1, SIG_up_priv,            &
+                SIG_outer, SIG_inner,                               &
+                dPSI_outer, dPSI_inner, dPSI_up_priv,               &
+                nout, xr1, sig1, xr2, sig2,                         &
+                R_begin, R_end, Z_begin, Z_end,                     &
+                R_geo, Z_geo, amin, mf, fbnd, fpsi, mode,           &
+                R_boundary, Z_boundary, psi_boundary, n_boundary,   &
+                n_pfc,                                              &
+                Rmin_pfc, Rmax_pfc, Zmin_pfc, Zmax_pfc, current_pfc,&
+                F0, time_evol_scheme,                               &
+                zjz_0, zjz_1, zj_coef,                              &
+                rho_0, rho_1, rho_coef, central_density,            &
+                T_0,   T_1,   T_coef,                               &
+                Ti_0,  Ti_1,  Ti_coef,                              &
+                Te_0,  Te_1,  Te_coef,                              &
+                FF_0,  FF_1,  FF_coef,                              &
+                K_i_par, ZK_i_perp, K_e_par, ZK_e_perp,             &
+                Zk_par, ZK_perp, D_par, D_perp,                     &
+                Q_bar, sigma, gamma_sheath,                         &
+                particlesource, heatsource,                         &
+                heatsource_i, heatsource_e, tauIC,                  &
+                eta_num, visco_num, visco_par_num,                  &
+                D_perp_num, ZK_perp_num,                            &
+                pellet_amplitude, pellet_R, pellet_Z, pellet_phi,   &
+                pellet_radius, pellet_sig, pellet_length,           &
+                pellet_psi, pellet_delta_psi,                       &
+                ellip,tria_u,tria_l,quad_u,quad_l,                  &
+                xampl,xwidth,xsig,xtheta,xshift,xleft,xpoint,xcase, &
+                D_perp_file, ZK_e_perp_file, ZK_i_perp_file,        &
+                rho_file, T_file, ffprime_file, freeboundary_equil, &
+                freeboundary,resistive_wall,                        &
+                use_mumps, use_pastix, use_murge, use_murge_element,&
+                use_wsmp,                                           &
+                pastix_smp_only, refinement, grid_to_wall,          &
+                adaptive_time, equil, bench_without_plot,           &
+                no_zeros_pastix, no_zeros_mumps,                    &
+                eta_T_dependent, visco_T_dependent,                 &
+                heatsource_psin, heatsource_sig,                    &
+                particlesource_psin, particlesource_sig,            &
+                produce_live_data, gmres, gmres_max_iter,           &
+                linear_run, export_for_nemec,                       &
+#ifdef USE_HDF5
+                save_diagnostics_HDF5,h5_diag_nbtime,               &
+#endif
+                output_bnd_elements,                                &
+                n_limiter, R_limiter, Z_limiter, bc_natural_open
+
+if (my_id .eq. 0) then
+
+  ! --- Preset input parameters to reasonable default values.
+  call preset_parameters()
+  call vacuum_preset(my_id, freeboundary_equil, freeboundary, resistive_wall)
+  
+  ! --- Model-specific presets
+  T_file         = 'none'
+  Te_file        = 'none'
+  Ti_file        = 'none'
+  ZK_e_perp_file = 'none'
+  ZK_i_perp_file = 'none'
+  
+  T_0   =  1.d-6
+  T_1   =  1.d-8
+  
+  T_coef     = 0.d0
+  T_coef(1)  = -1.d0
+
+  heatsource   = 1.e-7
+
+  ZK_perp(1) = 1.d-5; ZK_perp(2) = 0.d0; ZK_perp(3)= 0.d0; ZK_perp(4)= 99.d0; ZK_perp(5) = 99.d0
+  ZK_par     = 1.d+6
+  
+  ! --- Read input parameters from namelist.
+  if (trim(filename) .ne. "__NO_FILENAME__" ) then
+     open(42, file=filename, status='old', action='read', iostat=ierr)
+    if ( ierr /= 0 ) then
+      write(*,*) 'ERROR: COULD NOT OPEN NAMELIST FILE "', trim(filename), '".'
+      stop
+    end if
+    read(42,in1)
+    close(42)
+  else
+    read(5,in1)
+  end if
+  
+  if (sum(nstep_n) .gt. 0) then
+    nstep = sum(nstep_n)
+  else
+    tstep_n    = 0.d0
+    tstep_n(1) = tstep
+    nstep_n    = 0
+    nstep_n(1) = nstep
+  endif
+  
+  if (allocated(energies)) call tr_deallocate(energies,"energies",CAT_GRID)
+  if (nstep .gt. 0) call tr_allocate(energies,1,n_tor,1,2,1,nstep,"energies",CAT_GRID)
+  
+  if (allocated(xtime)) call tr_deallocate(xtime,"xtime",CAT_GRID)
+  if (nstep .gt. 0) call tr_allocate(xtime,1,nstep,"xtime",CAT_GRID)
+
+endif
+
+! --- Read numerical profiles for rho, T, and ff'.
+call read_num_profiles(my_id)
+
+! --- Determine the derivatives of the numerical input profiles.
+call derive_num_profiles(my_id)
+  
+return
+end subroutine initialise_parameters
