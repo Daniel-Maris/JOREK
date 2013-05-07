59928d3c5262ba0869c840c91c2b391697c12f16
Jane Pratt <J.L.Pratt@differ.nl>
2013-05-07 08:43:27 +0000

    changing eccd current source in model305
    git-svn-id: https://scm.gforge.inria.fr/svn/aster/jorek2/trunk@851 d232b540-7924-0410-b3cd-8064bb5aaf68

--- models/preset_parameters.f90
+++ models/preset_parameters.f90
@@ -295,10 +295,14 @@ subroutine preset_parameters
  nu_jec_fast=1.d1
  JJ_par=0.d1
  jecamp=1.d1
- jec_pos=0.6d0
+ jec_pos1=0.6d0
+ jec_pos2=0.6d0
+ jec_pos3=0.6d0
+ jec_pos4=0.6d0
  jec_width=0.5d0
+ jec_width2=0.5d0
  jw1=5.d-1 ! inner cut-off
  jw2=1.d0  ! outer cut-off
- 
+ jw3=1.d0  ! outer cut-off
 
 end subroutine preset_parameters
