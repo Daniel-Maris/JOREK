94aeb52233b0edfe410e884aba769a6bcd6dd928
Jane Pratt <J.L.Pratt@differ.nl>
2013-05-07 08:40:49 +0000

    changing eccd current source in model305
    git-svn-id: https://scm.gforge.inria.fr/svn/aster/jorek2/trunk@849 d232b540-7924-0410-b3cd-8064bb5aaf68

--- models/model305/initialise_parameters.f90
+++ models/model305/initialise_parameters.f90
@@ -43,7 +43,8 @@ namelist /in1/  tstep, nstep, tstep_n, nstep_n,                     &
                 T_0,   T_1,   T_coef,                               &
                 FF_0,  FF_1,  FF_coef,                              &
                 ZK_par, ZK_par_max, ZK_perp, D_par, D_perp,         &
-                JJ_par,nu_jec_fast, jecamp,jec_width,jec_pos,       &
+                JJ_par,nu_jec_fast, jecamp,jec_width,jec_width2,    &
+                jec_pos1, jec_pos2, jec_pos3, jec_pos4,             &
                 jw1,jw2,jw3,                                        &
                 particlesource, heatsource, tauIC,                  &
                 eta_num, visco_num, visco_par_num, D_perp_num,      &
