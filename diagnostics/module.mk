97b1435cfaf24d8a876dead78d2cec84d941c018
Jane Pratt <J.L.Pratt@differ.nl>
2013-08-09 10:42:22 +0000

    adding small diagnostic program to output an annulus of variables for plotting
    git-svn-id: https://scm.gforge.inria.fr/svn/aster/jorek2/trunk@896 d232b540-7924-0410-b3cd-8064bb5aaf68

--- diagnostics/module.mk
+++ diagnostics/module.mk
@@ -112,6 +112,16 @@ JORDEL_SRC := $(JORDEL_SRC)                     \
 	$(DIR)/psi_minmax.f90                   \
 	$(DIR)/RZ_minmax.f90
 
+JORPOL_SRC := $(JORPOL_SRC)                     \
+	$(DIR)/find_axis.f90                    \
+	$(DIR)/find_xpoint.f90                  \
+	$(DIR)/find_flux_surfaces.f90           \
+	$(DIR)/flux_surface_add_line.f90        \
+	$(DIR)/flux_surface_add_point.f90       \
+	$(DIR)/integrals_3D.f90                 \
+	$(DIR)/psi_minmax.f90                   \
+	$(DIR)/RZ_minmax.f90
+
 JOREK2_TARGET2VTK_SRC := $(JOREK2_TARGET2VTK_SRC) \
 	$(DIR)/find_axis.f90                      \
         $(DIR)/find_xpoint.f90                    \
