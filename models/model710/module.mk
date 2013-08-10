9735bbdafe417322da418acf63273acc293ccc0e
Jane Pratt <J.L.Pratt@differ.nl>
2013-08-10 10:21:50 +0000

    adding sources to module.mk so that diagnostics compile for model710
    git-svn-id: https://scm.gforge.inria.fr/svn/aster/jorek2/trunk@910 d232b540-7924-0410-b3cd-8064bb5aaf68

--- models/model710/module.mk
+++ models/model710/module.mk
@@ -12,40 +12,50 @@ JOREK2_MAIN_SRC := $(JOREK2_MAIN_SRC)		\
 
 JOREK2_FOUR_SRC := $(JOREK2_FOUR_SRC)	\
 	$(DIR)/initialise_parameters.f90 	\
+	$(DIR)/sources.f90                      \
 	$(DIR)/mod_parameters.f90
 
 JOREK2_POSTPROC_SRC := $(JOREK2_POSTPROC_SRC)		\
 	$(DIR)/initialise_parameters.f90 	\
+	$(DIR)/sources.f90                      \
 	$(DIR)/mod_parameters.f90
 
 JOREK2_POINCARE_SRC := $(JOREK2_POINCARE_SRC)	\
 	$(DIR)/initialise_parameters.f90 	\
+	$(DIR)/sources.f90                      \
 	$(DIR)/mod_parameters.f90
 
 JOREK2_CONNECTION2_SRC := $(JOREK2_CONNECTION2_SRC)	\
 	$(DIR)/initialise_parameters.f90 	\
+	$(DIR)/sources.f90                      \
 	$(DIR)/mod_parameters.f90
 
 JOREK2VTK_SRC := $(JOREK2VTK_SRC)	\
 	$(DIR)/initialise_parameters.f90 	\
+	$(DIR)/sources.f90                      \
 	$(DIR)/mod_parameters.f90
 
 JOREK2FLVTK_SRC := $(JOREK2FLVTK_SRC)	\
 	$(DIR)/initialise_parameters.f90 	\
+	$(DIR)/sources.f90                      \
 	$(DIR)/mod_parameters.f90
 
 JOREK2VTK3D_SRC := $(JOREK2VTK3D_SRC)	\
 	$(DIR)/initialise_parameters.f90 	\
+	$(DIR)/sources.f90                      \
 	$(DIR)/mod_parameters.f90
 
 JOREK2_DIAGNO_SRC := $(JOREK2_DIAGNO_SRC)	\
 	$(DIR)/initialise_parameters.f90 	\
+	$(DIR)/sources.f90                      \
 	$(DIR)/mod_parameters.f90
 
 JOREK2_TARGET2VTK_SRC := $(JOREK2_TARGET2VTK_SRC) \
         $(DIR)/initialise_parameters.f90        \
+	$(DIR)/sources.f90                      \
         $(DIR)/mod_parameters.f90
 
 JOREK2_POWERS_SRC := $(JOREK2_POWERS_SRC) \
         $(DIR)/initialise_parameters.f90        \
+	$(DIR)/sources.f90                      \
         $(DIR)/mod_parameters.f90
