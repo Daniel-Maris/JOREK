2b3428691a5684d6f3eb55ce30f0620c3ba7d731
Stanislas Pamela <stanislas.pamela@ccfe.ac.uk>
2013-10-29 15:28:52 +0000

    Including MAST boundary conditions for double-null in model400.
    git-svn-id: https://scm.gforge.inria.fr/svn/aster/jorek2/trunk@968 d232b540-7924-0410-b3cd-8064bb5aaf68

--- models/model400/boundary_conditions.f90
+++ models/model400/boundary_conditions.f90
@@ -35,7 +35,7 @@ contains
 
     use data_structure
     use global_distributed_matrix
-    use phys_module, only: F0, GAMMA, freeboundary
+    use phys_module, only: F0, GAMMA, freeboundary, tokamak_device
     USE murge_module, ONLY : MURGE_ASSEMBLYBEGIN_WRAPPER => MURGE_ASSEMBLYBEGIN,     &
          use_murge, use_murge_element, murge_id, murge_global_n, MURGE_ASSEMBLY_OVW, &
          MURGE_ASSEMBLY_FOOL, murge_sym, murge_id_prod, murge_global_n_prod,         &
