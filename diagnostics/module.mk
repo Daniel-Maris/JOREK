805faabca5b507e243b3c579049d059f015eda6a
Matthias Hoelzl <mhoelzl@ipp.mpg.de>
2013-11-25 19:26:51 +0000

    Fixing some issues in module.mk such that diagnosics compile again.
    git-svn-id: https://scm.gforge.inria.fr/svn/aster/jorek2/trunk@1011 d232b540-7924-0410-b3cd-8064bb5aaf68

--- diagnostics/module.mk
+++ diagnostics/module.mk
@@ -24,6 +24,7 @@ JOREK2_MAIN_SRC := $(JOREK2_MAIN_SRC)	  \
 	$(DIR)/hdf5_io.f90                
 
 JOREK2_FOUR_SRC := $(JOREK2_FOUR_SRC)	  \
+	$(DIR)/mod_diagnostics.f90        \
 	$(DIR)/boundary_check.f90         \
 	$(DIR)/RZ_minmax.f90              \
 	$(DIR)/find_axis.f90              \
@@ -32,6 +33,7 @@ JOREK2_FOUR_SRC := $(JOREK2_FOUR_SRC)	  \
 	$(DIR)/mod_fourier.f90
 
 JOREK2_POSTPROC_SRC := $(JOREK2_POSTPROC_SRC)	  \
+	$(DIR)/mod_diagnostics.f90        \
 	$(DIR)/boundary_check.f90         \
 	$(DIR)/flux_surface_add_line.f90  \
 	$(DIR)/flux_surface_add_point.f90 \
@@ -55,16 +57,18 @@ JOREK2_POVRAY_SRC := $(JOREK2_POVRAY_SRC)	  \
 	$(DIR)/find_xpoint.f90
 
 JOREK2_POINCARE_SRC := $(JOREK2_POINCARE_SRC) 	\
+	$(DIR)/mod_diagnostics.f90        \
 	$(DIR)/find_axis.f90			\
 	$(DIR)/find_xpoint.f90			\
         $(DIR)/integrals_3D.f90           \
 	$(DIR)/RZ_minmax.f90
 
 JOREK2_CONNECTION2_SRC := $(JOREK2_CONNECTION2_SRC) 	\
+	$(DIR)/mod_diagnostics.f90              \
 	$(DIR)/find_axis.f90				\
 	$(DIR)/find_xpoint.f90				\
 	$(DIR)/RZ_minmax.f90          \
-  $(DIR)/integrals_3D.f90
+        $(DIR)/integrals_3D.f90
 
 JOREK2VTK_SRC := $(JOREK2VTK_SRC)		\
 	$(DIR)/mod_diagnostics.f90              \
@@ -78,11 +82,13 @@ JOREK2VTK_SRC := $(JOREK2VTK_SRC)		\
 	$(DIR)/RZ_minmax.f90
 
 JOREK2FLVTK_SRC := $(JOREK2FLVTK_SRC)		\
+	$(DIR)/mod_diagnostics.f90        \
 	$(DIR)/find_axis.f90			\
 	$(DIR)/find_xpoint.f90    \
-  $(DIR)/integrals_3D.f90
+        $(DIR)/integrals_3D.f90
 
 JOREK2VTK3D_SRC := $(JOREK2VTK3D_SRC)           \
+	$(DIR)/mod_diagnostics.f90        \
 	$(DIR)/find_axis.f90			\
 	$(DIR)/find_xpoint.f90			\
 	$(DIR)/find_flux_surfaces.f90  		\
@@ -92,6 +98,7 @@ JOREK2VTK3D_SRC := $(JOREK2VTK3D_SRC)           \
 	$(DIR)/psi_minmax.f90
 
 JOREK2_DIAGNO_SRC := $(JOREK2_DIAGNO_SRC)	\
+	$(DIR)/mod_diagnostics.f90        \
 	$(DIR)/find_flux_surfaces.f90  		\
 	$(DIR)/flux_surface_add_line.f90  	\
 	$(DIR)/flux_surface_add_point.f90  	\
@@ -106,6 +113,7 @@ JOREK2_DIAGNO_SRC := $(JOREK2_DIAGNO_SRC)	\
 ENBIGGEN_SRC := $(ENBIGGEN_SRC)
 
 JORDEL_SRC := $(JORDEL_SRC)                     \
+	$(DIR)/mod_diagnostics.f90        \
 	$(DIR)/find_axis.f90                    \
 	$(DIR)/find_xpoint.f90                  \
 	$(DIR)/find_flux_surfaces.f90           \
@@ -116,6 +124,7 @@ JORDEL_SRC := $(JORDEL_SRC)                     \
 	$(DIR)/RZ_minmax.f90
 
 JORPOL_SRC := $(JORPOL_SRC)                     \
+	$(DIR)/mod_diagnostics.f90        \
 	$(DIR)/find_axis.f90                    \
 	$(DIR)/find_xpoint.f90                  \
 	$(DIR)/find_flux_surfaces.f90           \
@@ -126,11 +135,13 @@ JORPOL_SRC := $(JORPOL_SRC)                     \
 	$(DIR)/RZ_minmax.f90
 
 JOREK2_TARGET2VTK_SRC := $(JOREK2_TARGET2VTK_SRC) \
+	$(DIR)/mod_diagnostics.f90        \
 	$(DIR)/find_axis.f90                      \
         $(DIR)/find_xpoint.f90                    \
         $(DIR)/integrals_3D.f90
 
 JOREK2_POWERS_SRC := $(JOREK2_POWERS_SRC)   \
+	$(DIR)/mod_diagnostics.f90        \
         $(DIR)/find_axis.f90                \
 	$(DIR)/find_xpoint.f90              \
         $(DIR)/integrals_3D.f90             \
