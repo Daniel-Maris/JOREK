a66a5cd02b35df96480e00490d643afe462f6c3f
Jane Pratt <J.L.Pratt@differ.nl>
2014-04-16 06:28:22 +0000

    updating diagnostics for model306
    git-svn-id: https://scm.gforge.inria.fr/svn/aster/jorek2/trunk@1118 d232b540-7924-0410-b3cd-8064bb5aaf68

--- diagnostics/temp.f90
+++ diagnostics/temp.f90
@@ -1,4 +1,4 @@
-subroutine temp(node_list,element_list,A_tem,A_den,A_jen,A_jec)
+subroutine temp(node_list,element_list,A_tem,A_den,A_jen,A_jec,A_jec1,A_jec2)
 !---------------------------------------------------------------
 ! subroutine to drop global (broken down by n_tor) 
 ! temperature, density, current, and eccd current using
@@ -23,16 +23,19 @@ real*8     :: eq_g(n_var,n_gauss,n_gauss), eq_s(n_var,n_gauss,n_gauss), eq_t(n_v
 
 integer    :: i, j, k, in, ms, mt, iv, inode, ife, n_elements
 real*8     :: A_tem(n_tor), A_den(n_tor), xjac, BigR, wst
-real*8     :: A_jen(n_tor), A_jec(n_tor)
+real*8     :: A_jen(n_tor), A_jec(n_tor), A_jec1(n_tor),A_jec2(n_tor)
 real*8     :: r0, r0_x, r0_y, r0_p, r0_s, r0_t
 real*8     :: t0, t0_x, t0_y, t0_p, t0_s, t0_t
 real*8     :: p0, p0_s, p0_t, p0_x, p0_y, zj0
-real*8     :: jec
+real*8     :: jec, jec1, jec2
 
 A_tem = 0.d0
 A_den = 0.d0
 A_jen = 0.d0
 A_jec = 0.d0
+A_jec1 = 0.d0
+A_jec2 = 0.d0
+
 
 do ife =1,  element_list%n_elements
 
@@ -111,15 +114,23 @@ do ife =1,  element_list%n_elements
    if(jorek_model == 305) then
      jec=eq_g(n_var,ms,mt) ! the eccd current should always be the last equation
    elseif(jorek_model == 306) then
-     jec=eq_g(n_var,ms,mt)+eq_g(n_var-1,ms,mt) ! the eccd current is the sum of two currents
+     jec1=eq_g(n_var-1,ms,mt)
+     jec2=eq_g(n_var,ms,mt)
+     jec =eq_g(n_var,ms,mt)+eq_g(n_var-1,ms,mt) ! the eccd current is the sum of two currents
    else
      jec = p0  ! when not applying eccd, this last array is global pressure
    endif
-        A_tem(in) = A_tem(in) + t0* wst
-        A_den(in) = A_den(in) + r0* wst
-        A_jen(in) = A_jen(in) + zj0*wst
-        A_jec(in) = A_jec(in) + jec*wst
-
+        A_tem(in) = A_tem(in) + t0* xjac * wst
+        A_den(in) = A_den(in) + r0* xjac * wst
+        A_jen(in) = A_jen(in) + zj0* xjac * wst
+        if(jorek_model == 305) then
+           A_jec(in) = A_jec(in) + jec* xjac * wst
+        endif
+        if(jorek_model == 306) then
+           A_jec1(in) = A_jec1(in) + jec1* xjac * wst
+           A_jec2(in) = A_jec2(in) + jec2* xjac * wst
+           A_jec(in) = A_jec(in) + jec* xjac * wst
+        endif
       enddo
     enddo
   enddo
@@ -127,4 +138,4 @@ do ife =1,  element_list%n_elements
 enddo
 
 return
-end
+end subroutine temp
