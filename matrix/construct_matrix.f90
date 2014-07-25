91712a8afbcd99ae702dfa72de9fce490a2512db
Stanislas Pamela <stanislas.pamela@ccfe.ac.uk>
2014-07-25 12:49:55 +0000

    Forgot an omp variable declaration.
    git-svn-id: https://scm.gforge.inria.fr/svn/aster/jorek2/trunk@1173 d232b540-7924-0410-b3cd-8064bb5aaf68

--- matrix/construct_matrix.f90
+++ matrix/construct_matrix.f90
@@ -122,14 +122,14 @@ subroutine construct_matrix(my_id, local_elms, n_local_elms, index_min, index_ma
 
   ! --- Declare shared and private variables for omp
   !$omp parallel default(none) &
-  !$omp   shared(n_local_elms,irn_glob,jcn_glob,A_glob,RHS_loc,local_elms,element_list,node_list,   &
-  !$omp          index_min, index_max,xpoint2,xcase2,minRad,R_axis,Z_axis,psi_axis,psi_bnd,Z_xpoint,&
-  !$omp          R_xpoint,my_id,bc_natural_open,refinement,thread_struct,n_tor_fft_thresh,          &
-  !$omp          difference_found,rhs_problem,elm_problem)                           &
-  !$omp   private(ife,ielm,iv,inode,element,nodes,ELM,RHS,ELM2,RHS2,i,inode1,i_order,index_node1,   &
-  !$omp           index_large_i,j,index_ij,k,knode,k_order,index_node2,index_large_k,ijA_position,  &
-  !$omp           l,index_kl,ilarge2,iv2,vertex,direction,inode2,omp_nthreads,omp_tid,              &
-  !$omp           i_father,element_father, nodes_father, inode_father, node_out, ivertex, iorder,   &
+  !$omp   shared(n_local_elms,irn_glob,jcn_glob,A_glob,RHS_loc,local_elms,element_list,node_list,          &
+  !$omp          index_min, index_max,xpoint2,xcase2,minRad,R_axis,Z_axis,psi_axis,psi_bnd,Z_xpoint,       &
+  !$omp          R_xpoint,my_id,bc_natural_open,bc_natural_flux,refinement,thread_struct,n_tor_fft_thresh, &
+  !$omp          difference_found,rhs_problem,elm_problem)                                                 &
+  !$omp   private(ife,ielm,iv,inode,element,nodes,ELM,RHS,ELM2,RHS2,i,inode1,i_order,index_node1,          &
+  !$omp           index_large_i,j,index_ij,k,knode,k_order,index_node2,index_large_k,ijA_position,         &
+  !$omp           l,index_kl,ilarge2,iv2,vertex,direction,inode2,omp_nthreads,omp_tid,                     &
+  !$omp           i_father,element_father, nodes_father, inode_father, node_out, ivertex, iorder,          &
   !$omp           ivar, itor, jvertex, jorder, jvar, jtor)
 
 ! --- omp id
