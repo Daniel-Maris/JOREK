08fa015d251a1336430a0f1bfdde2dc4793e9ef5
Simon Pinches <Simon.Pinches@iter.org>
2015-06-10 16:33:40 +0000

    Adapt model 303.
    git-svn-id: https://scm.gforge.inria.fr/authscm/pinches/svn/aster/jorek2/trunk@1348 d232b540-7924-0410-b3cd-8064bb5aaf68

--- non_regression_tests/testcases/tearing_xpoint_303/settings.sh
+++ non_regression_tests/testcases/tearing_xpoint_303/settings.sh
@@ -21,14 +21,13 @@ function compile_jorek () {
     make $compilopt                                           || exit 1
     cp jorek_model${jorekmodel} jorek_model${jorekmodel}_1    || exit 1
 
-    ./util/config.sh model=$jorekmode ln_tor=3 n_plane=8 n_period=2
+    ./util/config.sh model=$jorekmodel n_tor=3 n_plane=8 n_period=2
     make clean                                                || exit 1
     make $compilopt rst_bin2hdf5 rst_hdf52bin                 || exit 1
     cp jorek_model${jorekmodel} jorek_model${jorekmodel}_3    || exit 1
     
     cp jorek_model${jorekmodel}_1 jorek_model${jorekmodel}_3 rst_hdf52bin rst_bin2hdf5 $testcasedir/ || exit 1
 }
-
 
 function restart_run () {
     if [ -n "$PRERUN" ]; then
@@ -37,13 +36,8 @@ function restart_run () {
     export OMP_NUM_THREADS=$ompthreads
     
     # Import restart file 
-    use_restartfile "00940"
-#CP  tstep=940
-#CP  printf "%5.5d \n" $tstep > ./file.out
-#CP  cp ${testcasedir}/jorek00${tstep}_export.h5 jorek00${tstep}.h5 || exit 1
-#CP  ./rst_hdf52bin
-#CP  rm file.out
-#CP  cp jorek00${tstep}.rst jorek_restart.rst
+    cp ${testcasedir}/jorek00940_export.h5 jorek_restart.h5 || exit 1
+    ./rst_hdf52bin # generate jorek_restart.rst
 
     ${codedir}/util/setinput.sh input restart=.t. nstep_n=1 tstep_n=10
     $MPIRUN $mpitasks ./jorek_model${jorekmodel}_3 < input >> logfile   
@@ -58,7 +52,6 @@ function initial_run () {
     # Equilibrium computation
     # => jorek_equil.rst
     ${codedir}/util/setinput.sh input restart=.f. nstep_n=0 tstep_n=1
-    cat input
     $MPIRUN $mpitasks ./jorek_model${jorekmodel}_1 < input > logfile_equil || exit 1
     cp jorek_restart.rst jorek_equil.rst || exit 1
 
@@ -74,22 +67,23 @@ function initial_run () {
     # => jorek00940.rst
     ${codedir}/util/setinput.sh input restart=.t. nstep_n=700 tstep_n=10 nout=10
     $MPIRUN $mpitasks ./jorek_model${jorekmodel}_3 < input >> logfile_3 || exit 1
-echo " ICI 1 "
+
     # Export  final restart file as HDF5 file
-    export_restartfile "00940"
-echo " ICI 2 "    
+    ./rst_bin2hdf5 # take the jorek_restart.rst as input 
+    cp jorek_restart.h5 ${testcasedir}/jorek00940_export.h5 || exit 1
+
     # Restart run (1 time step)
     restart_run
-echo " ICI 3 "    
-    # Export  final restart file as HDF5 file
-    export_restartfile "00941"
+
+    # Export final restart file as HDF5 file
+    ./rst_bin2hdf5 # take the jorek_restart.rst as input
+    cp jorek_restart.h5 ${testcasedir}/jorek00941_export.h5 || exit 1 
 }
 
 function compare_results () {
-    # convert binary restart file into hdf5 file
-echo " ICI 4 "    
-    export_restartfile "00941"
-    
+    # convert last binary restart file into hdf5 file
+    ./rst_bin2hdf5  # jorek00941.rst taken as input  
+
     # compare with reference file and return the result
     h5diff -p 1e-6 jorek_restart.h5 ${testcasedir}/jorek00941_export.h5 values; 
     returncode=$?
