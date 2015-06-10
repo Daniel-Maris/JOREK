7267c8e0bae0c986c9f6deb7027fd4d1280bc909
Simon Pinches <Simon.Pinches@iter.org>
2015-06-10 16:33:17 +0000

    Improve occigen environment (previous one leads to deadlock)
    git-svn-id: https://scm.gforge.inria.fr/authscm/pinches/svn/aster/jorek2/trunk@1340 d232b540-7924-0410-b3cd-8064bb5aaf68

--- non_regression_tests/job_scripts/occigen/env.sh
+++ non_regression_tests/job_scripts/occigen/env.sh
@@ -6,7 +6,9 @@ export HDF5HOME=/opt/software/libraries/hdf5/1.8.14
 export JOREK_HOST=occigen
 export BATCHCOMMAND="sbatch"
 export KMP_STACK_SIZE=16M
-export KMP_AFFINITY="granularity=fine,compact,1,0,verbose"
+#export KMP_AFFINITY="granularity=fine,compact,1,0,verbose"
+export KMP_AFFINITY="norespect,verbose"
 export MKL_NUM_THREADS=1
+export SLURM_CPU_BIND_VERBOSE=yes
 export MKL_DYNAMIC=0
-export MPIRUN="srun --mpi=pmi2 -K1 -m block:block -c 24 --resv-ports -n "
+export MPIRUN="srun  --cpu_bind=verbose,none --mpi=pmi2 -K1 -m block:block -c 24 --resv-ports -n "
