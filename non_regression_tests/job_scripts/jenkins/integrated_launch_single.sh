a2de2005960b76603a7604a6ff6574ad20122a85
Simon Pinches <Simon.Pinches@iter.org>
2015-06-10 16:37:08 +0000

    [NRT] Add jenkins scripts to have separate compilation process
    git-svn-id: https://scm.gforge.inria.fr/authscm/pinches/svn/aster/jorek2/trunk@1382 d232b540-7924-0410-b3cd-8064bb5aaf68

---
+++ non_regression_tests/job_scripts/jenkins/integrated_launch_single.sh
@@ -0,0 +1,6 @@
+startdir=`readlink -f $(dirname $0)`
+nonregdir=`readlink -f ${startdir}/../..` 
+cd $nonregdir || exit 1
+source job_scripts/jenkins/env.sh || exit 1
+cd job_scripts/jenkins || exit 1
+./$1.job || exit 1
