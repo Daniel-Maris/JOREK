47652642b21887e15451e605110af9e827203e37
Simon Pinches <Simon.Pinches@iter.org>
2015-06-10 16:32:53 +0000

    Modifications for Helios
    git-svn-id: https://scm.gforge.inria.fr/authscm/pinches/svn/aster/jorek2/trunk@1338 d232b540-7924-0410-b3cd-8064bb5aaf68

--- non-regression-tests/job_scripts/helios/env.sh
+++ non-regression-tests/job_scripts/helios/env.sh
@@ -2,9 +2,7 @@ module purge
 module load intel/14.0.4.211
 module load bullxmpi/1.2.8.2
 module load hdf5/1.8.14
-
+export LANG=C
 export JOREK_HOST=helios
-export PRERUN='export OMP_NUM_THREADS=16'
-export MPIRUN='mpirun -np '
-
-
+export MPIRUN="mpirun -np"
+export SUBCMD="sbatch -A JOREKIRQ"
