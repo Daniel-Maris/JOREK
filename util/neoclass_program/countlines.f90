32fc762eb5288ebb038180afb84231e604c479a3
François Orain <francois.orain@cea.fr>
2015-06-19 13:43:38 +0000

    add neoclassical program to calculate ki and mui neoclassical coefficients, with an example case for ITER. More explaination given on the wiki.
    git-svn-id: https://scm.gforge.inria.fr/authscm/pinches/svn/aster/jorek2/trunk@1392 d232b540-7924-0410-b3cd-8064bb5aaf68

---
+++ util/neoclass_program/countlines.f90
@@ -0,0 +1,17 @@
+module countlines_mod
+contains
+function countlines(fd)
+  integer countlines
+  integer, intent(in) :: fd
+  
+  integer i
+
+  i=1
+  DO
+     READ(fd,'()', END=100)
+     i=i+1
+  ENDDO
+100 REWIND(fd)
+  countlines = i-1
+end function countlines
+end module countlines_mod
