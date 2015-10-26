c9ee241af9f2237ed06911ba848be3e723933ed2
Chantal Passeron <chantal.passeron@cea.fr>
2015-10-26 15:00:17 +0100

    [REGRESSION|iter-hpc] Change path for Zlib library.

--- non_regression_tests/job_scripts/iter-hpc/env.sh
+++ non_regression_tests/job_scripts/iter-hpc/env.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 eval `tclsh /work/imas/opt/modules-tcl/modulecmd.tcl $(basename $SHELL) autoinit`
 
-module purge  
+module purge
 
 module use /work/imas/opt/EasyBuild/modules/all
-module load cURL
 
-module use /work/imas/etc/modulefiles
-module load hdf5
-
+module load cURL/7.40.0-GCC-4.8.3
+module load zlib/1.2.8-gompi-1.5.16
 module load intel/12.0.2
 module load mpich2/3.1.3-intel
 module load scotch/5.1.12b
 module load metis/5.1.0
 module load mumps/4.10.0
 module load pastix/5.2.2.16
-module load ppplib/14.4.8
 module load fftw/3.3.4
+module load hdf5
+
+export ZLIB_HOME=/work/imas/opt/EasyBuild/software/zlib/1.2.8-gompi-1.5.16
 
 export LANG=C
 export JOREK_HOST=iter-hpc
