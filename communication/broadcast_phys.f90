5bfd52197a34612a064f0723f1c24580cf7bef85
Daan van Vugt <dvanvugt@ignitioncomputing.com>
2015-11-10 13:10:33 +0100

    fix broadcast_phys for t_particles

--- communication/broadcast_phys.f90
+++ communication/broadcast_phys.f90
@@ -27,8 +27,7 @@ call MPI_PACK_SIZE(1,MPI_INTEGER,MPI_COMM_WORLD,INT_EXT,ierr)
 call MPI_PACK_SIZE(1,MPI_LOGICAL,MPI_COMM_WORLD,ILOG_EXT,ierr)
 call MPI_PACK_SIZE(1,MPI_CHARACTER,MPI_COMM_WORLD,CHAR_EXT,ierr)
 
-!CP!bufsize = ( (359+2*max_limiter+n_var) * IDBL_EXT + (37+n_tor) * INT_EXT + 45 * ILOG_EXT + (14*512+120) * CHAR_EXT )
-bufsize = ( (369+2*max_limiter+n_var) * IDBL_EXT + (39+n_tor) * INT_EXT + 48 * ILOG_EXT + (14*512+120) * CHAR_EXT )
+bufsize = ( (370+2*max_limiter+n_var) * IDBL_EXT + (40+n_tor) * INT_EXT + 48 * ILOG_EXT + (14*512+120) * CHAR_EXT )
 
 #ifdef USE_HDF5
    bufsize = bufsize + ( 1 * IDBL_EXT + 1 * INT_EXT + 1 * ILOG_EXT )
