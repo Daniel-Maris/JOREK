2faf9a6e160171aa929d470a27d36f129fb1a740
Mark Verbeek <mark.verbeek@users.noreply.github.com>
2016-02-24 09:55:06 +0100

    Added more comments in the code making, hopefully, more clear what this peace of code is doing.
    Also the writing of the variable names to screen is improved and now can handle every number of variable names from 0 to many (like more then 3).

--- diagnostics/jorek2help.f90
+++ diagnostics/jorek2help.f90
@@ -1,7 +1,21 @@
-!> Command line function, print help info or hardcoded parameter values to screen.
+!> Command line function, print help or hardcoded parameter values to screen
 !!
-!! To remove this function a -DNO_HELP flag should be added to the compiler flags
+!! NOTE: To remove this function a -DNO_HELP flag should be added to the 
+!! compiler flags
 !! 
+!! When one or more commandline argument is given this function will loop 
+!! through those options and will respond to each one till one is recognized.
+!! The following argument are supported:
+!! 
+!!   -h or --help  : print some basic info about JOREK_2
+!!   -p or --param : print hardcoded parameters
+!! 
+!! unknown argument give a short message that the argument is invalit. When 
+!! none of the given arguments is recognized a sugestion is given for valid
+!! arguments.
+!!
+!! writer: Mark Verbeek
+!! data  : 11-02-2016
 subroutine jorek2help()
   use parameters
   
@@ -9,8 +23,9 @@ subroutine jorek2help()
 
 #ifndef NO_HELP
   
-  integer::narg,cptArg            !> for commandline arguments
-  character(len=20)::ArgName      !> Argument name
+  integer           ::narg,cptArg   !> for commandline arguments
+  integer           :: i, j, n_rows !> do loop index
+  character(len=20) :: ArgName      !> Argument name
   
   narg = command_argument_count() !> get number of commandline agruments
   
@@ -46,7 +61,7 @@ subroutine jorek2help()
           write(*,'(A)') '* start date: 18-7-2008                                                       *' 
           write(*,'(A)') '*                                                                             *' 
           write(*,200)
-      stop
+          stop
       case("--param","-p")
         112 format(A, i12, 41X, A)
         write(*,200)
@@ -69,26 +84,38 @@ subroutine jorek2help()
         write(*,  112) '*      n_ref_list     =  ', n_ref_list        , '*'
         write(*,'(A)') '*                                                                             *' 
         write(*,'(A)') '*      Implemented variables:                                                 *' 
+        
+        ! determine number of rows needed to show all variable_names
+        n_rows = ceiling(n_var/4.0)  
+        
+        ! The first loop loops through the row needed. The the left eastectics is 
+        ! written followed by a loop that print out the variable_name of white space 
+        ! depending on it this variable_name exist. The last write is the eastectics 
+        ! on the right. 
+        do i = 0,n_rows-1
+          write(*,'(A)',advance='no') '*      '
+          do j = (i*4) + 1, (i*4) + 4
+            if ( j .gt. n_var) then
+              write(*,'(11x)',advance='no') 
+            else
+              write(*,'(A11)',advance='no') variable_names(j)
+            end if
+            if ( j .lt. (i*4 + 4)) then
+              write(*,'(7x)',advance='no') 
+            end if
+          end do 
+          write(*,'(A)') '      *'
+        end do
 
-        113 format(A, A11, A, A11, A, A11, A, A11, A)
-        write(*,113) '*      ', variable_names(1), '       ', variable_names(2), '       ', &
-                                variable_names(3), '       ', variable_names(4), '      *' 
-#if (JOREK_MODEL == 500 || JOREK_MODEL == 555)
-        write(*,113) '*      ', variable_names(5), '       ', variable_names(6), '       ', &
-                                variable_names(7), '       ', variable_names(8), '      *' 
-#else
-        write(*,113) '*      ', variable_names(5), '       ', variable_names(6), '       ', &
-                                variable_names(7), '       ', ' ',               '      *' 
-#endif
         write(*,'(A)') '*                                                                             *' 
         write(*,200) 
         stop
       case default
-        write(*,'(A)') 'Option ', adjustl(ArgName), 'unknown, try -h or -help for program info '
-        write(*,'(A)') 'or -p for parameters list'
-      stop
-        end select
+        write(*,'(A, A, A)') 'Option ', adjustl(ArgName), 'unknown'
+      end select
     end do
+    write(*,'(A)') 'try -h or -help for program info or -p for parameters list'
+    stop
   end if
 
 #endif
