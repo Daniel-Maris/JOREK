9f97b83847215ac425825c8e636ad0fcbacec137
Guillaume Latu <guillaume.latu@cea.fr>
2016-03-15 11:48:07 +0100

    Add info file in order to keep track of how restart files have been generated before beeing sent to jorek.eu/dav_nrt

--- non_regression_tests/job_scripts/jenkins/integrated_compile_single.sh
+++ non_regression_tests/job_scripts/jenkins/integrated_compile_single.sh
@@ -6,7 +6,7 @@ rm -f testcases/${1}/*tgz 2>/dev/null
 set -v 
 cp ../Make.inc/Makefile.jenkins_linux_gnu.inc ../Makefile.inc || exit 1
 source job_scripts/jenkins/env.sh | exit 1
-./get_all_data.sh || exit 1
+(cd testcases;./get_testcase_data.sh ${1}) || exit 1
 cd ..
 make cleanall
 non_regression_tests/run_test.sh $1 -p
--- non_regression_tests/testcases/send_testcase_data.sh
+++ non_regression_tests/testcases/send_testcase_data.sh
@@ -17,12 +17,21 @@ source ./settings.sh
 VERSION="_`md5sum end.h5 | sed -e 's/^ //' -e 's/ .*$//'`"
 echo ${VERSION} > .version
 
+printf "date: "      > .info
+date                >> .info
+printf "hostname: " >> .info
+hostname -f         >> .info
+printf "branch: "   >> .info
+git rev-parse --abbrev-ref HEAD >> .info
+printf "user: "     >> .info
+echo $USER          >> .info
+
 testname=$(basename $testcasedir)
 TGZFILE=${testname}${VERSION}.tgz
 
 echo "Creating tarball ${TGZFILE}"
-tar cvzf ${TGZFILE} begin.h5 end.h5                                           || exit 1
-
+tar cvzf ${TGZFILE} .info begin.h5 end.h5  || exit 1
+ 
 echo "Uploading ${TGZFILE}"
 TESTNAME=$(basename $TESTDIR)
 curl -s -u nrt:nrt_21745XtL -T ${TGZFILE} ${DAV_URL}
