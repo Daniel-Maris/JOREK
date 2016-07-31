d2226804eda1d9822b6207b1a0dc321b1db17a57
Daan van Vugt <dvanvugt@ignitioncomputing.com>
2016-07-31 15:27:02 +0200

    Create object dependencies script

--- particles/util/obj_deps.sh
+++ particles/util/obj_deps.sh
@@ -1,7 +1,28 @@
 #!/usr/bin/env bash
-make --dry-run --always-make --debug=b $1 |\
-	grep 'Must remake target' |\
-	grep -o "'.*'" |\
-	sed -e "s/^'.mod/.obj/" -e "s/mod'/o'/" |\
-	grep '[.]' |\
-	tr -d "'" 
+set -euf -o pipefail
+
+# Check all of the .dep/*.d files for dependencies of $1
+MAX_ITERATIONS=20 # mostly here to catch loops
+
+# to_check contains names of .d files that still need to be checked
+to_check=`echo "$1" | sed -e 's/^[.]obj/.dep/' -e 's/[.]o$/.d/'`
+# deps contains a list of dependencies
+deps=""
+
+while [ -n "$to_check" ]; do
+  to_check_new=""
+  for depfile in $to_check; do
+    stems=`grep -o ' .mod/[^.][^/ ]*[.]mod' "$depfile" |\
+      sed -e 's/[.]mod//g' -e 's/[/ ]//g'`
+    for stem in $stems; do
+      if ! $(echo "$deps" | grep -qF ".obj/$stem.o"); then
+        # if not already in dependency list
+        # Add to dependency and check lists
+        deps="$deps .obj/$stem.o"
+        echo ".obj/$stem.o" # echo here because $deps is unset after the loop
+        to_check_new="$to_check_new .dep/$stem.d"
+      fi
+    done
+  done
+  to_check="$to_check_new"
+done
