1464d78381900fc72f57ed9e63e688044924fdf8
Daan van Vugt <dvanvugt@ignitioncomputing.com>
2016-07-31 23:27:52 +0200

    Improve dependency check and automatically find programs

--- particles/util/obj_deps.sh
+++ particles/util/obj_deps.sh
@@ -12,17 +12,19 @@ deps=""
 while [ -n "$to_check" ]; do
   to_check_new=""
   for depfile in $to_check; do
-    stems=`grep -o ' .mod/[^.][^/ ]*[.]mod' "$depfile" |\
-      sed -e 's/[.]mod//g' -e 's/[/ ]//g'`
-    for stem in $stems; do
-      if ! $(echo "$deps" | grep -qF ".obj/$stem.o"); then
-        # if not already in dependency list
-        # Add to dependency and check lists
-        deps="$deps .obj/$stem.o"
-        echo ".obj/$stem.o" # echo here because $deps is unset after the loop
-        to_check_new="$to_check_new .dep/$stem.d"
-      fi
-    done
+    if [ -e "$depfile" ]; then
+      stems=`grep -o ' .mod/[^.][^/ ]*[.]mod' "$depfile" |\
+        sed -e 's/[.]mod//g' -e 's/[/ ]//g'`
+      for stem in $stems; do
+        if ! $(echo "$deps" | grep -qF ".obj/$stem.o"); then
+          # if not already in dependency list
+          # Add to dependency and check lists
+          deps="$deps .obj/$stem.o"
+          echo ".obj/$stem.o" # echo here because $deps is unset after the loop
+          to_check_new="$to_check_new .dep/$stem.d"
+        fi
+      done
+    fi
   done
   to_check="$to_check_new"
 done
