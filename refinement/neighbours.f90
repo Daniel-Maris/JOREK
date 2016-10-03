07ce3a33a7e92619296add0aee9636e605728498
SOMMARIVA    Cristian    03/11/2017    externe <CS241817@spica.intra.cea.fr>
2016-10-03 09:58:30 +0200

    bugfix of neighbours.f90 and update_neighbours.f90 in X-point element identification

--- refinement/neighbours.f90
+++ refinement/neighbours.f90
@@ -1,4 +1,4 @@
-logical function neighbours(elm1,elm2,inb1,inb2)
+logical function neighbours(node_list,elm1,elm2,inb1,inb2)
 !-------------------------------------------------------
 ! function to check if the two elements elm1 and elm2
 ! are neighbours. Two elements are neighbours if they
@@ -11,14 +11,20 @@ use data_structure
 implicit none
 
 type (type_element) :: elm1, elm2
-integer             :: inb1, inb2, iv(4,2), i, j, nb
+type (type_node_list) ::  node_list
+integer             :: inb1, inb2, iv(6,2), i, j, nb
+real*8,parameter :: toll=1.d-12
+real*8 :: dist
 
 neighbours = .false.
 nb = 0
 ! write(*,'(A13,4i6,A2,4i6)') ' NEIGHBOURS? ',elm1%vertex,'  ',elm2%vertex
 do i=1,4
   do j=1,4
-    if (elm1%vertex(i)==elm2%vertex(j)) then
+    dist = (node_list%node(elm1%vertex(i))%x(1,1)-node_list%node(elm2%vertex(j))%x(1,1))*(node_list%node(elm1%vertex(i))%x(1,1)-node_list%node(elm2%vertex(j))%x(1,1)) + &
+	(node_list%node(elm1%vertex(i))%x(1,2)-node_list%node(elm2%vertex(j))%x(1,2))*(node_list%node(elm1%vertex(i))%x(1,2)-node_list%node(elm2%vertex(j))%x(1,2))
+    !if (elm1%vertex(i)==elm2%vertex(j)) then
+    if(dist.lt.toll) then
       nb = nb + 1
       iv(nb,1) = i
       iv(nb,2) = j
