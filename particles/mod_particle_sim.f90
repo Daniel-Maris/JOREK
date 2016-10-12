0b9e3bb36b2fc868ece52999fb0f2731722ecf80
Daan van Vugt <dvanvugt@ignitioncomputing.com>
2016-10-12 15:29:55 +0200

    Write version output in mod_particle_sim

--- particles/mod_particle_sim.f90
+++ particles/mod_particle_sim.f90
@@ -24,15 +24,31 @@ end type particle_sim
 contains
 !> Actions to perform when setting up a simulation
 subroutine initialize(sim, num_groups)
+#include "version.h"
   use mpi
   class(particle_sim), intent(inout) :: sim !< why is this class() and not type()?
   integer, intent(in) :: num_groups
-  integer :: provided, ierr
+  integer :: provided, ierr, my_id
   call MPI_Init_thread(MPI_THREAD_MULTIPLE, provided, ierr)
   if (ierr .ne. 0) write(*,*) "Error ", ierr, " in MPI_Init_thread"
   if (provided .ne. MPI_THREAD_MULTIPLE) write(*,*) "WARNING: provided(", provided, ") != MPI_THREAD_MULTIPLE"
-
   allocate(sim%groups(num_groups))
+  call MPI_COMM_RANK(MPI_COMM_WORLD, my_id, ierr)
+
+  111 format(2x,a,': ',a)
+  if (my_id .eq. 0) then
+    write(*,*) ' ', trim(adjustl(RCS_VERSION))
+    write(*,111) 'compile_time        ', trim(adjustl(compile_time))
+    write(*,111) 'compile_user        ', trim(adjustl(compile_user))
+    write(*,111) 'compile_machine     ', trim(adjustl(compile_machine))
+    write(*,111) 'compile_dir         ', trim(adjustl(compile_dir))
+    write(*,111) 'compile_command     ', trim(adjustl(compile_command))
+    write(*,111) 'compile_flags       ', trim(adjustl(compile_flags))
+    write(*,111) 'compile_includes    ', trim(adjustl(compile_includes))
+    write(*,111) 'compile_defines     ', trim(adjustl(compile_defines))
+    write(*,111) 'compile_libs        ', trim(adjustl(compile_libs))
+    write(*,111) 'compile_modules     ', trim(adjustl(compile_modules))
+  end if
 end subroutine
 
 !> Actions to perform when stopping the simulation.
