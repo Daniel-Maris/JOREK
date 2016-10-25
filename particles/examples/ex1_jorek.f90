ca5729190c94b1957aa83ff2e41dce900d7c305b
Daan van Vugt <dvanvugt@ignitioncomputing.com>
2016-10-25 16:58:28 +0200

    Update ex1_jorek

--- particles/examples/ex1_jorek.f90
+++ particles/examples/ex1_jorek.f90
@@ -4,16 +4,16 @@ program ex1_jorek
 use particle_tracer
 implicit none
 
-real*8 :: timesteps(1) = [1d-7] !< seconds
+real*8 :: timesteps(2) = [1d-7,1d-7] !< seconds
 
 type(adf11_all) :: adas
 type(coronal)   :: cor
-integer :: i, j, k, n_steps, i_elm_old, ifail
+integer :: i, j, k, n_steps, i_elm_old, ifail, n_lost
 real*8 :: target_time, t
 real*8 :: E(3), B(3), rz_old(2), st_old(2), psi, U
 
 ! Start up MPI, jorek
-call sim%initialize(num_groups=1)
+call sim%initialize(num_groups=2)
 
 ! Set up the field reader
 fields = read_jorek_fields_interp_linear(basename='jorek_restart', i=-1)
@@ -21,19 +21,25 @@ call with(sim, fields)
 
 ! Set up particles
 allocate(particle_kinetic_leapfrog::sim%groups(1)%particles(100000))
-sim%groups(1)%Z    = 74
-sim%groups(1)%mass = 183.84 !< atomic mass units
+allocate(particle_kinetic_leapfrog::sim%groups(2)%particles(100000))
+sim%groups(:)%Z    = 74
+sim%groups(:)%mass = 183.84 !< atomic mass units
 
 ! Prepare the coronal equilibrium
 adas = read_adf11('50_w')
 cor  = coronal(adas)
 
 ! Distribute particles uniformly throughout the domain
-call seed_positions(sim%groups(1)%particles, &
-    fields%node_list, fields%element_list, pcg32_rng())
-call adjust_particle_weights(sim%groups(1)%particles, num_atoms_total=1d23)
+call initialise_particles(sim%groups(1)%particles, &
+    fields%node_list, fields%element_list, sobseq_rng())
 call set_velocity_from_T(sim%groups(1)%particles, sim%groups(1)%mass, &
-    fields%node_list, fields%element_list, pcg32_rng(), cor, v_par=.true.)
+    fields%node_list, fields%element_list, cor, v_par=.false.)
+call initialise_particles(sim%groups(2)%particles, &
+    fields%node_list, fields%element_list, pcg32_rng())
+call set_velocity_from_T(sim%groups(2)%particles, sim%groups(2)%mass, &
+    fields%node_list, fields%element_list, cor, v_par=.false.)
+call adjust_particle_weights(sim%groups(1)%particles, num_atoms_total=1d23)
+call adjust_particle_weights(sim%groups(2)%particles, num_atoms_total=1d23)
 
 events = [event(write_action(basename='test'),   step=1d-4), &
           !event(diag_print_kinetic_energy(),     step=1d-6), &
@@ -45,13 +51,15 @@ call with(sim, events, at=0.d0)
 do while (.not. sim%stop_now)
   target_time = next_event_at(sim, events)
 
-  do i=1,1 ! loop over groups
+  do i=1,size(sim%groups,1)
     n_steps = nint((target_time - sim%time)/timesteps(i))
+    n_lost = 0
 
     select type (particles => sim%groups(i)%particles)
     type is (particle_kinetic_leapfrog)
       !$omp parallel do default(private) &
-      !$omp shared(sim, n_steps, timesteps, i, fields)
+      !$omp shared(sim, n_steps, timesteps, i, fields) &
+      !$omp reduction(+:n_lost)
       do j=1,size(particles,1)
         do k=1,n_steps
           if (particles(j)%i_elm .eq. 0) exit
@@ -65,11 +73,12 @@ do while (.not. sim%stop_now)
           call boris_push_cylindrical(particles(j), sim%groups(i)%mass, E, B, timesteps(i))
           call find_RZ_nearby(fields%node_list, fields%element_list, particles(j)%x(1:2), rz_old, &
               st_old, particles(j)%st, i_elm_old, particles(j)%i_elm, ifail)
-          if (particles(j)%i_elm .eq. 0) write(*,*) "particle ", j, " lost at step ", k
+          if (particles(j)%i_elm .eq. 0) n_lost = n_lost + 1
         end do ! steps
       end do ! particles
       !$omp end parallel do
     end select
+    write(*,*) "number of lost particles: ", n_lost
   end do ! groups
   sim%time = target_time
   call with(sim, events, at=sim%time)
