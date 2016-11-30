9d0234e25e24e7a42802cfccc397dd5198827ace
Matthias Hoelzl <mhoelzl@ipp.mpg.de>
2016-11-30 08:23:17 +0100

    Adding -warn nointerfaces to default.mk to avoid compilation problems ('internal compiler errors') when compiling in parallel.

--- defaults.mk
+++ defaults.mk
@@ -64,6 +64,7 @@ ifeq ($(COMPILER_FAMILY), intel)
     FLAGS += -openmp
   endif
   FFLAGS += -warn all
+  FFLAGS += -warn nointerfaces
   FFLAGS += -warn nounused
   FFLAGS += -fpp
   FFLAGS += -r8
