038d3c14f6ec67ccb93c68a9a65c2de35eb9a93e
Javier Artola <javier.artola@iter.org>
2016-12-20 13:47:45 +0100

    Add new parameters to initialise_parameters in all models

--- models/model001/initialise_parameters.f90
+++ models/model001/initialise_parameters.f90
@@ -6,7 +6,7 @@ use phys_module
 use mumps_module,  only: use_mumps, no_zeros_mumps
 use murge_module,  only: use_murge, use_murge_element
 use pastix_module, only: use_pastix, no_zeros_pastix, pastix_smp_only
-use vacuum,        only: vacuum_preset, wall_resistivity
+use vacuum
 use wsmp_module,   only: use_wsmp
 
 implicit none
@@ -45,7 +45,7 @@ namelist /in1/  tstep, nstep, tstep_n, nstep_n,                     &
                 FF_0,  FF_1,  FF_coef,                              &
                 ZK_par, ZK_perp, D_par, D_perp,                     &
                 particlesource, heatsource,                         &
-                eta_num, visco_num, tgnum,                           &
+                eta_num, visco_num, tgnum,                          &
                 ellip, tria_u, tria_l, quad_u, quad_l,              &
                 xampl, xwidth, xsig, xtheta, xshift, xleft, xpoint, &
                 xcase, D_perp_file, ZK_perp_file,                   &
@@ -64,9 +64,16 @@ namelist /in1/  tstep, nstep, tstep_n, nstep_n,                     &
                 save_diagnostics_HDF5,h5_diag_nbtime,               &
 #endif
                 wall_file,                                          &
-                n_limiter, R_limiter, Z_limiter,		    &
-                first_target_point, last_target_point,		    &
-                linear_run, export_for_nemec
+                n_limiter, R_limiter, Z_limiter,		                &
+                first_target_point, last_target_point,		          &
+                linear_run, export_for_nemec,                       &
+                amix, amix_freeb, equil_accuracy,                   &
+                equil_accuracy_freeb, current_ref, FB_Ip_position,  &
+                FB_Ip_integral, Z_axis_ref, FB_Zaxis_position,      &
+                FB_Zaxis_derivative,FB_Zaxis_integral, start_VFB,   &
+                n_feedback_current, n_feedback_vertical,            &
+                n_iter_freeb, n_coils_nml, coils0,                  &
+                Zaxis_find_limit
 
 if (my_id .eq. 0) then
 
