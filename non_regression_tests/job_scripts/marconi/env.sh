3493003d00e7e638f191b7f2c063a93a443e7581
Guillaume Emmanuel LATU <glatu000@r000u06l01.marconi.cineca.it>
2017-01-11 15:20:52 +0100

    Improve env.sh on marconi to handle openmpi

--- non_regression_tests/job_scripts/marconi/env.sh
+++ non_regression_tests/job_scripts/marconi/env.sh
@@ -3,6 +3,14 @@
 export LANG=C
 export JOREK_HOST=marconi
 export compilethreads=32
-export MPIRUN="mpirun -genv KMP_AFFINITY=verbose -genv I_MPI_DEBUG=4 -genv I_MPI_PIN=1 -genv I_MPI_PIN_DOMAIN=socket -genv I_MPI_PIN_ORDER=spread -genv OMP_PROC_BIND=1 --map-by ppr:1:socket:pe=$OMP_NUM_THREADS -np"
+USE_INTELMPI=$(mpirun -V | grep -c -i "Intel.*MPI")
+USE_OPENMPI=$(mpirun -V | grep -c -i "Open.*MPI")
+MPIRUN="mpirun -np"
+if [ ${USE_INTELMPI} -gt 0 ]; then
+  MPIRUN="mpirun -genv KMP_AFFINITY=verbose -genv I_MPI_DEBUG=4 -genv I_MPI_PIN=1 -genv I_MPI_PIN_DOMAIN=socket -genv I_MPI_PIN_ORDER=spread -genv OMP_PROC_BIND=1 --map-by ppr:1:socket:pe=18 -np"
+fi
+if [ ${USE_OPENMPI} -gt 0 ]; then
+  MPIRUN="mpirun -x KMP_AFFINITY=verbose -x OMP_PROC_BIND=1 -n"
+fi
+export MPIRUN
 export BATCHCOMMAND="qsub"
-export http_proxy=http://proxy:3128 
