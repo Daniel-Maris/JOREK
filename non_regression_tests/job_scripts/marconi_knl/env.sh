88fbd3139a87d2d5de6bf4b65fbe99895006f148
Guillaume Emmanuel LATU <glatu000@r000u06l01.marconi.cineca.it>
2017-01-11 15:42:02 +0100

    Improve env.sh

--- non_regression_tests/job_scripts/marconi_knl/env.sh
+++ non_regression_tests/job_scripts/marconi_knl/env.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 export LANG=C
+export ARCH=marconi_knl
 export JOREK_HOST=marconi_knl
 export compilethreads=32
 
@@ -8,6 +9,5 @@ export MKL_DOMAIN_NUM_THREADS="MKL_BLAS=1"
 export MKL_NUM_THREADS="1"
 export MKL_DYNAMIC="FALSE"
 export MKL_ENABLE_INSTRUCTIONS=AVX512_MIC
-export MPIRUN="mpirun -genv I_MPI_DEBUG=4 -genv KMP_AFFINITY=verbose,scatter --map-by ppr:1:socket:pe=$OMP_NUM_THREADS -np"
+export MPIRUN="mpirun -genv I_MPI_DEBUG=4 -genv KMP_AFFINITY=verbose,scatter --map-by ppr:1:socket:pe=34 -np"
 export BATCHCOMMAND="qsub"
-export http_proxy=http://proxy:3128 
