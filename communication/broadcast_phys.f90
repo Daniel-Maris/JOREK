754f7ea4c533ce571085bed74e34cfb39bf1951e
feng <feng.liu@iter.org>
2017-01-30 14:28:02 +0100

    pack unpack ëdge particle source in broadcast

--- communication/broadcast_phys.f90
+++ communication/broadcast_phys.f90
@@ -76,7 +76,11 @@ if (my_id .eq. 0) then
   call MPI_PACK(heatsource_i,           1,MPI_REAL8,buffer,bufsize,position,MPI_COMM_WORLD,ierr)
   call MPI_PACK(heatsource_e,           1,MPI_REAL8,buffer,bufsize,position,MPI_COMM_WORLD,ierr)
   call MPI_PACK(particlesource,         1,MPI_REAL8,buffer,bufsize,position,MPI_COMM_WORLD,ierr)
-  
+
+  call MPI_PACK(edgesource,             1,MPI_REAL8,buffer,bufsize,position,MPI_COMM_WORLD,ierr) 
+  call MPI_PACK(edgesource_psin,        1,MPI_REAL8,buffer,bufsize,position,MPI_COMM_WORLD,ierr)
+  call MPI_PACK(edgesource_sig,         1,MPI_REAL8,buffer,bufsize,position,MPI_COMM_WORLD,ierr)
+ 
   call MPI_PACK(ZK_perp,               10,MPI_REAL8,buffer,bufsize,position,MPI_COMM_WORLD,ierr)
   call MPI_PACK(ZK_par,                 1,MPI_REAL8,buffer,bufsize,position,MPI_COMM_WORLD,ierr)
   call MPI_PACK(ZK_par_max,             1,MPI_REAL8,buffer,bufsize,position,MPI_COMM_WORLD,ierr)
