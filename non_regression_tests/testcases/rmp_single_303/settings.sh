6a5331989156b1c1223bb6739ee46d5351cf917e
Matthias Hoelzl <mhoelzl@ipp.mpg.de>
2017-02-01 19:30:19 +0100

    Changing n_plane for the rmp_single_303 testcase from 32 to 4 in order to speed it up massively.

--- non_regression_tests/testcases/rmp_single_303/settings.sh
+++ non_regression_tests/testcases/rmp_single_303/settings.sh
@@ -18,7 +18,7 @@ function compile_jorek () {
   else
     touch jorek_model${jorekmodel}_1
   fi
-  ./util/config.sh model=$jorekmodel n_tor=3 n_plane=32 n_period=3                   || exit 1
+  ./util/config.sh model=$jorekmodel n_tor=3 n_plane=4 n_period=3                    || exit 1
   make cleanall                                                                      || exit 1
   make $compilopt jorek_model${jorekmodel} rst_bin2hdf5 rst_hdf52bin                 || exit 1
   mv jorek_model${jorekmodel} jorek_model${jorekmodel}_3                             || exit 1
@@ -27,7 +27,7 @@ function compile_jorek () {
 
 # --- Re-run the whole case from scratch into the non-linear phase
 function initial_run () {
-  $MPIRUN $mpitasks ./jorek_model${jorekmodel}_1 < input | tee -a logfile            || exit 1
+  ./jorek_model${jorekmodel}_1 < input | tee -a logfile                              || exit 1
   ${codedir}/util/setinput.sh input restart=.t. tstep_n=5. nstep_n=222 iter_precon=0 gmres_4=1.d4 RMP_on=.t. RMP_psi_cos_file=\'RMP_psi_cos_JET_N3.txt\' RMP_psi_sin_file=\'RMP_psi_sin_JET_N3.txt\' nout=1       || exit 1
   $MPIRUN $mpitasks ./jorek_model${jorekmodel}_3 < input  | tee -a logfile           || exit 1
 }
