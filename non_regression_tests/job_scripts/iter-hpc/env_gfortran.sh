d9866701b828bfd76ce2248bb02aab506768618d
Daan van Vugt <dvanvugt@ignitioncomputing.com>
2017-02-06 16:17:36 +0100

    Try goolf modules for iter-hpc

--- non_regression_tests/job_scripts/iter-hpc/env_gfortran.sh
+++ non_regression_tests/job_scripts/iter-hpc/env_gfortran.sh
@@ -3,20 +3,16 @@ eval `tclsh /work/imas/opt/modules-tcl/modulecmd.tcl $(basename $SHELL) autoinit
 
 module purge
 
+module use /work/imas/etc/modulefiles
 module use /work/imas/opt/EasyBuild/modules/all
 
-module load cURL/7.40.0-GCC-4.8.3
-module load zlib/1.2.8-gompi-1.5.16
-module load GCC/4.9.3-2.25
-module load mpich2/3.1.3-intel
-module load scotch/5.1.12b
-module load metis/5.1.0
-module load mumps/4.10.0
-module load pastix/5.2.2.16
-module load fftw/3.3.4
-module load hdf5/1.8.15p1-gompi-1.5.16
-
-export ZLIB_HOME=/work/imas/opt/EasyBuild/software/zlib/1.2.8-gompi-1.5.16
+module load mpich2/3.1.3-gnu
+module load SCOTCH/6.0.0_esmumps-goolf-1.5.16
+module load METIS/4.0.3-goolf-1.5.16
+module load MUMPS/4.10.0-goolf-1.5.16-metis
+module load pastix/5.2.2.16 # TODO: get goolf version?
+module load FFTW/3.3.4-gompi-1.5.16 
+module load HDF5/1.8.9-goolf-1.5.16
 
 export LANG=C
 export JOREK_HOST=iter-hpc
