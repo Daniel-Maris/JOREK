da2abc0a41570fc305872a9365d6af4200253b93
Daan van Vugt <dvanvugt@ignitioncomputing.com>
2017-03-02 15:58:29 +0100

    Default settings for gfortran compilation iter hpc

---
+++ non_regression_tests/job_scripts/iter-hpc/env_gfortran.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+eval `tclsh /work/imas/opt/modules-tcl/modulecmd.tcl $(basename $SHELL) autoinit`
+
+module purge
+
+module use /work/imas/opt/EasyBuild/modules/all
+
+module load GCC/4.8.3
+module load openmpi/1.4.2-gnu
+#module load fftw/3.3.4
+module load hwloc/1.8.1-GCC-4.8.3
+module load hdf5/1.8.15p1-gompi-1.5.16
+module load zlib/1.2.8-GCC-4.8.3
+
+export ZLIB_HOME=/work/imas/opt/EasyBuild/software/zlib/1.2.8-gompi-1.5.16
+
+export LANG=C
+export JOREK_HOST=iter-hpc
+export compilethreads=4
+export MAKEFLAGS="-j$compilethreads"
+export PRERUN="export OMP_NUM_THREADS=4"
+export MPIRUN="mpirun -np "
+export BATCHCOMMAND="qsub"
+
+export http_proxy=${JOREK_HTTP_PROXY}
+export https_proxy=${JOREK_HTTP_PROXY}
