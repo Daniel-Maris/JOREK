5b9f1e5b4ec5cc9bcbd4a740cde7b8d2627f7d1a
Guillaume Emmanuel LATU <glatu000@r000u08l03.marconi.cineca.it>
2017-03-15 10:22:18 +0100

    Modif

--- non_regression_tests/testcases/big_test_circ_303/settings.sh
+++ non_regression_tests/testcases/big_test_circ_303/settings.sh
@@ -1,8 +1,8 @@
 # --- General settings
 jorekmodel="303"
-description="Tearing mode, circular plasma, model$jorekmodel, n_tor=9."
+description="Tearing mode, circular plasma, model$jorekmodel, n_tor=7."
 mpitasks=10
-binaries="jorek_model${jorekmodel}_9 rst_bin2hdf5 rst_hdf52bin"
+binaries="jorek_model${jorekmodel}_7 rst_bin2hdf5 rst_hdf52bin"
 binaries_initial="jorek_model${jorekmodel}_1 jorek_model${jorekmodel}_3"
 requiredfiles="input"
 extra_remote_files=""
@@ -21,10 +21,10 @@ function compile_jorek () {
     make $compilopt jorek_model${jorekmodel} rst_bin2hdf5 rst_hdf52bin                 || exit 1
     mv jorek_model${jorekmodel} jorek_model${jorekmodel}_3                             || exit 1
   fi
-  ./util/config.sh model=$jorekmodel n_tor=9 n_plane=32 n_period=1                    || exit 1
+  ./util/config.sh model=$jorekmodel n_tor=7 n_plane=32 n_period=1                    || exit 1
   make cleanall                                                                      || exit 1
   make $compilopt jorek_model${jorekmodel} rst_bin2hdf5 rst_hdf52bin                 || exit 1
-  mv jorek_model${jorekmodel} jorek_model${jorekmodel}_9                             || exit 1
+  mv jorek_model${jorekmodel} jorek_model${jorekmodel}_7                             || exit 1
 }
 
 
@@ -37,14 +37,14 @@ function initial_run () {
   ${codedir}/util/setinput.sh input nstep_n=30 tstep_n=3000. restart=.t.             || exit 1
   ${MPIRUN} $mpitasks ./jorek_model${jorekmodel}_3 < input | tee -a logfile          || exit 1
   ${codedir}/util/setinput.sh input nstep_n=1 tstep_n=300. restart=.t.             || exit 1
-  ${MPIRUN} $mpitasks ./jorek_model${jorekmodel}_9 < input | tee -a logfile          || exit 1
+  ${MPIRUN} $mpitasks ./jorek_model${jorekmodel}_7 < input | tee -a logfile          || exit 1
 }
 
 
 # --- Carry out the test case, i.e., run a single time step in the non-linear phase
 function restart_run () {
   ${codedir}/util/setinput.sh input restart=.t. nstep_n=1 tstep_n=300. nout=1       || exit 1
-  $MPIRUN $mpitasks ./jorek_model${jorekmodel}_9 < input | tee -a logfile                  || exit 1
+  $MPIRUN $mpitasks ./jorek_model${jorekmodel}_7 < input | tee -a logfile                  || exit 1
 }
 
 
