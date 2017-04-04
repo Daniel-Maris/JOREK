49b5c97354fbf89997565a83f9633a6a7b46afd1
Daan van Vugt <dvanvugt@ignitioncomputing.com>
2017-04-04 16:06:50 +0200

    Copy RTree implementation from feature/particles

---
+++ tools/mod_element_rtree.f90
@@ -0,0 +1,80 @@
+!> Module for creating an RTree of elements and searching in this tree
+module mod_element_rtree
+use iso_c_binding
+use data_structure
+implicit none
+public
+
+interface
+  !> Name is element_tree to match filename `element_rtree.cpp`.
+  !> `void PopulateTree(int nelm, double minx[], double miny[], double maxx[], double maxy[])`
+  subroutine element_rtree(n, minx, miny, maxx, maxy) bind(C,name="PopulateTree")
+    import C_DOUBLE, C_INT
+    integer(C_INT), intent(in), value :: n
+    real(C_DOUBLE), intent(in), dimension(n)    :: minx, miny, maxx, maxy
+  end subroutine element_rtree
+
+  !> `int NumElementsInRect(double minx, double miny, double maxx, double maxy)`
+  function num_elements_in_rect(minx, miny, maxx, maxy) bind(C,name='NumElementsInRect')
+    import C_DOUBLE, C_INT
+    real(C_DOUBLE), intent(in), value           :: minx, miny, maxx, maxy
+    integer(C_INT) :: num_elements_in_rect
+  end function num_elements_in_rect
+  !> `void ElementsInRect(double minx, double miny, double maxx, double maxy, int *ielm)`
+  subroutine elements_in_rect(minx, miny, maxx, maxy, ielm) bind(C,name='ElementsInRect')
+    import C_DOUBLE, C_INT
+    real(C_DOUBLE), intent(in), value           :: minx, miny, maxx, maxy
+    integer(C_INT), intent(inout), dimension(*) :: ielm
+  end subroutine elements_in_rect
+end interface
+
+contains
+
+!> Populate the RTree with the squares containing nodes of elements.
+!> x=R, y=Z
+subroutine populate_element_rtree(node_list, element_list)
+  type(type_node_list), intent(in)    :: node_list
+  type(type_element_list), intent(in) :: element_list
+  real(C_DOUBLE), dimension(:), allocatable :: minx, miny, maxx, maxy
+  integer :: i, n, vertices(n_vertex_max)
+  n = element_list%n_elements
+  allocate(minx(n), miny(n), maxx(n), maxy(n))
+  do i=1,n
+    vertices = element_list%element(i)%vertex(:)
+    minx(i) = minval(node_list%node(vertices)%x(1,1))
+    maxx(i) = maxval(node_list%node(vertices)%x(1,1))
+    miny(i) = minval(node_list%node(vertices)%x(1,2))
+    maxy(i) = maxval(node_list%node(vertices)%x(1,2))
+  end do
+  call element_rtree(int(n,C_INT), minx, miny, maxx, maxy)
+end subroutine populate_element_rtree
+
+!> Find probable neighbours of element i and return their indices.
+!> This is done by taking the bounding box of an element and expanding
+!> it slightly (10^-6 in absolute value) and returning all elements in
+!> this box, except element i
+subroutine nearby_elements(node_list, element_list, i_elm, i_nearby)
+  type(type_node_list), intent(in)    :: node_list
+  type(type_element_list), intent(in) :: element_list
+  integer, intent(in)                 :: i_elm
+  integer(C_INT), dimension(:), allocatable, intent(out) :: i_nearby
+
+  integer :: i
+  integer, dimension(n_vertex_max) :: vertices
+  real(C_DOUBLE) :: minx, miny, maxx, maxy
+  integer(C_INT) :: num_elements
+
+  vertices = element_list%element(i_elm)%vertex(:)
+  minx = minval(node_list%node(vertices)%x(1,1)) - 1d-6
+  maxx = maxval(node_list%node(vertices)%x(1,1)) + 1d-6
+  miny = minval(node_list%node(vertices)%x(1,2)) - 1d-6
+  maxy = maxval(node_list%node(vertices)%x(1,2)) + 1d-6
+
+  ! This calls the search routine twice! To get around that either pass a large enough array alway
+  ! or implement it as a mask/bitfield of the total number of elements.
+  ! I expect this to be unnecessary, as the speed improvement here is dramatic enough.
+  num_elements = num_elements_in_rect(minx, miny, maxx, maxy)
+  allocate(i_nearby(num_elements))
+  call elements_in_rect(minx, miny, maxx, maxy, i_nearby)
+end subroutine nearby_elements
+end module mod_element_rtree
