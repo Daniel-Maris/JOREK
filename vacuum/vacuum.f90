ac63ce88ea1dfb7f50d63b375f5e6cdc9e4123d4
Javier Artola <javier.artola@iter.org>
2017-05-17 13:02:42 +0200

    fix small bug while reading analytical expressions in coil currents time traces

--- vacuum/vacuum.f90
+++ vacuum/vacuum.f90
@@ -210,7 +210,7 @@ module vacuum
         
         ! --- Read the result
         call readProf(coil_curr_time_trace(i)%x, coil_curr_time_trace(i)%y, &
-          coil_curr_time_trace(i)%len, './jorek_curr_expr'//trim(s)//'.dat')
+          coil_curr_time_trace(i)%len, './jorek_curr_expr_'//trim(s)//'.dat')
         
         ! --- Delete temporary files
         call system('rm ./jorek_curr_expr_'//trim(s)//'.py ./jorek_curr_expr_'//trim(s)//'.dat')
