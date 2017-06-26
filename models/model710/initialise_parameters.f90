fab764c72deb77fcd8567027b1fab1ce63b84204
Guido Huijsmans <guido.huijsmans@iter.org>
2017-06-26 08:16:52 +0200

    add missing namelist variables for gmres

--- models/model710/initialise_parameters.f90
+++ models/model710/initialise_parameters.f90
@@ -66,6 +66,7 @@ namelist /in1/  tstep, nstep, tstep_n, nstep_n,                     &
                 edgeparticlesource, edgeparticlesource_psin,        &
                 edgeparticlesource_sig,                             &
                 produce_live_data, gmres, gmres_max_iter,           &
+                iter_precon, gmres_4, gmres_m, gmres_tol,           &
                 linear_run, export_for_nemec,                       &
                 output_bnd_elements,                                &
                 wall_file,                                          &
