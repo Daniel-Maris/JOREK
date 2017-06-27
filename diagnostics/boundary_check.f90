98d7f1cc3c2ec4d0d8a485a5a766eabee2cf82e8
Matthias Hoelzl <mhoelzl@ipp.mpg.de>
2017-06-27 14:55:20 +0200

    Adding OpenMP parallelization for the boundary check and reducing the number of points per element, B_tan is evaluated inside it from 11 to 3. This should speed up the boundary check a lot. We will see whether a further optimization of an MPI parallelization is required for it. NOTE that the boundary check is still switched off in this branch to make it easier for Feng/Eric to test whether the boundary integral improvements work properly.

--- diagnostics/boundary_check.f90
+++ diagnostics/boundary_check.f90
@@ -13,7 +13,7 @@ subroutine boundary_check()
   implicit none
   
   ! --- Local variables
-  integer, parameter     :: N_POINTS = 11 ! Number of evaluation points per element
+  integer, parameter     :: N_POINTS = 3 ! Number of evaluation points per element
   type(type_bnd_element) :: bndelem_m
   real*8, allocatable    :: B_par(:), B_par_v(:)
   real*8, allocatable    :: val_integral(:), err_integral(:)
@@ -52,6 +52,14 @@ subroutine boundary_check()
   err_integral(:) = 0.d0
 
   ! --- For every boundary element, do...
+  !$OMP PARALLEL DO DEFAULT(NONE)                                                                  &
+  !$OMP SHARED(bnd_elm_list, sr, resistive_wall, node_list, element_list, bnd_node_list,           &
+  !$OMP   starwall_equil_coils, psibnd_vec, psibnd_coils, wall_curr, bext_tan, I_coils)            &
+  !$OMP PRIVATE(m_bndelem, bndelem_m, m_elm, mv1, R1, Z1, R2, Z2, m_pt, B_par_v, s_or_t, H1, H1_s, &
+  !$OMP   H1_ss, s_pt, t_pt, s_const, R, R_s, R_t, R_st, R_ss, R_tt, Z, Z_s, Z_t, Z_st, Z_ss, Z_tt,&
+  !$OMP   xjac, e_par, l_starwall, P, P_s, P_t, P_st, P_ss, P_tt, P_R, P_Z, B_pol, B_par, i_vertex,&
+  !$OMP   i_node, i_node_bnd, i_dof, i_size, i_resp_old, i_resp, i_resp_0, basfunc_i, val_integral,&
+  !$OMP   err_integral, l_tor )
   L_MB: do m_bndelem = 1, bnd_elm_list%n_bnd_elements
 
     bndelem_m = bnd_elm_list%bnd_element(m_bndelem)
@@ -180,6 +188,7 @@ subroutine boundary_check()
     end do L_MP
 
   end do L_MB
+  !$OMP END PARALLEL DO
 
   if ( minval(abs(val_integral)) /= 0.d0 ) then
     write(*,'(1x,A,20ES15.5)') 'Relative errors in harmonics:', err_integral(:) / val_integral(:), err_integral(:), val_integral(:)
