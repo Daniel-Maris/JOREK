5a790a5968ce00b30f3d811d3e02893e115e873a
Stanislas Pamela <pstanis@freia003.hpc.l>
2017-07-20 10:22:50 +0100

    1000 is far too small as a default

--- models/model333/mod_parameters.f90
+++ models/model333/mod_parameters.f90
@@ -14,7 +14,7 @@ module mod_parameters
   integer, parameter :: n_vertex_max   = 4  	       !< maximum number of corners of an element
   integer, parameter :: n_nodes_max    = 100001        !< maximum number of nodes
   integer, parameter :: n_elements_max = 100001        !< maximum number of elements
-  integer, parameter :: n_boundary_max = 1001          !< maximum number of boundary elements
+  integer, parameter :: n_boundary_max = 10001         !< maximum number of boundary elements
   integer, parameter :: n_pieces_max   = 100001        !< maximum number of line pieces describing a flux surface
   integer, parameter :: n_degrees      = n_order+1     !< degrees of freedom per variable per node
   integer, parameter :: nref_max       = 10000         !< (refinement)
