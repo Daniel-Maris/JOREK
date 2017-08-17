facf9c8188b1f79e9bdeea0233b395d895b08b1f
Daan van Vugt <dvanvugt@ignitioncomputing.com>
2017-08-17 22:45:59 +0200

    Update interp_deltas to new code

--- elements/mod_interp_PRZ.f90
+++ elements/mod_interp_PRZ.f90
@@ -1,98 +1,5 @@
 module mod_interp_PRZ
 contains
-!> This subroutine interpolates some variables at positions within one element.
-!> Assumes a very simple mode numbering! Namely that every nth mode is a simple multiple of mode 2
-!>
-!> This is slightly faster for many particles since it only needs to precalculate
-!> the values once. Save 10-20% for > 10 particles in one call.
-pure subroutine interp_PRZ_vec(node_list, element_list, i_elm, i_v, n_v, n_p, s, t, phi, P, P_s, P_t, P_phi, R, R_s, R_t, Z, Z_s, Z_t)
-
-use data_structure
-use mod_parameters, only: n_period
-use mod_basisfunctions
-implicit none
-
-! --- Routine parameters
-type (type_node_list),      intent(in)  :: node_list
-type (type_element_list),   intent(in)  :: element_list
-integer,                    intent(in)  :: i_elm
-integer,                    intent(in)  :: n_v, i_v(n_v), n_p
-real*8, dimension(n_p),     intent(in)  :: s, t, phi
-real*8, dimension(n_p,n_v), intent(out) :: P, P_s, P_t, P_phi
-real*8, dimension(n_p),     intent(out) :: R, R_s, R_t, Z, Z_s, Z_t
-
-integer, parameter :: n_mode = (n_tor-1)/2 ! number of modes excluding 0
-
-! --- Local variables
-real*8  :: H(4,4), H_s(4,4), H_t(4,4), HZ(n_tor,n_p), dHZ(n_tor,n_p)
-real*8  :: e1r(n_p), e1i(n_p), enr(n_p), eni(n_p), enrtmp(n_p) !< complex and real parts
-integer :: kv, iv, kf, i, j, i_harm, i_mode, i_tor, i_p
-real*8  :: values(n_tor,n_order+1,n_v,n_vertex_max)
-real*8  :: xR(n_order+1,n_vertex_max), xZ(n_order+1,n_vertex_max)
-real*8  :: sizes(n_order+1), v, vp
-
-P = 0.d0; P_s = 0.d0; P_t = 0.d0; P_phi = 0.d0
-
-! Apply De Moivre formula to calculate the series of sines.
-! Assumes that mode is of the form [0 1 1 2 2 3 3 4 4] ([0 4 4 8 8 12 12])
-! This is roughly 3-4 times faster in my tests than just calculating the sines
-! and cosines (even when that is vectorized). Perhaps that changes for n_tor >> 10
-! I tested n_tor = 17.
-e1r = cos(n_period*phi)
-e1i = sin(n_period*phi)
-enr = 1.d0
-eni = 0.d0
-HZ(1,:) = 1.d0
-dHZ(1,:) = 0.d0
-do i=1,n_mode
-  enrtmp = enr
-  enr = enr*e1r - eni*e1i
-  eni = eni*e1r + enrtmp*e1i
-  HZ(2*i,:)   = enr
-  HZ(2*i+1,:) = eni
-end do
-do i=1,n_mode
-  dHZ(2*i,:)   = HZ(2*i+1,:)*(-n_period*i)
-  dHZ(2*i+1,:) = HZ(2*i,:)*(n_period*i)
-end do
-
-! Preload values and premultiply with sizes(:,kv)
-do kv = 1,n_vertex_max  ! 4 vertices
-  iv = element_list%element(i_elm)%vertex(kv)
-  sizes(:) = element_list%element(i_elm)%size(kv,:)
-  do i = 1, n_v
-    do kf=1,n_order+1
-      values(1:n_tor,kf,i,kv) = node_list%node(iv)%values(1:n_tor,kf,i_v(i)) * sizes(kf)
-    end do
-  end do
-  xR(:,kv) = node_list%node(iv)%x(:,1) * sizes(:)
-  xZ(:,kv) = node_list%node(iv)%x(:,2) * sizes(:)
-end do
-
-do i_p = 1,n_p
-  call basisfunctions6(s(i_p),t(i_p),H,H_s,H_t)
-  R(i_p)   = sum(xR*H)
-  R_s(i_p) = sum(xR*H_s)
-  R_t(i_p) = sum(xR*H_t)
-  Z(i_p)   = sum(xZ*H)
-  Z_s(i_p) = sum(xZ*H_s)
-  Z_t(i_p) = sum(xZ*H_t)
-
-  do kv = 1,n_vertex_max
-    do i = 1, n_v
-      do kf = 1, n_order+1
-        v = sum(values(1:n_tor,kf,i,kv)*HZ(1:n_tor, i_p))
-        P(i_p,i)     = P(i_p,i)     + v * H(kf, kv)
-        P_s(i_p,i)   = P_s(i_p,i)   + v * H_s(kf, kv)
-        P_t(i_p,i)   = P_t(i_p,i)   + v * H_t(kf, kv)
-        vp = sum(values(1:n_tor,kf,i,kv)*dHZ(1:n_tor, i_p))
-        P_phi(i_p,i) = P_phi(i_p,i) + vp * H(kf, kv)
-      enddo
-    enddo
-  enddo
-enddo
-end subroutine interp_PRZ_vec
-
 !> This subroutine interpolates some variables at a specific position within one element at a given position (s,t)
 pure subroutine interp_PRZ(node_list, element_list, i_elm, i_v, n_v, s, t, phi, P, P_s, P_t, P_phi, R, R_s, R_t, Z, Z_s, Z_t)
 
@@ -121,7 +28,6 @@ real*8  :: values(n_tor,n_order+1,n_v,n_vertex_max)
 real*8  :: xR(n_order+1,n_vertex_max), xZ(n_order+1,n_vertex_max)
 real*8  :: sizes(n_order+1), v, vp
 
-!maybe try !DIR$ FORCEINLINE 
 call basisfunctions6(s,t,H,H_s,H_t)
 
 P = 0.d0; P_s = 0.d0; P_t = 0.d0; P_phi = 0.d0
@@ -181,12 +87,11 @@ do kv = 1,n_vertex_max
 enddo
 end subroutine interp_PRZ
 
-
-!> This subroutine interpolates the change (u_n-u_(n-1)) in some variables at a specific position within one element at a given position (s,t)
+!> This subroutine interpolates some variables at a specific position within one element at a given position (s,t)
 pure subroutine interp_PRZ_delta(node_list, element_list, i_elm, i_v, n_v, s, t, phi, P, P_s, P_t, P_phi, R, R_s, R_t, Z, Z_s, Z_t)
 
 use data_structure
-use phys_module, only : mode
+use mod_parameters, only: n_period
 use mod_basisfunctions
 implicit none
 
@@ -203,52 +108,67 @@ real*8,                   intent(out) :: P_phi(n_v)
 integer, parameter :: n_mode = (n_tor-1)/2 ! number of modes excluding 0
 
 ! --- Local variables
-real*8  :: H(4,4), H_s(4,4), H_t(4,4), xx1, xx2, ss, coss(n_mode), sins(n_mode)
-integer :: kv, iv, kf, i, i_harm, i_mode, mymodes(n_mode)
+real*8  :: H(4,4), H_s(4,4), H_t(4,4), HZ(n_tor), dHZ(n_tor)
+integer :: kv, iv, kf, i, i_tor
+real*8  :: e1r, e1i, enr, eni, enrtmp
+real*8  :: deltas(n_tor,n_order+1,n_v,n_vertex_max)
+real*8  :: xR(n_order+1,n_vertex_max), xZ(n_order+1,n_vertex_max)
+real*8  :: sizes(n_order+1), v, vp
 
-call basisfunctions3(s,t,H,H_s,H_t)
+call basisfunctions6(s,t,H,H_s,H_t)
 
 P = 0.d0; P_s = 0.d0; P_t = 0.d0; P_phi = 0.d0
-R = 0.d0; R_s = 0.d0; R_t = 0.d0;
-Z = 0.d0; Z_s = 0.d0; Z_t = 0.d0;
 
-mymodes = mode(3:n_tor:2)
-coss = cos(mymodes*phi)
-sins = sin(mymodes*phi)
+! Apply De Moivre formula to calculate the series of sines.
+! Assumes that mode is of the form [0 1 1 2 2 3 3 4 4] ([0 4 4 8 8 12 12])
+! This is roughly 3-4 times faster in my tests than just calculating the sines
+! and cosines (even when that is vectorized). Perhaps that changes for n_tor >> 10
+! I tested n_tor = 17.
+e1r = cos(n_period*phi)
+e1i = sin(n_period*phi)
+enr = 1.d0
+eni = 0.d0
+HZ(1) = 1.d0
+dHZ(1) = 0.d0
+do i=1,n_mode
+  enrtmp = enr
+  enr = enr*e1r - eni*e1i
+  eni = eni*e1r + enrtmp*e1i
+  HZ(2*i)   = enr
+  HZ(2*i+1) = eni
+  dHZ(2*i) = eni*(-n_period*i)
+  dHZ(2*i+1) = enr*(n_period*i)
+end do
 
+! Preload deltas and premultiply with sizes(:,kv)
 do kv = 1,n_vertex_max  ! 4 vertices
   iv = element_list%element(i_elm)%vertex(kv)
-  do kf = 1, n_order+1       ! 4 basis functions
+  sizes(:) = element_list%element(i_elm)%size(kv,:)
+  do i = 1, n_v
+    do kf=1,n_order+1
+      deltas(1:n_tor,kf,i,kv) = node_list%node(iv)%deltas(1:n_tor,kf,i_v(i)) * sizes(kf)
+    end do
+  end do
+  xR(:,kv) = node_list%node(iv)%x(:,1) * sizes(:)
+  xZ(:,kv) = node_list%node(iv)%x(:,2) * sizes(:)
+end do
 
-    xx1 = node_list%node(iv)%x(kf,1)
-    xx2 = node_list%node(iv)%x(kf,2)
-    ss  = element_list%element(i_elm)%size(kv,kf)
+R   = sum(xR*H)
+R_s = sum(xR*H_s)
+R_t = sum(xR*H_t)
+Z   = sum(xZ*H)
+Z_s = sum(xZ*H_s)
+Z_t = sum(xZ*H_t)
 
-    R    = R    + xx1 * ss * H(kv,kf)
-    R_s  = R_s  + xx1 * ss * H_s(kv,kf)
-    R_t  = R_t  + xx1 * ss * H_t(kv,kf)
-
-    Z    = Z    + xx2 * ss * H(kv,kf)
-    Z_s  = Z_s  + xx2 * ss * H_s(kv,kf)
-    Z_t  = Z_t  + xx2 * ss * H_t(kv,kf)
-
-    do i = 1, n_v
-      P(i)     = P(i)     + node_list%node(iv)%deltas(1,kf,i_v(i)) * ss * H(kv,kf)
-      P_s(i)   = P_s(i)   + node_list%node(iv)%deltas(1,kf,i_v(i)) * ss * H_s(kv,kf)
-      P_t(i)   = P_t(i)   + node_list%node(iv)%deltas(1,kf,i_v(i)) * ss * H_t(kv,kf)
-      do i_mode = 1, n_mode
-        i_harm = i_mode*2
-        P(i)     = P(i)     + node_list%node(iv)%deltas(i_harm,kf,i_v(i)) * ss * H(kv,kf)   * coss(i_mode)
-        P_s(i)   = P_s(i)   + node_list%node(iv)%deltas(i_harm,kf,i_v(i)) * ss * H_s(kv,kf) * coss(i_mode)
-        P_t(i)   = P_t(i)   + node_list%node(iv)%deltas(i_harm,kf,i_v(i)) * ss * H_t(kv,kf) * coss(i_mode)
-        P_phi(i) = P_phi(i) + node_list%node(iv)%deltas(i_harm,kf,i_v(i)) * ss * H(kv,kf)   * sins(i_mode)*(-mymodes(i_mode))
-
-        i_harm = i_mode*2+1
-        P(i)     = P(i)     + node_list%node(iv)%deltas(i_harm,kf,i_v(i)) * ss * H(kv,kf)   * sins(i_mode)
-        P_s(i)   = P_s(i)   + node_list%node(iv)%deltas(i_harm,kf,i_v(i)) * ss * H_s(kv,kf) * sins(i_mode)
-        P_t(i)   = P_t(i)   + node_list%node(iv)%deltas(i_harm,kf,i_v(i)) * ss * H_t(kv,kf) * sins(i_mode)
-        P_phi(i) = P_phi(i) + node_list%node(iv)%deltas(i_harm,kf,i_v(i)) * ss * H(kv,kf)   * coss(i_mode)*mymodes(i_mode)
-      enddo
+do kv = 1,n_vertex_max
+  do i = 1, n_v
+    do kf = 1, n_order+1
+      v = sum(deltas(1:n_tor,kf,i,kv)*HZ(1:n_tor))
+      P(i)     = P(i)     + v * H(kf, kv)
+      P_s(i)   = P_s(i)   + v * H_s(kf, kv)
+      P_t(i)   = P_t(i)   + v * H_t(kf, kv)
+      vp = sum(deltas(1:n_tor,kf,i,kv)*dHZ(1:n_tor))
+      P_phi(i) = P_phi(i) + vp * H(kf, kv)
     enddo
   enddo
 enddo
--- particles/benchmarks/interp/interp_PRZ_bench.f90
+++ particles/benchmarks/interp/interp_PRZ_bench.f90
@@ -13,7 +13,6 @@ implicit none
 integer, parameter :: n_switch = 100 !< Go to a new element every 100 steps (mimic jorek particle pattern)
 ! we go to a new position inside the element every step.
 integer, parameter :: N_loops = 1000000
-integer, parameter :: N_vector = 128
 integer, parameter :: n_v = 1, i_var(n_v) = [1]
 
 integer :: ierr, provided, i, j, k, i_elm
@@ -21,9 +20,9 @@ type(type_node_list) :: node_list
 type(type_element_list) :: element_list
 real*8 :: t0, t1
 type(pcg32_rng) :: rng
-real*8, dimension(n_vector) :: R, R_s, R_t, Z, Z_s, Z_t
-real*8, dimension(n_vector,n_v) :: P, P_s, P_t, P_phi
-real*8, dimension(n_vector) :: u, s, t, phi
+real*8 :: R, R_s, R_t, Z, Z_s, Z_t
+real*8, dimension(n_v) :: P, P_s, P_t, P_phi
+real*8 :: u(1), s, t, phi
 
 ! Setup the grid
 call MPI_INIT_THREAD(MPI_THREAD_SINGLE, provided, ierr)
@@ -42,7 +41,7 @@ enddo
 ! Wait after this omp part for a while
 call sleep(3)
 
-call rng%initialize(n_vector, random_seed(), 1, 1)
+call rng%initialize(1, random_seed(), 1, 1)
 
 call cpu_time(t0)
 ! Start the test
@@ -52,24 +51,19 @@ do i=1,N_loops
     call rng%next(u)
     i_elm = nint(1.d0+u(1)*real(element_list%n_elements-1))
   end if
-  call rng%next(u); s = u
-  call rng%next(u); t = u
-  call rng%next(u); phi = u
-  call interp_PRZ_vec(node_list, element_list, i_elm, i_var, n_v, n_vector, s, t, phi, P, P_s, P_t, P_phi, R, R_s, R_t, Z, Z_s, Z_t)
-  !do j=1,n_vector
-  !  call interp_PRZ(node_list, element_list, i_elm, i_var, n_v, s(j), t(j), phi(j), &
-  !    P(j,:), P_s(j,:), P_t(j,:), P_phi(j,:), R(j), R_s(j), R_t(j), Z(j), Z_s(j), Z_t(j))
-  !end do
+  call rng%next(u); s = u(1)
+  call rng%next(u); t = u(1)
+  call rng%next(u); phi = u(1)
+  call interp_PRZ(node_list, element_list, i_elm, i_var, n_v, s, t, phi, P, P_s, &
+  P_t, P_phi, R, R_s, R_t, Z, Z_s, Z_t)
   call rng%next(u)
-  do j=1,n_vector
-    do k=1,n_v
-      if (u(j) .gt. 0.999999) write(*,"(10g16.7)") P(j,k), P_s(j,k), P_t(j,k), P_phi(j,k), R(j), R_s(j), R_t(j), Z(j), Z_s(j), Z_t(j)
-    end do
+  do k=1,n_v
+    if (u(1) .gt. 0.999999) write(*,"(10g16.7)") P(k), P_s(k), P_t(k), P_phi(k), R, R_s, R_t, Z, Z_s, Z_t
   end do
 end do
 call cpu_time(t1)
 
-write(*,*) "Time for ", N_loops*N_vector, " iterations: ", t1-t0, "s"
-write(*,*) "Time per iteration: ", ((t1-t0)/real(N_loops*N_vector)) *1d6, " microseconds"
+write(*,*) "Time for ", N_loops, " iterations: ", t1-t0, "s"
+write(*,*) "Time per iteration: ", ((t1-t0)/real(N_loops)) *1d6, " microseconds"
 
 end program interp_PRZ_bench
