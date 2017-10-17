daecd658e294f422f92dfe23da72b4d829d0eeac
Mochalskyy, Serhiy (smo) <serhiy.mochalskyy@ipp.mpg.de>
2017-10-17 14:07:06 +0200

    cleaned the debugging printing

--- communication/live_data.f90
+++ communication/live_data.f90
@@ -38,7 +38,6 @@ module live_data
     logical :: opened
     integer :: n, i
  
-    write(*,*) "CCC111", produce_live_data   
     if ( .not. produce_live_data ) return
     
     ! --- Check, that the file handle is not already in use.
@@ -143,13 +142,9 @@ module live_data
     write(LIVE_DATA_HANDLE,'(A)') '@particlesource: %"time"   "inside separatrix"   "outside separatrix"'
     write(LIVE_DATA_HANDLE,*)
     
-    write(*,*) "CCC222"
-
     ! --- Call the model-specific part of the init_live_data routine
     call init_live_data_model(LIVE_DATA_HANDLE) 
     
-      write(*,*) "CCC333"
-
     close(LIVE_DATA_HANDLE)
     
   end subroutine init_live_data
