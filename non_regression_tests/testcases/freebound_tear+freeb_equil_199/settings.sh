afc38fcfc048b8f968cc25044126e5cb3c8e5f81
Francisco Javier Artola Such <fartolas@r000u07l02.marconi.cineca.it>
2017-10-27 14:54:38 +0200

    New regression testcase using 2 STARWALL freeboundary modes

---
+++ non_regression_tests/testcases/freebound_tear+freeb_equil_199/settings.sh
@@ -0,0 +1,40 @@
+# --- General settings
+jorekmodel="199"
+description="free-boundary 2/1 mode togehter with a freebnd_equil (2 STARWALL modes).   \
+If freebound_equil=.f. the same growth rate 2.29d-4 is found.                           \
+This case is a slightly modified version of the working benchmark with CASTOR."
+mpitasks=2
+binaries="jorek_model${jorekmodel}_1 rst_bin2hdf5 rst_hdf52bin"
+binaries_initial=""
+requiredfiles="input starwall-response.dat"
+extra_remote_files="starwall-response.dat"
+
+
+# --- Compile the code for the test case
+function compile_jorek () {
+  ./util/config.sh model=$jorekmodel n_tor=3 n_plane=4 n_period=1                    || exit 1
+  make cleanall                                                                      || exit 1
+  make $compilopt jorek_model${jorekmodel} rst_bin2hdf5 rst_hdf52bin                 || exit 1
+  mv jorek_model${jorekmodel} jorek_model${jorekmodel}_1                             || exit 1
+}
+
+
+# --- Re-run the whole case from scratch
+function initial_run () {
+  ${codedir}/util/setinput.sh input restart=.f. nstep_n=40 tstep_n=1000.d0 || exit 1
+  $MPIRUN $mpitasks ./jorek_model${jorekmodel}_1 < input | tee logfile               || exit 1
+}
+
+
+# --- Carry out the test case.
+function restart_run () {
+  ${codedir}/util/setinput.sh input restart=.t. nstep_n=1 tstep_n=1000.d0 || exit 1
+  $MPIRUN $mpitasks ./jorek_model${jorekmodel}_1 < input | tee -a logfile            || exit 1
+}
+
+
+# --- Compare the results of the test case to the reference solution
+function compare_results () {
+  ./rst_bin2hdf5 < ./input                                                           || exit 1
+  h5diff -d 1e-5 jorek_restart.h5 ${testcasedir}/end.h5 values                      || exit 1
+}
