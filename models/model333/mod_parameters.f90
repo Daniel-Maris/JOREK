5fbb629ffb4fc69ce8e69aff10a48c9ca2f4b516
Matthias Hoelzl <mhoelzl@ipp.mpg.de>
2017-11-20 08:28:14 +0100

    Undoing unnecessary changes in mod_parameters.

--- models/model333/mod_parameters.f90
+++ models/model333/mod_parameters.f90
@@ -8,9 +8,9 @@ module mod_parameters
   integer, parameter :: n_var          = 7  	       !< number of variables
   integer, parameter :: n_dim          = 2  	       !< number of dimensions
   integer, parameter :: n_order        = 3  	       !< order of the polynomial basis
-  integer, parameter :: n_tor          = 3  	       !< number of toroidal harmonics
+  integer, parameter :: n_tor          = 1  	       !< number of toroidal harmonics
   integer, parameter :: n_period       = 1  	       !< periodicity in toroidal direction
-  integer, parameter :: n_plane        = 4  	       !< number of toroidal angles
+  integer, parameter :: n_plane        = 1  	       !< number of toroidal angles
   integer, parameter :: n_vertex_max   = 4  	       !< maximum number of corners of an element
   integer, parameter :: n_nodes_max    = 100001        !< maximum number of nodes
   integer, parameter :: n_elements_max = 100001        !< maximum number of elements
