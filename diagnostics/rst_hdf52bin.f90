f28f2f296c620c59321a43c53e10da2515178534
Daan van Vugt <dvanvugt@ignitioncomputing.com>
2017-11-20 22:08:14 +0100

    Add missing my_id to import_hdf5_restart

--- diagnostics/rst_hdf52bin.f90
+++ diagnostics/rst_hdf52bin.f90
@@ -56,7 +56,7 @@ program RST_convert_hdf52bin
 
   ! --- Read the restart HDF5 file
   if (verbose) write (6,*) " =============> rst_hdf52bin for filename = ",filein
-  call import_hdf5_restart(node_list, element_list, filein, rst_format, ierr)
+  call import_hdf5_restart(node_list, element_list, filein, rst_format, 0, ierr)
 
   index_now = index_start
   t_now     = t_start
