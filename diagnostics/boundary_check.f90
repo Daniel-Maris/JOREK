b6fd546c741441256c74cf85120241bab85537af
Matthias Hoelzl <mhoelzl@ipp.mpg.de>
2017-11-27 16:33:56 +0100

    First attempt to add OpenMP parallelization to diagnostics/boundary_check.f90, might not be correct yet.

--- diagnostics/boundary_check.f90
+++ diagnostics/boundary_check.f90
@@ -33,7 +33,7 @@ subroutine boundary_check(my_id)
   real*8   :: B_pol(2)           ! Poloidal magnetic field
   real*8   :: e_par(2)           ! Vector tangential to interface
   real*8   :: P_R, P_Z           ! dPsi/dR, dPsi/dZ
-  real*8   :: R1, R2, Z1, Z2
+  real*8   :: R1, R2, Z1, Z2, B_par_v_tmp
   logical  :: s_const            ! Is the bound. elem. an s=const side of the 2D element?
   integer  :: ierr,step          ! variables for parallel version 
   
@@ -62,16 +62,14 @@ subroutine boundary_check(my_id)
   ! --- For every boundary element, do...
   L_MB: do m_bndelem = 1, bnd_elm_list%n_bnd_elements
   
-    if (my_id == 0) then
-      bndelem_m = bnd_elm_list%bnd_element(m_bndelem)
-      m_elm     = bnd_elm_list%bnd_element(m_bndelem)%element
-      mv1       = bnd_elm_list%bnd_element(m_bndelem)%side
+    bndelem_m = bnd_elm_list%bnd_element(m_bndelem)
+    m_elm     = bnd_elm_list%bnd_element(m_bndelem)%element
+    mv1       = bnd_elm_list%bnd_element(m_bndelem)%side
 
-      R1 = node_list%node(bndelem_m%vertex(1))%x(1,1)
-      Z1 = node_list%node(bndelem_m%vertex(1))%x(1,2)
-      R2 = node_list%node(bndelem_m%vertex(2))%x(1,1)
-      Z2 = node_list%node(bndelem_m%vertex(2))%x(1,2)
-    endif ! my_id == 0
+    R1 = node_list%node(bndelem_m%vertex(1))%x(1,1)
+    Z1 = node_list%node(bndelem_m%vertex(1))%x(1,2)
+    R2 = node_list%node(bndelem_m%vertex(2))%x(1,1)
+    Z2 = node_list%node(bndelem_m%vertex(2))%x(1,2)
    
     ! --- For several points in the boundary element, do...
     L_MP: do m_pt = 1, N_POINTS
@@ -79,103 +77,94 @@ subroutine boundary_check(my_id)
       B_par(:)   = 0.d0
       B_par_v(:) = 0.d0
 
-      if (my_id == 0) then
-        ! --- Determine 1D basis function (and derivatives) at current point
-        s_or_t = float(m_pt-1)/float(N_POINTS-1)
+      ! --- Determine 1D basis function (and derivatives) at current point
+      s_or_t = float(m_pt-1)/float(N_POINTS-1)
+    
+      call basisfunctions1(s_or_t, H1, H1_s, H1_ss)
+
+      ! --- Which s and t values correspond to the current point and is the
+      !     boundary element an s=const or t=const side of the 2D element?
+      select case (mv1)
+      case (1)
+        s_pt = s_or_t;  t_pt = 0.d0;    s_const = .false.
+      case (2)
+        s_pt = 1.d0;    t_pt = s_or_t;  s_const = .true.
+      case (3)
+        s_pt = s_or_t;  t_pt = 1.d0;    s_const = .false.
+      case (4)
+        s_pt = 0.d0;    t_pt = s_or_t;  s_const = .true.
+      end select
+
+      ! --- Determine coordinate values (plus derivatives)
+      call interp_RZ(node_list, element_list, m_elm, s_pt, t_pt, R, R_s, R_t, R_st, R_ss, R_tt, Z, Z_s, Z_t, Z_st, Z_ss, Z_tt)
+
+      ! --- 2D Jacobian
+      xjac = R_s * Z_t - R_t * Z_s
+
+      ! --- Tangential vector to the interface
+      if ( s_const ) then
+        e_par = (/ R_t, Z_t /) / sqrt( R_t**2 + Z_t**2 ) * (R_t * (R2-R1) + Z_t * (Z2-Z1))/abs(R_t * (R2-R1) + Z_t * (Z2-Z1))
+      else
+        e_par = (/ R_s, Z_s /) / sqrt( R_s**2 + Z_s**2 ) * (R_s * (R2-R1) + Z_s * (Z2-Z1))/abs(R_s * (R2-R1) + Z_s * (Z2-Z1))
+      end if
+
+      step = sr%a_ye%step
       
-        call basisfunctions1(s_or_t, H1, H1_s, H1_ss)
-
-        ! --- Which s and t values correspond to the current point and is the
-        !     boundary element an s=const or t=const side of the 2D element?
-        select case (mv1)
-        case (1)
-          s_pt = s_or_t;  t_pt = 0.d0;    s_const = .false.
-        case (2)
-          s_pt = 1.d0;    t_pt = s_or_t;  s_const = .true.
-        case (3)
-          s_pt = s_or_t;  t_pt = 1.d0;    s_const = .false.
-        case (4)
-          s_pt = 0.d0;    t_pt = s_or_t;  s_const = .true.
-        end select
-
-        ! --- Determine coordinate values (plus derivatives)
-        call interp_RZ(node_list, element_list, m_elm, s_pt, t_pt, R, R_s, R_t, R_st, R_ss, R_tt, Z, Z_s, Z_t, Z_st, Z_ss, Z_tt)
-
-        ! --- 2D Jacobian
-        xjac = R_s * Z_t - R_t * Z_s
-
-        ! --- Tangential vector to the interface
-        if ( s_const ) then
-          e_par = (/ R_t, Z_t /) / sqrt( R_t**2 + Z_t**2 ) * (R_t * (R2-R1) + Z_t * (Z2-Z1))/abs(R_t * (R2-R1) + Z_t * (Z2-Z1))
-        else
-          e_par = (/ R_s, Z_s /) / sqrt( R_s**2 + Z_s**2 ) * (R_s * (R2-R1) + Z_s * (Z2-Z1))/abs(R_s * (R2-R1) + Z_s * (Z2-Z1))
-        end if
-      endif ! my_id == 0
-
       ! --- Select one STARWALL harmonic
+!$omp parallel do                                                                                  &
+!$omp   schedule(dynamic)                                                                          &
+!$omp   default(none)                                                                              &
+!$omp   shared(node_list, element_list, m_elm, s_pt, t_pt, sr, bndelem_m, bnd_node_list, H1,       &
+!$omp     resistive_wall, starwall_equil_coils, my_id, psibnd_vec, psibnd_coils, wall_curr,        &
+!$omp     bext_tan, I_coils, B_par_v, R, R_s, R_t, R_st, R_ss, R_tt, Z, Z_s, Z_t, Z_st, Z_ss, Z_tt,&
+!$omp     xjac, e_par, B_par, step)                                                                &
+!$omp   private(l_starwall, i_vertex, i_dof, l_tor, P, P_s, P_t, P_st, P_ss, P_tt, P_R, P_Z, B_pol,&
+!$omp     i_node, i_node_bnd, i_size, i_resp_old, i_resp, i_resp_0, basfunc_i, B_par_v_tmp)
       L_LS: do l_starwall = 1, sr%n_tor
 
-        if (my_id == 0 ) then
-          l_tor = sr%i_tor(l_starwall)
+        l_tor = sr%i_tor(l_starwall)
 
-          ! --- Psi value (plus derivatives) at current point (l_tor mode)
-          call interp(node_list, element_list, m_elm, 1, l_tor, s_pt, t_pt, P, P_s, P_t, P_st, P_ss, P_tt)
+        ! --- Psi value (plus derivatives) at current point (l_tor mode)
+        call interp(node_list, element_list, m_elm, 1, l_tor, s_pt, t_pt, P, P_s, P_t, P_st, P_ss, P_tt)
 
-          ! --- Poloidal magnetic field at current point
-          P_R   = (   P_s * Z_t - P_t * Z_s ) / xjac ! dPsi/dR
-          P_Z   = ( - P_s * R_t + P_t * R_s ) / xjac ! dPsi/dZ
-          B_pol = (/ P_Z, -P_R /) / R
+        ! --- Poloidal magnetic field at current point
+        P_R   = (   P_s * Z_t - P_t * Z_s ) / xjac ! dPsi/dR
+        P_Z   = ( - P_s * R_t + P_t * R_s ) / xjac ! dPsi/dZ
+        B_pol = (/ P_Z, -P_R /) / R
 
-          ! --- Tangential magnetic field B_{||} reconstructed from the plasma
-          B_par(l_starwall) = - sum( B_pol * e_par )
-        endif ! my_id == 0
+        ! --- Tangential magnetic field B_{||} reconstructed from the plasma
+!$omp critical
+        B_par(l_starwall) = - sum( B_pol * e_par )
+!$omp end critical
 
         ! --- Sum over boundary dofs at which response is calculated
         L_IV: do i_vertex = 1, 2 ! (loop over nodes in element m_bndelem)
 
-          if (my_id == 0 ) then
-            i_node      = bndelem_m%vertex(i_vertex)
-            i_node_bnd  = bndelem_m%bnd_vertex(i_vertex)
-          endif ! my_id == 0
+          i_node      = bndelem_m%vertex(i_vertex)
+          i_node_bnd  = bndelem_m%bnd_vertex(i_vertex)
 
           L_ID: do i_dof = 1, 2 ! (loop over node dofs)
 
-            if (my_id == 0 ) then
-              i_size      = bndelem_m%size(i_vertex,i_dof)
+            i_size      = bndelem_m%size(i_vertex,i_dof)
 
-              i_resp_old  = response_index(i_node_bnd,l_starwall,i_dof)
+            i_resp_old  = response_index(i_node_bnd,l_starwall,i_dof)
 
-              i_resp   = (bnd_node_list%bnd_node(i_node_bnd)%index_starwall(1) - 1)*sr%n_tor &
-                       + bnd_node_list%bnd_node(i_node_bnd)%n_dof*(l_starwall-1) &
-                       + bnd_node_list%bnd_node(i_node_bnd)%index_starwall(i_dof)-bnd_node_list%bnd_node(i_node_bnd)%index_starwall(1) + 1
-                     
-              i_resp_0 = response_index_eq(i_node_bnd,i_dof)
+            i_resp   = (bnd_node_list%bnd_node(i_node_bnd)%index_starwall(1) - 1)*sr%n_tor &
+                     + bnd_node_list%bnd_node(i_node_bnd)%n_dof*(l_starwall-1) &
+                     + bnd_node_list%bnd_node(i_node_bnd)%index_starwall(i_dof)-bnd_node_list%bnd_node(i_node_bnd)%index_starwall(1) + 1
+                   
+            i_resp_0 = response_index_eq(i_node_bnd,i_dof)
 
-              ! --- Determine basis function
-              basfunc_i = H1(i_vertex,i_dof) * i_size
-            endif ! my_id == 0
+            ! --- Determine basis function
+            basfunc_i = H1(i_vertex,i_dof) * i_size
 
-            ! All work is doing mpi rank 0 and after bcast all necessarry data
-            ! to other ranks for calculation using distributed matrices
-            call MPI_bcast(l_tor,        1,                  MPI_INTEGER,           0, MPI_COMM_WORLD, ierr)   
-            call MPI_bcast(l_starwall,   1,                  MPI_INTEGER,           0, MPI_COMM_WORLD, ierr)
-            call MPI_bcast(basfunc_i,    1,                  MPI_DOUBLE_PRECISION,  0, MPI_COMM_WORLD, ierr)
-            call MPI_bcast(i_resp,       1,                  MPI_INTEGER,           0, MPI_COMM_WORLD, ierr)
-            call MPI_bcast(i_resp_0,     1,                  MPI_INTEGER,           0, MPI_COMM_WORLD, ierr)
-            call MPI_bcast(psibnd_coils, size(psibnd_coils), MPI_DOUBLE_PRECISION,  0, MPI_COMM_WORLD, ierr)
-            call MPI_bcast(psibnd_vec,   size(psibnd_vec),   MPI_DOUBLE_PRECISION,  0, MPI_COMM_WORLD, ierr)
-
-            if(my_id == 0)   step = sr%a_ey%ind_end-sr%a_ey%ind_start+1
-            call MPI_bcast(step, 1, MPI_INTEGER,  0, MPI_COMM_WORLD, ierr)
-
-            
             ! --- Determine B_{||,v} as prescribed by the vacuum.
             if ( resistive_wall ) then
               if (  (l_tor == 1) .and. (.not. starwall_equil_coils)  )  then
 
                 if (i_resp>=sr%a_ey%ind_start .AND. i_resp<=sr%a_ey%ind_end) then
 
-                  B_par_v(l_starwall) = B_par_v(l_starwall) + basfunc_i * (                           &
+                  B_par_v_tmp = basfunc_i * (                           &
                     + sum( sr%a_ee%loc_mat(i_resp-step*my_id, :) * (psibnd_vec(:) - psibnd_coils(:))) &
                     + sum( sr%a_ey%loc_mat(i_resp-step*my_id, :) * wall_curr(:)  )                    &
                     - sum( bext_tan(i_resp_0, :)                                                      &
@@ -186,7 +175,7 @@ subroutine boundary_check(my_id)
 
                 if (i_resp>=sr%a_ey%ind_start .AND. i_resp<=sr%a_ey%ind_end) then
 
-                  B_par_v(l_starwall) = B_par_v(l_starwall) + basfunc_i * (           &
+                  B_par_v_tmp = basfunc_i * (           &
                     + sum( sr%a_ee%loc_mat(i_resp-step*my_id, :) * psibnd_vec(:) )    &
                     + sum( sr%a_ey%loc_mat(i_resp-step*my_id, :) * wall_curr(:)  ) )
 
@@ -199,24 +188,29 @@ subroutine boundary_check(my_id)
               if (  (l_tor == 1) .and. (.not. starwall_equil_coils)  )  then
 
                 if (i_resp>=sr%a_ey%ind_start .AND. i_resp<=sr%a_ey%ind_end) then
-                  B_par_v(l_starwall) = B_par_v(l_starwall) + basfunc_i                                &
+                  B_par_v_tmp = basfunc_i                                &
                     * (sum( sr%a_id%loc_mat(i_resp-my_id*step, :) * (psibnd_vec(:) - psibnd_coils(:))) &
                     - sum( bext_tan(i_resp_0, :) * I_coils(:) ))
                 endif
 
               else
                 if (i_resp>=sr%a_ey%ind_start .AND. i_resp<=sr%a_ey%ind_end) then
-                  B_par_v(l_starwall) = B_par_v(l_starwall) + basfunc_i            &
+                  B_par_v_tmp = basfunc_i            &
                     * sum( sr%a_id%loc_mat(i_resp-step*my_id, :) * psibnd_vec(:) )
                 endif
               end if
             end if
+            
+!$omp critical
+            B_par_v(l_starwall) = B_par_v(l_starwall) + B_par_v_tmp
+!$omp end critical
 
 !            write(*,'(6i5,8e12.4)') l_starwall,i_vertex,i_dof,i_node,i_node_bnd,i_resp,i_size,basfunc_i
           end do L_ID
         end do L_IV
 
       end do L_LS
+!$omp end parallel do
  
       call MPI_AllREDUCE(MPI_IN_PLACE,B_par_v, size(B_par_v), MPI_DOUBLE_PRECISION,MPI_SUM, MPI_COMM_WORLD,ierr)
 
