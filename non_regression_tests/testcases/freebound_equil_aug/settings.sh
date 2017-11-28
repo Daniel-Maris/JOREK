5ee0516a4e18c327c106db165db084d4b029af74
Francisco Javier Artola Such <fartolas@r000u06l01.marconi.cineca.it>
2017-11-28 13:15:27 +0100

    use 1 mpi task for the freeboundary regression tests

--- non_regression_tests/testcases/freebound_equil_aug/settings.sh
+++ non_regression_tests/testcases/freebound_equil_aug/settings.sh
@@ -1,7 +1,7 @@
 # --- General settings
 jorekmodel="199"
 description="Free boundary equilibrium for ASDEX Upgrade plasma (JOREK-STARWALL)."
-mpitasks=2
+mpitasks=1
 binaries="jorek_model${jorekmodel}_1 rst_bin2hdf5 rst_hdf52bin"
 binaries_initial=""
 requiredfiles="input starwall-response.dat coil_field.dat ffp.dat t.dat rho.dat"
