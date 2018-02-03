0537404a4553127e537ccb6b3d932ef458c5b146
Daan van Vugt <dvanvugt@ignitioncomputing.com>
2018-02-03 17:48:56 +0100

    Fix unallocated array bug in vacuum

--- vacuum/vacuum.f90
+++ vacuum/vacuum.f90
@@ -733,7 +733,7 @@ module vacuum
     
     ! --- Local variables
     integer :: ierr, sz(2)
-    
+
     call MPI_BCAST(n_dof_starwall,1,MPI_INTEGER,0,MPI_COMM_WORLD,ierr)
     
     if ( resistive_wall ) then
@@ -755,10 +755,10 @@ module vacuum
         if ( allocated(old_dpsibnd_vec) ) deallocate(old_dpsibnd_vec)
         allocate( old_dpsibnd_vec(n_dof_starwall) )
         
-        if ( allocated(diag_coil_curr) ) deallocate(diag_coil_curr)
-        if ( minval(sz) > 0 ) allocate( diag_coil_curr(sz(1),sz(2)) )
       else
       end if
+      if ( allocated(diag_coil_curr) ) deallocate(diag_coil_curr)
+      if ( minval(sz) > 0 ) allocate( diag_coil_curr(sz(1),sz(2)) )
       
       call MPI_BCAST(wall_curr,n_wall_curr,MPI_DOUBLE_PRECISION,0,MPI_COMM_WORLD,ierr)
       call MPI_BCAST(dwall_curr,n_wall_curr,MPI_DOUBLE_PRECISION,0,MPI_COMM_WORLD,ierr)
