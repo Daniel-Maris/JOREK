becdf72f4943c5e0635ef9fdc6982759ba669cfc
Matthias Hoelzl <mhoelzl@ipp.mpg.de>
2018-02-07 13:05:53 +0100

    Adding two new regression tests pellet_303_aug and tear_circ_333_all.

---
+++ non_regression_tests/testcases/pellet_303_aug/settings.sh
@@ -0,0 +1,45 @@
+# --- General settings
+jorekmodel="303"
+description="Pellet testcase in X-point plasma, model$jorekmodel, n_tor=3."
+mpitasks=2
+binaries="jorek_model${jorekmodel}_3 rst_bin2hdf5 rst_hdf52bin"
+binaries_initial="jorek_model${jorekmodel}_1"
+requiredfiles="input"
+extra_remote_files=""
+
+
+# --- Compile the code for the test case
+function compile_jorek () {
+  if [ "$initialrun" == "yes" ]; then
+    ./util/config.sh model=$jorekmodel n_tor=1 n_plane=1 n_period=1                  || exit 1
+    make cleanall                                                                    || exit 1
+    make $compilopt jorek_model${jorekmodel}                                         || exit 1
+    mv jorek_model${jorekmodel} jorek_model${jorekmodel}_1                           || exit 1
+  fi
+  ./util/config.sh model=$jorekmodel n_tor=3 n_plane=4 n_period=6                    || exit 1
+  make cleanall                                                                      || exit 1
+  make $compilopt jorek_model${jorekmodel} rst_bin2hdf5 rst_hdf52bin                 || exit 1
+  mv jorek_model${jorekmodel} jorek_model${jorekmodel}_3                             || exit 1
+}
+
+
+# --- Re-run the whole case from scratch into the non-linear phase
+function initial_run () {
+  ${codedir}/util/setinput.sh input nstep_n=10,10,10,20 tstep_n=0.001,0.03,1.,5.     || exit 1
+  ./jorek_model${jorekmodel}_1 < input | tee logfile_initial                         || exit 1
+  ${codedir}/util/setinput.sh input nstep_n=150 tstep_n=5. restart=.t.               || exit 1
+  $MPIRUN $mpitasks ./jorek_model${jorekmodel}_3 < input | tee -a logfile_initial    || exit 1
+}
+
+
+# --- Carry out the test case, i.e., run a single time step in the non-linear phase
+function restart_run () {
+  ${codedir}/util/setinput.sh input restart=.t. nstep_n=1 tstep_n=5. nout=1          || exit 1
+  $MPIRUN $mpitasks ./jorek_model${jorekmodel}_3 < input | tee logfile               || exit 1
+}
+
+
+# --- Compare the results of the test case to the reference solution
+function compare_results () {
+  compare_results_generic 3.e-9                                                      || exit 1
+}
