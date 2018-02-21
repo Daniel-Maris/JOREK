55ded2ac10f23b56efca0d45f1e36a0dd85c85bc
Di Hu <dhu00000@r000u08l03.marconi.cineca.it>
2018-02-21 19:41:39 +0100

    Add back the current source term for bugfixing

--- models/model500/mod_elt_matrix_fft.f90
+++ models/model500/mod_elt_matrix_fft.f90
@@ -288,7 +288,7 @@ do ms=1, n_gauss
 
       call sources(xpoint2, xcase2, y_g(ms,mt), Z_xpoint, eq_g(1,1,ms,mt),psi_axis,psi_bnd,particle_source(ms,mt),heat_source(ms,mt))
 
-      ! current_source(ms,mt) = 0.d0
+       current_source(ms,mt) = 0.d0
     
     call density(xpoint2, xcase2, y_g(ms,mt), Z_xpoint, eq_g(1,1,ms,mt),psi_axis,psi_bnd,eq_zne(ms,mt), &
                  dn_dpsi,dn_dz,dn_dpsi2,dn_dz2,dn_dpsi_dz,dn_dpsi3,dn_dpsi_dz2, dn_dpsi2_dz)
@@ -826,7 +826,7 @@ do ms=1, n_gauss
 !#  equation 1   (induction equation)                                                              #
 !###################################################################################################
 
-           rhs_ij_1 =   v * (eta_T  * zj0)/ BigR  * xjac * tstep &
+           rhs_ij_1 =   v * (eta_T  * (zj0-current_source(ms,mt)))/ BigR  * xjac * tstep &
                       + v * (ps0_s * u0_t - ps0_t * u0_s)                        * tstep &
                       - v * eps_cyl * F0 / BigR  * u0_p                   * xjac * tstep &
                       + eta_num_T * (v_x * zj0_x + v_y * zj0_y)           * xjac * tstep &
@@ -1160,7 +1160,7 @@ do ms=1, n_gauss
 
              amat_15_n = - v * tauIC/(r0_corr*BB2) * F0**3/BigR**3 * eps_cyl * T0  * rho_p * xjac * theta * tstep 
 
-             amat_16 = - deta_dT * v * T * (zj0)/ BigR * xjac         * theta * tstep &
+             amat_16 = - deta_dT * v * T * (zj0-current_source(ms,mt))/ BigR * xjac         * theta * tstep &
 		     + v * tauIC/(r0_corr*BB2) * F0**2/BigR**2 * r0 * (ps0_s * T_t  - ps0_t * T_s) * theta * tstep &
 		     + v * tauIC/(r0_corr*BB2) * F0**2/BigR**2 * T  * (ps0_s * r0_t - ps0_t * r0_s)* theta * tstep &
 	             - v * tauIC/(r0_corr*BB2) * F0**3/BigR**3 * eps_cyl * T  * r0_p * xjac        * theta * tstep 
