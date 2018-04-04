3ffff4398182451fcdff285decae1d1fa88781b0
Matthias Hoelzl <mhoelzl@ipp.mpg.de>
2018-04-04 12:20:18 +0200

    Switching to intel/2018a

--- non_regression_tests/job_scripts/iter-hpc/env.sh
+++ non_regression_tests/job_scripts/iter-hpc/env.sh
@@ -4,9 +4,10 @@ eval `tclsh /work/imas/opt/modules-tcl/modulecmd.tcl $(basename $SHELL) autoinit
 module purge
 
 module use /work/imas/opt/EasyBuild/modules/all
+module use /work/imas/opt/modules/all
 
 module load cURL/7.40.0-GCC-4.8.3
-module load intel/12.0.2
+module load intel/2018a
 module load mpich2/3.1.3-intel
 module load scotch/5.1.12b
 module load metis/5.1.0
