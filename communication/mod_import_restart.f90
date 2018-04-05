943c19ffbffe9e49078a6797d7c6ac9d1fa6cec4
Di Hu <dhu00000@r000u08l03.marconi.cineca.it>
2018-04-05 16:31:56 +0200

    Added the toroidal position output in mod_import

--- communication/mod_import_restart.f90
+++ communication/mod_import_restart.f90
@@ -351,7 +351,7 @@ endif
         pellets(i)%spi_abl     = spi_abl_arr(i)
 
         write(*,'(A,I5,5ES10.2)') ' *** SHATTERED PELLET PARAMETERS : ',i, pellets(i)%spi_R, pellets(i)%spi_Z, &
-                              pellets(i)%spi_Vel_R, pellets(i)%spi_Vel_Z, pellets(i)%spi_radius
+                        pellets(i)%spi_phi, pellets(i)%spi_Vel_R, pellets(i)%spi_Vel_Z, pellets(i)%spi_radius
       end do
 
       deallocate (spi_R_arr)
@@ -1075,7 +1075,7 @@ subroutine import_hdf5_restart(node_list, element_list, filename, format_rst, er
         pellets(i)%spi_abl     = spi_abl_arr(i)
 
         write(*,'(A,I5,5ES10.2)') ' *** SHATTERED PELLET PARAMETERS : ',i, pellets(i)%spi_R, pellets(i)%spi_Z, &
-                              pellets(i)%spi_Vel_R, pellets(i)%spi_Vel_Z, pellets(i)%spi_radius
+                        pellets(i)%spi_phi, pellets(i)%spi_Vel_R, pellets(i)%spi_Vel_Z, pellets(i)%spi_radius
       end do
 
       deallocate (spi_R_arr)
