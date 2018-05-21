dbc2d19123dde1e2ddba16e1effff2a1a6f166f9
Daan van Vugt <dvanvugt@ignitioncomputing.com>
2018-05-21 14:37:41 +0200

    Fix issue with CCD in mod_coronal
    Alter the coronal equilibrium eigenvalue calculation.
    Remove the corona_matrix timestepping in favor of a direct method.

--- tools/mod_coronal.f90
+++ tools/mod_coronal.f90
@@ -1,11 +1,13 @@
 !> module takes the OPEN-ADAS data to calculate the coronal equil-
-!> ibrium temperature and radiation, optionally in time.
+!> ibrium temperature and radiation.
+!> If you need time-dependent solutions of the corona matrix timestepping look
+!> in the revision history for this file.
 module mod_coronal
 use mod_openadas
 implicit none
 private
 public coronal
-public coronal_timestep ! tested in [[compare_mc_coronal]]
+public output_coronal
 
 !> Coronal equilibrium datatype
 type coronal
@@ -23,42 +25,6 @@ interface coronal
 end interface coronal
 
 contains
-
-!> Calculate the coronal matrix in tridiagonal form
-!> This matrix defines the time derivatives of population sizes
-!> As drho/dt = A rho where A is this matrix.
-!> See http://jorek.eu/wiki/doku.php?id=adf11 for more info
-pure function corona_matrix(ad, density, temperature)
-type (ADF11_all), intent(in)         :: ad !< ADF11 datatype
-real*8, intent(in)                   :: density !< log10 density in m^-3
-real*8, intent(in)                   :: temperature !< log10 temperature in K
-real*8, dimension(1:3,0:ad%n_Z)      :: corona_matrix !< Output the coronal matrix diagonals
-
-real*8, dimension(0:ad%n_Z+1) :: ion_rate, rec_rate
-integer :: iz, i
-
-! Get the ionization and recombination
-ion_rate = 0.d0
-rec_rate = 0.d0
-do iz=1,ad%n_Z
-  ion_rate(iz) = ad%SCD%interp(iz, density, temperature) ! ionizing to level iz (0 is neutral)
-  rec_rate(iz) = ad%ACD%interp(iz, density, temperature) + &
-                 ad%CCD%interp(iz, density, temperature) ! recombining from level iz
-  ! These should actually be multiplied by the electron and hydrogen ion densities (done below)
-  ! In the trace impurity limit these are the same
-end do
-
-! Fill in tridiagonal elements (implicitly shifted down one row per column)
-do i=0,ad%n_Z
-  corona_matrix(1,i) = + ion_rate(i) ! increase from ionisation
-  corona_matrix(2,i) = - ion_rate(i+1) - rec_rate(i) ! Loss in this state
-  corona_matrix(3,i) =                 + rec_rate(i+1) ! increase from recombination of higher level atoms
-enddo
-
-corona_matrix = corona_matrix * 10.d0**density
-end function corona_matrix
-
-
 !> Radiated power in a specific coronal equilibrium configuration and temperature
 pure function coronal_Prad(ad, density, temperature, fractions, neutral_density)
 type (ADF11_all), intent(in)            :: ad !< ADF11 datatype
@@ -97,100 +63,34 @@ use constants
 type (ADF11_all), intent(in) :: ad !< ADF11 datatype
 type (coronal)               :: cor !< Coronal equilibrium datatype
 
-real*8, dimension(0:ad%n_Z) :: p, p_old, Z
-integer :: n_d, n_T, iz, k, m, i, j
-real*8, parameter :: tolerance = 1d-12 ! on max(abs(new - old))
-logical :: converged
-real*8, dimension(1:3,0:ad%n_Z) :: cmat
-real*8 :: dt
+real*8, dimension(0:ad%n_Z) :: p
+integer :: n_d, n_T, iz, k, m
+real*8 :: ion_rate, rec_rate
 
 cor%n_Z = ad%n_Z
 n_d = 10
-n_T = 200
-do iz=0,ad%n_Z
-  Z(iz) = real(iz,8)
-enddo
+n_T = 1000
 
 allocate(cor%density(n_d), cor%temperature(n_T), cor%Z(n_d,n_T,0:cor%n_Z), cor%Prad(n_d,n_T))
-!$omp parallel do default(none) shared(cor, ad, n_d, n_T, Z) private(p, p_old, m, k, converged, cmat, i, j, dt)
 do m=1, n_d
   cor%density(m) = 18.d0 + float(m-1)/n_d * (21.-18.) ! log10 [m^-3], linear between 18 and 21
 
-  ! Here we are using the distribution from the previous temperature as an
-  ! initial guess of the next temperature. So only initialize at a different
-  ! density scan instead of every temperature scan.
-  p = 0.d0
-  p(0) = 1.d0
-
   do k=1, n_T
     cor%temperature(k) = log10( 1.d0 + exp(log(4.d4)*float(k-1)/(float(n_T-1))) - 1.d0 ) + log10(EL_CHG) - log10(K_BOLTZ) ! in log10 [K]
+    ! 1 to 40000 eV in logscale
 
-    converged = .false.
-    ! Iterate until converged or we run out of steps
-    cmat = corona_matrix(ad, cor%density(m), cor%temperature(k))
-    pow_loop: do i=0,5
-      dt = 10.d0**(-i)
-      step_loop: do j=1,10**i + 100 ! extra 100 to perform more large steps
-        p_old = p
-        call coronal_timestep(ad, p, dt, cmat) ! use a fixed timestep of 0.1s to solve the equilibrium state
-        if (maxval(abs(p_old - p)) .lt. tolerance) then
-          converged = .true.
-          exit pow_loop
-        end if
-      end do step_loop
-    end do pow_loop
-
-    !$omp critical
-    if (.not. converged) write(*,*) "WARNING: coronal equilibrium not converged, max delta", maxval(abs(p_old - p))
-    !$omp end critical
+    p(0) = 1.d0
+    do iz=1,ad%n_Z
+      ion_rate = ad%SCD%interp(iz, cor%density(m), cor%temperature(k)) ! ionizing to level iz (0 is neutral)
+      rec_rate = ad%ACD%interp(iz, cor%density(m), cor%temperature(k)) ! recombining from iz+1
+      p(iz) = p(iz-1) * ion_rate/rec_rate
+    end do
 
     cor%Z(m,k,:)  = p/sum(p)
     cor%Prad(m,k) = coronal_Prad(ad, cor%density(m), cor%temperature(k), p/sum(p)) ! Do not set neutral density yet
   enddo
 enddo
-!$omp end parallel do
 end function coronal_equilibrium
-
-
-!> Perform one timestep with the coronal equilibrium matrix
-!>
-!> Equation to solve is: \(p' = A p\)
-!> Discretize as \(p^{n+1} - p^n = \Delta t \left((1-\theta)Ap^n + \theta A p^{n+1}\right)\)
-!> Leading to a matrix equation
-!> \((1-\theta \Delta t A)p^{n+1}a = (1 + \Delta t (1-\theta) A) p^n \)
-subroutine coronal_timestep(ad, p, tstep, cmat)
-type (ADF11_all), intent(in)               :: ad !< ADF11 datatype
-real*8, dimension(0:ad%n_Z), intent(inout) :: p !< Population in each level
-real*8, intent(in)                         :: tstep !< Timestep size in s
-real*8, dimension(1:3,0:ad%n_Z)            :: cmat !< tridiagonal corona matrix
-
-real*8, parameter :: theta = 1.d0 ! 0.5 = Crank-Nicholson, 1.d0 = Backward Euler
-
-real*8, dimension(0:ad%n_Z)     :: b
-real*8 :: A_l(1:ad%n_Z), &
-          A_d(0:ad%n_Z), &
-          A_u(1:ad%n_Z)
-integer :: info, i
-
-! Lower, diagonal and upper components (I - theta dt A)
-A_l =      - theta * tstep * cmat(1,1:ad%n_Z)
-A_d = 1.d0 - theta * tstep * cmat(2,0:ad%n_Z)
-A_u =      - theta * tstep * cmat(3,0:ad%n_Z-1)
-
-! We have to do this manually because cmat is not a regular matrix
-do i=1,ad%n_Z-1
-  b(i)    = p(i) + (1.d0 - theta) * tstep * (p(i-1)*cmat(1,i) + p(i)*cmat(2,i) + p(i+1)*cmat(3,i))
-enddo
-b(0)      = p(0) + (1.d0 - theta) * tstep * (p(0)*cmat(2,i) + p(1)*cmat(3,i))
-b(ad%n_Z) = p(ad%n_Z) + (1.d0 - theta) * tstep * (p(ad%n_Z-1)*cmat(1,ad%n_Z) + p(ad%n_Z)*cmat(2,ad%n_Z))
-
-! Solve Ax=b for tridiagonal matrices. Result stored in b
-call dgtsv(ad%n_Z+1,1,A_l,A_d,A_u,b,ad%n_Z+1,info)
-if (info .ne. 0) write(*,*) 'info : ',info
-
-! Output result
-p = b
-end subroutine coronal_timestep
 
 
 !> Linear interpolation of coronal model charge at specific density and temperature
@@ -278,6 +178,10 @@ end subroutine interpolate_coronal_gradients
 !> This is to output a coronal equilibrium charge distribution as a
 !> function of temperature assuming constant density 10^20/m^3
 !> to a file charge_distribution.dat
+!> plot with gnuplot like
+!> 
+!> set logscale y
+!> p for [i=2:20] 'charge_distribution.dat' u 1:i t ''.(i-2) w l
 subroutine output_coronal(cor)
 class(coronal), intent(in)      :: cor !< Coronal equilibrium type
 
@@ -293,9 +197,8 @@ open(20,file="charge_distribution.dat")
 write(20,'(A11)',advance='no') 'temperature (log10(K))', 'charge states'
 write(20,'(A11)') 'summation of all states'
 
-do i_T = 0, 100
-
-  T_rad = 2. + 0.06*real(i_T,8)
+do i_T = 1, size(cor%temperature,1)
+  T_rad = cor%temperature(i_T)
 
   if (allocated(P_imp)) deallocate(P_imp)
   if (allocated(dP_imp_dT)) deallocate(dP_imp_dT)
@@ -303,11 +206,11 @@ do i_T = 0, 100
   allocate(P_imp(0:cor%n_Z))
   allocate(dP_imp_dT(0:cor%n_Z))
   call cor%interp(density=20.d0,temperature=T_rad,p_out=P_imp,z_eff=Z_imp)
-  write(20,'(f12.3)',advance='no'), T_rad
+  write(20,'(f12.3)',advance='no') T_rad
   do i_ion = 0, cor%n_Z
-    write(20,'(f12.5)',advance='no'), P_imp(i_ion)
+    write(20,'(f12.5)',advance='no') P_imp(i_ion)
   end do
-  write(20,'(f12.5)'), sum(P_imp)
+  write(20,'(f12.5)') sum(P_imp)
 end do
 close (20)
 end subroutine output_coronal
