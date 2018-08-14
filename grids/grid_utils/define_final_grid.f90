56975a62cab4b2b12a4a2ac9887324a921647a13
Stanislas Pamela <spamela0@r000u06l01.marconi.cineca.it>
2018-08-14 13:26:23 +0200

    gfortran again...

--- grids/grid_utils/define_final_grid.f90
+++ grids/grid_utils/define_final_grid.f90
@@ -1179,7 +1179,7 @@ do i=1,newnode_list%n_nodes
   call find_RZ(node_list,element_list,R1,Z1,R_out,Z_out,ielm_out,s_out,t_out,ifail)
 
   if (ifail .ne. 0) then
-    write(*,'(A,2f)')'Warning! did not find node one previous grid!',R1,Z1
+    write(*,'(A,2f15.4)')'Warning! did not find node one previous grid!',R1,Z1
     write(*,*)'Unable to extract psi information, the grid might be flawed.'
   endif
   
