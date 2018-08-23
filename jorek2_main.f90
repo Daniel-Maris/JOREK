66aecd1499a3cfe72bb405c0062f57fe63c5f863
Stanislas Pamela <spamela0@r000u06l01.marconi.cineca.it>
2018-08-23 11:30:00 +0200

    same forking as for the standard grid build

--- jorek2_main.f90
+++ jorek2_main.f90
@@ -365,7 +365,7 @@ required = 0
     ! --- Optional: Redo flux aligned grid (DOES NOT WORK CURRENTLY)
     if (regrid) then
       if (xpoint)  then
-        if (xcase .ge. 2) then
+        if ( (xcase .ge. 2) .or. (RZ_grid_inside_wall) ) then
 	  if (grid_to_wall) then
 	    call grid_double_xpoint_inside_wall(node_list, element_list)
 	  else
