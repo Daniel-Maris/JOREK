93313a0280ad47e9bd72c2fb7713e65f73277ce1
Stanislas Pamela <spamela0@r000u07l02.marconi.cineca.it>
2018-08-29 14:07:16 +0200

    This was a mistake. Need to follow jorek convention about signs of psi and FFp.

--- grids/grid_utils/mod_eqdsk_tools.f90
+++ grids/grid_utils/mod_eqdsk_tools.f90
@@ -445,9 +445,9 @@ subroutine interpolate_psi_from_eqdsk_grid(nr_eqdsk, nz_eqdsk, xx, yy, psirz, R_
   endif
 
   ! --- min sign because of JOREK definition of psi
-  psi   = + psi  
-  psi_R = + psi_R
-  psi_Z = + psi_Z
+  psi   = - psi  
+  psi_R = - psi_R
+  psi_Z = - psi_Z
 
   return
 
