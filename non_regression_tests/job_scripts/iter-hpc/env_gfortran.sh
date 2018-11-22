3f344ddcaeefb1bc77702ad24bdf7b37f48748ad
Daan van Vugt <dvanvugt@ignitioncomputing.com>
2018-11-22 17:06:03 +0100

    Another try at allow run as root

--- non_regression_tests/job_scripts/iter-hpc/env_gfortran.sh
+++ non_regression_tests/job_scripts/iter-hpc/env_gfortran.sh
@@ -22,4 +22,4 @@ export CXXFLAGS=-O0 # problem with stdio library on ITER http://gcc.1065356.n8.n
 export http_proxy=${JOREK_HTTP_PROXY}
 export https_proxy=${JOREK_HTTP_PROXY}
 
-alias mpirun='mpirun --allow-run-as-root'
+export MPIRUN='mpirun --allow-run-as-root -n'
