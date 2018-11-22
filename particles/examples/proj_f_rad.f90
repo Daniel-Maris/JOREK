6a6331c0eaed8d8d56ffca9f33720458d331b822
Daan van Vugt <dvanvugt@ignitioncomputing.com>
2018-11-22 16:46:16 +0100

    fix proj_f_rad

--- particles/examples/proj_f_rad.f90
+++ particles/examples/proj_f_rad.f90
@@ -40,7 +40,6 @@ sim%groups(:)%mass = -1
 n = sim%fields%element_list%n_elements * n_gauss_2 * n_plane
 
 allocate(particle_fieldline::sim%groups(1)%particles(n))
-associate(p => sim%groups(1)%particles)
 !!$omp parallel do default(none) private(i_p, i, j, k, l, R, R_s, R_t, Z, Z_s, Z_t, xjac) &
 !!$    shared(sim)
 do i_p=1,n_plane
@@ -48,22 +47,23 @@ do i_p=1,n_plane
     do j=1,n_gauss
       do k=1,n_gauss
         l = (i_p-1) * n_gauss_2*sim%fields%element_list%n_elements + (i-1)*n_gauss_2 + (j-1)*n_gauss + k
-        p(l)%x(3) = TWOPI*real(i_p,8)/real(n_plane,8)/real(n_period,8)
-        p(l)%i_elm = i
-        p(l)%st = [Xgauss(j), Xgauss(k)]
-        ! Every particle represents a sample in the integral in an element
-        ! with gaussian quadrature. We need the weights and the area here
-        ! and a correction for the number of planes
-        call interp_RZ(sim%fields%node_list,sim%fields%element_list,i,Xgauss(j), Xgauss(k), &
-          R,R_s,R_t,Z,Z_s,Z_t)
-        xjac = R_s*Z_t - R_t*Z_s
-        p(l)%weight = real(Wgauss(j)*Wgauss(k),4)*xjac*R*TWOPI/real(n_plane,4)
+        associate(p => sim%groups(1)%particles(l))
+          p%x(3) = TWOPI*real(i_p,8)/real(n_plane,8)/real(n_period,8)
+          p%i_elm = i
+          p%st = [Xgauss(j), Xgauss(k)]
+          ! Every particle represents a sample in the integral in an element
+          ! with gaussian quadrature. We need the weights and the area here
+          ! and a correction for the number of planes
+          call interp_RZ(sim%fields%node_list,sim%fields%element_list,i,Xgauss(j), Xgauss(k), &
+            R,R_s,R_t,Z,Z_s,Z_t)
+          xjac = R_s*Z_t - R_t*Z_s
+          p%weight = real(Wgauss(j)*Wgauss(k),4)*xjac*R*TWOPI/real(n_plane,4)
+        end associate
       end do
     end do
   end do
 end do
 !!$omp end parallel do
-end associate
 
 ! Set up the diagnostics output
 proj = new_projection(sim%fields%node_list, sim%fields%element_list, smoothing=6d-5, &
