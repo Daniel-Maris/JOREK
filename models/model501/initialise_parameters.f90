c0ebdd134e5656c2fa6ee9e2b62f136e46e1faac
Eric Nardon <eric.nardon@cea.fr>
2019-03-18 08:48:42 +0100

    Bugifx in initialise_parameters.f90

--- models/model501/initialise_parameters.f90
+++ models/model501/initialise_parameters.f90
@@ -91,7 +91,7 @@ namelist /in1/  tstep, nstep, tstep_n, nstep_n,                     &
                 bc_natural_open,                                    &
                 use_mumps, use_pastix, use_murge, use_murge_element,&
                 use_wsmp, n_tor_fft_thresh,                         &
-                pastix_smp_only, refinement, force_central_node     &
+                pastix_smp_only, refinement, force_central_node,    &
                 grid_to_wall,                                       &
                 adaptive_time, equil, bench_without_plot,           &
                 no_zeros_pastix, no_zeros_mumps,                    &
