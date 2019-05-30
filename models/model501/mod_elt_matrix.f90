f371bf0b37e52fa4349c849bc0e682f4982affb8
Di Hu <dhu00000@r000u07l02.marconi.cineca.it>
2019-05-30 07:11:00 +0200

    Amended minor inconsistency between SPImodel501 and develop

--- models/model501/mod_elt_matrix.f90
+++ models/model501/mod_elt_matrix.f90
@@ -2088,7 +2088,7 @@ do ms=1, n_gauss
                            + v * F0 / BigR * (T_p * rn0 * alpha_imp_bis + T * rn0_p * alpha_imp)  * xjac * theta * tstep 		   
 
 
-                 amat_77 = v * Vpar * r0 * F0**2 / BigR * xjac * (1.d0 + zeta) &
+                 amat_77 = v * Vpar * abs(r0) * F0**2 / BigR * xjac * (1.d0 + zeta) &
                          + visco_par * (v_x * Vpar_x + v_y * Vpar_y) * BigR        * xjac  * theta * tstep &
 
                          ! New terms coming from -(\partial_t \rho + \nabla \cdot (\rho \mathbf{v})) \mathbf{v} in RHS of momentum equation
