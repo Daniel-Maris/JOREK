e0159f8c990a6a6151c19002a124625e669041c4
Cristian Sommariva <cristian.sommariva@epfl.ch>
2019-10-28 18:09:26 +0100

    added relativistic particle type in mod_particle_io read/write operations

--- particles/mod_particle_io.f90
+++ particles/mod_particle_io.f90
@@ -230,6 +230,36 @@ if (allocated(sim%groups)) then
         call HDF5_array1D_saving(file,v1_all,n_total,group_name//"v")
       end if
       deallocate(v1, v1_all)
+    ! add relativistic Lorentz particle
+    type is (particle_kinetic_relativistic)
+      ! set the particle type name
+      particle_type_name = 'particle_kinetic_relativistic'
+      ! allocate local and global momenta array
+      allocate(v(3,n_here), v_all(3,n_total)) !< maybe only master process requries to allocate v_all
+      ! loop on the local particle array
+      do j=1,n_here
+        v(:,j) = p(j)%v ! store all momenta
+      end do
+      ! retrive momenta from all particle table and store them in the global one in master process
+      call MPI_Gatherv(v(:,:), 3*n_here, MPI_REAL8, &
+        v_all(:,:), particles_per_proc*3, [(sum(particles_per_proc(1:i),1)*3, i=0,n_cpu-1)], &
+        MPI_REAL8, 0, MPI_COMM_WORLD, ierr)
+      ! allocate local and global charge array
+      allocate(q(n_here), q_all(n_total)) !< maybe only master process requries to allocate q_all
+      ! loop on all particles
+      do j=1,n_here
+        q(j) = p(j)%q ! store all charged in local array
+      end do
+      ! retrive charges from all particle table and store them in the global one in master process
+      call MPI_Gatherv(q(:), n_here, MPI_INTEGER, &
+        q_all(:), particles_per_proc, [(sum(particles_per_proc(1:i),1), i=0,n_cpu-1)], &
+        MPI_INTEGER, 0, MPI_COMM_WORLD, ierr)
+      ! if master process
+      if (my_id .eq. 0) then
+        call HDF5_array2D_saving(file,v_all,3,n_total,group_name//"v") ! store momenta in HDF5
+        call HDF5_array1D_saving_int(file,q_all,n_total,group_name//"q") ! store charged in HDF5
+      end if
+      deallocate(v,q,v_all,q_all) ! deallocate all momenta and charges arrays
     class default
       write(*,*) "error: missing type name declaration for write"
       call exit(1)
@@ -352,6 +382,9 @@ do i=1,n
     allocate(particle_gc::sim%groups(i)%particles(n_here), stat=ierr)
   case ('particle_fieldline')
     allocate(particle_fieldline::sim%groups(i)%particles(n_here), stat=ierr)
+  case ('particle_kinetic_relativistic')
+    ! allocate a lorentz particle list in i-th group
+    allocate(particle_kinetic_relativistic::sim%groups(i)%particles(n_here), stat=ierr)
   case default
     write(*,*) "error: missing type name declaration for read"
     call exit(1)
@@ -454,6 +487,22 @@ do i=1,n
       p(j)%v = real8_1D(j)
     end do
     deallocate(real8_1D)
+  ! add relativistic Lorentz particle
+  type is (particle_kinetic_relativistic)
+    ! momenta v [AMU*m/s]
+    allocate(real8_2D(3,n_here))
+    call HDF5_array2D_reading(file, real8_2D, group_name//"v",start=[0_HSIZE_T,i_here])
+    do j=1,n_here
+      p(j)%v = real8_2D(:,j)
+    end do
+    deallocate(real8_2D)
+    ! electric charge q
+    allocate(int4_1D(n_here))
+    call HDF5_array1D_reading_int(file, int4_1D, group_name//"q", start=[i_here])
+    do j=1,n_here
+      p(j)%q = int4_1D(j)
+    end do
+    deallocate(int4_1D)    
   end select
 
   call HDF5_integer_reading(file,sim%groups(i)%Z,group_name//"Z")
