1347c3c5953ec9cc147445670cc44aca5cdb30eb
Prabal Singh Verma <psv@ipp.mpg.de>
2019-11-18 16:48:22 +0100

    In this module we pass the variables ijA_index, ijA_size, irn_jcn, irn_glob, jcn_glob, A_glob, i_tor_min, i_tor_max as arguments.

--- models/mod_assembly.f90
+++ models/mod_assembly.f90
@@ -30,9 +30,12 @@ contains
        &   index_node2, k2, in2,                &
        &   zbig, solve_only, gmres,             &
        &   cnt, cnt_prod, only_count,           &
-       &   index_min, index_max)
+       &   index_min, index_max,                & 
+       &   ijA_index, ijA_size, irn_jcn,        & 
+       &   irn_glob, jcn_glob, A_glob,          & 
+       &   i_tor_min, i_tor_max)
     use mod_parameters
-    use global_distributed_matrix
+!    use global_distributed_matrix
     use mod_locate_irn_jcn
 
     integer, intent(in)    :: index_node,  k,  in
@@ -42,20 +45,25 @@ contains
     integer, intent(inout) :: cnt, cnt_prod
     logical, intent(in)    :: only_count
     integer, intent(in)    :: index_min, index_max
-
+    integer, intent(in)    :: i_tor_min, i_tor_max 
+    integer, allocatable :: ijA_index(:,:), ijA_size(:), irn_jcn(:,:) 
+    integer :: irn_glob(:), jcn_glob(:) 
+    real*8  :: A_glob(:) 
     logical :: is_local
     integer :: ija_position, ilarge_vp
 
     if ((index_node .ge. index_min) .and. (index_node .le. index_max)) then
 
-       call locate_irn_jcn(index_node,index_node2,index_min,index_max,ijA_position)
+       call locate_irn_jcn(index_node,index_node2,index_min,index_max,ijA_position,& 
+                                    ijA_index, ijA_size, irn_jcn)
                              
        !-------- index dans A_glob
-       ilarge_vp  = ijA_position  - 1 + ((k-1)*n_tor + in-1) * n_var*n_tor + (k2-1)*n_tor + in2
+       ilarge_vp  = ijA_position  - 1 + ((k-1)*(i_tor_max - i_tor_min +1) + in-i_tor_min ) * n_var*(i_tor_max - i_tor_min +1) + (k2-1)*(i_tor_max - i_tor_min +1) + in2&
+                    -i_tor_min + 1 
                                
                              
-       irn_glob(ilarge_vp) =  n_tor * n_var * (index_node -1) + (k -1)*n_tor + in
-       jcn_glob(ilarge_vp) =  n_tor * n_var * (index_node2-1) + (k2-1)*n_tor + in2
+       irn_glob(ilarge_vp) =  (i_tor_max - i_tor_min +1) * n_var * (index_node -1) + (k -1)*(i_tor_max - i_tor_min +1) + in - i_tor_min + 1
+       jcn_glob(ilarge_vp) =  (i_tor_max - i_tor_min +1) * n_var * (index_node2-1) + (k2-1)*(i_tor_max - i_tor_min +1) + in2 - i_tor_min + 1
        A_glob(ilarge_vp)   = ZBIG
     endif
   end subroutine boundary_conditions_add_one_entry
@@ -83,17 +91,19 @@ contains
   subroutine boundary_conditions_add_RHS( &
        &   index_node, k, in,             &
        &   index_min, index_max,          &
-       &   rhs_loc,  val)
+       &   rhs_loc,  val,                 &
+       &   i_tor_min, i_tor_max)
     use mod_parameters
     integer, intent(in)    :: index_node,  k,  in
     integer, intent(in)    :: index_min, index_max
+    integer, intent(in)    :: i_tor_min, i_tor_max 
     real*8,  intent(in)    :: val
     real*8,  intent(inOUT) :: rhs_loc(*)
 
     logical :: is_local
 
     if ((index_node .ge. index_min) .and. (index_node .le. index_max)) then
-       RHS_loc(n_tor*n_var * (index_node-1) + (k-1)*n_tor + in) = val
+       RHS_loc((i_tor_max - i_tor_min +1)*n_var * (index_node-1) + (k-1)*(i_tor_max - i_tor_min +1) + in - i_tor_min + 1) = val
     endif
   end subroutine boundary_conditions_add_RHS
 end module mod_assembly
