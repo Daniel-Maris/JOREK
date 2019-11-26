01a6de5b7d9b43d9a37834f0c0fb2694d9fb8385
Stanislas Pamela <stanislas.pamela@ukaea.uk>
2019-11-26 20:27:22 +0000

    ok, trying incrementally to isolate the issue...

--- grids/grid_xpoint.f90
+++ grids/grid_xpoint.f90
@@ -656,11 +656,12 @@ enddo
 
 call tr_allocate(k_cross,1,n_flux_2+n_open_2+n_private_2+1,1,n_tht_2+2*n_leg_2,"k_cross",CAT_GRID)
 
+write(*,*)'before',3
 k_cross = 0
 
 k_cross(1,:) = 1
 
-write(*,*)'before',4
+!write(*,*)'before',4
 do i=1,n_flux_2+n_open_2
 
   do j=1, n_tht_2
@@ -692,7 +693,6 @@ do i=1,n_flux_2+n_open_2
 
 enddo
 
-!write(*,*)'before',5
 do i=n_flux_2,n_flux_2+n_open_2+n_private_2
 
   do j=n_tht_2+1, n_tht_2 + 2*n_leg_2
