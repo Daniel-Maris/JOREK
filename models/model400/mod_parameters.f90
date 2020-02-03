074ba06446253a277c768b19019f7f90f69bad31
Eric Nardon <eric.nardon@cea.fr>
2020-02-03 21:48:03 +0100

    Undoing changes in mod_parameters

--- models/model400/mod_parameters.f90
+++ models/model400/mod_parameters.f90
@@ -8,9 +8,9 @@ module mod_parameters
   integer, parameter :: n_var          = 8         !< number of variables
   integer, parameter :: n_dim          = 2         !< number of dimensions
   integer, parameter :: n_order        = 3         !< order of the polynomial basis
-  integer, parameter :: n_tor          = 3!3!15!11!9!3!3!11!3!21!1!21         !< number of toroidal harmonics
+  integer, parameter :: n_tor          = 1!3!15!11!9!3!3!11!3!21!1!21         !< number of toroidal harmonics
   integer, parameter :: n_period       = 1!3!3!4!6!6!6!4!4!2!1!2         !< periodicity in toroidal direction
-  integer, parameter :: n_plane        = 4!8!64!64!32!8!8!64!8!128!1!128         !< number of toroidal angles
+  integer, parameter :: n_plane        = 1!8!64!64!32!8!8!64!8!128!1!128         !< number of toroidal angles
   integer, parameter :: n_vertex_max   = 4         !< maximum number of corners of an element
   integer, parameter :: n_nodes_max    = 100001     !< maximum number of nodes
   integer, parameter :: n_elements_max = 100001     !< maximum number of elements
