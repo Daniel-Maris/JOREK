b15b4256e0d05f8c5feaaacf3636785c83174655
Matthias Hoelzl <mhoelzl@ipp.mpg.de>
2020-02-13 15:22:26 +0100

    removing duplicate time_evol_scheme in namelists

--- models/model500/initialise_parameters.f90
+++ models/model500/initialise_parameters.f90
@@ -91,7 +91,7 @@ namelist /in1/  tstep, nstep, tstep_n, nstep_n,                     &
                 linear_run, export_for_nemec,                       &
                 V_0,V_1,V_coef, output_bnd_elements,                &
                 n_limiter, R_limiter, Z_limiter,                    &
-                R_Z_psi_bnd_file, wall_file,time_evol_scheme,       &
+                R_Z_psi_bnd_file, wall_file,                        &
                 spi_tor_rot, tor_frequency,                         &
                 D_prof_neg, ZK_prof_neg,                            &
                 D_prof_neg_thresh, ZK_prof_neg_thresh, T_min,       &
--- models/model701/initialise_parameters.f90
+++ models/model701/initialise_parameters.f90
@@ -51,7 +51,7 @@ namelist /in1/  tstep, nstep, eta, visco, visco_par,                &
                 pellet_psi, pellet_delta_psi,                       &
                 ellip,tria_u,tria_l,quad_u,quad_l,                  &
                 xampl,xwidth,xsig,xtheta,xshift,xleft, xpoint,      &
-                xcase, time_evol_scheme,                            &
+                xcase,                                              &
                 rho_file, T_file, ffprime_file, freeboundary_equil, &
                 freeboundary, resistive_wall, freeb_change_indices, &
                 wall_resistivity, wall_resistivity_fact,            &
