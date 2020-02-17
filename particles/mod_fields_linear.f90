ed1d7f5adb78fe43a768b51e957ac907f713035b
Cristian Sommariva <cristian.sommariva@epfl.ch>
2020-02-17 13:51:19 +0100

    introducing abs in condtion linear interpolation

--- particles/mod_fields_linear.f90
+++ particles/mod_fields_linear.f90
@@ -68,7 +68,7 @@ pure subroutine do_interp_PRZ_1(this, time, i_elm, i_v, n_v, s, t, phi, P, P_s, 
     call interp_PRZ(this%node_list,this%element_list,i_elm,i_v,n_v,s,t,phi,&
          Pd,Pd_s,Pd_t,Pd_phi,R,R_s,R_t,Z,Z_s,Z_t,deltas=.true.)
     !> check if linear interpolations has to be performed
-    if((this%time_now-this%time_prev) .gt. 1d-10 .and. .not. this%static) then
+    if(abs(this%time_now-this%time_prev) .gt. 1d-10 .and. .not. this%static) then
       !> compute inverse time interval
       dt = 1.d0/(this%time_now - this%time_prev)
       !> compute time fraction
