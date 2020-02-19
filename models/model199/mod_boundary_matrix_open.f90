36142ac01cec9a6e1ace2a95d47a23f3cbc373e7
Prabal Singh Verma <psv@ipp.mpg.de>
2020-02-19 15:22:16 +0100

    Making mod_boundary_matrix_open.f90 consistent in other models as well.

--- models/model199/mod_boundary_matrix_open.f90
+++ models/model199/mod_boundary_matrix_open.f90
@@ -2,7 +2,7 @@ module mod_boundary_matrix_open
   implicit none
 contains
 subroutine boundary_matrix_open(vertex, direction, element, nodes, xpoint2, xcase2, R_axis, Z_axis, psi_axis, &
-                                psi_bnd, R_xpoint, Z_xpoint, ELM, RHS)
+                                psi_bnd, R_xpoint, Z_xpoint, ELM, RHS, i_tor_min, i_tor_max)
 !---------------------------------------------------------------------
 ! calculates the matrix contribution of the boundaries of one element
 ! implements the natural boundary conditions
@@ -20,6 +20,8 @@ type (type_node)      :: nodes(2)        ! the two nodes containing the boundary
 
 real*8, dimension (:,:), allocatable  :: ELM
 real*8, dimension (:)  , allocatable  :: RHS
+integer,               intent(in)     :: i_tor_min   !*psv!
+integer,               intent(in)     :: i_tor_max   !*psv!
 
 integer    :: vertex(2), direction(2), xcase2
 real*8     :: psi_axis, R_axis, Z_axis, psi_bnd, R_xpoint(2), Z_xpoint(2)
--- models/model303/mod_boundary_matrix_open.f90
+++ models/model303/mod_boundary_matrix_open.f90
@@ -3,7 +3,7 @@ module mod_boundary_matrix_open
 contains
 
 subroutine boundary_matrix_open(vertex, direction, element, nodes, xpoint2, xcase2, R_axis, Z_axis, psi_axis, &
-                                psi_bnd, R_xpoint, Z_xpoint, ELM, RHS)
+                                psi_bnd, R_xpoint, Z_xpoint, ELM, RHS, i_tor_min, i_tor_max)
 !---------------------------------------------------------------------
 ! calculates the matrix contribution of the boundaries of one element
 ! implements the natural boundary conditions
@@ -19,6 +19,8 @@ implicit none
 
 type (type_element)   :: element
 type (type_node)      :: nodes(2)        ! the two nodes containing the boundary nodes
+integer,               intent(in)     :: i_tor_min   !*psv!
+integer,               intent(in)     :: i_tor_max   !*psv!
 
 real*8     :: x_g(n_gauss), x_s(n_gauss), x_ss(n_gauss)
 real*8     :: y_g(n_gauss), y_s(n_gauss), y_ss(n_gauss)
