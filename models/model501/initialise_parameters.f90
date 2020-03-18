fb871ab67fe16c1a5b05d7be3d6c860409105442
Eric Nardon <eric.nardon@cea.fr>
2020-03-18 21:59:27 +0100

    Adding fix_axis_nodes

--- models/model501/initialise_parameters.f90
+++ models/model501/initialise_parameters.f90
@@ -91,6 +91,7 @@ namelist /in1/  tstep, nstep, tstep_n, nstep_n,                     &
                 use_BLR_compression, epsilon_BLR, just_in_time_BLR, &
                 use_pastix, use_wsmp, n_tor_fft_thresh,             &
                 pastix_smp_only, refinement, force_central_node,    &
+		fix_axis_nodes,                                     &
                 grid_to_wall,                                       &
                 adaptive_time, equil, bench_without_plot,           &
                 no_zeros_pastix, no_zeros_mumps,                    &
