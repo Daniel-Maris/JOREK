119606875bcea39dc0e0a454f7d909aef016c0bf
Bandaru Vinodh kumar <vinodh.kumar.bandaru@ipp.mpg.de>
2020-04-01 17:14:10 +0200

    jorek2_main.f90 edited online with Bitbucket

--- jorek2_main.f90
+++ jorek2_main.f90
@@ -483,6 +483,8 @@ required = 0
   if(restart) then
     call MPI_BCAST(wall_curr_initialized, 1 , MPI_LOGICAl,          0, MPI_COMM_WORLD, ierr)
     call MPI_BCAST(tstep,                 1 , MPI_DOUBLE_PRECISION, 0, MPI_COMM_WORLD, ierr)
+    call MPI_BCAST(index_start,           1 , MPI_INTEGER, 0, MPI_COMM_WORLD, ierr)
+    call MPI_BCAST(index_now,             1 , MPI_INTEGER, 0, MPI_COMM_WORLD, ierr)      
   end if
   call populate_element_rtree(node_list, element_list)
   
