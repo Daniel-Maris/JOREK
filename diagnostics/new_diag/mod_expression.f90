42207b5fb21c8f172146b850b3d5d5089cebdbd4
Eric Nardon <eric.nardon@cea.fr>
2020-04-08 08:54:52 +0200

    Making mod_expression consistent with mod_elt_matrix for model 501

--- diagnostics/new_diag/mod_expression.f90
+++ diagnostics/new_diag/mod_expression.f90
@@ -593,6 +593,7 @@ module mod_expression
     real*8  :: Arad_bg, Brad_bg, Crad_bg, frad_bg, dfrad_bg_dT
 #endif
 #if JOREK_MODEL == 501
+    ! See https://www.jorek.eu/wiki/doku.php?id=model500_501_555 for details
     real*8 :: T0_corr, r0_corr, rn0_corr
     ! Atomic physics coefficients:
     !   -Mass ratio between main ions and impurites (m_i/m_imp)
@@ -603,11 +604,11 @@ module mod_expression
     real*8  :: alpha_imp
     real*8  :: beta_imp
     !   -Radiation from injected impurities
-    real*8  :: Lrad, dLrad_dT                      ! Radiation rate and its derivative wrt. temperature
+    real*8  :: Lrad                                ! Radiation rate
     real*8  :: ne_rad                              ! Electron density used in radiation rate
     real*8  :: A0_rad, A1_rad, T1_rad, sig1_rad    ! Radiation rate parameters
     real*8  :: A2_rad, T2_rad, sig2_rad
-	!   -Temporary variable for charge state distribution
+    !   -Temporary variable for charge state distribution
     real*8, allocatable :: P_imp(:)
     real*8  :: E_ion
     integer*8  :: ion_i, ion_k
@@ -642,11 +643,11 @@ module mod_expression
 #if JOREK_MODEL == 501
      select case ( trim(gas_type) )
        case('D2')
-         m_i_over_m_imp = central_mass/2.
+         m_i_over_m_imp = central_mass/2.  ! Deuterium mass = 2 u
        case('Ar')
-         m_i_over_m_imp = central_mass/40. ! Argon mass = 40 u and main ion (D) mass = 2 u
+         m_i_over_m_imp = central_mass/40. ! Argon mass = 40 u 
        case('Ne')
-         m_i_over_m_imp = central_mass/20. ! Neon mass = 20 u and main ion (D) mass = 2 u
+         m_i_over_m_imp = central_mass/20. ! Neon mass = 20 u 
        case default
          write(*,*) 'ERROR: Unknown gas_type.'
          stop
@@ -1284,7 +1285,11 @@ module mod_expression
 
 #if JOREK_MODEL == 501
 
-          T0_corr = corr_neg_temp(T0,(/5.d-1,5.d-1/))
+          if (T_min > T_1) then
+            T0_corr = corr_neg_temp(T0,(/5.d-1,5.d-1/),2.*T_min)
+          else
+            T0_corr = corr_neg_temp(T0,(/5.d-1,5.d-1/),2.*T_1)
+          end if
           T_rad   = T0_corr/(2.d0*EL_CHG*MU_ZERO*central_density*1.d20)  ! Te in eV
           T_rad_real = T0/(2.d0*EL_CHG*MU_ZERO*central_density * 1.d20)
   
@@ -1293,23 +1298,22 @@ module mod_expression
           alpha_imp = 0.5*m_i_over_m_imp*(Z_imp+1.) - 1.
           beta_imp  = m_i_over_m_imp*Z_imp - 1.
                   
-		  r0_corr  = corr_neg_dens(r0,(/1.d-8,1.d-5/))
-		  rn0_corr = corr_neg_dens(rn0, (/ 1.d-12, 1.d-5 /))
-		  ne_rad   = (r0_corr + beta_imp * rn0_corr) * 1.d20 * central_density ! electron density (SI)
+          r0_corr  = corr_neg_dens(r0,(/1.d-9,1.d-5/),1.d-3)
+          rn0_corr = corr_neg_dens(rn0,(/1.d-9,1.d-5 /),1.d-3)
+          ne_rad   = (r0_corr + beta_imp * rn0_corr) * 1.d20 * central_density ! electron density (SI)
 		  
-		  ! Normalization coefficient for radiation rate from SI units (W.m^3) to JOREK units:
+          ! Normalization coefficient for radiation rate from SI units (W.m^3) to JOREK units:
           coef_rad_1 = 2.d0/3.d0*MU_ZERO**1.5d0*(central_mass*MASS_PROTON)**0.5d0*(central_density*1.d20)**2.5d0*m_i_over_m_imp
 		  
-          if (ne_rad > 1.d16 .and. T_rad_real > 3.) then
-            Lrad = 0.0
-            dLrad_dT = 0.0
-            call radiation_function(imp_adas(1),imp_cor(1),log10(ne_rad),log10(T_rad*EL_CHG/K_BOLTZ),Lrad,dLrad_dT)
+          if (ne_rad > 1.d18 .and. T_rad_real > 5. .and. rn0 > 1.d-8) then
+	    Lrad = 0.
+            call radiation_function(imp_adas(1),imp_cor(1),log10(ne_rad),log10(T_rad*EL_CHG/K_BOLTZ),Lrad)
             Lrad = Lrad * coef_rad_1
           else
             Lrad = 0.
           end if
 		  
-	  ne_rad = ne_rad / 1.d20 / central_density ! Put ne_rad back to JOREK units to have consistent fact_ne factor with other models (see below)
+          ne_rad = ne_rad / 1.d20 / central_density ! Put ne_rad back to JOREK units to have consistent fact_ne factor with other models (see below)
 		  
 #endif
 
