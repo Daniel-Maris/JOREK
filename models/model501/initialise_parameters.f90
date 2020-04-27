dbbcf8f5e1b50d9e654a3e93d8c1d5dd3584e96d
Di Hu <Faust@IBN5100.localdomain>
2020-04-27 20:48:12 +0800

    Eliminate a potential problem when tauIC = 0

--- models/model501/initialise_parameters.f90
+++ models/model501/initialise_parameters.f90
@@ -369,7 +369,7 @@ call read_num_profiles(my_id)
 call derive_num_profiles(my_id)
 
 ! --- For now the diamagnetic term has not been implemented properly
-if (tauIC /= 0.0) then
+if (tauIC .ne. 0.0) then
   tauIC = 0.0
   write(*,*) "WARNING! The diamagnetic term has not been implemented properly for model 501, setting tauIC = 0 now."
 endif
