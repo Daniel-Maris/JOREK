e8ad310ad64d327a8628c40b6156a8bc006c8554
Di Hu <Faust@IBN5100.localdomain>
2020-07-31 10:53:09 +0800

    Quick fix to previous oversight

--- diagnostics/integrals.f90
+++ diagnostics/integrals.f90
@@ -9,6 +9,15 @@ use gauss
 use basis_at_gaussian
 use phys_module
 use domains
+use corr_neg
+#if (JOREK_MODEL == 500 || JOREK_MODEL == 555)
+  use mod_neutral_source, only: neutral_source, total_n_particles, total_n_particles_inj, total_n_particles_inj_all
+#endif
+#if (JOREK_MODEL == 501)
+  use mod_injection_source, only: inj_source, radiation_function, total_n_particles, total_n_particles_inj, &
+                                  total_n_particles_inj_all
+#endif
+
 
 implicit none
 
@@ -173,7 +182,7 @@ do ife =1, element_list%n_elements
       PS_0  = eq_g(var_psi,ms,mt) 
       
 #if (JOREK_MODEL == 500) || (JOREK_MODEL == 501) || (JOREK_MODEL == 555)
-      rhon_00 = eq_g(mp,var_rhon,ms,mt)
+      rhon_00 = eq_g(var_rhon,ms,mt)
       rn0_corr = corr_neg_dens1(rhon_00)
 #endif
 
