9d1feb129c4eecaf15546e1fb10bba015fc73a52
Matthias Hoelzl <mhoelzl@ipp.mpg.de>
2020-07-31 12:28:50 +0200

    Removing (commented) D_prof terms in amat_65

--- models/model500/mod_elt_matrix_fft.f90
+++ models/model500/mod_elt_matrix_fft.f90
@@ -1628,8 +1628,6 @@ do i=1,n_vertex_max
                             + v * rho * GAMMA * T0 * (vpar0_s * ps0_t - vpar0_t * ps0_s)        * theta * tstep &
                             + v * rho * GAMMA * T0 * F0 / BigR * vpar0_p                 * xjac * theta * tstep &
 
-!                           + D_prof * T0 * BigR  * (v_x*rho_x + v_y*rho_y)              * xjac * theta * tstep &
-
                            + v * BigR * rho * rn0_corr * ksiion * Sion_T                      * xjac * theta * tstep &
                            + v * BigR * rho * rn0_corr * LradDrays_T                          * xjac * theta * tstep &
                            + v * BigR * rho * 2d0 * r0_corr * LradDcont_T                * xjac * theta * tstep &
@@ -1659,10 +1657,7 @@ do i=1,n_vertex_max
                                    * rho * (T0_x * ps0_y - T0_y * ps0_x + F0 / BigR * T0_p)                    &
                                    * (                            + F0 / BigR * v_p) * xjac * theta * tstep * tstep
 
-                  amat_kn(6,5) = &
-!                              + D_prof * T0 * BigR  * (v_p*rho_p /BigR**2) * xjac * theta * tstep &
-
-                               + TG_num6 * 0.25d0 / BigR * vpar0**2                &
+                  amat_kn(6,5) = + TG_num6 * 0.25d0 / BigR * vpar0**2                &
                                    * T0 * (+ F0 / BigR * rho_p)                      &
                                    * (     + F0 / BigR * v_p) * xjac * theta * tstep * tstep
 
