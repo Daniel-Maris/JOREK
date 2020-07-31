92292ead1ebe40dcfd11d355ca89561e63f86cba
Matthias Hoelzl <mhoelzl@ipp.mpg.de>
2020-07-31 09:31:33 +0200

    Mini modification after merge

--- diagnostics/postproc/exec_commands.f90
+++ diagnostics/postproc/exec_commands.f90
@@ -2231,7 +2231,7 @@ module exec_commands
     
     npts    = get_int_setting('surfaces', ierr)
    
-    write(filename,'(5a)') DIR, 'q_surface_', trim(real2str(qvalue,'(f12.4)')), &
+    write(filename,'(5a)') trim(DIR), 'q_surface_', trim(real2str(qvalue,'(f12.4)')), &
        trim(step_range_string(index_start,index_start)), '.txt'
     status = 'replace'
     access = 'sequential'
