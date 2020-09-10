71729bb945b6fb3f708691a67a9b3b253d805dc1
Andres Cathey <andres.cathey@ipp.mpg.de>
2020-09-10 12:00:52 +0200

    curvature vector added to jorek2_postproc expressions

--- diagnostics/new_diag/mod_expression.f90
+++ diagnostics/new_diag/mod_expression.f90
@@ -133,6 +133,10 @@ module mod_expression
     call add(exprs_all, 'gradP_R     ', 'Pressure gradient force (R component)                 ')
     call add(exprs_all, 'gradP_Z     ', 'Pressure gradient force (Z component)                 ')
     call add(exprs_all, 'gradP_phi   ', 'Pressure gradient force (phi component)               ')
+    call add(exprs_all, 'gradPdotCurv', 'grad p dot curvature                                  ')
+    call add(exprs_all, 'curvat_R    ', 'curvature (= b . grad ( b )) in the R direction       ')
+    call add(exprs_all, 'curvat_Z    ', 'curvature (= b . grad ( b )) in the Z direction       ')
+    call add(exprs_all, 'curvat_phi  ', 'curvature (= b . grad ( b )) in the phi direction     ')
     call add(exprs_all, 'omega       ', 'Toroidal Vorticity Component                          ')
     call add(exprs_all, 'rho         ', 'Mass Density                                          ')
     call add(exprs_all, 'ne          ', 'Electron Density                                      ')
@@ -586,7 +590,9 @@ module mod_expression
       Vpar0_R, Vpar0_Z, Vpar0_RR, Vpar0_ZZ, Vpar0_RZ, P0, P0_R, P0_Z, P0_s, P0_t, P0_p, P0_pp,     &
       P0_RR, P0_ZZ, P0_RZ, BB2, Btor, BR, BZ, BR_Z, BZ_R, Btheta, psi_abs, E_par, E_crit,          &
       E_dreicer, AR0_R, AR0_Z, AZ0_R, AZ0_Z, A30_R, A30_Z, AR0_Rp, AZ0_Zp, A30_RR, A30_ZZ, AR0_ZZ, &
-      AR0_RR, AZ0_RR, AZ0_ZZ, A30_Rp, A30_Zp, AR0_RZ, AZ0_RZ, BR_p, BZ_p, BP_Z, BP_R 
+      AR0_RR, AZ0_RR, AZ0_ZZ, A30_Rp, A30_Zp, AR0_RZ, AZ0_RZ, BR_p, BZ_p, BP_Z, BP_R,              &
+      BR_R, BZ_Z, B_R, B_Z,                                                                        &
+      BdotNablaBR, BdotNablaBZ, BdotNablaBphi, BdotNablaB, Kappa_R, Kappa_Z, Kappa_phi, gradPdotKappa  
     real*8  :: eta_T, deta_dT, d2eta_d2T, visco_T, dvisco_dT, ZKpar_T, dZKpar_dT, D_prof, ZK_prof
     real*8 :: Ti0, Ti0_s, Ti0_t, Ti0_st, Ti0_ss, Ti0_tt, Ti0_p, Ti0_pp, Te0, Te0_s, Te0_t, Te0_st, &
       Te0_ss, Te0_tt, Te0_p, Te0_pp, Ti0_R, Ti0_Z, Te0_R, Te0_Z, Er, Vtheta, Mach_par, Mach_pol,   &
@@ -1154,6 +1160,20 @@ module mod_expression
           psi_norm = get_psi_n(ps0, Z)
           psi_abs  = sqrt(ps0_R*ps0_R + ps0_Z * ps0_Z)
 
+          BR_R     = + ps0_RZ / BigR - ps0_Z / BigR**2
+          BR_Z     = + ps0_ZZ / BigR
+          BZ_R     = - ps0_RR / BigR + ps0_R / BigR**2
+          BZ_Z     = - ps0_RZ / BigR
+          Bp_R     = - F0     / BigR**2
+          B_R      = ( BR_R + BZ_R + Bp_R ) / Btot
+          B_Z      = ( BR_Z + BZ_Z        ) / Btot
+
+          Kappa_R    = ( Btot*BR*BR_R - BR*BR*B_R   - BZ*BR*B_Z   + Btot*BZ*BR_Z - Btot*Btor**2/BigR ) / Btot**3.
+          Kappa_Z    = ( Btot*BR*BZ_R - BR*BZ*B_R   - BZ*BZ*B_Z   + Btot*BZ*BZ_Z                     ) / Btot**3.
+          Kappa_phi  = ( Btot*BR*Bp_R - BR*Btor*B_R - BZ*Btor*B_Z                + Btot*Btor*BR/BigR ) / Btot**3.
+
+          gradPdotKappa = P0_R*Kappa_R + P0_Z*Kappa_Z + P0_p / BigR * Kappa_phi
+
           if (psi_abs > 1.d-6) then
             FFprime_loc = zj0 + (R**2.d0) * (ps0_R*P0_R + ps0_Z*P0_Z)/(psi_abs**2.d0)
           else
@@ -1538,6 +1558,18 @@ module mod_expression
               case ( 'gradP_phi' )
                 res = P0_p / BigR / fact_mu_zero
  
+              case ( 'gradPdotCurv' )
+                res = gradPdotKappa / fact_mu_zero
+      
+              case ( 'curvat_R' )
+                res = Kappa_R
+   
+              case ( 'curvat_Z' )
+                res = Kappa_Z
+
+              case ( 'curvat_phi' )
+                res = Kappa_phi
+
               case ( 'Er' )
                 res = Er * fact_Er
                 
