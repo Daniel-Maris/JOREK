ab3d5ac70e1b0ad51eadf0d7fbd2f685cc8f263b
Di Hu <Faust@IBN5100.localdomain>
2020-09-19 11:28:16 +0800

    Keep updated with develop

--- models/model502/initialise_parameters.f90
+++ models/model502/initialise_parameters.f90
@@ -99,6 +99,7 @@ namelist /in1/  tstep, nstep, tstep_n, nstep_n,                     &
                 resistive_wall, SDN_threshold,                      &
                 wall_resistivity, wall_resistivity_fact,            &
                 bc_natural_open,                                    &
+                use_mumps_eq, use_pastix_eq, use_strumpack_eq,      &
                 use_mumps, mumps_ordering,                          &
                 use_BLR_compression, epsilon_BLR, just_in_time_BLR, &
                 use_pastix, use_wsmp, n_tor_fft_thresh,             &
