5b69550fcb79b0d8e530efd1a93ed71d4937c27a
Stan Pamela <stanislas.pamela@ukaea.uk>
2020-10-14 07:40:02 +0100

    I dont think this will be needed in the end, Im not going to look at Mumps, just Pastix and Strumpack...

--- datatypes/mod_integer_types.f90
+++ datatypes/mod_integer_types.f90
@@ -6,24 +6,13 @@ module mod_integer_types
 
 ! --- Generic integers, valid for all solvers
 #ifdef INTSIZE64
-  integer, parameter :: int_all = int64!selected_int_kind (8)
+  integer, parameter :: int_all = int64!selected_int_kind (8) ! apparently, integer*8 and integer(kind=selected_int_kind(8)) is not the same!
   integer, parameter :: MPI_INTEGER_ALL = MPI_INTEGER8
   integer, parameter :: C_INT_ALL = C_LONG
 #else
   integer, parameter :: int_all = int32!selected_int_kind (4)
   integer, parameter :: MPI_INTEGER_ALL = MPI_INTEGER
   integer, parameter :: C_INT_ALL = C_INT
-#endif
-
-! --- Solver-specific integers, that may change between solvers
-#ifdef INTSIZE64
-#ifdef USE_MUMPS
-  integer, parameter :: int_spec1 = selected_int_kind (4)
-#else
-  integer, parameter :: int_spec1 = selected_int_kind (8)
-#endif
-#else
-  integer, parameter :: int_spec1 = selected_int_kind (4)
 #endif
 
 end module mod_integer_types
