98e861b0069e7eead1d6da9df2fd07576362de52
Di Hu <Faust@IBN5100.localdomain>
2020-11-06 21:42:36 +0800

    Hotfix

--- models/model501/mod_elt_matrix_fft.f90
+++ models/model501/mod_elt_matrix_fft.f90
@@ -680,7 +680,7 @@ do ms=1, n_gauss
      if (D_diff_flag .and. num_d_perp) then
        D_prof_imp = get_dperp(psi_norm,num_d_prof_x=num_d_perp_x_imp,&
                               num_d_prof_y=num_d_perp_y_imp,num_d_prof_len=num_d_perp_len_imp)
-     else if (D_diff_flag)
+     else if (D_diff_flag) then
        D_prof_imp = get_dperp(psi_norm,D_perp_sp=D_perp_imp)
      else
        D_prof_imp = get_dperp(psi_norm)
