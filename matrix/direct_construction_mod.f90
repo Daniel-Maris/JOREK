a7c9d87b4547e9636eebd0e6bc22159226180ca9
Prabal Singh Verma <psv@ipp.mpg.de>
2020-11-18 20:15:56 +0100

    making minor correction

--- matrix/direct_construction_mod.f90
+++ matrix/direct_construction_mod.f90
@@ -69,7 +69,7 @@ contains
   call construct_matrix(my_id, MPI_COMM_N, my_id_n, MPI_COMM_MASTER, my_id_master,                &
     local_elms_harm, n_local_elms_harm, index_min_harm(my_id+1), index_max_harm(my_id+1), xpoint2,&
     xcase2, ES%R_axis, ES%Z_axis, ES%psi_axis, ES%psi_bnd, ES%R_xpoint, ES%Z_xpoint,              &
-    ES%psi_xpoint, i_tor_min, i_tor_max, n_harm, nz_harm, ndof_harm, n_matrix_block_size, A_harm,  &
+    ES%psi_xpoint, i_tor_min, i_tor_max, n_harm, nz_harm, ndof_harm, n_matrix_block_size_harm, A_harm,  &
     rhs_harm, irn_harm, jcn_harm, ijA_index_harm, ijA_size_harm, irn_jcn_harm,     &
     direct_construction)
 
