fa9b98e8b37f4d4244ddb105d1d193226c59c9b9
Stanislas Pamela <stanislas.pamela@ukaea.uk>
2020-12-09 08:32:45 +0100

    thats why they were commented I guess

--- models/mod_log_params.f90
+++ models/mod_log_params.f90
@@ -567,8 +567,8 @@ write(*,'(1x,a)',advance='no') ' USE_COMPLEX_PRECOND          : '
   write(*,REAL_FMT) 'gamma_sheath_e        ', gamma_sheath_e
   write(*,REAL_FMT) 'gamma_sheath_i        ', gamma_sheath_i
 #if ( (JOREK_MODEL == 400) || (JOREK_MODEL == 711) )
-  write(*,REAL_FMT) 'gamma_stangeby_e      ', gamma_stangeby_e
-  write(*,REAL_FMT) 'gamma_stangeby_i      ', gamma_stangeby_i
+  write(*,REAL_FMT) 'gamma_i_stangeby      ', gamma_i_stangeby
+  write(*,REAL_FMT) 'gamma_e_stangeby      ', gamma_e_stangeby
 #endif
   write(*,LOGI_FMT) 'vpar_smoothing        ', vpar_smoothing
   if ( vpar_smoothing ) then
