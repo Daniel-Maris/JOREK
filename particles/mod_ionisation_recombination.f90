24c437f832ddc2f35fb95b3d52ec7ebd5fa3a423
Stanislas Pamela <stanislas.pamela@ukaea.uk>
2020-12-09 08:12:36 +0100

    using var_rho

--- particles/mod_ionisation_recombination.f90
+++ particles/mod_ionisation_recombination.f90
@@ -47,9 +47,9 @@ real*8, dimension(2) :: P, P_s, P_t, P_phi, P_time
 real*8               :: R, R_s, R_t, Z, Z_s, Z_t
 call fields%interp_PRZ(time,i_elm,&
 #if ( (JOREK_MODEL == 400) || (JOREK_MODEL == 711) )
-      [5,var_Te],& ! electron temperature
+      [var_rho,var_Te],& ! electron temperature
 #else
-      [5,var_T],& ! electron temperature + ion temperature (assumed equal)
+      [var_rho,var_T],& ! electron temperature + ion temperature (assumed equal)
 #endif
           2,s,t,phi,P,P_s,P_t,P_phi,P_time,R,R_s,R_t,Z,Z_s,Z_t)
 
