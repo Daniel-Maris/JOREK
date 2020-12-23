4237d3ced624e4eb0b8cf16b561dd32682cf9e2b
Di Hu <Faust@IBN5100.localdomain>
2020-12-23 12:03:21 +0800

    Removed remnent D_neutrals

--- models/model501/initialise_parameters.f90
+++ models/model501/initialise_parameters.f90
@@ -130,7 +130,6 @@ namelist /in1/  tstep, nstep, tstep_n, nstep_n,                     &
                 D_prof_neg, ZK_prof_neg, ZK_par_neg,                &
                 D_prof_neg_thresh, ZK_prof_neg_thresh, T_min,       &
                 ne_SI_min, Te_eV_min, rn0_min,                      &
-                D_neutral_x, D_neutral_y, D_neutral_p,              &
                 rho_min, ZK_par_neg_thresh,         &
                 ns_sig, ns_deltaphi, ksi_ion, spi_rnd_seed,         &
                 ns_amplitude, ns_R, ns_Z, ns_phi, ns_radius,        &
