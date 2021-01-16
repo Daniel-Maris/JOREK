8065bd49eac7bebe0570877a81b61c0238a6e80b
Di Hu <Faust@IBN5100.localdomain>
2021-01-16 10:43:51 +0800

    Add back rho_min

--- models/mod_log_params.f90
+++ models/mod_log_params.f90
@@ -468,6 +468,7 @@ write(*,'(1x,a)',advance='no') ' USE_COMPLEX_PRECOND          : '
   write(*,REAL_FMT) 'ZK_prof_neg_thresh    ', ZK_prof_neg_thresh
   write(*,REAL_FMT) 'ZK_par_neg_thresh     ', ZK_par_neg_thresh
   write(*,REAL_FMT) 'T_min                 ', T_min
+  write(*,REAL_FMT) 'rho_min               ', rho_min
   write(*,REAL_FMT) 'ne_SI_min             ', ne_SI_min
   write(*,REAL_FMT) 'Te_eV_min             ', Te_eV_min
   write(*,REAL_FMT) 'rn0_min               ', rn0_min
