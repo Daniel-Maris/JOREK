13d5b35bbb2e881559d812807834c97de452d79e
Mengdi Kong <mengdi.kong@ukaea.uk>
2021-01-24 19:24:05 +0100

    Fix a typo

--- models/model501/mod_elt_matrix_fft.f90
+++ models/model501/mod_elt_matrix_fft.f90
@@ -3,7 +3,7 @@ module mod_elt_matrix_fft
   implicit none
 
 contains
-%
+
 subroutine element_matrix_fft(element,nodes, xpoint2, xcase2, R_axis, Z_axis, psi_axis, psi_bnd, R_xpoint, Z_xpoint, ELM, RHS, tid,&
   ELM_p, ELM_n, ELM_k, ELM_kn, RHS_p, RHS_k,  eq_g, eq_s, eq_t, eq_p, eq_ss, eq_st, eq_tt, delta_g, delta_s, delta_t, i_tor_min, i_tor_max)
 !---------------------------------------------------------------
