f1b8937c5b55bd6f9ee046cddd81ddfcaeccae0d
Ashish Bhole <ashish.bhole@inria.fr>
2021-01-29 01:13:25 +0900

    added force_central_node flag in model 710 and 711 input parameters

--- models/model711/initialise_parameters.f90
+++ models/model711/initialise_parameters.f90
@@ -104,7 +104,8 @@ namelist /in1/  tstep, nstep, tstep_n, nstep_n,                     &
                 starwall_equil_coils, freeb_equil_iterate_area,     &
                 psi_offset_freeb, diag_coils, rmp_coils,            &
                 voltage_coils, vert_FB_amp, find_pf_coil_currents,  &
-                pastix_maxthrd, centralize_harm_mat, treat_axis
+                pastix_maxthrd, centralize_harm_mat, treat_axis,    &
+                force_central_node
 
 if (my_id .eq. 0) then
 
