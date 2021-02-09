27deac3ab052b147dd254ffc035a1881af77defb
Rohan Ramasamy <rohanr@toki06.bc.rzg.mpg.de>
2021-02-09 18:25:30 +0100

    IMAS-3508: Fixed unintentional change in n_coord_tor

--- models/model199/mod_parameters.f90
+++ models/model199/mod_parameters.f90
@@ -34,11 +34,11 @@ module mod_parameters
   integer, parameter :: n_var          = 6         !< number of variables
   integer, parameter :: n_dim          = 2         !< number of dimensions
   integer, parameter :: n_order        = 3         !< order of the polynomial basis
-  integer, parameter :: n_tor          = 3         !< number of toroidal harmonics in physics variables
-  integer, parameter :: n_coord_tor    = 7         !< number of toroidal harmonics in (R, Z) coordinates
+  integer, parameter :: n_tor          = 1         !< number of toroidal harmonics in physics variables
+  integer, parameter :: n_coord_tor    = 1         !< number of toroidal harmonics in (R, Z) coordinates
   integer, parameter :: n_period       = 1         !< periodicity in toroidal direction
   integer, parameter :: n_coord_period = 1         !< periodicity of device in toroidal direction: equivalent to number of field periods
-  integer, parameter :: n_plane        = 4         !< number of toroidal angles
+  integer, parameter :: n_plane        = 1         !< number of toroidal angles
   integer, parameter :: n_vertex_max   = 4         !< maximum number of corners of an element
   integer, parameter :: n_nodes_max    = 15001     !< maximum number of nodes
   integer, parameter :: n_elements_max = 15001     !< maximum number of elements
