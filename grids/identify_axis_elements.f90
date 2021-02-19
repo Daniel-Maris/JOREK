0d5a54b3c296bc06d024e4d41ae6ecf67efb44b0
Ashish Bhole <ashish.bhole@inria.fr>
2021-02-20 07:29:35 +0900

    improve algorithm to detect elements on the axis. this should work for simulations with both flux and without flux aligned grids

--- grids/identify_axis_elements.f90
+++ grids/identify_axis_elements.f90
@@ -1,20 +1,24 @@
 subroutine identify_axis_elements(node_list,element_list)
 use data_structure
-use phys_module, only: treat_axis, n_flux
+use phys_module, only: treat_axis
 
 implicit none
-integer :: ie, iv, j
+integer :: ie, iv, j1, j2 ,j3 ,j4
 type(type_node_list),    intent(inout) :: node_list
 type(type_element_list), intent(inout) :: element_list
 
 do ie = 1, element_list%n_elements
-  do iv = 1, 4 
-    j = element_list%element(ie)%vertex(iv)
-    element_list%element(ie)%axis_element = .false.
-    if (treat_axis .and. node_list%node(j)%axis_node) then
-       element_list%element(ie)%axis_element = .true.
-    endif
-  enddo
+  j1 = element_list%element(ie)%vertex(1)
+  j2 = element_list%element(ie)%vertex(2)
+  j3 = element_list%element(ie)%vertex(3)
+  j4 = element_list%element(ie)%vertex(4)
+
+  element_list%element(ie)%axis_element = .false.
+
+  ! The first and fourth vertex is on the grid-axis.
+  if (treat_axis .and. ( node_list%node(j1)%axis_node .and. node_list%node(j4)%axis_node) ) then
+     element_list%element(ie)%axis_element = .true.
+  endif
 enddo
 
 end subroutine identify_axis_elements
