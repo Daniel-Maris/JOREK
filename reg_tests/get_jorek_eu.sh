f253da151d5546ca2fc55e5a8063728b2c75ae70
Matthias Hoelzl <mhoelzl@ipp.mpg.de>
2021-03-18 20:54:59 +0100

    Fix

--- reg_tests/get_jorek_eu.sh
+++ reg_tests/get_jorek_eu.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
-# This script download a jorek test environment and Makefile.inc
-TESTNAME=`echo "$1" | sed -e 's|/$||'`
 if [ -z "${DAV_URL}" ]; then
     DAV_URL="http://jorek.eu/dav_nrt"
 fi
 
 while [ $# -gt 0 ]; do 
-wget -q --user=nrt --password=nrt_21745XtL ${DAV_URL}/$1 || exit 1
-shift
+  wget -q --user=nrt --password=nrt_21745XtL ${DAV_URL}/$1 || exit 1
+  echo "successfully downloaded '$1'"
+  shift
 done
 
