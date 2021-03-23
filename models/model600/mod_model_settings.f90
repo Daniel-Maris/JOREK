7ff8aa4c740b1ca35a7d010a4dea868a07780f6d
Matthias Hoelzl <mhoelzl@ipp.mpg.de>
2021-03-23 10:54:12 +0100

    Adding a first regression test that would just be a copy of the one for 401

--- models/model600/mod_model_settings.f90
+++ models/model600/mod_model_settings.f90
@@ -3,7 +3,7 @@ module mod_model_settings
 
   implicit none
 
-  logical, parameter :: with_vpar       = .false.
+  logical, parameter :: with_vpar       = .true.
   logical, parameter :: with_TiTe       = .true.
   logical, parameter :: with_neutrals   = .true.
   logical, parameter :: with_impurities = .false.
