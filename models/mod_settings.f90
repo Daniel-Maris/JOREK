154c67c32bc807b48e34f185803a841d5fb391e6
Stanislas Pamela <stanislas.pamela@ukaea.uk>
2021-03-25 17:58:22 +0100

    merge develop

--- models/mod_settings.f90
+++ models/mod_settings.f90
@@ -22,7 +22,7 @@ integer, parameter :: n_pieces_max      = 6001   !< maximum number of line piece
 ! --- a few constants that should not be touched
 integer, parameter :: n_dim             = 2         !< number of dimensions
 integer, parameter :: n_vertex_max      = 4         !< maximum number of corners of an element
-integer, parameter :: n_degrees         = n_order+1 !< degrees of freedom per variable per node
+integer, parameter :: n_degrees         = (n_order+1)**2/4 !< degrees of freedom per variable per node
 integer, parameter :: nref_max          = 10        !< (refinement; not presently working)
 integer, parameter :: n_ref_list        = 10        !< (refinement; not presently working)
 
