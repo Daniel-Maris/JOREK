0ca91079f3a3bf142f66463c633cead7e2167ac1
SangJun Lee <sangjun11316@snu.ac.kr>
2021-04-25 17:30:45 +0900

    fixed preset values

--- models/preset_parameters.f90
+++ models/preset_parameters.f90
@@ -61,7 +61,7 @@ subroutine preset_parameters
   gamma_stangeby     = -1.d99 ! sheath transmission factor (single fluid) given by Stangeby
   gamma_sheath_e     = 3.00d0 ! sheath transmission factor (electron fluid) in the JOREK definition
   gamma_e_stangeby   = -1.d99 ! sheath transmission factor (electron fluid) given by Stangeby
-  gamma_sheath_i     = 1.11d-1! sheath transmission factor (ion fluid) in the JOREK definition
+  gamma_sheath_i     = -1.11d-1! sheath transmission factor (ion fluid) in the JOREK definition
   gamma_i_stangeby   = -1.d99 ! sheath transmission factor (ion fluid) given by Stangeby
   density_reflection = 0.d0   ! reflection coefficient for outgoing density
   neutral_reflection = 0.d0   ! reflection coefficient for (fluid) neutrals
