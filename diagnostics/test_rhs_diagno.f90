1276cc5363202fa45d286ed72c003e54cf64a46e
Javier Artola <javier.artola@iter.org>
2021-05-29 15:31:59 +0200

    update

--- diagnostics/test_rhs_diagno.f90
+++ diagnostics/test_rhs_diagno.f90
@@ -12,7 +12,8 @@ program test_rhs_diagno
   use mod_vtk
   use equil_info
   use mod_elt_matrix_fft
-  !$ use omp_lib
+  use omp_lib
+  use mpi_mod
 
   implicit none
 
@@ -36,9 +37,9 @@ program test_rhs_diagno
   integer :: index_RHS, k_var, i_tor, only_term(2), iterm, term_count, nsub, nnos, ielm, it_o 
   real*8, allocatable :: result(:,:,:,:), res2d(:,:,:)
   real*8,     allocatable :: rhs(:)
-  integer, parameter      :: max_terms=7
+  integer, parameter      :: max_terms=20
   character(len=64)       :: file_name, label 
-
+integer   :: required,provided,StatInfo
 #ifdef USE_FFTW
   real*8     :: in_fft(1:n_plane)
   complex*16 :: out_fft(1:n_plane)
@@ -48,7 +49,10 @@ program test_rhs_diagno
 #ifdef USE_FFTW
   call dfftw_plan_dft_r2c_1d(fftw_plan,n_plane,in_fft,out_fft,FFTW_PATIENT)
 #endif
- 
+
+  call MPI_Init_thread(required, provided, StatInfo)
+  call init_threads()  ! on some systems init_threads needs to come after mpi_init_thread
+
   ! --- Normal initialization
   allocate(node_list)
   allocate(element_list)
@@ -68,26 +72,20 @@ program test_rhs_diagno
   ! --- Initialize the new_diag framework and print some information (.true.)
   call init_new_diag(.true.)
   
- 
-  call init_threads()  ! on some systems init_threads needs to come after mpi_init_thread
-
-
   call tr_meminit(my_id, 1)
   call new_thread_buffers()
 
-#ifdef _OPENMP
-  omp_nthreads = omp_get_num_threads()
-  omp_tid      = 1+omp_get_thread_num()
-#else
-  omp_nthreads = 1
-  omp_tid      = 1
-#endif
+  write(*,*) 'OpenMP threads = ', nbthreads
 
-  write(*,*) 'OpenMP threads = ', omp_nthreads
-  ! --- initialize properly!
-  tstep = 1
-  tstep_prev = 1
-  time_evol_zeta =1
+  ! --- Initialize time stepping parameters
+  call update_time_evol_params()
+
+  if ( index_start <= 1 ) then
+    tstep_prev = tstep
+  else
+    tstep_prev = xtime(index_start) - xtime(index_start-1)
+  end if
+
 
   ! --- Calculate DOFs
   index_total = -1
@@ -96,16 +94,17 @@ program test_rhs_diagno
   enddo
   node_list%n_dof = index_total * n_tor * n_var
 
+  allocate(rhs(node_list%n_dof))
+
   ! --- Create grid points and save them for the vtk
   nsub    = 4
   nnos    = nsub*nsub*element_list%n_elements
   allocate(xyz(3,nnos))
   xyz     = 0
 
-  allocate(rhs(node_list%n_dof))
 
   call create_pol_pos(pol_pos_list, ierr, node_list, element_list, ES, grid=.true., nsub=nsub)
-  call create_tor_pos(tor_pos_list, ierr, nphi=2)
+  call create_tor_pos(tor_pos_list, ierr, nphi=1)
 
   do i = 1, pol_pos_list%n_pos(1)
     do j = 1, pol_pos_list%n_pos(2)
@@ -138,20 +137,35 @@ program test_rhs_diagno
     enddo
   enddo
 
-  term_count = 0
+  term_count = 0  ! Counts terms with non-zero RHS
 
   ! --- Go over variables 
   do k_var=1, n_var
-
 
     do iterm=1, max_terms
 
       rhs = 0.0d0 
 
-      ! --- Get RHS of ZK_perp of the T equation
       only_term = (/ k_var, iterm /)
 
+      ! --- Declare shared and private variables for omp
+      !$omp parallel default(none) &
+      !$omp   shared(element_list,node_list, ES, only_term,        &
+      !$omp          xpoint,xcase, rhs, my_id, thread_struct,n_tor_fft_thresh)                      &
+      !$omp   private(ife,iv,inode,element,nodes,i,i_order,               &
+      !$omp           index_large_i,j,index_ij,k, index_node,         &
+      !$omp           omp_nthreads,omp_tid  )
 
+! --- omp id
+#ifdef _OPENMP
+      omp_nthreads = omp_get_num_threads()
+      omp_tid      = 1+omp_get_thread_num()
+#else
+      omp_nthreads = 1
+      omp_tid      = 1
+#endif
+
+      !$omp do schedule(runtime)
       do ife = 1, element_list%n_elements 
         
         element = element_list%element(ife)
@@ -160,7 +174,6 @@ program test_rhs_diagno
          inode     = element%vertex(iv)
          nodes(iv) = node_list%node(inode)
         enddo
-    
     
         call element_matrix_fft(element,nodes, xpoint, xcase, ES%R_axis, ES%Z_axis, ES%psi_axis, ES%psi_bnd,   &
          ES%R_xpoint, ES%Z_xpoint, thread_struct(omp_tid)%ELM, thread_struct(omp_tid)%RHS, omp_tid,       &
@@ -194,7 +207,9 @@ program test_rhs_diagno
         enddo ! vertex
     
       enddo ! --- elements
-
+      !$omp end do
+      !$omp end parallel
+ 
       if (sum(abs(rhs)) < 1.d-30) cycle
    
       term_count = term_count + 1
@@ -225,6 +240,7 @@ program test_rhs_diagno
         enddo
       enddo
 
+      
       it_o = term_count - 1
       if (term_count/=1) then 
         scalars_o(:, 1:it_o) = scalars(:,1:it_o)
@@ -235,15 +251,17 @@ program test_rhs_diagno
       if (allocated(scalar_names)) deallocate(scalar_names)
       allocate(  scalars(nnos,1:term_count),   scalar_names(term_count))
 
-      expr_list = exprs((/'R         ', 'Z         ',  'Psi       '/), 3, 2)
+      ! --- Evaluate RHS term (Psi) in the grid points
+      expr_list = exprs((/'Psi       '/), 1, 0)
       call eval_expr(ES, JOREK_UNITS, expr_list, pol_pos_list, tor_pos_list, result, ierr)
       call reduce_result_to_2d(ierr, result, res2d, i1=1)
 
-      scalars(:,term_count)    = res2d(:,1,3)
+      ! --- Save RHS values into scalar vtk vector 
+      scalars(:,term_count)    = res2d(:,1,1)
       write (label,'(a, i2.2, a, i3.3)') 'RHS_', k_var, '_', iterm
       scalar_names(term_count) = label
 
-      ! Recover old values 
+      ! Recover old values (to append the array in fortran...) 
       if (term_count/=1) then
         scalars(:, 1:it_o) = scalars_o(:,1:it_o)
         scalar_names(1:it_o) = scalar_names_o(1:it_o)
@@ -263,5 +281,11 @@ program test_rhs_diagno
   write (file_name,'(a, i5.5, a)') 'RHS.', index_start, '.vtk'
   call write_vtk(file_name,xyz,ien,9,scalar_names,scalars)
  
- 
+  deallocate(rhs, scalars, scalar_names, scalars_o, scalar_names_o, result, res2d)
+
+#ifdef USE_FFTW
+  call dfftw_destroy_plan(fftw_plan)
+#endif
+
+
 end program test_rhs_diagno
