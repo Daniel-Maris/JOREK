262390c10013e317b78dc669caaaef22b27d9c0b
Matthias Hoelzl <mhoelzl@ipp.mpg.de>
2021-06-11 08:13:34 +0200

    Implementing solution to improve exchange indices

--- grids/grid_utils/exchange_indices_for_vacuum.f90
+++ grids/grid_utils/exchange_indices_for_vacuum.f90
@@ -11,6 +11,7 @@ subroutine exchange_indices_for_vacuum(node_list, my_id, n_cpu)
   ! --- Local variables
   logical, parameter   :: DEBUG_OUTPUT = .false.
   integer, allocatable :: exchange_table(:,:)
+  integer, allocatable :: first_index_usable(:), mm(:)
   integer :: i, j, k, l, ind_max, n_bnd, ind_bnd, ind1, ind2, len_exchange
   logical :: skip
   
@@ -22,37 +23,47 @@ subroutine exchange_indices_for_vacuum(node_list, my_id, n_cpu)
     if ( node_list%node(i)%boundary > 0 ) n_bnd = n_bnd + 1
   end do
   allocate(exchange_table(n_bnd*8,2))
+  allocate(first_index_usable(n_cpu))
+  allocate(mm(n_cpu))
   if ( DEBUG_OUTPUT .and. (my_id == 0) ) then
     write(*,*) 'ind_max =', ind_max
     write(*,*) 'n_bnd   =', n_bnd
   end if
   
-  ! --- Prepare exchange table
+  ! --- Find out which grid nodes are usable for "exchanging indices"; store the number of the
+  !     first index for each MPI rank that can be used; skip the grid center nodes as these might
+  !     not have four independent grid indices
+  ii: do i = 1, n_cpu
+    do j = 1, node_list%n_nodes
+      if ( (node_list%node(j)%index(1) > (real(i-1)/real(n_cpu))*ind_max + 1) .and. (.not. (node_list%node(j)%axis_node)) ) then
+        first_index_usable(i) = node_list%node(j)%index(1)
+        cycle ii
+      end if
+    end do
+  end do ii
+  
+  ! --- Prepare a "table" of indices to be exchanged
   ind_bnd = 0
-  k       = 0
+  j       = 1
+  mm(:)   = first_index_usable(:)
+  if ( DEBUG_OUTPUT ) write(*,*) 'mm before:', mm(:)
   do i = 1, node_list%n_nodes
     if ( node_list%node(i)%boundary > 0 ) then
-      do j = 1, 4
-        ind_bnd = ind_bnd + 1
-        ind1    = node_list%node(i)%index(j)
-        ind2    = real(ind_bnd)/real(4*n_bnd)*ind_max
-        
-        skip = (ind1==ind2)
-        do l = 1, k
-          skip = skip .or. ( exchange_table(l,1) == ind2 )
-        end do
-        if ( skip ) cycle
-
-        if ( DEBUG_OUTPUT .and. (my_id == 0) ) write(*,*) 'ex:', ind1, ind2
-        k = k + 1
-        exchange_table(k,:) = (/ ind1, ind2 /)
-        k = k + 1
-        exchange_table(k,:) = (/ ind2, ind1 /)
-        
+      k = (real(j)/real(8*n_bnd))*n_cpu + 1 ! with which MPI rank to we want to exchange this index?
+      if ( k == n_cpu ) cycle ! the last MPI rank doesn't need to exchange with itself
+      do l = 1, 4 ! the four dofs of one node
+        ind1    = node_list%node(i)%index(l) ! exchange this index
+        ind2    = mm(k) + l - 1              ! with this one for which MPI rank k is responsible
+        exchange_table(j,:) = (/ind1, ind2/)
+        j = j + 1
+        exchange_table(j,:) = (/ind2, ind1/)
+        j = j + 1
+        if ( DEBUG_OUTPUT ) write(*,*) 'ex: ', ind1, '<->', ind2
       end do
+      mm(k) = mm(k) + 4
     end if
   end do
-  len_exchange = k
+  len_exchange = j - 1
   if ( DEBUG_OUTPUT .and. (my_id == 0) ) write (*,*) 'len_exchange ', len_exchange
   
   ! --- Exchange the indices
@@ -69,6 +80,6 @@ subroutine exchange_indices_for_vacuum(node_list, my_id, n_cpu)
     end do
   end do
   if ( DEBUG_OUTPUT .and. (my_id == 0) ) write (*,*) 'num exchanged ', l
-  deallocate(exchange_table)
+  deallocate(exchange_table, first_index_usable, mm)
   
 end subroutine exchange_indices_for_vacuum
