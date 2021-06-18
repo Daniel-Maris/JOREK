abcb8e0e2ebcb7bd1a2514de7cd2cd6b8f8b8735
Javier Artola <javier.artola@iter.org>
2021-06-18 11:47:23 +0200

    Avoid mod_boundary_matrix_open for t-derivatives for models 7XX (they need to be adapted)

--- models/model710/mod_boundary_matrix_open.f90
+++ models/model710/mod_boundary_matrix_open.f90
@@ -89,6 +89,9 @@ theta = time_evol_theta
 ! change zeta for variable dt
 zeta  = time_evol_zeta * 2.0d0 * tstep / (tstep + tstep_prev)
 
+! --- Needs adaptation for t-derivatives
+if (direction(2) == 3) return
+
 ! --- Flag to switch Mach-1 between boundary_conditions and boundary_matrix_open
 Mach1 = 0.d0
 if (Mach1_openBC) Mach1 = 1.d0
