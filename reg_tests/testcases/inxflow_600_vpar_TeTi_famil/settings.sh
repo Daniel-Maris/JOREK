4bcc6b00258f2f2d446d3f192a4464f6ab15f5e5
Matthias Hoelzl <mhoelzl@ipp.mpg.de>
2021-07-09 14:32:45 +0200

    Updating regtest inxflow_600_vpar_TeTi_famil

--- reg_tests/testcases/inxflow_600_vpar_TeTi_famil/settings.sh
+++ reg_tests/testcases/inxflow_600_vpar_TeTi_famil/settings.sh
@@ -1,5 +1,6 @@
 # --- General settings
 jorekmodel="600"
+options="with_vpar=.true. with_TiTe=.true. with_neutrals=.false. with_impurities=.false. with_refluid=.false."
 description="Ballooning mode, simple X-point plasma, model$jorekmodel, n_tor=7 + FFT."
 mpitasks=5
 binaries="jorek_model${jorekmodel}_7"
@@ -11,12 +12,12 @@ extra_remote_files=""
 # --- Compile the code for the test case
 function compile_jorek () {
   if [ "$initialrun" == "yes" ]; then
-    ./util/config.sh model=$jorekmodel n_tor=1 n_plane=1 n_period=1 with_vpar=.true. || exit 1
+    ./util/config.sh model=$jorekmodel n_tor=1 n_plane=1 n_period=1 $options         || exit 1
     make $compilopt $debugoptions jorek_model${jorekmodel}                           || exit 1
     mv jorek_model${jorekmodel} jorek_model${jorekmodel}_1                           || exit 1
     make cleanall                                                                    || exit 1
   fi
-  ./util/config.sh model=$jorekmodel n_tor=7 n_plane=16 n_period=2 with_vpar=.true.  || exit 1
+  ./util/config.sh model=$jorekmodel n_tor=7 n_plane=16 n_period=2 $options          || exit 1
   make $compilopt $debugoptions jorek_model${jorekmodel}                             || exit 1
   mv jorek_model${jorekmodel} jorek_model${jorekmodel}_7                             || exit 1
 }
