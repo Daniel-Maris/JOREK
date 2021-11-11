84871cf73e6ae0833ecb1907f923822f051f258a
Ihor Holod <ihor.holod@ipp.mpg.de>
2021-11-11 13:56:36 +0100

    change check for centralize_harm_mat to be applicable only without WITH_PASTIX62

--- solvers/preconditioner_module.f90
+++ solvers/preconditioner_module.f90
@@ -268,10 +268,14 @@ module preconditioner_module
       endif
     endif
 
+#ifndef WITH_PASTIX62
     if ((.not.use_strumpack).and.(.not.centralize_harm_mat)) then
-      if (my_id.eq.0) write(*,*) "Warning: PC matrix distribution is only supported by STRUMPACK"
-      !centralize_harm_mat = .true. !!! temporary to test Pastix 6.2
-    elseif ((use_strumpack).and.(centralize_harm_mat)) then
+      if (my_id.eq.0) write(*,*) "Warning: PC matrix distribution is only supported by STRUMPACK"      
+      centralize_harm_mat = .true.
+    endif
+#endif
+    
+    if ((use_strumpack).and.(centralize_harm_mat)) then
       if (my_id.eq.0) write(*,*) "Warning: centralization of PC matrix is not advized when using STRUMPACK"
     endif
 
