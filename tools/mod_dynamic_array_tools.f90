6e4d035f9fdf543af77847a7499bf76b4f222131
cristian sommariva <cristian.sommariva@epfl.ch>
2021-11-15 11:37:13 +0100

    added allocation/deallocation integer array 2d

--- tools/mod_dynamic_array_tools.f90
+++ tools/mod_dynamic_array_tools.f90
@@ -9,6 +9,7 @@ public :: allocate_check,deallocate_check
 ! procedures for checking and allocating arrays
 interface allocate_check
   module procedure allocate_check_integer
+  module procedure allocate_check_integer_2d
   module procedure allocate_check_double
   module procedure allocate_check_double_2d
 end interface
@@ -16,6 +17,7 @@ end interface
 ! procedures for checking and deallocating arrays
 interface deallocate_check
   module procedure deallocate_check_integer
+  module procedure deallocate_check_integer_2d
   module procedure deallocate_check_double
   module procedure deallocate_check_double_2d
 end interface
@@ -31,6 +33,14 @@ subroutine allocate_check_integer(N,array)
   integer,dimension(:),allocatable,intent(inout) :: array
   if(.not.allocated(array)) allocate(array(N))
 end subroutine allocate_check_integer
+
+! allocate integer 2D-integer array if not allocated
+subroutine allocate_check_integer_2d(N1,N2,array)
+  implicit none
+  integer,intent(in) :: N1,N2
+  integer,dimension(:,:),allocatable,intent(inout) :: array
+  if(.not.allocated(array)) allocate(array(N1,N2))
+end subroutine allocate_check_integer_2d
 
 ! allocate double array if not allocated
 subroutine allocate_check_double(N,array)
@@ -54,6 +64,13 @@ subroutine deallocate_check_integer(array)
   integer,dimension(:),allocatable,intent(inout) :: array
   if(allocated(array)) deallocate(array)
 end subroutine deallocate_check_integer
+
+! deallocate 2D-integer array if allocated
+subroutine deallocate_check_integer_2d(array)
+  implicit none
+  integer,dimension(:,:),allocatable,intent(inout) :: array
+  if(allocated(array)) deallocate(array)
+end subroutine deallocate_check_integer_2d
 
 ! deallocate double array if allocated
 subroutine deallocate_check_double(array)
