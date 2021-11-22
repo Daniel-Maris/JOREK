793c4e58574ef6cd29154939a12256a81194c028
cristian sommariva <cristian.sommariva@epfl.ch>
2021-11-22 11:50:39 +0100

    first implementation test uniform random spectra generation and some cleanup

--- particles/postprocessors/tests/mod_spectra_uniform_rng_test.f90
+++ particles/postprocessors/tests/mod_spectra_uniform_rng_test.f90
@@ -3,19 +3,21 @@
 ! random number distributions 
 module mod_spectra_uniform_rng_test
 use fruit
-use mod_rng
+use mod_rng, only: type_rng
 implicit none
 
 private
 public :: run_fruit_spectra_uniform_rng_test
 
 !> Variables -----------------------------------------------------------------
-integer,parameter :: n_points=1000
+integer,parameter :: n_points=1000000
 integer,parameter :: n_spectra=2
 real*8,parameter  :: tol_real8=1.d-16 !< tolerance for assert
 real*8,dimension(2),parameter :: min_wlen=(/3.d-6,3.0d-7/) !< minimum wavelength
 real*8,dimension(2),parameter :: max_wlen=(/3.5d-6,4.d-7/) !< maximum wavelength
-real*8,dimension(2)           :: i_pdf !< 1/pdf=min_wlen,max_wlen
+class(type_rng),dimension(:),allocatable :: rngs !< random number generators
+integer                                    :: n_threads,thread_id !< N# and id omp threads
+real*8,dimension(2)                        :: i_pdf !< 1/pdf=min_wlen,max_wlen
 !>----------------------------------------------------------------------------
 
 contains
@@ -29,6 +31,7 @@ implicit none
   call test_spectrum_rng_uniform_construction_noinit
   call test_set_uniform_spectrum_interval
   call test_spectrum_rng_uniform_construction_init
+  call test_spectrum_generation_rng_uniform
   write(*,'(/A)') "  ... tearing-down: spectra uniform rng tests"
   call teardown
 end subroutine run_fruit_spectra_uniform_rng_test
@@ -36,15 +39,32 @@ end subroutine run_fruit_spectra_uniform_rng_test
 !> Set-up and tear-down ------------------------------------------------------
 !> Set-up test variables
 subroutine setup()
+  use mod_random_seed,only: random_seed
+  use mod_pcg32_rng,  only: pcg32_rng
+  !$ use omp_lib
   implicit none
+  !> variables
+  integer :: n_streams,thread_id,ifail
+
   !> set the inverse of the pdf
   i_pdf = max_wlen-min_wlen
+  !> initialise the rngs using the pcg32
+  n_threads = 1
+  thread_id = 1
+  !$ n_threads = omp_get_max_threads()
+  allocate(pcg32_rng::rngs(n_threads))
+  !$omp parallel default(private) shared(rngs,n_threads,ifail)
+  !$ thread_id = omp_get_thread_num()+1
+  call rngs(thread_id)%initialize(n_dims=n_spectra,seed=random_seed(),&
+  n_streams=n_threads*n_spectra,i_stream=thread_id,ierr=ifail)
+  !$omp end parallel 
 end subroutine setup
 
 !> clean up all test variables
 subroutine teardown()
   implicit none
   i_pdf = 0.d0
+  deallocate(rngs)
 end subroutine teardown
 
 !> Tests ---------------------------------------------------------------------
@@ -174,6 +194,31 @@ subroutine test_spectrum_rng_uniform_construction_init()
   call spectrum%deallocate_spectrum
 
 end subroutine test_spectrum_rng_uniform_construction_init
+
+!> test the random generation of uniform spectra within interval
+subroutine test_spectrum_generation_rng_uniform()
+  use mod_spectra, only: spectrum_rng_uniform
+  implicit none
+  !> variables
+  type(spectrum_rng_uniform) :: spectrum
+  integer :: ii
+
+  !> construct the spectrum class
+  spectrum = spectrum_rng_uniform(n_points,n_spectra,min_wlen,max_wlen)
+  !> generate new random spectra, check intervals bounds
+  call spectrum%generate_spectrum(rngs)
+  do ii=1,n_spectra
+    call assert_true(minval(spectrum%points(:,ii)).ge.min_wlen(ii),&
+    "Error generate uniform random spectrum: minmum point value out-of-bound")
+    call assert_true(maxval(spectrum%points(:,ii)).le.max_wlen(ii),&
+    "Error generate uniform random spectrum: maximum point value out-of-bound")
+  enddo
+  !> clean-up the spectrum class
+  call spectrum%deallocate_spectrum
+
+end subroutine test_spectrum_generation_rng_uniform
+
+!> test the integration via Monte-Carlo method (uniform distribution)
 
 !>----------------------------------------------------------------------------
 
