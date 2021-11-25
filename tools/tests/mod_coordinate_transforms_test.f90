9846dd5039d7bd1c71fc033a5db5c9dd469de3c3
cristian sommariva <cristian.sommariva@epfl.ch>
2021-11-25 15:33:56 +0100

    implemented setup and teardown functions for mod_coordinate_transforms_test

--- tools/tests/mod_coordinate_transforms_test.f90
+++ tools/tests/mod_coordinate_transforms_test.f90
@@ -9,22 +9,33 @@ private
 public :: run_fruit_coordinate_transforms
 
 !> Variables--------------------------------------------
-integer,parameter           :: n_points=4
-integer,parameter           :: n_origins=4
+integer,parameter :: n_points=4                !< number of test positions
+integer,parameter :: n_origins=4               !< number of sphere origins
+real*8,parameter  :: tol_r8=1.d-14             !< tolerance double
+real*4,parameter  :: tol_r4=real(1.d-6,kind=4) !< tolerance float
+real*4,parameter  :: zero_r4=real(0.d0,kind=4)
+real*4,parameter  :: one_r4=real(1.d0,kind=4)
 !> intervals for randomly chosing the first, second and third
 !> position components
-real*8,dimension(2),parameter :: x1_interval=(/-2.3d1,4.23d2/)
-real*8,dimension(2),parameter :: x2_interval=(/-3.2d2,1.45d1/)
-real*8,dimension(2),parameter :: x3_interval=(/-9.d-1,7.50d1/)
+real*8,dimension(3),parameter :: x_lowbnd=(/-2.3d1,-3.2d2,-9.d-1/)
+real*8,dimension(3),parameter :: x_uppbnd=(/4.23d2,1.45d1,7.50d1/)
 !> intervals for randomly chosing the first, second and third
 !> vector components
-real*8,dimension(2),parameter :: a1_interval=(/-3.41d2,6.75d1/)
-real*8,dimension(2),parameter :: a2_interval=(/-4.67d1,8.70d1/)
-real*8,dimension(2),parameter :: a3_interval=(/-9.35d1,2.43d2/)
-real*8,dimension(n_points,3)  :: x       !< set o positions
-real*8,dimension(n_origins,3) :: origins !< set of origins
-real*8,dimension(n_origins,3) :: T,N,B   !< sphere directions
-!>------------------------------------------------------
+real*8,dimension(3),parameter :: a_lowbnd=(/-3.41d2,-4.67d1,-9.35d1/)
+real*8,dimension(3),parameter :: a_uppbnd=(/6.75d1,8.70d1,2.43d2/)
+real*4,dimension(3,n_points)  :: x_r4           !< set o positions
+real*4,dimension(3,n_origins) :: origin_r4      !< set of origins
+real*4,dimension(3,n_origins) :: T_r4,N_r4,B_r4 !< sphere directions
+real*8,dimension(3,n_points)  :: x_r8           !< set o positions
+real*8,dimension(3,n_origins) :: origin_r8      !< set of origins
+real*8,dimension(3,n_origins) :: T_r8,N_r8,B_r8 !< sphere directions
+
+!> Interfaces ------------------------------------------
+!> function for testing basis orthonormality
+interface test_orthonormality_basis
+  module procedure test_orthonormality_basis_r4
+  module procedure test_orthonormality_basis_r8
+end interface test_orthonormality_basis
 
 contains
 
@@ -42,17 +53,91 @@ end subroutine run_fruit_coordinate_transforms
 !> Set-up and tear-down --------------------------------
 !> Set-up test features common to all tests
 subroutine setup()
+  use mod_gnu_rng,        only: gnu_rng_interval
+  use mod_math_operators, only: cross_product
   implicit none
-  x=0.d0
+  !> variables
+  integer :: ii
+  
+  !> generate random positions (assume cartesian coord.)
+  do ii=1,n_points
+    call gnu_rng_interval(n_points,x_lowbnd,x_uppbnd,x_r8(:,ii))
+  enddo
+  !> generate random origins (assume cartesian coord.)
+  do ii=1,n_origins
+    call gnu_rng_interval(n_points,x_lowbnd,x_uppbnd,origin_r8(:,ii))
+    !> generate random orthonormal directions (assume cartesian coord.)
+    call gnu_rng_interval(n_points,a_lowbnd,a_uppbnd,T_r8(:,ii))
+    call gnu_rng_interval(n_points,a_lowbnd,a_uppbnd,N_r8(:,ii))
+    !> compute the vector for the ith origin
+    T_r8(:,ii) = T_r8(:,ii)-origin_r8(:,ii)
+    N_r8(:,ii) = N_r8(:,ii)-origin_r8(:,ii)
+    !> compute basis
+    T_r8(:,ii) = T_r8(:,ii)/norm2(T_r8(:,ii))
+    N_r8(:,ii) = N_r8(:,ii) - (dot_product(T_r8(:,ii),N_r8(:,ii)))*T_r8(:,ii)
+    N_r8(:,ii) = N_r8(:,ii)/norm2(N_r8(:,ii))
+    B_r8(:,ii) = cross_product(T_r8(:,ii),N_r8(:,ii))
+    B_r8(:,ii) = B_r8(:,ii)/norm2(B_r8(:,ii))
+    !> test the correct generation of the orthonormal basis
+    call test_orthonormality_basis(T_r8(:,ii),N_r8(:,ii),B_r8(:,ii))
+  enddo
+
+  !> convert to float precision
+  x_r4 = real(x_r8,kind=4); origin_r4 = real(origin_r8,kind=8); 
+  T_r4 = real(T_r8,kind=4); N_r4 = real(N_r8,kind=4); B_r4 = real(B_r8,kind=4);
+  do ii=1,n_origins
+    call test_orthonormality_basis(T_r4(:,ii),N_r4(:,ii),B_r4(:,ii))
+  enddo
 end subroutine setup
 
 !> Clean-up all common test features
 subroutine teardown()
   implicit none
   !> set all variables to 0
-  x = 0.d0; origins = 0.d0;
-  T = 0.d0; N = 0.d0; B = 0.d0;
+  x_r4 = zero_r4; origin_r4 = zero_r4;
+  T_r4 = zero_r4; N_r4 = zero_r4; B_r4 = zero_r4;
+  x_r8 = 0.d0; origin_r8 = 0.d0;
+  T_r8 = 0.d0; N_r8 = 0.d0; B_r8 = 0.d0;
 end subroutine teardown
-!> Procedure -------------------------------------------
+!> Tests -----------------------------------------------
+!> Tools -----------------------------------------------
+!> method for testing the orthonormality 
+!> of a basis function. Single precision.
+subroutine test_orthonormality_basis_r4(v1,v2,v3)
+  implicit none
+  real*4,dimension(3),intent(in) :: v1,v2,v3
+  call assert_equals(dot_product(v1,v1),one_r4,tol_r4,&
+  "Error basis orthonormality (float): v1 is not normalized!")
+  call assert_equals(dot_product(v2,v2),one_r4,tol_r4,&
+  "Error basis orthonormality (float): v2 is not normalized!")
+  call assert_equals(dot_product(v3,v3),one_r4,tol_r4,&
+  "Error basis orthonormality (float): v3 is not normalized!")
+  call assert_equals(dot_product(v1,v2),zero_r4,tol_r4,&
+  "Error basis orthonormality (float): v1 and v2 are not orthogonal!")
+  call assert_equals(dot_product(v1,v3),zero_r4,tol_r4,&
+  "Error basis orthonormality (float): v1 and v3 are not orthogonal!")
+  call assert_equals(dot_product(v2,v3),zero_r4,tol_r4,&
+  "Error basis orthonormality (float): v2 and v3 are not orthogonal!")
+end subroutine test_orthonormality_basis_r4
+
+!> method for testing the orthonormality 
+!> of a basis function. Double precision.
+subroutine test_orthonormality_basis_r8(v1,v2,v3)
+  implicit none
+  real*8,dimension(3),intent(in) :: v1,v2,v3
+  call assert_equals(dot_product(v1,v1),1.d0,tol_r8,&
+  "Error basis orthonormality (double): v1 is not normalized!")
+  call assert_equals(dot_product(v2,v2),1.d0,tol_r8,&
+  "Error basis orthonormality (double): v2 is not normalized!")
+  call assert_equals(dot_product(v3,v3),1.d0,tol_r8,&
+  "Error basis orthonormality (double): v3 is not normalized!")
+  call assert_equals(dot_product(v1,v2),0.d0,tol_r8,&
+  "Error basis orthonormality (double): v1 and v2 are not orthogonal!")
+  call assert_equals(dot_product(v1,v3),0.d0,tol_r8,&
+  "Error basis orthonormality (double): v1 and v3 are not orthogonal!")
+  call assert_equals(dot_product(v2,v3),0.d0,tol_r8,&
+  "Error basis orthonormality (double): v2 and v3 are not orthogonal!")
+end subroutine test_orthonormality_basis_r8
+
 !>------------------------------------------------------
 end module mod_coordinate_transforms_test
