bdc21f1226d34252c5104fee4842353c136f08ba
cristian sommariva <cristian.sommariva@epfl.ch>
2021-12-02 16:18:29 +0100

    added mpi tools mpi get time and finilaze procedures with tests

---
+++ tools/tests/drivers/mpi_tools_mpi_test_driver.f90
@@ -0,0 +1,19 @@
+! This program is the driver of the boost besselk tests
+program mpi_tools_mpi_test_driver
+use fruit
+use mod_mpi_tools_mpi_test, only: run_fruit_mpi_tools_mpi
+  implicit none
+
+  ! init fruit suite
+  call init_fruit
+  call init_fruit_xml
+
+  ! run then mpi_tools test basket
+  call run_fruit_mpi_tools_mpi
+
+  ! write test summary and finilize test suit
+  call fruit_summary
+  call fruit_summary_xml
+  call fruit_finalize
+
+end program mpi_tools_mpi_test_driver
