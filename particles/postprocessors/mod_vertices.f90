eaccdd82cce63d0ee357b1f22bbf6a3e48ba50a3
cristian sommariva <cristian.sommariva@epfl.ch>
2021-12-07 09:25:15 +0100

    added time coordinate to vertices

--- particles/postprocessors/mod_vertices.f90
+++ particles/postprocessors/mod_vertices.f90
@@ -7,16 +7,18 @@ private
 public :: vertices
 
 !> Variable and type definitions ------------------------------------------
-!> vertices: abstract class containing the basic types
-!> and procedures defining light or gather points
+!> vertices: abstract class containing the basic types and procedures
+!> defining light or gather points.
 type,abstract :: vertices
   !> type variables
   integer,parameter     :: n_x=3             !< number of coordinates
-  integer               :: n_vertices        !< total number of vertices
-  integer               :: n_active_vertices !< number of active verticies
+  integer               :: n_times           !< number of time slices
+  integer               :: n_vertices        !< total number of vertices equals per all time
   integer               :: n_property_vertex !< total number of properties per vertex
-  real*8,dimension(:,:) :: x                 !< position of the point (cartesian)
-  real*8,dimension(:,:) :: properties        !< properties of the vertices
+  integer,dimension(:)  :: n_active_vertices !< total number of active vertices per time
+  real*8,dimension(:)   :: times             !< time of the time slices
+  real*8,dimension(:,:,:) :: x               !< position of the point (cartesian)
+  real*8,dimension(:,:,:) :: properties      !< properties of the vertices
   contains
   !> type procedures
   procedure,pass(vertices) :: allocate_vertices
@@ -32,22 +34,28 @@ contains
 !> for preallocated tables
 !> inputs:
 !>   vert_inout:        (vertices) vertices to be allocated
+!>   n_times:           (integer) number of times
 !>   n_vertices:        (integer) number of vertices
 !> outputs:
 !>   vert_inout: (vertices) allocated vertices
-subroutine allocate_vertices(vert_inout,n_vertices)
+subroutine allocate_vertices(vert_inout,n_times,n_vertices)
   implicit none
   !> inputs
-  integer,intent(in) :: n_vertices
+  integer,intent(in) :: n_vertices,n_time
   !> inputs-outputs
   class(vertices),intent(inout) :: vert_inout
   !> check,allocate and initialize to 0 x and properties array
+  if(allocated(vert_inout%n_active_vertices)) deallocate(vert_inout%n_active_vertices)
+  if(allocated(vert_inout%times)) deallocate(vert_inout%times)
   if(allocated(vert_inout%x)) deallocate(vert_inout%x)
   if(allocated(vert_inout%properties)) deallocate(vert_inout%properties)
-  allocate(vert_inout%x(vert_inout%n_x,n_vertices))
-  allocate(vert_inout%properties(vert_inout%n_property_vertex,n_vertices))
+  allocate(vert_inout%n_active_vertices(n_times))
+  allocate(vert_inout%times(n_times))
+  allocate(vert_inout%x(vert_inout%n_x,n_vertices,n_times))
+  allocate(vert_inout%properties(vert_inout%n_property_vertex,n_vertices,n_times))
+  vert_inout%n_active_vertices = 0; vert_inout%times = 0.d0;
   vert_inout%x = 0.d0; vert_inout%properties = 0.d0;
-  vert_inout%n_vertices = n_vertices; vert_inout%n_active_vertices = 0;
+  vert_inout%n_vertices = n_vertice; svert_inout%n_times = n_times;
 end subroutine allocate_vertices
 
 !> deallocate vertex tables. Data loss is expected
@@ -59,9 +67,11 @@ subroutine deallocate_vertices(vert_inout)
   implicit none
   !> input-outputs
   class(vertices),intent(inout) :: vert_inout
+  if(allocated(vert_inout%n_active_vertices) deallocate(vert_inout%n_active_vertices))
+  if(allocated(vert_inout%times)) deallocate(vert_inout%times)
   if(allocated(vert_inout%x)) deallocate(vert_inout%x)
   if(allocated(vert_inout%properties)) deallocate(vert_inout%properties)
-  vert_inout%n_vertices = 0; vert_inout%n_active_vertices = 0;
+  vert_inout%n_vertices = 0
 end subroutine deallocate_vertices
 
 !> resize the vertex tables without loss of data. This operation can be
@@ -81,35 +91,63 @@ subroutine resize_vertices_noloss(vert_inout,n_vertex_new,ifail)
   !> inputs
   integer,intent(in)            :: n_vertex_new 
   !> variables
-  integer :: ii
-  real*8,dimension(vert_inout%n_x,vert_inout%n_active_vertices) :: x_table
-  real*8,dimension(vert_inout%n_property_vertex,vert_inout%n_active_vertices) :: property_table
+  integer :: ii,jj,n_active_vertices_max
+  real*8,dimension(:,:,:),allocatable :: x_table
+  real*8,dimension(:,:,:),allocatable :: property_table
 
+  !> initialisation
+  n_active_vertices_max = maxval(vert_inout%n_active_vertices)
   !> check for possible data losses
-  if(n_vertex.lt.vert_inout%n_active_vertices) then
+  if(n_vertex.lt.n_active_vertices_max) then
     write(*,*) "Try to resize vertex tables but possible data loss detected!"
     ifail = 11
     return
   endif
+  allocate(x_table(vert_inout%n_x,n_active_vertices_max,vert_inout%n_times))
+  allocate(property_table(vert_inout%n_property_vertex,n_active_vertices_max,vert_inout%n_times))
+
+#ifdef _OPENMP
   !> copy data and resize tables
-  !$omp parallel default(private) shared(vert_inou,n_vertex_new,&
+  !$omp parallel default(private) shared(vert_inout,n_vertex_new,&
   !$omp x_table,property_table)
   !$omp do
-  do ii=1,vert_inout%n_active_vertices
-    x_table(:,ii) = vert_inout%x(:,ii)
-    property_table(:,ii) = vert_inout%properties(:,ii)
+  do ii=1,vert_inout%n_times
+    do jj=1,n_active_vertices_max
+      x_table(:,jj,ii) = vert_inout%x(:,jj,ii)
+      property_table(:,jj,ii) = vert_inout%properties(:,jj,ii)
+    enddo
   enddo
   !$omp end do
   !$omp single
-  call vert_inout%allocate_vertices(n_vertex_new)
+  call vert_inout%allocate_vertices(vert_inout%n_times,n_vertex_new)
   !$omp end single
-  !$omp do
-  do ii=1,vert_inout%n_active_vertices
-    vert_inout%x(:,ii) = x_table(:,ii)
-    vert_inout%properties(:,ii) = property_table(:,ii)
+  !$omp do collapse(2)
+  do ii=1,vert_inout%n_times
+    do jj=1,n_active_vertices_max
+      vert_inout%x(:,jj,ii) = x_table(:,jj,ii)
+      vert_inout%properties(:,jj,ii) = property_table(:,jj,ii)
+    enddo
   enddo
   !$omp end do
   !$omp end parallel
+#else
+  do ii=1,vert_inout%n_times
+    x_table(:,1:vert_inout%n_active_vertices(ii),ii) = &
+    vert_inout%x(:,1:vert_inout%n_active_vertices(ii),ii)
+    property_table(:,1:vert_inout%n_active_vertices(ii),ii) = &
+    vert_inout%properties(:,1:vert_inout%n_active_vertices(ii),ii)
+  enddo
+  call vert_inout%allocate_vertices(vert_inout%n_times,n_vertex_new)
+  do ii=1,vert_inout%n_times
+    vert_inout%x(:,1:vert_inout%n_active_vertices(ii),ii) = &
+    x_table(:,1:vert_inout%n_active_vertices(ii),ii)
+    vert_inout%properties(:,1:vert_inout%n_active_vertices(ii),ii) = &
+    property_table(:,1:vert_inout%n_active_vertices(ii),ii)
+  enddo
+#endif
+
+  !> cleanup
+  deallocate(x_table); deallocate(property_table)
 end subroutine resize_vertices_noloss
 
 !> fit the vertices table to the number of active tables.
@@ -125,7 +163,7 @@ subroutine fit_tables_to_active_vertices(vert_inout,ifail)
   implicit none
   class(vertices),intent(inout) :: vert_inout
   integer,intent(inout)         :: ifail
-  call vert_inout%resize_vertices_noloss(vert_inout%n_active_vertices,ifail)
+  call vert_inout%resize_vertices_noloss(maxval(vert_inout%n_active_vertices),ifail)
 end subroutine fit_tables_to_active_vertices
 
 !>-------------------------------------------------------------------------
