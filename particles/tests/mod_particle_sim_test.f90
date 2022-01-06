89cdf8d1762cfd3a281ecb9a09ee90d9bbc776be
cristian sommariva <cristian.sommariva@epfl.ch>
2022-01-06 16:47:30 +0100

    implemented all tests not required mpi of particle _sim

--- particles/tests/mod_particle_sim_test.f90
+++ particles/tests/mod_particle_sim_test.f90
@@ -13,7 +13,9 @@ public :: run_fruit_particle_sim
 !> Variables ------------------------------------
 integer,parameter :: n_particles=1000
 real*8,parameter  :: survival_prob=6.25d-1
-real*8,parameter  :: t_test=2.345d-6
+real*8,parameter  :: tol_real8=5.d-16
+real*8,parameter  :: central_mass_test=2.d0
+real*8,parameter  :: central_density_test=1.5d-1
 integer,dimension(n_particle_types) :: n_active_particles_sol
 integer,dimension(n_particle_types) :: particle_type_list_sol
 integer*1,dimension(n_particles,n_particle_types) :: particle_charge_list_sol
@@ -31,15 +33,21 @@ subroutine run_fruit_particle_sim()
   write(*,'(/A)') "  ... setting-up: particle sim tests"
   call setup
   write(*,'(/A)') "  ... running: particle sim tests"
+  call test_set_t_norm
+  call test_compute_group_size
+  call test_compute_particle_size
+  call test_allocate_groups_sim
   call test_codify_particle_sim
   call test_find_active_particle_id
   call test_find_active_particle_id_type
   write(*,'(/A)') "  ... tearing-up: particle sim tests"
+  call teardown
 end subroutine run_fruit_particle_sim
 
 !> Set-up and tear-down -------------------------
 !> set-up particle types test features
 subroutine setup()
+  use phys_module, only: central_mass,central_density
   use mod_particle_types, only: particle_fieldline_id,particle_gc_id,particle_gc_vpar_id
   use mod_particle_types, only: particle_kinetic_id,particle_kinetic_leapfrog_id
   use mod_particle_types, only: particle_kinetic_relativistic_id,particle_gc_relativistic_id
@@ -53,7 +61,10 @@ subroutine setup()
   implicit none
   !> variables
   integer :: ifail
-  
+
+  !> set test values
+  central_mass = central_mass_test; central_density = central_density_test;
+
   !> allocate the particle lists
   ifail = 0; allocate(sim_sol%groups(n_particle_types))
   call allocate_one_particle_list_type(n_particle_types,n_particles,sim_sol%groups,ifail)
@@ -79,7 +90,66 @@ subroutine setup()
   particle_charge_list_sol,sim_sol%groups)
 end subroutine setup
 
+!> feature teardown
+subroutine teardown()
+  use phys_module, only: central_mass,central_density
+  implicit none
+  central_mass = 0.d0; central_density = 0.d0;
+end subroutine teardown
+
 !> Tests ----------------------------------------
+!> test set_t_norm
+subroutine test_set_t_norm()
+  use phys_module, only: central_mass,central_density
+  use constants,   only: MU_ZERO,MASS_PROTON
+  implicit none
+  real*8 :: t_norm_sol
+  t_norm_sol = sqrt(MU_ZERO*MASS_PROTON*central_mass*central_density*1.d20)
+  call sim_sol%set_t_norm()
+  call assert_equals(sim_sol%t_norm,t_norm_sol,tol_real8,&
+  "Error particle_sim set t_norm: t_norm mismatch!")
+end subroutine test_set_t_norm
+
+!> test compute group size
+subroutine test_compute_group_size()
+  implicit none
+  call assert_equals(sim_sol%compute_group_size(),n_particle_types,&
+  "Error particle_sim compute group: group size mismatch!")
+end subroutine test_compute_group_size
+
+!> test compute particle size
+subroutine test_compute_particle_size()
+  implicit none
+  integer :: n_groups
+  integer,dimension(n_particle_types) :: n_particle_array,n_part_array_sol
+  n_part_array_sol = n_particles; n_groups = n_particle_types;
+  call sim_sol%compute_particle_sizes(n_groups,n_particle_array)
+  call assert_equals(n_groups,n_particle_types,&
+  "Error in particle_sim compute particle sizes: n_groups mismatch!")
+  call assert_equals(n_particle_array,n_part_array_sol,n_particle_types,&
+  "Error in particle_sim compute particle sizes: particle sizes mismatch!")
+end subroutine test_compute_particle_size
+
+!> test allocate groups
+subroutine test_allocate_groups_sim()
+  use mod_particle_sim, only: particle_sim
+  implicit none
+  integer,parameter :: n_groups=2
+  type(particle_sim) :: sim_test
+
+  !> test group allocation
+  call sim_test%allocate_groups(n_particle_types)
+  call assert_true((allocated(sim_test%groups)).and.&
+  (size(sim_test%groups).eq.n_particle_types),&
+  "Error in particle_sim allocate group: unallocated group not allocated!")
+  call sim_test%allocate_groups(n_groups)
+  call assert_true((size(sim_test%groups).eq.n_groups),&
+  "Error in particle_sim allocate group: allocated group not resized!")
+  call sim_sol%allocate_groups(n_particle_types)
+  call assert_equals(size(sim_sol%groups(1)%particles),n_particles,&
+  "Error in particle_sim allocate group: allocated group modified one it should not!") 
+end subroutine test_allocate_groups_sim
+
 !> test return particle type code for the whole particle list
 subroutine test_codify_particle_sim()
   implicit none
@@ -90,7 +160,7 @@ subroutine test_codify_particle_sim()
   !> extract particle list code 
   call sim_sol%find_particle_types(n_particle_types,list_code)
   call assert_equals(list_code,particle_type_list_sol,n_particle_types,&
-  "Error in particle_sim codify particle sim: particle types mismatch!")
+  "Error in particle_sim codify particle: particle types mismatch!")
 end subroutine test_codify_particle_sim
 
 !> test find active particle id interface notype
