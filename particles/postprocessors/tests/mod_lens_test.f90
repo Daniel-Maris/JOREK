e7f62d59b5693feef859a4d2888e25da273b1c67
cristian sommariva <cristian.sommariva@epfl.ch>
2022-01-19 12:15:32 +0100

    implemented setup and teardown procedures for lens test

--- particles/postprocessors/tests/mod_lens_test.f90
+++ particles/postprocessors/tests/mod_lens_test.f90
@@ -10,6 +10,12 @@ module mod_lens_test
   public :: run_fruit_lens
 
 !> Variables --------------------------------------------------
+integer,parameter :: n_x=3
+real*8,parameter :: tol_real8=5.d-16
+real*8,dimension(n_x),parameter :: center_uppbnd=(/4.5d3,4.67d2,-2.34d-1/)
+real*8,dimension(n_x),parameter :: center_lowbnd=(/-1.d2,2.45d1,-2.3d2/)
+real*8,dimension(n_x) :: center_sol
+
 !> Interfaces -------------------------------------------------
 contains
 !> Fruit basket -----------------------------------------------
@@ -17,13 +23,29 @@ contains
 subroutine run_fruit_lens()
   implicit none
   write(*,'(/A)') "  ... setting-up: lens tests"
+  call setup()
   write(*,'(/A)') "  ... running: lens tests"
   write(*,'(/A)') "  ... tearing-down: lens tests"
+  call teardown()
 end subroutine run_fruit_lens
 
 !> Set-up and teard-down---------------------------------------
+!> set-up test features
+subroutine setup()
+  use mod_gnu_rng, only: gnu_rng_interval
+  implicit none
+  !> initialize center array
+  call gnu_rng_interval(n_x,center_lowbnd,center_uppbnd,center_sol)
+end subroutine setup
+
+!> tear-down test features
+subroutine teardown()
+  implicit none
+  center_sol = 0.d0;
+end subroutine teardown
 
 !> Tests ------------------------------------------------------
+
 !> Tools ------------------------------------------------------
 !>-------------------------------------------------------------
 end module mod_lens_test
