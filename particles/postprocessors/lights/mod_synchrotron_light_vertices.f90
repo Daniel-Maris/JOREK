295739092504bb54363cee76e95d4f69a98c3725
cristian sommariva <cristian.sommariva@epfl.ch>
2022-02-03 09:38:36 +0100

    changed name input for n_sync_lights_in to n_lights_in in light initialisation procedure

--- particles/postprocessors/lights/mod_synchrotron_light_vertices.f90
+++ particles/postprocessors/lights/mod_synchrotron_light_vertices.f90
@@ -34,15 +34,15 @@ contains
 !> of the synchrotron light for each particle and stores them 
 !> in the proprerties array
 !> inputs:
-!>   light_vert:       (synchrotron_light_vertices) empty synchrotron lights
-!>   n_times:          (integer) number of simulation times
-!>   sims_particles:   (particle_sim)(n_times) array of particle simulations
-!>   n_sync_lights_in: (integer)(optional) number of requested synchrotron lights
+!>   light_vert:     (synchrotron_light_vertices) empty synchrotron lights
+!>   n_times:        (integer) number of simulation times
+!>   sims_particles: (particle_sim)(n_times) array of particle simulations
+!>   n_lights_in:    (integer)(optional) number of requested lights
 !> outputs:
 !>   light_vert: (synchrotron_light_vertices) initialised synchrotron lights
 !>   sims_particles: (particle_sim)(n_times) array of particle simulations
 subroutine init_synchrotron_lights_from_particles(light_vert,n_times,&
-sims_particles,n_sync_lights_in)
+sims_particles,n_lights_in)
   use mod_particle_types,        only: particle_kinetic_relativistic_id
   use mod_particle_sim,          only: particle_sim
   implicit none
@@ -51,7 +51,7 @@ sims_particles,n_sync_lights_in)
   type(particle_sim),dimension(n_times),intent(inout) :: sims_particles
   !> inputs
   integer,intent(in)                              :: n_times
-  integer,intent(in),optional                     :: n_sync_lights_in
+  integer,intent(in),optional                     :: n_lights_in
   !> variables
   integer :: ii
   integer :: n_sync_lights,n_groups_max,n_particles_max
@@ -79,9 +79,9 @@ sims_particles,n_sync_lights_in)
   enddo
   n_particles_max = maxval(n_particle_relativistics)
   n_sync_lights = n_particles_max
-  if(present(n_sync_lights_in)) then
-    if(n_sync_lights.lt.n_sync_lights_in) then
-      n_sync_lights =  n_sync_lights_in   
+  if(present(n_lights_in)) then
+    if(n_sync_lights.lt.n_lights_in) then
+      n_sync_lights =  n_lights_in   
     else
       write(*,*) "Error initialise synchrotron lights from particles"
       write(*,*) "Requested number of lights < number of particles,use: ",n_sync_lights
