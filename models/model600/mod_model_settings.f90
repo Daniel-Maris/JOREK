c559af7880b44ff44e1e8d0d4b7b22bf30901438
Di Hu <Faust@IBN5100.localdomain>
2022-02-17 17:17:50 +0800

    Make model_setting consistent with develop

--- models/model600/mod_model_settings.f90
+++ models/model600/mod_model_settings.f90
@@ -56,12 +56,12 @@ integer, parameter :: var_w    = 4
 integer, parameter :: var_rho  = 5
 integer, parameter :: var_T    = sum(merge( (/0/), (/6/), with_TiTe ))
 ! --- variable indices for the model extensions
-integer, parameter :: var_Ti     = sum(merge((/                               6/), (/0/), with_TiTe    ))
-integer, parameter :: var_Te     = sum(merge((/n_var_base+n_var_vpar         +1/), (/0/), with_TiTe    ))
-integer, parameter :: var_Vpar   = sum(merge((/                               7/), (/0/), with_vpar    ))
-integer, parameter :: var_rhon   = sum(merge((/n_var_base+sum(n_var_ext(1:2))+1/), (/0/), with_neutrals))
-integer, parameter :: var_rhoimp = sum(merge((/n_var_base+sum(n_var_ext(1:3))+1/), (/0/), with_impurities))
-integer, parameter :: var_nre    = sum(merge((/n_var_base+sum(n_var_ext(1:4))+1/), (/0/), with_refluid ))
+integer, parameter :: var_Ti       = sum(merge((/                               6/), (/0/), with_TiTe      ))
+integer, parameter :: var_Te       = sum(merge((/n_var_base+n_var_vpar         +1/), (/0/), with_TiTe      ))
+integer, parameter :: var_Vpar     = sum(merge((/                               7/), (/0/), with_vpar      ))
+integer, parameter :: var_rhon     = sum(merge((/n_var_base+sum(n_var_ext(1:2))+1/), (/0/), with_neutrals  ))
+integer, parameter :: var_nre      = sum(merge((/n_var_base+sum(n_var_ext(1:3))+1/), (/0/), with_refluid   ))
+integer, parameter :: var_rho_imp  = sum(merge((/n_var_base+sum(n_var_ext(1:4))+1/), (/0/), with_impurities))
 ! --- variables not relevant to this model
 integer, parameter :: var_AR   = 0
 integer, parameter :: var_AZ   = 0
