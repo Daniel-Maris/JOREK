a6942a9652d20eb47321457695ba7648d64c693f
Van hoof Emma EXT <vanhooe@sdcc-login02.iter.org>
2022-02-24 14:16:05 +0100

    succeeded to change fueling rate with controller using input from a datafile

--- particles/mod_controller.f90
+++ particles/mod_controller.f90
@@ -1,14 +1,11 @@
-!> Module for controlling a paramtere in the plasma
-!> As an example, this module will control the time-varying gas puff rate at the locations where gas valves will be.
+!> Module for controlling a parameter in the plasma
+!> As an example, this module will control the time-varying gas puff rate at the locations where a gas valve is present
 !> But it is aimed at being usable for various other applications
 
 !note: all variables that you allocate here cannot be allocated at the same time in the module where you use mod_controller
 
 module mod_controller
     
-    ! The two commented modules were used for testing and can be deleted when remaining redundant
-    !use phys_module, only: CENTRAL_MASS, CENTRAL_DENSITY, tstep, tstep_particles
-    !use constants,   only: MU_ZERO, MASS_PROTON
     use mod_particle_puffing
     use mod_particle_sim
     use phys_module, only: index_now
@@ -16,21 +13,13 @@ module mod_controller
     implicit none
         
     ! controller parameters
-    logical                :: use_controller
-    character(len=256) :: controller_timedependentsignal_file
-    !integer :: index_now
+    logical                          :: use_controller
+    character(len=256)               :: controller_timedependentsignal_file
 
-    ! usage parameters
-    type(particle_puffing) :: gas_puff
+    ! use-case specific parameters
+    type(particle_puffing)           :: gas_puff
     class(particle_puffing), pointer :: this
-    real*8                 :: t_norm!, n_norm, timesteps, tstep_si, rho_norm !!!!must be uncommented in my_example when commented here!!!!!
-    !integer                :: n_steps !!!!! must be uncommented in my_example when commented here!!!!!
-
-    !All commented variables below and above were used for testing and can later be deleted when they remain redundant
-    !real*8,intent(in)   :: max_puff, min_puff
-    !real*8              :: to_puff
-    !real*8,intent(in)    :: t_puff_start,t_puff_slope
-    !real*8,intent(in)    :: time
+    real*8                           :: t_norm
 
     !defining a type for a general time dependent signal
     type :: time_dependent_signal
@@ -50,42 +39,32 @@ subroutine controller_function(use_controller,this,sim, time_dependent_signal_co
     type(particle_sim), intent(inout)       :: sim
     type(time_dependent_signal), intent(inout) ::time_dependent_signal_controller
     logical,intent(in) :: use_controller
+
     controller_timedependentsignal_file = '/home/ITER/vanhooe/Documents/Datafiles/datafile.dat' ! Note: check the directory to the datafile
     
-    !All commented variables below were used for testing and can later be deleted when they remain redundant
-    !real*8,intent(in)   :: max_puff, min_puff
-    !real*8              :: to_puff
-    !real*8,intent(in)    :: t_puff_start,t_puff_slope
-    !real*8,intent(in)    :: time
-    !n_norm    = CENTRAL_DENSITY * 1.d20                              ! (number) density normalisation
-    !rho_norm  = CENTRAL_MASS * MASS_PROTON * n_norm                  ! rho_SI = rho_norm * rho
-    !t_norm    = sqrt((MU_ZERO * rho_norm))                           ! t_SI   = t_norm * t_jorek 
-    !timesteps         = tstep_particles
-    !tstep_si          = tstep * t_norm
-    !n_steps           = floor(tstep_si / timesteps)
-    !timesteps         = tstep_si / n_steps
-    !n_steps           = tstep_si / timesteps
-
-    ! The following if-statement determines what the controller does
-    ! note: if you want to change parameters that are part of an action within an event, the event must be a pointer (use new_event_ptr() instead of event())
+    !> The following if-statement determines what the controller does
+    !> note: if you want to change parameters that are part of an action within an event, the event must be a pointer (use new_event_ptr() instead of event())
     if (use_controller) then
         write(*,*) "The controller_function works. The controller is on"
-        !the lines below were used for run 16 and part of commit 10
+        !> Example how to change the defined !time-dependent! signal                       the lines below were used for run 16 and part of commit 10
         !gas_puff%fueling_rate = 60.d21    ! we can adjust this%fuelling_rate here depending on the required input signal
         !gas_puff%t_puff_slope = 1.d-3
         !gas_puff%t_puff_start = 10*t_norm
         
-        !A test to make sure sim%time works properly and is usable to make the controller function time dependent
+        !test to make sure sim%time and index_now work properly and are usable to make the controller function time dependent
         write(*,"(A,g12.4)") "test voor controller, this is the time now", sim%time 
         write(*,"(A,g12.4)") "test voor controller, this is index_now", index_now 
-        !First test succeeded to define a certain time dependent fuelling rate signal from within the controller function. Currently it is the same function as the time-dependent_puff function within mod_particle_puffing
+       
+        !> Example of how to define a certain time dependent fuelling rate signal from within the controller function. 
+        !> Currently it is the same function as the time-dependent_puff function within mod_particle_puffing (but copied and renamed below)
+        !> To use it, puff_t_dependent must be set .false. in my_example.
         !gas_puff%fueling_rate = time_dependent_puff_controller(25.d21,sim%time, 10*t_norm,500*t_norm, 20.d21)
          
-        !Test to import time trace using a datafile
+        !> Example of how to import a time dependent signal using a datafile
         call readProf(time_dependent_signal_controller%time, time_dependent_signal_controller%signal, &
         time_dependent_signal_controller%len, controller_timedependentsignal_file)
         write(*,"(A,g12.4,A,g12.4)") "current time signal", time_dependent_signal_controller%time(index_now), "current signal", time_dependent_signal_controller%signal(index_now) 
-        
+        gas_puff%fueling_rate = time_dependent_signal_controller%signal(index_now)
 
         ! Next steps to implement in the controller function:
         ! call the setpoint on this timestep 
