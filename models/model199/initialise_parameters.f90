e1961329521ef2c7dfdbce04e9d140998cfd5da2
Sven Korving <Sven.Korving@iter.org>
2022-03-10 13:48:20 +0100

    rho_ and T_min_neg now availbale as input parameter for all models

--- models/model199/initialise_parameters.f90
+++ models/model199/initialise_parameters.f90
@@ -101,6 +101,7 @@ namelist /in1/  tstep, nstep, tstep_n, nstep_n,                     &
                 time_evol_scheme, corr_neg_temp_coef,               &
                 corr_neg_dens_coef, D_prof_neg, ZK_prof_neg,        &
                 D_prof_neg_thresh, ZK_prof_neg_thresh, T_min,rho_min,&
+				T_min_neg,rho_min_neg,                              &
                 amix, amix_freeb, equil_accuracy,                   &
                 equil_accuracy_freeb, current_ref, FB_Ip_position,  &
                 FB_Ip_integral, Z_axis_ref, FB_Zaxis_position,      &
