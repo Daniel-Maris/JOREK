afb8256c9127109506ee246ddf2c33121ed642d4
Timo Bogaarts <t.j.bogaarts@student.tue.nl>
2022-03-10 16:22:36 +0100

    Updated example

--- particles/examples/tae_phase_space_project.f90
+++ particles/examples/tae_phase_space_project.f90
@@ -21,7 +21,7 @@ program tae_loop
   use phys_module, only: n_mode_families
 
   use constants,   only: MU_ZERO, MASS_PROTON, ATOMIC_MASS_UNIT, K_BOLTZ, EL_CHG
-
+  use mod_math_operators, only: cross_product
   use mod_particle_sputtering, only: particle_sputter, sample_fluid_particle_energy
   use mod_projection_functions, only: proj_f_combined_density, &
       proj_f_combined_energy, proj_f_combined_par_momentum
@@ -58,7 +58,7 @@ program tae_loop
   real*8 :: test_x(3), test_E(3), test_B(3), test_psi, test_U
   !real*8 :: b_norm_r, b_norm_z, b_norm_phi
 
-  integer   :: n_particles_local,n_reflect,ifail
+  integer   :: n_particles_local,n_reflect,ifail, ino
   integer   :: i, j, k, l, m, n_steps, i_elm_old, i_diagno
   integer   :: seed, i_rng, n_stream,i_tor,ierr
 
@@ -66,12 +66,11 @@ program tae_loop
   call sim%initialize(num_groups=1)
 
   rho_part    = 1.195d19 !(corrected value to obtain density=1.441e17 (as in benchmark, for original profile with toroidal flux)
-  rho_part = n_particles
   n_particles_local = int(n_particles/sim%n_cpu)
   timesteps         = tstep_particles
-
+  write(*,*) tstep
   ! Set up the field reader
-  fieldreader = event(read_jorek_fields_interp_linear(basename='jorek', i=-1))
+  fieldreader = event(read_jorek_fields_interp_linear(basename='jorek', i=-1,mode_divisor=100))
   call with(sim, fieldreader)
 
   write(*,*) 'main : t_start = ',t_start
@@ -82,7 +81,7 @@ program tae_loop
   call broadcast_boundary(sim%my_id, bnd_elm_list, bnd_node_list)
 
   call update_equil_state(sim%my_id,sim%fields%node_list, sim%fields%element_list, bnd_elm_list, xpoint, xcase)
-  
+
   n_norm   = CENTRAL_DENSITY * 1.d20                              ! (number) density normalisation
   rho_norm = CENTRAL_MASS * MASS_PROTON * n_norm                  ! rho_SI = rho_norm * rho
   t_norm   = sqrt((MU_ZERO * rho_norm))                           ! t_SI   = t_norm * t_jorek
@@ -110,17 +109,16 @@ program tae_loop
 
     allocate(particle_kinetic_leapfrog::sim%groups(1)%particles(n_particles_local))
 
-    !< If projecting phase space, it is vital to use a by construction $phi$ independent initial distribtuion, i.e. phi planes.
-    call initialise_particles_H_mu_psi(sim%groups(1)%particles, sim%fields, pcg32_rng(),sim%groups(1)%mass, &
+    !< If projecting phase space, it is vital to use a by construction phi independent initial distribtuion, i.e. phi planes.
+    call initialise_particles_H_mu_psi_phiplanes(sim%groups(1)%particles, sim%fields, pcg32_rng(),sim%groups(1)%mass, &
          uniform_space=.true., uniform_space_rej_f=f_toroidal_flux, &
-         uniform_space_rej_vars=[1], charge = 1, T_maxwell = 4d5)
+         uniform_space_rej_vars=[1], charge = 1, T_maxwell = 4d5,n_phi_planes_in=12)
 
 
     call adjust_particle_weights(sim%groups(1)%particles, rho_part)
-    if (sim%my_id .eq. 0) write(*,*) "Particle density was adjusted to:", rho_part, sim%groups(1)%particles(1:10)%weight
+    if (sim%my_id .eq. 0) write(*,*) "Particle density was adjusted to:", rho_part, sim%groups(1)%particles(1)%weight
     diag = write_particle_diagnostics(filename='diag.h5', append=.false.)
     call with(sim,diag)
-
     select type (p => sim%groups(1)%particles)
       type is (particle_kinetic_leapfrog)
 
@@ -147,24 +145,29 @@ program tae_loop
       filter_n0 = filter_perp, filter_hyper_n0 = filter_hyper, filter_parallel_n0 = filter_par_n0, &
       calc_integrals=.false., to_vtk=.true., to_h5 = .false., basename='projections')
 
- ! A few examples of phase space projections.
+  ! A few examples of phase space projections.
 
   ! Simple 2D density histogram
   !test_phase = new_phase_space_projection(sim,ndim=2,res=[200,210],start=[9.d0,-1.d0],end=[11.d0,1.d0],f_proj=proj_f(proj_one, group = 1),&
-  !                                        f_grids= [proj_f(proj_R,group =1 ),proj_f(proj_Z,group=1)],bandwidths=[0.5,0.5])
+  !                                        f_grids= [proj_f(proj_R,group =1 ),proj_f(proj_Z,group=1)],bandwidths=[0.2,0.2])
   ! Power versus minor radius (f_proj is not relevant, as we fill the value arrays manually by averaging over particle orbits.)
   ! If you want to use this, you'll have to use a restart file with some mode structure (i.e. a linear-phase TAE mode from previous simulations)
-  test_phase = new_phase_space_projection(sim,ndim=2,res=[200,200],start=[9.d0,-1.d0],end=[11.d0,1.d0],f_proj=proj_f(proj_one, group = 1),&
-                                         f_grids= [proj_f(proj_R,group = 1 ),proj_f(proj_Z,group = 1 )],bandwidths=[0.3,0.3])
-  ! Initial density of mu 
+  test_phase = new_phase_space_projection(sim,ndim=2,res=[150,150],start=[-5.d-19,-0.1d0],end=[5.d-19,1500.d0],f_proj=proj_f(proj_one, group = 1),&
+                                         f_grids= [proj_f(proj_R,group = 1 ),proj_f(proj_Z,group = 1 )],bandwidths=[1d-19,100d0])
+
+ 
+  ! Initial density of mu
   !test_phase = new_phase_space_projection(sim,ndim=1,res=[200],start=[0.d0],end=[1.d6],f_proj=proj_f(proj_one, group = 1),&
   !                                         f_grids= [proj_f(proj_mu,group = 1 )],bandwidths=[0.05d6])
 
-  ! Example calling whole sim projection and outputting 
+  ! Example calling whole sim projection and outputting
   !call with(sim,test_phase)
   !call output_phase_project(test_phase)
-  ! Full tensor, but is not needed in this example.
-  allocate(jorek_feedback%rhs(n_order+1, n_vertex_max, sim%fields%element_list%n_elements, n_tor, 6))
+  ! This only works w/ nearest neighbour projection (not shaped kernels), so be careful! Manual filling (as in particle loop)
+  ! works for both.
+  
+  ! Full tensor + density (for density flattening was the idea)
+  allocate(jorek_feedback%rhs(n_order+1, n_vertex_max, sim%fields%element_list%n_elements, n_tor, 7))
 
   jorek_feedback%rhs = 0.d0
 
@@ -197,6 +200,7 @@ program tae_loop
 
   ! Call events at sim%time once to help event scheduler, before entering particle loop
   step_rest_time = 0.d0
+  ino = 0
   if (.not. restart_particles) call with(sim, events, at=sim%time)
 
   do while (.not. sim%stop_now)
@@ -218,20 +222,13 @@ program tae_loop
       write(*,*) "PARTICLE : n_steps             : ",n_steps
       write(*,*) "PARTICLE : step_rest_time      : ",step_rest_time
     endif
-
-    ! If e.g. <100 timesteps, you will see initial power exchange (linear) while for longer 
-    ! the flattening of the distribution will be seen.
-    call loop_particle_kinetic_local(sim, jorek_feedback, rng, timesteps, 2000 , particle_start_time,test_phase)
-    call output_phase_project(test_phase)
-
+    test_phase%values = 0.d0
+    call loop_particle_kinetic_local(sim, jorek_feedback, rng, timesteps, n_steps , particle_start_time,test_phase)
+    call output_phase_project(test_phase,ino)
+    ino = ino +1
     ! Output 2D pressure projection as well for completeness (& verify the initialization worked, to_vtk = .true.)
-    call with(sim,jorek_feedback);
-    
-    ! For this example we can abort here, but it could be extended to include multiple fluid timesteps.
-
-    call MPI_BARRIER(MPI_COMM_WORLD,ierr)
-    call MPI_ABORT(MPI_COMM_WORLD,10, ierr)
     sim%time = target_time
+    call with(sim,events, at=sim%time)
 
   end do
 
@@ -269,6 +266,8 @@ subroutine loop_particle_kinetic_local(sim, jorek_feedback, rng, timesteps, n_st
   real*8    :: v_temp(3), T_eV, K_eV, v_kin_temp, B_norm(3), v,E_diff
   real*8    :: R_g, Z_g, R_s, R_t, Z_s, Z_t, xjac, HZ(n_tor), HH(4,4), HH_s(4,4), HH_t(4,4), E_tot,E_tot_red, E_after, E_after_red
   real*8    :: b_norm_r, b_norm_z, b_norm_phi, vr_tilde,vz_tilde,v_par, p_par, p_perp, p_atrop,val_tmp(test_phase%totsupport),val_tmp2
+  real*8    :: Zephi
+  real*8    :: Zephi_red
 !$ real*8 :: w0, w1, mmm(3)
 
 
@@ -301,16 +300,23 @@ subroutine loop_particle_kinetic_local(sim, jorek_feedback, rng, timesteps, n_st
   E_tot_red=0.d0
   E_after =0.d0
   E_after_red = 0.d0
+  Zephi=0.d0
   select type (particles => sim%groups(1)%particles)
     type is (particle_kinetic_leapfrog)
-      do j=1,size(particles,1)
+       do j=1,size(particles,1)
+          if (particles(j)%i_elm > 0) then
+             call sim%fields%calc_EBpsiU(t, particles(j)%i_elm, particle_tmp%st, particle_tmp%x(3), E, B, psi, U)
+             Zephi = Zephi + particles(j)%q*particles(j)%weight*el_chg*U*F0
+          endif
         E_tot = E_tot + 0.5d0*particles(j)%weight*sim%groups(1)%mass*mass_proton*dot_product(particles(j)%v, particles(j)%v)
       enddo
   end select
   call MPI_REDUCE(E_tot,E_tot_red,1,MPI_REAL8, MPI_SUM, 0, MPI_COMM_WORLD, ierr)
+  call MPI_REDUCE(Zephi, Zephi_red,1,MPI_REAL8, MPI_SUM,0,MPI_COMM_WORLD,ierr)
   write(*,*) "On process", sim%my_id, "tot energy: ", E_tot
+  write(*,*) "On process", sim%my_id, "tot ezphi: ", Zephi
   if(sim%my_id .eq. 0 ) write(*,*) "Total energy:", E_tot_red
-  
+  if(sim%my_id .eq. 0) write(*,*) "Total ezphi:", Zephi_red
 
   select type (particles => sim%groups(1)%particles)
     type is (particle_kinetic_leapfrog)
@@ -342,11 +348,9 @@ subroutine loop_particle_kinetic_local(sim, jorek_feedback, rng, timesteps, n_st
           t = particle_start_time + (k-1)*timesteps
 
           call sim%fields%calc_EBpsiU(t, particle_tmp%i_elm, particle_tmp%st, particle_tmp%x(3), E, B, psi, U)
-         
+
           rz_old    = particle_tmp%x(1:2)
           rzp_old   = particle_tmp%x
-          vcart_old = particle_tmp%v !(If time permits, I will use this to average v(t+1/2) & v(t-1/2), you have to do this cartesian as reference frame changes!)
-          ! but for now it's just the v(t-1/2) velocity in the r(t) reference frame, as this is when E was calculated so we have to use the non-pushed particle velocity.
           st_old    = particle_tmp%st
           i_elm_old = particle_tmp%i_elm
 
@@ -357,7 +361,7 @@ subroutine loop_particle_kinetic_local(sim, jorek_feedback, rng, timesteps, n_st
             ! if(index_phase_tmp2 > 0) then
             !  phase_proj(index_phase_tmp2)=phase_proj(index_phase_tmp2)+1.d0/n_steps*particle_tmp%weight*dot_product(particle_tmp%v,E)!
             ! endif
- 
+
             ! Push the particle and determine its new location.
             E_diff = particle_tmp%weight*0.5d0*sim%groups(1)%mass*MASS_PROTON*dot_product(particle_tmp%v,particle_tmp%v)
             call boris_push_cylindrical(particle_tmp, sim%groups(1)%mass, E, B, timesteps)
@@ -365,39 +369,42 @@ subroutine loop_particle_kinetic_local(sim, jorek_feedback, rng, timesteps, n_st
             call find_RZ_nearby(sim%fields%node_list, sim%fields%element_list, rz_old(1), rz_old(2), st_old(1), st_old(2), i_elm_old, &
                      particle_tmp%x(1), particle_tmp%x(2), particle_tmp%st(1), particle_tmp%st(2), particle_tmp%i_elm, ifail)
             E_diff =-E_diff+ particle_tmp%weight*0.5d0*sim%groups(1)%mass*MASS_PROTON*dot_product(particle_tmp%v,particle_tmp%v)
-          
-            call calc_index_shaped_part(test_phase,particle_tmp,index_phase_tmp, val_tmp,sim)                        
-              do i_phase=1, test_phase%totsupport
-                if(index_phase_tmp(i_phase) > 0)then
-                  phase_proj(index_phase_tmp(i_phase))=phase_proj(index_phase_tmp(i_phase))+1.d0*particle_tmp%weight*val_tmp(i_phase)*E_diff
-                endif
+            !Particle perpendicular & parallel pressure averaging
+            !Normalized b vector
+            b_norm_r= B(1)/sqrt(B(1)**2+B(2)**2+B(3)**2)
+            b_norm_z= B(2)/sqrt(B(1)**2+B(2)**2+B(3)**2)
+            b_norm_phi= B(3)/sqrt(B(1)**2+B(2)**2+B(3)**2)
+            !Orthonormal (compared to magnetic field and eachother)
+            vr_tilde=(-(b_norm_r)*particle_tmp%v(3)+b_norm_phi*particle_tmp%v(1))/sqrt(b_norm_phi**2+b_norm_r**2)
+            vz_tilde = (particle_tmp%v(2)-b_norm_z*(b_norm_phi*particle_tmp%v(3)+b_norm_r*particle_tmp%v(1)+b_norm_z*particle_tmp%v(2)))/sqrt(b_norm_phi**2+b_norm_r**2)
+            v_par= b_norm_phi*particle_tmp%v(3)+b_norm_r*particle_tmp%v(1)+b_norm_z*particle_tmp%v(2)
+            !write(*,*) v_par, dot_product(particle_tmp%v, B)/norm2(B)
+
+            !Parallel & perpendicular pressures
+            p_perp = 1.d0/2.d0*(vr_tilde**2+vz_tilde**2)
+            p_par = v_par**2
+            p_atrop = p_par-p_perp
+
+            ! Calculating indices of the support in the values array of the test_phase, the array is the [p_phi, E] value of the particle.
+            ! This is a bit trial and error to get good 'looking' bandwidths. (call loop w/ 1 step & E_diff ->pctls weight for dist function
+            call calc_index_shaped_part_x(test_phase,particle_tmp,index_phase_tmp, val_tmp,sim,[particle_tmp%q*el_chg*psi+sim%groups(1)%mass*MASS_PROTON*particle_tmp%v(3)*particle_tmp%x(1), dot_product(particle_tmp%v,particle_tmp%v)*0.5d0*sim%groups(1)%mass*MASS_PROTON/EL_CHG/1d3])
+
+            ! Adding to main test_phase array of all the particle contributions.
+            do i_phase=1, test_phase%totsupport
+              if(index_phase_tmp(i_phase) > 0)then
+                phase_proj(index_phase_tmp(i_phase))=phase_proj(index_phase_tmp(i_phase))+1.d0*val_tmp(i_phase)*E_diff
+              endif
             enddo
             if(particle_tmp%i_elm .gt.0) then
-              
+
 
 
 
 
               call basisfunctions(particle_tmp%st(1), particle_tmp%st(2), HH, HH_s, HH_t)
               call mode_moivre(particle_tmp%x(3), HZ)
-              
+
               i_elm=particle_tmp%i_elm
-
-              !Particle perpendicular & parallel pressure averaging
-              !Normalized b vector
-              b_norm_r= B(1)/sqrt(B(1)**2+B(2)**2+B(3)**2)
-              b_norm_z= B(2)/sqrt(B(1)**2+B(2)**2+B(3)**2)
-              b_norm_phi= B(3)/sqrt(B(1)**2+B(2)**2+B(3)**2)
-              !Orthonormal (compared to magnetic field and eachother)
-              vr_tilde=(-(b_norm_r)*particle_tmp%v(3)+b_norm_phi*particle_tmp%v(1))/sqrt(b_norm_phi**2+b_norm_r**2)
-              vz_tilde = (particle_tmp%v(2)-b_norm_z*(b_norm_phi*particle_tmp%v(3)+b_norm_r*particle_tmp%v(1)+b_norm_z*particle_tmp%v(2)))/sqrt(b_norm_phi**2+b_norm_r**2)
-              v_par= b_norm_phi*particle_tmp%v(3)+b_norm_r*particle_tmp%v(1)+b_norm_z*particle_tmp%v(2)
-              !Parallel & perpendicular pressures
-              p_perp = 1.d0/2.d0*(vr_tilde**2+vz_tilde**2)
-              p_par = v_par**2
-              p_atrop = p_par-p_perp
-
-
 
               do l=1,n_vertex_max
                 do m=1,n_order+1
@@ -407,38 +414,41 @@ subroutine loop_particle_kinetic_local(sim, jorek_feedback, rng, timesteps, n_st
                   v = HH(l,m) * sim%fields%element_list%element(i_elm)%size(l,m)
 
                   do i_tor=1,n_tor
-      
 
-                        feedback_rhs(m,l,i_elm,i_tor,1) = feedback_rhs(m,l,i_elm,i_tor,1) &
+
+                    feedback_rhs(m,l,i_elm,i_tor,1) = feedback_rhs(m,l,i_elm,i_tor,1) &
 
                                                            + HZ(i_tor) * v * particle_tmp%weight * sim%groups(1)%mass * mass_proton &
 
                                                             * (p_perp+b_norm_r**2*p_atrop) * mu_zero !PI_RR
-                        feedback_rhs(m,l,i_elm,i_tor,2) = feedback_rhs(m,l,i_elm,i_tor,2) &
+                    feedback_rhs(m,l,i_elm,i_tor,2) = feedback_rhs(m,l,i_elm,i_tor,2) &
 
                                                             + HZ(i_tor) * v * particle_tmp%weight * sim%groups(1)%mass * mass_proton &
 
                                                             * ( p_perp+b_norm_z**2*p_atrop ) * mu_zero                       !PI_ZZ
-                        feedback_rhs(m,l,i_elm,i_tor,3) = feedback_rhs(m,l,i_elm,i_tor,3) &
+                    feedback_rhs(m,l,i_elm,i_tor,3) = feedback_rhs(m,l,i_elm,i_tor,3) &
 
                                                             + HZ(i_tor) * v * particle_tmp%weight * sim%groups(1)%mass * mass_proton &
 
                                                             * (p_perp+b_norm_phi**2*p_atrop) * mu_zero !PI_PHIPHI
-                        feedback_rhs(m,l,i_elm,i_tor,4) = feedback_rhs(m,l,i_elm,i_tor,4) &
+                    feedback_rhs(m,l,i_elm,i_tor,4) = feedback_rhs(m,l,i_elm,i_tor,4) &
 
                                                             + HZ(i_tor) * v * particle_tmp%weight * sim%groups(1)%mass * mass_proton &
 
                                                             * ( b_norm_r*b_norm_z*p_atrop ) * mu_zero                            !PI_RZ
-                       feedback_rhs(m,l,i_elm,i_tor,5) = feedback_rhs(m,l,i_elm,i_tor,5) &
+                    feedback_rhs(m,l,i_elm,i_tor,5) = feedback_rhs(m,l,i_elm,i_tor,5) &
 
                                                            + HZ(i_tor) * v * particle_tmp%weight * sim%groups(1)%mass * mass_proton &
-                     
+
                                                             * (b_norm_r*b_norm_phi*p_atrop ) * mu_zero !PI_RPHI
-                      feedback_rhs(m,l,i_elm,i_tor,6) = feedback_rhs(m,l,i_elm,i_tor,6) &
-                      
+                    feedback_rhs(m,l,i_elm,i_tor,6) = feedback_rhs(m,l,i_elm,i_tor,6) &
+
                                                            + HZ(i_tor) * v * particle_tmp%weight * sim%groups(1)%mass * mass_proton &
-                      
+
                                                             *(b_norm_z*b_norm_phi*p_atrop ) * mu_zero !PI_ZPHI
+                    feedback_rhs(m,l,i_elm,i_tor,7) = feedback_rhs(m,l,i_elm,i_tor,7) &
+
+                                                           + HZ(i_tor) * v * particle_tmp%weight  !Density
 
 
                   enddo! <tor harmonic
@@ -455,11 +465,11 @@ subroutine loop_particle_kinetic_local(sim, jorek_feedback, rng, timesteps, n_st
 
 
 
-      
+
 
       end do   ! particles
       !$omp end parallel do
-      
+
       if (sim%my_id .eq. 0) write(*,*) "End of the particle loop"
 
 
