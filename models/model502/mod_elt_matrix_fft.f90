b8f6e9ad5b82b4ef086a6fe146e6b6b3a1b92d4d
Di Hu <Faust@IBN5100.localdomain>
2022-03-18 23:00:31 +0800

    Revert "Revised the implementation of the visco_par heating"
    This reverts commit 662515d03f5dd2bbe9264c38d41ecb7e68242641.

--- models/model502/mod_elt_matrix_fft.f90
+++ models/model502/mod_elt_matrix_fft.f90
@@ -1430,7 +1430,8 @@ do ms=1, n_gauss
                     + zeta * v * Ti0 * delta_g(mp,5,ms,mt) * BigR                                      * xjac &
                     + zeta * v * alpha_i * Ti0 * delta_g(mp,8,ms,mt) * BigR                            * xjac &
 !============================Behold, the parallel viscous heating terms!=============
-                    + (GAMMA - 1.) * v * BigR * vpar0*visco_par * (vpar0_xx + vpar0_x/Bigr + vpar0_yy) * xjac * tstep &
+                    + (GAMMA - 1.) * v * BigR * visco_par * (vpar0_x * vpar0_x + vpar0_y * vpar0_y) * xjac * tstep &
+                    + (GAMMA - 1.) * vpar0 * BigR * visco_par * (v_x * vpar0_x     + v_y * vpar0_y)      * xjac * tstep &
 !==========================End of viscous heating terms==============================
 !===================== Additional terms from friction terms============
                     + v * BigR * ((GAMMA - 1.)/2.) * vpar0**2 * BB2 * (source_bg + source_imp) * xjac * tstep &
@@ -2310,8 +2311,9 @@ do ms=1, n_gauss
                        - v * BigR *(GAMMA - 1.) * vpar0 * Vpar * BB2 * (source_bg + source_imp) * xjac * theta * tstep &
 !==============================End of friction terms=================
 !============================Behold, the parallel viscous heating terms!=============
-                       - (GAMMA - 1.) * v * BigR * vpar*visco_par*(vpar0_xx + vpar0_x/Bigr + vpar0_yy)  * xjac * theta * tstep &
-                       - (GAMMA - 1.) * v * BigR * vpar0*visco_par*(vpar_xx + vpar_x/Bigr + vpar_yy)    * xjac * theta * tstep &
+                       - (GAMMA - 1.) * v * BigR * visco_par * 2.d0 * (vpar_x*vpar0_x + vpar_y*vpar0_y) * xjac * theta * tstep  &
+                       - (GAMMA - 1.) * vpar0 * BigR * visco_par    * (vpar_x*v_x     + vpar_y*v_y)     * xjac * theta * tstep  &
+                       - (GAMMA - 1.) * vpar * BigR * visco_par    * (vpar0_x*v_x     + vpar0_y*v_y)    * xjac * theta * tstep  &
 !==========================End of viscous heating terms==============================
 
                        + TG_num6 * 0.25d0 / BigR * 2.d0 * vpar0*vpar &
