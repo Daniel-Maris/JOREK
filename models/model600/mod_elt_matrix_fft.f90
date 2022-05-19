bf98cf33c0896609f79e1d536e2e89763aa61437
Di Hu <Faust@IBN5100.localdomain>
2022-05-19 11:05:05 +0800

    Fixed some bugs in the thermalization terms

--- models/model600/mod_elt_matrix_fft.f90
+++ models/model600/mod_elt_matrix_fft.f90
@@ -4930,84 +4930,80 @@ subroutine construct_thermalization_terms()
   if (ne_SI < 1.d16) ne_SI = 1.d16 ! To prevent absurd number in the coulomb lambda
 
   if (with_impurities) then
-     if (Te_corr_eV < 10.*Z_imp**2) then
-        lambda_e_imp = 23. - log((ne_SI*1.d-6)**0.5*Z_imp*Te_corr_eV**(-1.5))
-     else
-        lambda_e_imp = 24. - log((ne_SI*1.d-6)**0.5*Te_corr_eV**(-1.0))
-     endif
-     if (Te_corr_eV < 10.) then
-        lambda_e_bg  = 23. - log((ne_SI*1.d-6)**0.5*Te_corr_eV**(-1.5)) ! Assuming bg_charge is 1! 
-     else
-        lambda_e_bg  = 24. - log((ne_SI*1.d-6)**0.5*Te_corr_eV**(-1.0))
-     endif
-     nu_e_imp     = 1.8d-19*(1.d6*MASS_ELECTRON*MASS_PROTON*m_imp) ** 0.5&
-          * Z_eff_imp * (1.d14*central_density*rimp0_corr*m_i_over_m_imp) * lambda_e_imp &
-          / (1.d3*(MASS_ELECTRON*Ti0_corr+Te0_corr*MASS_PROTON*m_imp)&
-          / (EL_CHG * MU_ZERO * central_density * 1.d20)) ** 1.5
-     nu_e_bg      = 1.8d-19*(1.d6*MASS_ELECTRON*MASS_PROTON*central_mass) ** 0.5&
-          * (1.d14*central_density*(r0_corr-rimp0_corr)) * lambda_e_bg &
-          / (1.d3*(MASS_ELECTRON*Ti0_corr+Te0_corr*MASS_PROTON*central_mass)&
-          / (EL_CHG * MU_ZERO * central_density * 1.d20)) ** 1.5 ! Assuming bg_charge is 1!
+    if (Te_corr_eV < 10.*Z_imp**2) then
+       lambda_e_imp = 23. - log((ne_SI*1.d-6)**0.5*Z_imp*Te_corr_eV**(-1.5))
+    else
+       lambda_e_imp = 24. - log((ne_SI*1.d-6)**0.5*Te_corr_eV**(-1.0))
+    endif
+    if (Te_corr_eV < 10.) then
+       lambda_e_bg  = 23. - log((ne_SI*1.d-6)**0.5*Te_corr_eV**(-1.5)) ! Assuming bg_charge is 1! 
+    else
+       lambda_e_bg  = 24. - log((ne_SI*1.d-6)**0.5*Te_corr_eV**(-1.0))
+    endif
+    nu_e_imp     = 1.8d-19*(1.d6*MASS_ELECTRON*MASS_PROTON*m_imp) ** 0.5&
+         * Z_eff_imp * (1.d14*central_density*rimp0_corr*m_i_over_m_imp) * lambda_e_imp &
+         / (1.d3*(MASS_ELECTRON*Ti0_corr+Te0_corr*MASS_PROTON*m_imp)&
+         / (EL_CHG * MU_ZERO * central_density * 1.d20)) ** 1.5
+    nu_e_bg      = 1.8d-19*(1.d6*MASS_ELECTRON*MASS_PROTON*central_mass) ** 0.5&
+         * (1.d14*central_density*(r0_corr-rimp0_corr)) * lambda_e_bg &
+         / (1.d3*(MASS_ELECTRON*Ti0_corr+Te0_corr*MASS_PROTON*central_mass)&
+         / (EL_CHG * MU_ZERO * central_density * 1.d20)) ** 1.5 ! Assuming bg_charge is 1!
 
-     if (nu_e_imp < 0.) nu_e_imp = 0.
-     if (nu_e_bg < 0.)  nu_e_bg  = 0.
+    if (nu_e_imp < 0.) nu_e_imp = 0.
+    if (nu_e_bg < 0.)  nu_e_bg  = 0.
 
-     !Converting the energy transfer rate from s^-1 to JOREK unit
-     t_norm   = sqrt(MU_ZERO * central_mass * MASS_PROTON * central_density * 1.d20)
-     nu_e_imp = nu_e_imp * t_norm
-     nu_e_bg  = nu_e_bg * t_norm
+    !Converting the energy transfer rate from s^-1 to JOREK unit
+    t_norm   = sqrt(MU_ZERO * central_mass * MASS_PROTON * central_density * 1.d20)
+    nu_e_imp = nu_e_imp * t_norm
+    nu_e_bg  = nu_e_bg * t_norm
 
-     dTe_i    = (nu_e_imp + nu_e_bg) * (Ti0_corr - Te0_corr) * (r0_corr + alpha_e*rimp0_corr)
-     dTi_e    = -dTe_i
+    dTe_i    = (nu_e_imp + nu_e_bg) * (Ti0_corr - Te0_corr) * (r0_corr + alpha_e*rimp0_corr)
 
-     !Calculating the density and temperature derivative for amats
-     !We negelect the coulomb log's dericatives due to their smallness
-     dnu_e_imp_dTi   = -1.5*MASS_ELECTRON*nu_e_imp*dTi0_corr_dT &
-          / (MASS_ELECTRON*Ti0_corr + MASS_PROTON*m_imp*Te0_corr)
-     dnu_e_imp_dTe   = -1.5*MASS_PROTON*m_imp*nu_e_imp*dTe0_corr_dT &
-          / (MASS_ELECTRON*Ti0_corr + MASS_PROTON*m_imp*Te0_corr) &
-          + 1.8d-19*(1.d6*MASS_ELECTRON*MASS_PROTON*m_imp) ** 0.5&
-          * dZ_eff_imp_dT*(1.d14*central_density*rimp0_corr*m_i_over_m_imp)*lambda_e_imp &
-          / (1.d3*(MASS_ELECTRON*Ti0_corr+Te0_corr*MASS_PROTON*m_imp)&
-          / (EL_CHG * MU_ZERO * central_density * 1.d20)) ** 1.5
+    !Calculating the density and temperature derivative for amats
+    !We negelect the coulomb log's dericatives due to their smallness
+    dnu_e_imp_dTi   = -1.5*MASS_ELECTRON*nu_e_imp*dTi0_corr_dT &
+         / (MASS_ELECTRON*Ti0_corr + MASS_PROTON*m_imp*Te0_corr)
+    dnu_e_imp_dTe   = -1.5*MASS_PROTON*m_imp*nu_e_imp*dTe0_corr_dT &
+         / (MASS_ELECTRON*Ti0_corr + MASS_PROTON*m_imp*Te0_corr) &
+         + nu_e_imp * dZ_eff_imp_dT / Z_eff_imp
 
-     dnu_e_imp_drhoimp = nu_e_imp * drimp0_corr_dn / rimp0_corr
-     dnu_e_imp_drho    = 0.
+    dnu_e_imp_drhoimp = nu_e_imp * drimp0_corr_dn / rimp0_corr
+    dnu_e_imp_drho    = 0.
 
-     dnu_e_bg_dTi    = -1.5*MASS_ELECTRON*nu_e_bg*dTi0_corr_dT &
-          / (MASS_ELECTRON*Ti0_corr + MASS_PROTON*central_mass*Te0_corr)
-     dnu_e_bg_dTe    = -1.5*MASS_PROTON*central_mass*nu_e_bg*dTe0_corr_dT &
-          / (MASS_ELECTRON*Ti0_corr + MASS_PROTON*central_mass*Te0_corr)
-     if (r0_corr-rimp0_corr <= 0.) then
-        dnu_e_bg_drhoimp = 0.
-        dnu_e_bg_drho    = 0.
-     else
-        dnu_e_bg_drhoimp = -nu_e_bg * drimp0_corr_dn / (r0_corr-rimp0_corr)
-        dnu_e_bg_drho    = nu_e_bg * dr0_corr_dn / (r0_corr-rimp0_corr)
-     end if
+    dnu_e_bg_dTi    = -1.5*MASS_ELECTRON*nu_e_bg*dTi0_corr_dT &
+         / (MASS_ELECTRON*Ti0_corr + MASS_PROTON*central_mass*Te0_corr)
+    dnu_e_bg_dTe    = -1.5*MASS_PROTON*central_mass*nu_e_bg*dTe0_corr_dT &
+         / (MASS_ELECTRON*Ti0_corr + MASS_PROTON*central_mass*Te0_corr)
+    if (r0_corr-rimp0_corr <= 0.) then
+       dnu_e_bg_drhoimp = 0.
+       dnu_e_bg_drho    = 0.
+    else
+       dnu_e_bg_drhoimp = -nu_e_bg * drimp0_corr_dn / (r0_corr-rimp0_corr)
+       dnu_e_bg_drho    = nu_e_bg * dr0_corr_dn / (r0_corr-rimp0_corr)
+    end if
 
-     ddTe_i_dTi      = (dnu_e_imp_dTi+dnu_e_bg_dTi)*(Ti0_corr-Te0_corr)*(r0_corr+alpha_e*rimp0_corr)&
-          + (nu_e_imp + nu_e_bg) * dTi0_corr_dT * (r0_corr + alpha_e*rimp0_corr)
-     ddTe_i_dTe      = (dnu_e_imp_dTe+dnu_e_bg_dTe)*(Ti0_corr-Te0_corr)*(r0_corr + alpha_e*rimp0_corr)&
-          - (nu_e_imp + nu_e_bg) * dTe0_corr_dT * (r0_corr + alpha_e*rimp0_corr)        &
-          - (nu_e_imp + nu_e_bg) * Te0_corr     * dalpha_e_dT * rimp0_corr
-     ddTe_i_drhoimp  = (dnu_e_imp_drhoimp+dnu_e_bg_drhoimp)*(Ti0_corr-Te0_corr)*(r0_corr+alpha_e*rimp0_corr)&
-          +(nu_e_imp + nu_e_bg) * (Ti0_corr - Te0_corr) * alpha_e * drimp0_corr_dn
-     ddTe_i_drho     = (dnu_e_imp_drho+dnu_e_bg_drho)*(Ti0_corr-Te0_corr)*(r0_corr + alpha_e*rimp0_corr)&
-          +(nu_e_imp + nu_e_bg) * (Ti0_corr - Te0_corr) * dr0_corr_dn
+    ddTe_i_dTi      = (dnu_e_imp_dTi+dnu_e_bg_dTi)*(Ti0_corr-Te0_corr)*(r0_corr+alpha_e*rimp0_corr)&
+         + (nu_e_imp + nu_e_bg) * dTi0_corr_dT * (r0_corr + alpha_e*rimp0_corr)
+    ddTe_i_dTe      = (dnu_e_imp_dTe+dnu_e_bg_dTe)*(Ti0_corr-Te0_corr)*(r0_corr + alpha_e*rimp0_corr)&
+         - (nu_e_imp + nu_e_bg) * dTe0_corr_dT * (r0_corr + alpha_e*rimp0_corr)        &
+         + (nu_e_imp + nu_e_bg) * (Ti0_corr-Te0_corr) * dalpha_e_dT * rimp0_corr
+    ddTe_i_drhoimp  = (dnu_e_imp_drhoimp+dnu_e_bg_drhoimp)*(Ti0_corr-Te0_corr)*(r0_corr+alpha_e*rimp0_corr)&
+         +(nu_e_imp + nu_e_bg) * (Ti0_corr - Te0_corr) * alpha_e * drimp0_corr_dn
+    ddTe_i_drho     = (dnu_e_imp_drho+dnu_e_bg_drho)*(Ti0_corr-Te0_corr)*(r0_corr + alpha_e*rimp0_corr)&
+         +(nu_e_imp + nu_e_bg) * (Ti0_corr - Te0_corr) * dr0_corr_dn
 
-     ddTi_e_dTi      = -ddTe_i_dTi
-     ddTi_e_dTe      = -ddTe_i_dTe
-     ddTi_e_drhoimp  = -ddTe_i_drhoimp
-     ddTi_e_drho     = -ddTe_i_drho
-
-     if (r0_corr+alpha_e*rimp0_corr < 0.) then
-        dTi_e         = 0.
-        ddTi_e_dTi    = 0.
-        ddTe_i_dTe    = 0.
-        ddTi_e_drhoimp= 0.
-        ddTi_e_drho   = 0.
-     end if
+    if (r0_corr+alpha_e*rimp0_corr < 0.) then
+      dTe_i         = 0.
+      ddTe_i_dTi    = 0.
+      ddTe_i_dTe    = 0.
+      ddTe_i_drhon  = 0.
+      ddTe_i_drho   = 0.
+    end if
+    dTi_e    = -dTe_i
+    ddTi_e_dTi      = -ddTe_i_dTi
+    ddTi_e_dTe      = -ddTe_i_dTe
+    ddTi_e_drhon    = -ddTe_i_drhon
+    ddTi_e_drho     = -ddTe_i_drho
   else
      Te_corr_eV     = Te0_corr/(EL_CHG*MU_ZERO*central_density*1.d20)
      dTe_corr_eV_dT = dTe0_corr_dT/(EL_CHG*MU_ZERO*central_density*1.d20)
