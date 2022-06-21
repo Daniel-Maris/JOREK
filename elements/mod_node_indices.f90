dfc11f98a4320c1d74032a134d66f45eb7e5d5f2
Stanislas Pamela <stanislas.pamela@ukaea.uk>
2022-06-21 09:43:40 +0200

    update following Matthias comments

--- elements/mod_node_indices.f90
+++ elements/mod_node_indices.f90
@@ -1,13 +1,30 @@
-!> With bi-cubic elements, ordering is easy, 1-4 nodes.
-!> With n_order=5 and higher, ordering becomes quite complex
-!> This module contains a set of routines to find the indices for you
+!> This module takes care of the relation between the 2D-indexing of our FEM vectorial basis,
+!> (which is directly related to Bezier control points and (s,t)-derivatives)
+!> and the matrix indexing, defined in the structure
+!> node_list%node(i_node)%index(i_index)
+!> With bi-cubic elements, ordering of the matrix index at each node is trivial, 1-4 nodes.
+!> At each node, the 4 control points, with (s,t)-indices (1,1), (1,2), (2,1), (2,2)
+!> correspond to matrix indices
+!>
+!>              (s,t)-indices                             matrix-index                                  (s,t)-derivatives
+!>                                                                                             
+!>      (1,2) --------------- (2,2)                 (3) -------------------- (4)                dpsi/dt ------------------ d2psi/dsdt
+!>        |                     |                    |                        |                    |                        |
+!>        |                     |                    |                        |                    |                        |
+!>        |                     |        ==>         |                        |        ==>         |                        |
+!>        |                     |                    |                        |                    |                        |
+!>      (1,1) --------------- (2,1)                 (1) -------------------- (2)                  psi -------------------- dpsi/ds
+!>
+!> With n_order=5 and higher, this ordering quickly becomes very complex
+!> This module contains a set of routines to find the matrix-index given the (s,t)-indices,
+!> and vice versa, the (s,t)-indices given the matrix index.
 module mod_node_indices
 
 use mod_parameters
 
 contains
 
-!> calculate the node indices as a function of (s,t)-coord indices
+!> calculate the node matrix indices as a function of (s,t)-coord indices
 subroutine calculate_node_indices(node_indices)
 
   implicit none
@@ -39,7 +56,7 @@ subroutine calculate_node_indices(node_indices)
 end subroutine calculate_node_indices
 
 
-!> given the node index, find the (s,t)-coord indices of a node
+!> given the node matrix index, find the (s,t)-coord indices of a node
 subroutine get_node_coords_from_index(node_indices, index, i, j)
 
   implicit none
