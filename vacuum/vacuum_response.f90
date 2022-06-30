e5706f2b4057fbffbfe8a8d6cccfc2285d2e59d1
Di Hu <Faust@IBN5100.localdomain>
2022-06-30 19:45:46 +0800

    Revised the fetching of the size of character

--- vacuum/vacuum_response.f90
+++ vacuum/vacuum_response.f90
@@ -335,7 +335,7 @@ module vacuum_response
       disp = disp + sizeof(float2d)
 
     else if (present(char1d)) then
-      str_len = dim(1) * sizeof(char1d)
+      str_len = dim(1) * sizeof('a')
       if ( allocated(tmp_char1d) ) deallocate( tmp_char1d ); allocate( tmp_char1d(str_len))
       if ( allocated(char1d) ) deallocate( char1d ); allocate( char1d(dim(1)))
       call MPI_FILE_READ(filehandle, tmp_char1d, str_len, MPI_CHARACTER  ,status,ierr)
