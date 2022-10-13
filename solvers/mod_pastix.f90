30bae6d582f0254288121acdaecd51e0e573cfe2
Ihor Holod <ihor.holod@ipp.mpg.de>
2022-10-13 15:21:11 +0200

    moved matrix preparation into pastix_set_mat

--- solvers/mod_pastix.f90
+++ solvers/mod_pastix.f90
@@ -150,7 +150,117 @@ module mod_pastix
 
   contains
   
-  subroutine pastix_set_mat(ptss, a_mat)
+  subroutine pastix_set_mat(ptss, ad_mat, ac_mat)
+    use mpi_mod
+    use mod_clock
+    use mod_coicsr
+    use data_structure, only: type_SP_MATRIX
+    
+    implicit none
+    
+    type(type_PASTIX_SOLVER)          :: ptss
+    type(type_SP_MATRIX)              :: ad_mat, ac_mat
+    type(clcktype)                    :: t_itstart, t0, t1, t2, t3
+    real*8                            :: tsecond
+    integer                           :: n_cpu, my_id, ierr, comm
+    integer                           :: i, j
+    integer(kind=int_all)             :: k, nnz
+    integer*8                         :: check_data
+    integer                           :: block_size2
+    integer(kind=int_all)             :: nblock, nnz_block
+    integer(kind=int_all), allocatable         :: sparskit_work(:)
+    
+    comm = ad_mat%comm
+  
+    call MPI_COMM_RANK(comm, my_id, ierr)
+    call MPI_COMM_SIZE(comm, n_cpu, ierr)    
+  
+    if (.not.ptss%equilibrium) then
+    
+      call scale_by_cols(ad_mat)
+      if (associated(ptss%solution_scaling)) then
+        deallocate(ptss%solution_scaling); ptss%solution_scaling => Null()
+      endif
+      allocate(ptss%solution_scaling(ad_mat%ng))
+      do k = 1, ad_mat%ng
+        ptss%solution_scaling(k) = ad_mat%column_scaling(k)
+      enddo
+      ptss%scaled = .true.
+      
+    endif
+    
+    if (n_cpu>1) then
+    
+      call MPI_Allreduce(ad_mat%nnz,ac_mat%nnz,1,MPI_INTEGER_ALL,MPI_SUM,comm,ierr)
+      
+      ac_mat%ng = ad_mat%ng
+      ac_mat%block_size = ad_mat%block_size
+      ac_mat%comm = ad_mat%comm  
+      
+      allocate(ac_mat%irn(ac_mat%nnz))
+      allocate(ac_mat%jcn(ac_mat%nnz))
+      allocate(ac_mat%val(ac_mat%nnz))
+      
+      call clck_time(t0)
+      
+      call split_allgathersolve(n_cpu,my_id,ad_mat,ac_mat)
+      
+      call clck_time(t1); call clck_ldiff(t0,t1,tsecond)
+      if (my_id .eq. 0)  write(*,FMT_TIMING) my_id, '## Elapsed time mpi_gather :', tsecond
+      
+    else
+      ac_mat = ad_mat
+    endif
+    
+    call clck_time(t0)
+  
+    block_size2 = ac_mat%block_size**2
+    
+    nblock   = ac_mat%ng/ac_mat%block_size
+    nnz_block = ac_mat%nnz/block_size2
+    
+    ac_mat%nblock = nblock
+    ac_mat%nzblock = nnz_block
+    
+    if (ac_mat%block_size > 1) then
+      do i = 1,nnz_block  
+        ac_mat%irn(i) = (ac_mat%irn((i-1)*block_size2+1) - 1)/ac_mat%block_size + 1 
+        ac_mat%jcn(i) = (ac_mat%jcn((i-1)*block_size2+1) - 1)/ac_mat%block_size + 1 
+      enddo
+    endif
+    
+    allocate(sparskit_work(nblock+1))
+    
+    call coicsr2(nblock,nnz_block,ac_mat%val,ac_mat%irn(1:nnz_block),ac_mat%jcn(1:nnz_block),ac_mat%block_size,sparskit_work)
+    
+    deallocate(sparskit_work)
+    
+    call clck_time(t1)
+    call clck_ldiff(t0,t1,tsecond)
+    if (my_id .eq. 0)  write(*,FMT_TIMING) my_id, '## Elapsed time coicsr :', tsecond
+    
+    ! End of matrix preparation
+    ptss%irn => ac_mat%irn
+    ptss%jcn => ac_mat%jcn
+    ptss%val => ac_mat%val
+    ptss%comm = ac_mat%comm
+    ptss%nblock = ac_mat%nblock
+    ptss%iparm(IPARM_DOF_NBR) = ac_mat%block_size
+    
+    if (ptss%equilibrium) then
+    ! combine duplicated values
+      nnz = ac_mat%jcn(ac_mat%ng + 1) - 1
+      call pastix_fortran_checkmatrix(check_data, ac_mat%comm, &
+       Int1, ptss%sym, Int1, ac_mat%ng, ac_mat%jcn, ac_mat%irn, ac_mat%val, -Int1, Int1)
+
+      ac_mat%nnz = ac_mat%jcn(ac_mat%ng+1) - 1
+      if (ac_mat%nnz < nnz) then
+         call pastix_fortran_checkmatrix_end(check_data, Int1, ac_mat%irn, ac_mat%val, Int1)
+      endif
+    endif
+    
+    return
+  
   end subroutine pastix_set_mat
   
   subroutine pastix_initialize(ptss)
@@ -164,10 +274,7 @@ module mod_pastix
     type(clcktype)                    :: t_itstart, t0, t1, t2, t3
     real*8                            :: tsecond
     
-    allocate(ptss%perm_vars(ptss%nblock))
-    allocate(ptss%iperm_vars(ptss%nblock))
-    ptss%perm_vars(1:ptss%nblock) = 0
-    ptss%iperm_vars(1:ptss%nblock) = 0    
+  
     
     call MPI_COMM_RANK(ptss%comm, my_id, ierr)
     
@@ -202,7 +309,7 @@ module mod_pastix
     ptss%iparm(IPARM_THREAD_COMM_MODE)      = API_THREAD_MULTIPLE
 #endif
   
-    ptss%initialized = .true.      
+    ptss%initialized = .true.
     
     return
     
@@ -266,6 +373,11 @@ module mod_pastix
     call MPI_COMM_RANK(ptss%comm, my_id, ierr)
     
     if (my_id .eq. 0) write(*,*) "PaStiX solver: analyzing matrix"
+    
+    allocate(ptss%perm_vars(ptss%nblock))
+    allocate(ptss%iperm_vars(ptss%nblock))
+    ptss%perm_vars(1:ptss%nblock) = 0
+    ptss%iperm_vars(1:ptss%nblock) = 0      
     
     ptss%iparm(IPARM_START_TASK) = API_TASK_ORDERING
     ptss%iparm(IPARM_END_TASK)   = API_TASK_ANALYSE
