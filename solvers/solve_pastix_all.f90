25a045d90b56552435733190b99411ebbf05c585
Ihor Holod <ihor.holod@ipp.mpg.de>
2022-10-17 14:05:27 +0200

    vome rhs assignment before init as pastix fails to deal with null pointers

--- solvers/solve_pastix_all.f90
+++ solvers/solve_pastix_all.f90
@@ -3,15 +3,15 @@
 !  distributed matrix ad_mat on the main group mpi_comm_world.
 !  For pastix5 solver matrix is centralized into ac_mat
 subroutine solve_pastix_all(ptss, ad_mat, rhs_vec, solve_only)
-  use tr_module 
+  use tr_module
   use mod_parameters, only: n_tor, n_var
   use mpi_mod
   use mod_clock
-  
+
   use mod_integer_types
   use data_structure, only: type_SP_MATRIX, type_RHS
   use mod_pastix, only:     type_PASTIX_SOLVER, pastix_set_mat, pastix_solve, pastix_factorize, pastix_analyze, pastix_initialize
-   
+
   implicit none
 
   type(clcktype)                    :: t_itstart, t0, t1, t2, t3
@@ -21,21 +21,23 @@ subroutine solve_pastix_all(ptss, ad_mat, rhs_vec, solve_only)
   type(type_RHS)                    :: rhs_vec
   type(type_PASTIX_SOLVER)          :: ptss
   logical                           :: solve_only
-  
+
   comm = ad_mat%comm
-  
+
   call MPI_COMM_RANK(comm, my_id, ierr)
   call MPI_COMM_SIZE(comm, n_cpu, ierr)
-  
+
   if (.not.solve_only) then
-  
+
     !write(*,*) my_id,'*********************************'
     !write(*,*) my_id,'*  solve global matrix (PaStiX) *'
     !write(*,*) my_id,'*********************************'
-        
+
+    ptss%rhs_val => rhs_vec%val
+
 #ifdef USE_PASTIX
     call pastix_set_mat(ptss, ad_mat, ac_mat)
-    
+
     if (.not. ptss%initialized) then
       ptss%comm = ad_mat%comm
       call pastix_initialize(ptss)
@@ -45,45 +47,43 @@ subroutine solve_pastix_all(ptss, ad_mat, rhs_vec, solve_only)
       ptss%comm = ad_mat%comm
       call pastix_initialize(ptss)
     endif
-    
-    call pastix_set_mat(ptss, ad_mat, ac_mat)          
+
+    call pastix_set_mat(ptss, ad_mat, ac_mat)
 #endif
-    
-    ptss%rhs_val => rhs_vec%val
-    
+
     if (.not. ptss%analyzed) then
       if (my_id .eq. 0) write(*,*) "PaStiX: analyzing matrix"
       call clck_time(t0)
-      
+
       call pastix_analyze(ptss)
-      
+
       call clck_time(t1); call clck_ldiff(t0,t1,tsecond)
-      if (my_id .eq. 0)  write(*,FMT_TIMING) my_id, '## Elapsed time analysis:', tsecond      
+      if (my_id .eq. 0)  write(*,FMT_TIMING) my_id, '## Elapsed time analysis:', tsecond
     endif
-    
+
     if (my_id .eq. 0) write(*,*) "PaStiX: factorizing matrix"
     call clck_time(t0)
-    
+
     call pastix_factorize(ptss)
-    
+
     call clck_time(t1); call clck_ldiff(t0,t1,tsecond)
     if (my_id .eq. 0)  write(*,FMT_TIMING) my_id, '## Elapsed time factorization:', tsecond
-    
+
     if (n_cpu>1) then
       deallocate(ac_mat%irn)
       deallocate(ac_mat%jcn)
       deallocate(ac_mat%val)
     endif
-   
+
   endif
-  
+
   call clck_time(t0)
-  
+
   call pastix_solve(ptss,rhs_vec)
-    
+
   call clck_time(t1); call clck_ldiff(t0,t1,tsecond)
   if (my_id .eq. 0)  write(*,FMT_TIMING) my_id, '## Elapsed time solve:', tsecond
-  
+
   return
 end
 #endif
