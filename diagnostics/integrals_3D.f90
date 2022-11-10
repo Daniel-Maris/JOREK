852b339d56839625b67370fca9319a5a8cdd0d05
Cristian Sommariva <cristian.sommariva@epfl.ch>
2022-11-10 15:37:43 +0100

    fix omp declaration of local_n_particles_inj and local_nparticles

--- diagnostics/integrals_3D.f90
+++ diagnostics/integrals_3D.f90
@@ -153,11 +153,13 @@ endif
 !$omp          central_density, pellet_particles,pellet_density, pellet_volume,                &
 !$omp          local_pellet_particles, local_plasma_particles, local_pellet_volume,            &
 #if (defined WITH_Neutrals) && (!defined WITH_Impurities)
-!$omp          local_n_particles_inj, local_n_particles, ns_amplitude, ns_R, ns_Z,             &
-!$omp          ns_phi, ns_radius, ns_deltaphi, ns_tor_norm, spi_tor_rot,                       &
-!$omp          t_now, A_Dmv, K_Dmv, V_Dmv, P_Dmv, t_ns, L_tube, JET_MGI,ASDEX_MGI,             &
-!$omp          central_mass, pellets, tor_frequency,                                           &
+!$omp          ns_amplitude, ns_R, ns_Z, ns_phi, ns_radius, ns_deltaphi, ns_tor_norm           &
+!$omp          spi_tor_rot, t_now, A_Dmv, K_Dmv, V_Dmv, P_Dmv, t_ns, L_tube,                   &
+!$omp          JET_MGI,ASDEX_MGI, central_mass, pellets, tor_frequency,                        &
 !$omp          ns_radius_ratio, ns_radius_min, spi_shard_file,                                 &
+#endif
+#if (defined WITH_Neutrals)
+!$omp          local_n_particles_inj, local_n_particles,                                       &
 #endif
 !$omp          wgauss_copy,varmin,varmax)                                                      &
 !$omp   private(ife,iv,inode,element,nodes,i,j, k,in, mp, ms, mt, spi_i,                       &
