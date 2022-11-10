421ea04d31bae6fd1a29ab1c9aa1c15c5030bfe5
Cristian Sommariva <cristian.sommariva@epfl.ch>
2022-11-10 20:57:12 +0100

    modified integrals_3D for wired segmentation fault for test case pellet_303_aug

--- diagnostics/integrals_3D.f90
+++ diagnostics/integrals_3D.f90
@@ -158,7 +158,7 @@ endif
 !$omp          JET_MGI,ASDEX_MGI, central_mass, pellets, tor_frequency,                        &
 !$omp          ns_radius_ratio, ns_radius_min, spi_shard_file,                                 &
 #endif
-#if (defined WITH_Neutrals)
+#ifdef WITH_Neutrals
 !$omp          local_n_particles_inj, local_n_particles,                                       &
 #endif
 !$omp          wgauss_copy,varmin,varmax)                                                      &
@@ -191,7 +191,8 @@ omp_tid      = 0
 #endif
 !$omp                D_int, D_ext, P_int, H_int, S_int, H_ext, S_ext, P_ext, C_intern, C_ext, &
 !$omp                TVP_int, TVP_ext, TVP_tot, VP_int, VP_ext, VP_tot, VK_tot, VK_int, VK_ext, VM_ext,                  &
-!$omp                VM_int, VM_tot, Vol, P_tot, D_tot,J2_tot, J2_int, J2_ext)
+!$omp                VM_int, VM_tot, Vol, P_tot, D_tot,J2_tot, J2_int, J2_ext)                &
+!$omp reduction(min:varmin) reduction(max:varmax)
 
 do ife = ife_min, ife_max
 
@@ -251,12 +252,10 @@ do ife = ife_min, ife_max
 
   ! --- Determine smallest and largest values of the variables in the whole domain (on Gauss points and toroidal integration
   ! surfaces)
-  !$omp critical
   do k=1,n_var
     varmin(k) = min(varmin(k),minval(eq_g(:,k,:,:)))
     varmax(k) = max(varmax(k),maxval(eq_g(:,k,:,:))) 
   enddo
-  !$omp end critical
 
   do ms=1, n_gauss
     do mt=1, n_gauss
