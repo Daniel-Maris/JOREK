2bd18e414c3e894be3290823e79c77de68b34319
Cristian Sommariva <cristian.sommariva@epfl.ch>
2022-11-11 11:21:12 +0100

    bugfix of unidentify issue with pellet_module

--- models/pellet_module.f90
+++ models/pellet_module.f90
@@ -149,9 +149,9 @@ module pellet_module
     
     if (pellet_amplitude .gt. 0) return
     
-          call Integrals_3D(my_id, node_list,element_list,density,density_in,density_out,pressure,pressure_in,pressure_out, &
-                                                          kin_par_tot, kin_par_in, kin_par_out, mom_par_tot, mom_par_in, mom_par_out)
-                                                          
+          call  Integrals_3D(my_id,node_list,element_list,density,density_in,density_out,pressure,pressure_in,pressure_out, &
+                        kin_par_tot,kin_par_in,kin_par_out,mom_par_tot,mom_par_in,mom_par_out)
+                                                      
     V_normalisation = 1.d0 / sqrt(central_density * 1d20 * mass_proton * central_mass * MU_ZERO)
     
     pellet_R = pellet_R + pellet_velocity_R * tstep / V_normalisation
