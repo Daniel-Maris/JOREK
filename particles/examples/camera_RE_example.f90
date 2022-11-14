a722a9e91c14e0db562a294f1fcaa9f65a6a0d08
Cristian Sommariva <cristian.sommariva@epfl.ch>
2022-11-14 11:59:00 +0100

    template for synthetic imaging system example

---
+++ particles/examples/camera_RE_example.f90
@@ -0,0 +1,15 @@
+!> The camera_example.f90 contains an executable programme which
+!> tests and shows how to run the synthetic camera diagnostic
+!> for generating images of plasma radiation from a JORE-particle
+!> population. The test proposed below concerns the imaging of the
+!> sychrotron radiation of a runaway electron population.
+!> More specifically, we consider the JET tokamak configuration
+!> having an IR and fast visible camera looking at the RE beam
+!> from octant 5. Note that the RE toroidal motion
+!> is counter-clockeise. Only one snapshot of the RE beam is 
+!> considered hereater hence, only one particle population and 
+!> JOREK MHD fields are used.
+program camera_RE_example
+implicit none
+write(*,*) 'Hello world'
+end program camera_RE_example
