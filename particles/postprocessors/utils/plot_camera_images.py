5b40ffccfaccab98e224a1da99e3a487f204d637
Cristian Sommariva <cristian.sommariva@epfl.ch>
2022-11-16 14:31:37 +0100

    added python tool for plotting the camera images

---
+++ particles/postprocessors/utils/plot_camera_images.py
@@ -0,0 +1,43 @@
+# --------------------------------------------------------------- #
+# Read and plot the camera pixel and filter intensities HDF5 file
+# --------------------------------------------------------------- #
+
+import h5py
+import numpy as np
+from matplotlib import pyplot as plt
+
+# Data ---------------------------------------------------------- #
+filepath = "/home/sommariv/jorek_simulations/test_synchrotron_soft_pulse95135_press0_parabolicq_q95_6dot8_res1r5dot88m5_res2r4dot705m4_Ip612en1MA_fast_sim"
+filename = "pixel_filter_intensities.h5"
+datasetname = "pixel_filter_intensities"
+separator = "/"
+
+# Initialisation ------------------------------------------------ #
+# Read image
+fhandler = h5py.File("".join([filepath,separator,filename]),'r')
+pixel_filter_intensities = np.array(fhandler[datasetname])
+fhandler.close()
+
+# Reorder pixel and filter intensities for plotting
+pixel_filter_intensities = np.transpose(pixel_filter_intensities,axes=[3,4,0,1,2])
+pixel_intensities = pixel_filter_intensities[0]
+filter_intensities = pixel_filter_intensities[1]
+
+# Plot pixel and filter intensities ----------------------------- #
+for frame_id,frame in enumerate(pixel_intensities):
+  for spectrum_id,spectrum in enumerate(frame):
+    plt.figure()
+    plt.imshow(spectrum)
+    plt.title("".join(['Image for spectrum N# ',str(spectrum_id+1),\
+    ' time N#: ',str(frame_id+1)]))
+
+for img_filter_id,img_filter in enumerate(filter_intensities):
+  for spectrum_id,spectrum in enumerate(img_filter):
+    plt.figure()
+    plt.imshow(spectrum)
+    plt.title("".join(['Filter for spectrum N# ',str(spectrum_id+1),\
+    ' time N#: ',str(img_filter_id+1)]))
+
+plt.show()
+
+# --------------------------------------------------------------- #
