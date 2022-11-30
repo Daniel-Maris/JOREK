bd4817a8f89d897cd31e83acc2e91f29bd126130
hudi <hudi2@buaa.edu.cn>
2022-11-30 09:54:28 +0800

    Changed the var_rhoimp ordering to be consistent with n_var_ext(n_mod_ext)

--- models/model600/mod_model_settings.f90
+++ models/model600/mod_model_settings.f90
@@ -60,8 +60,8 @@ integer, parameter :: var_Ti       = sum(merge((/                               
 integer, parameter :: var_Te       = sum(merge((/n_var_base+n_var_vpar         +1/), (/0/), with_TiTe      ))
 integer, parameter :: var_Vpar     = sum(merge((/                               7/), (/0/), with_vpar      ))
 integer, parameter :: var_rhon     = sum(merge((/n_var_base+sum(n_var_ext(1:2))+1/), (/0/), with_neutrals  ))
-integer, parameter :: var_nre      = sum(merge((/n_var_base+sum(n_var_ext(1:3))+1/), (/0/), with_refluid   ))
-integer, parameter :: var_rhoimp   = sum(merge((/n_var_base+sum(n_var_ext(1:4))+1/), (/0/), with_impurities))
+integer, parameter :: var_rhoimp   = sum(merge((/n_var_base+sum(n_var_ext(1:3))+1/), (/0/), with_impurities))
+integer, parameter :: var_nre      = sum(merge((/n_var_base+sum(n_var_ext(1:4))+1/), (/0/), with_refluid   ))
 ! --- variables not relevant to this model
 integer, parameter :: var_AR   = 0
 integer, parameter :: var_AZ   = 0
