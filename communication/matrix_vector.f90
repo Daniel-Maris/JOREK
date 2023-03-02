9a7d396e2a38408b90eb77a8a58dc6332538d21c
Ihor Holod <ihor.holod@ipp.mpg.de>
2023-03-02 10:18:55 +0100

    fixed bug in matrix_vector

--- communication/matrix_vector.f90
+++ communication/matrix_vector.f90
@@ -3,40 +3,29 @@
 subroutine matrix_vector(x_vec, a_mat, y_vec)
   use mpi_mod
   use mod_integer_types
-  use mod_parameters, only: n_tor
   use data_structure, only: type_SP_MATRIX, type_RHS
 
   implicit none
   
   type(type_SP_MATRIX)  :: a_mat
-  type(type_RHS)        :: x_vec, y_vec
-
-  logical, parameter :: PRINT_TIMING_INFO = .false.
+  type(type_RHS)        :: x_vec
+  type(type_RHS)        :: y_vec
 
   ! --- Local variables
   real*8                :: t1, t2, t3, t4, t5
-  real*8, allocatable   :: y_tmp(:)
-  real*8                :: y_tmp_block(a_mat%block_size)
+  real*8, allocatable   :: y_tmp(:), y_tmp_block(:)
   integer,allocatable   :: recv_counts(:), recv_disp(:)
   integer               :: n, i, ir, jc
   integer               :: my_id, n_cpu, ierr, counts
   integer(kind=int_all) :: n_blocksize, n_blocks, iA_start, ix_start, iy_start, index_offset, Int_tmp
   integer               :: ndof_local
+  external              :: dgemv
   
-  if ( PRINT_TIMING_INFO ) then
-    call cpu_time(t1)
-    call MPI_Barrier(MPI_COMM_WORLD,ierr)
-    call cpu_time(t2)
-  end if
-
   call MPI_COMM_SIZE(a_mat%comm, n_cpu, ierr)
   call MPI_COMM_RANK(a_mat%comm, my_id, ierr) 
 
   counts = x_vec%n
-  call MPI_BCAST(x_vec%val,counts,MPI_DOUBLE_PRECISION,0,a_mat%comm,ierr)
-
-  if ( PRINT_TIMING_INFO ) call cpu_time(t3)
-
+  if (.not.associated(y_vec%val)) allocate(y_vec%val(y_vec%n))
   y_vec%val(1:y_vec%n)  = 0.d0
   n_blocksize  = a_mat%block_size
   n_blocks     = a_mat%nnz/n_blocksize**2
@@ -46,61 +35,32 @@ subroutine matrix_vector(x_vec, a_mat, y_vec)
   
   allocate(y_tmp(ndof_local))
   y_tmp        = 0.d0
+  allocate(y_tmp_block(a_mat%block_size))
   
 ! --- The actual matrix vector multiplication uses dense matrix-vector products for the small
 !     dense blocks within our sparse matrix. The size of these blocks depends on n_tor. Depending on
 !     this block size (so depending on n_tor), two slightly different kernels are implemented.
 !     Warning: this simply seg-faults with long-integers.
-#ifndef INTSIZE64
-  if ( n_tor <= 7 ) then
-
-!$omp parallel default(none) &
-!$omp   shared(a_mat, x_vec, n_blocks, n_blocksize, index_offset) &
-!$omp   private(i, iA_start, ix_start, iy_start, ir, jc, y_tmp_block ) &
-!$omp   reduction(+:y_tmp)
-!$omp do schedule(guided)
-    do i = 1, n_blocks
-
-      iA_start = (i-1) * n_blocksize**2
-      ix_start = a_mat%jcn(iA_start+1)
-      iy_start = a_mat%irn(iA_start+1) - index_offset
-
-      call dgemv('T', n_blocksize, n_blocksize, 1.d0, a_mat%val(iA_start+1), n_blocksize, x_vec%val(ix_start), Int1, 0.d0, y_tmp_block, Int1)
-
-      y_tmp(iy_start:iy_start+n_blocksize-1) = y_tmp(iy_start:iy_start+n_blocksize-1) + y_tmp_block(1:n_blocksize)
-
-    end do
-!$omp end do
-!$omp end parallel
-  else ! ... so in case n_tor is larger than 7
-#endif
 
 !$omp parallel default(none) &
 !$omp   shared(y_tmp, a_mat, x_vec, n_blocks, n_blocksize, index_offset)  &
 !$omp   private(i,iA_start,ix_start, iy_start, ir, jc, y_tmp_block)
 !$omp do schedule(guided)
-    do i = 1, n_blocks
+  do i = 1, n_blocks
 
-      iA_start = (i-1) * n_blocksize**2
-      ix_start = a_mat%jcn(iA_start+1)
-      iy_start = a_mat%irn(iA_start+1) - index_offset
+    iA_start = (i-1) * n_blocksize**2
+    ix_start = a_mat%jcn(iA_start+1)
+    iy_start = a_mat%irn(iA_start+1) - index_offset
 
-      call dgemv('T',n_blocksize,n_blocksize,1.d0,a_mat%val(iA_start+1),n_blocksize,x_vec%val(ix_start),Int1,0.d0,y_tmp_block,Int1)
+    call dgemv('T',n_blocksize,n_blocksize,1.d0,a_mat%val(iA_start+1),n_blocksize,x_vec%val(ix_start),Int1,0.d0,y_tmp_block,Int1)
 
 !$omp critical
-      y_tmp(iy_start:iy_start+n_blocksize-1) = y_tmp(iy_start:iy_start+n_blocksize-1) + y_tmp_block(1:n_blocksize)
+    y_tmp(iy_start:iy_start+n_blocksize-1) = y_tmp(iy_start:iy_start+n_blocksize-1) + y_tmp_block(1:n_blocksize)
 !$omp end critical
 
-    end do
+  enddo
 !$omp end do
 !$omp end parallel
-
-#ifndef INTSIZE64
-  end if
-! --- End: Two different kernels for matrix-vector multiplication depending on n_tor
-#endif
-
-  if ( PRINT_TIMING_INFO ) call cpu_time(t4)
 
   y_vec%val(1:y_vec%n) = 0.d0
 
@@ -117,12 +77,11 @@ subroutine matrix_vector(x_vec, a_mat, y_vec)
      recv_disp(i) = recv_disp(i-1) + recv_counts(i-1)
   enddo
 
-  call mpi_allgatherv(y_tmp, ndof_local, MPI_DOUBLE_PRECISION, y_vec, recv_counts, recv_disp, MPI_DOUBLE_PRECISION, a_mat%comm,ierr)
+  call mpi_allgatherv(y_tmp, ndof_local, MPI_DOUBLE_PRECISION, y_vec%val, recv_counts, recv_disp, MPI_DOUBLE_PRECISION, a_mat%comm,ierr)
   
-  deallocate(y_tmp)
+  deallocate(y_tmp,y_tmp_block)
   deallocate(recv_counts)
   deallocate(recv_disp)
-
  
   return
 
