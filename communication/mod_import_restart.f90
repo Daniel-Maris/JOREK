c60a07f443c68954f3dece0e24fdd05d3756fd01
Matthias Hoelzl <mhoelzl@ipp.mpg.de>
2023-03-03 16:45:37 +0100

    Adding a flag to avoid calculating LCFS shape parameters when the equilibrium hasn't been initialized

--- communication/mod_import_restart.f90
+++ communication/mod_import_restart.f90
@@ -826,6 +826,8 @@ endif
   if (allocated(deltas_tmp)) call tr_deallocate(deltas_tmp,"deltas_tmp",CAT_UNKNOWN)
 
   call populate_element_rtree(node_list, element_list)
+  
+  equil_initialized = .true.
 
   return
 end subroutine import_binary_restart
@@ -2029,6 +2031,8 @@ subroutine import_hdf5_restart(node_list, element_list, filename, format_rst, er
   write (6,*) " ERROR: trying to import with hdf5 but USE_HDF5 was not set at compile-time"
 #endif
   call populate_element_rtree(node_list, element_list)
+
+  equil_initialized = .true.
 
   return
 end subroutine import_hdf5_restart
