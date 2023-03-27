dc7ca7d94c745d2193317d068fd024a6f48765f6
Ihor Holod <ihor.holod@ipp.mpg.de>
2023-03-27 13:16:45 +0200

    fixed solve_only condition for Newton solver

--- solvers/mod_newton_solver.f90
+++ solvers/mod_newton_solver.f90
@@ -58,7 +58,7 @@ module mod_newton_solver
     solve_tol = solver%iter_tol
 
     newton_loop: do inewton=1, solver%newton%maxNewton
-
+      solver%newton%it = inewton
       if (inewton.gt.1) call matrix_vector(deln, a_mat, rhsk)
       normRHSprevious = normRHScurrent
       rhsk%val(1:rhsk%n) = rhs_vec%val(1:rhs_vec%n) - rhsk%val(1:rhsk%n)
@@ -102,7 +102,6 @@ module mod_newton_solver
 
       solver%iter_tol = tol_Gmres
       call solve_sparse_system(a_matk, rhsk, deltas, solver, mhd_sim)
-      solver%solve_only = .true.
 
       if (.not.solver%step_success) return
 
