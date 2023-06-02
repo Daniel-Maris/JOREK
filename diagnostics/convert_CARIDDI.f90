2131bf87c409a854cc62618130af10de6703c401
Matthias Hoelzl <mhoelzl@ipp.mpg.de>
2023-06-02 08:34:24 +0200

    Adding first povray parts to convert_CARIDDI (8)

--- diagnostics/convert_CARIDDI.f90
+++ diagnostics/convert_CARIDDI.f90
@@ -501,9 +501,21 @@ subroutine write_hexahedron_as_triangles_pov(ifile, x, y, z, rgbt)
   
   call write_triangle_pov(ifile, (/x(1), x(2), x(3)/), (/y(1), y(2), y(3)/), (/z(1), z(2), z(3)/), rgbt)
   call write_triangle_pov(ifile, (/x(1), x(3), x(4)/), (/y(1), y(3), y(4)/), (/z(1), z(3), z(4)/), rgbt)
+  
   call write_triangle_pov(ifile, (/x(5), x(6), x(7)/), (/y(5), y(6), y(7)/), (/z(5), z(6), z(7)/), rgbt)
   call write_triangle_pov(ifile, (/x(5), x(7), x(8)/), (/y(5), y(7), y(8)/), (/z(5), z(7), z(8)/), rgbt)
-  ! ### ...
+  
+  call write_triangle_pov(ifile, (/x(1), x(2), x(5)/), (/y(1), y(2), y(5)/), (/z(1), z(2), z(5)/), rgbt)
+  call write_triangle_pov(ifile, (/x(2), x(5), x(6)/), (/y(2), y(5), y(6)/), (/z(2), z(5), z(6)/), rgbt)
+  
+  call write_triangle_pov(ifile, (/x(2), x(3), x(6)/), (/y(2), y(3), y(6)/), (/z(2), z(3), z(6)/), rgbt)
+  call write_triangle_pov(ifile, (/x(3), x(6), x(7)/), (/y(3), y(6), y(7)/), (/z(3), z(6), z(7)/), rgbt)
+  
+  call write_triangle_pov(ifile, (/x(3), x(4), x(7)/), (/y(3), y(4), y(7)/), (/z(3), z(4), z(7)/), rgbt)
+  call write_triangle_pov(ifile, (/x(4), x(7), x(8)/), (/y(4), y(7), y(8)/), (/z(4), z(7), z(8)/), rgbt)
+  
+  call write_triangle_pov(ifile, (/x(4), x(1), x(8)/), (/y(4), y(1), y(8)/), (/z(4), z(1), z(8)/), rgbt)
+  call write_triangle_pov(ifile, (/x(1), x(8), x(5)/), (/y(1), y(8), y(5)/), (/z(1), z(8), z(5)/), rgbt)
   
 end subroutine write_hexahedron_as_triangles_pov
 
