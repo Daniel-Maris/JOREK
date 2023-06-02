e96765f1515a67bce35df0bbdc7915446a3bb815
Matthias Hoelzl <mhoelzl@ipp.mpg.de>
2023-06-02 16:19:38 +0200

    Doing new jorek2_povray (11)

--- diagnostics/jorek2_povray.f90
+++ diagnostics/jorek2_povray.f90
@@ -249,7 +249,7 @@ function colormap1(v, vmin, vmax)
   else
     v_norm = min(max(v,vmin),vmax) / (vmax-vmin) ! crop to min/max range and normalize
     
-    colormap1 = (/ v_norm, 1.d0, 1.d0-v_norm, 0.0d0 /)
+    colormap1 = (/ min(1.d0,2.d0-2.d0*v_norm), 1.d0-2.d0*(v_norm-0.5d0), min(1.d0,2.d0*v_norm), 0.0d0 /)
   end if
   
 end function colormap1
