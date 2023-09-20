eb78e6df2c70d47927a660ff2a60549735db3a64
Cristian Sommariva <cristian.sommariva@epfl.ch>
2023-09-20 17:12:37 +0200

    bufixed unit test mod_particle_types_test, test function called into fruit run_case_name

--- particles/tests/mod_particle_types_test.f90
+++ particles/tests/mod_particle_types_test.f90
@@ -28,9 +28,9 @@ subroutine run_fruit_particle_types
   write(*,'(/A)') "  ... setting-up: particle types tests"
   call setup
   write(*,'(/A)') "  ... running: particle types tests"
-  call test_particle_copy
-  call test_individual_particle_copy
-  call test_particle_get_q
+!  call run_test_case(test_particle_copy,'test_particle_copy')
+  call run_test_case(test_individual_particle_copy,'test_individual_particle_copy')
+  call run_test_case(test_particle_get_q,'test_particle_get_q')
   write(*,'(/A)') "  ... tearing-up: particle types tests"
 end subroutine run_fruit_particle_types
 
