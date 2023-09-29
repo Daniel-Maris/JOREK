ae7f467978c8d78ad42f9714616c9008fd838096
Eric Nardon <eric.nardon@cea.fr>
2023-09-29 10:40:04 +0200

    Setting L_tube=0 by default

--- models/preset_parameters.f90
+++ models/preset_parameters.f90
@@ -710,6 +710,7 @@ subroutine preset_parameters
 
   JET_MGI = .false.
   ASDEX_MGI = .false.
+  t_ns  = 2.d3
   ns_amplitude = 0.d0
   ns_R      = 3.2d0
   ns_Z      =  1.5d0
@@ -734,12 +735,14 @@ subroutine preset_parameters
   energy_teleported = 0.d0 
   constant_imp_source = 0.d0
 
+  L_tube = 0. ! Needed to ensure injection starts at t_ns when JET_MGI=ASDEX_MGI=.false.
+
   !====== JET DMV-2 parameters
-  L_tube = 2.4d0
+  !L_tube = 2.4d0
   K_Dmv = 4.d-2
   A_Dmv = 1.77d-2
   V_Dmv = 9.75d-4
-  t_ns  = 2.d3
+
   !======= Additional parameters for SPI =======
   spi_Vel_Rref    = 0.0d0
   spi_Vel_Zref    = 0.0d0
