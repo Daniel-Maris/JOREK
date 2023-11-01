9408a169f54e6ff24641b662c3102726aee11290
Cristian Sommariva <cristian.sommariva@epfl.ch>
2023-11-01 19:48:54 +0100

    updated the test folder from non_regression/unit_tests to unit_test

--- util/compile_run_unit_tests.py
+++ util/compile_run_unit_tests.py
@@ -499,7 +499,7 @@ def generate_argument_parser():
   description='generate, compile and execute JOREK unit tests')
   parser.add_argument('--directories','-d',type=str,nargs='*',\
   required=False,action='store',dest='test_dirs',\
-  default=['./particles/tests','./non_regression_tests/unit_tests'],\
+  default=['./particles/tests','./unit_tests'],\
   help='relative paths of the directories containing unit tests')
   parser.add_argument('--parallelisms','-p',type=str,nargs='*',\
   required=False,action='store',dest='test_parallel',\
