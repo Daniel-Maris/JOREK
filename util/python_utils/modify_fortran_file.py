6a183fea629e6a01c495082e9a21465d40135b88
Cristian Sommariva <cristian.sommariva@epfl.ch>
2023-11-20 17:20:22 +0100

    added method for removing lines returned from variable to be modified and ad pendices to python string when converted to fortran

--- util/python_utils/modify_fortran_file.py
+++ util/python_utils/modify_fortran_file.py
@@ -8,6 +8,14 @@
 # to a copy of the source file but not on the source. #
 # --------------------------------------------------- #
 
+# add ' as characters to string
+# inputs: 
+#   string: (string) string to be modified
+# outputs:
+#   string: (string) string beginning and ending with '
+def fortran_compatible_string(string):
+  return "'"+string+"'"
+
 # Class containing procedures used for modifying
 # FORTRAN files
 class ModifyFortranFile():
@@ -43,7 +51,11 @@ class ModifyFortranFile():
     if(variable is dict):
       print('Warning dictionaries are not converted in fortran string!')
       return ''
-    fortran_string = str(variable)
+    if(type(variable) is str):
+      fortran_string = fortran_compatible_string(variable)
+    else:
+      fortran_string = variable
+    fortran_string = str(fortran_string)
     fortran_string = fortran_string.replace('False','.false.')
     fortran_string = fortran_string.replace('True','.true.')
     fortran_string = fortran_string.replace('(','[')
@@ -61,11 +73,35 @@ class ModifyFortranFile():
   #                    compiled regexes
   # outputs:
   #   string: (string) modified string
-  def modify_string_regex(self,string,dict_substitutes,dict_regex):
+  def modify_string_regex(self,string,remove_next_line,dict_substitutes,\
+  dict_regex_sub,dict_regex_return,regex_return_break,regex_double_return):
+    import re 
+    # check if the line or part of it must be remobed
+    if(remove_next_line):
+      # check if there is a separator from the beginning of the line
+      if(regex_return_break.match(string)):
+        # remove all characters from the beginning of the line up to
+        # the separaor ; and do not remove the next line line
+        string = regex_return_break.sub('',string); 
+        remove_next_line = False;
+      # check if there is a retun at the end of the line without separators
+      elif(regex_double_return.match(string)):
+        string = '' # remove all line
+      # there is no separator but the line is not returned at the end
+      # remove all line and do not remove the next line
+      else:
+        string = ''; remove_next_line = False;
+    # check if the line contains a variable to be modified
     if(any(key in string for key in dict_substitutes.keys())):
+      # check if there is a variable to be modified at the end
+      # of the string and the string is returned, if yes remove
+      # the next line of the beginning of it
+      remove_next_line = any(regex_return.match(string) \
+      for regex_return in dict_regex_return.values())
+      # modify all selected variables with the new ones
       for key,substitute in dict_substitutes.items():
-        string = dict_regex[key].sub(substitute,string)
-    return string
+        string = dict_regex_sub[key].sub(substitute,string)
+    return string,remove_next_line
 
   # Modify variables in destination files from dictionary.
   # variables are identified looking for 'key =' substrings
@@ -77,8 +113,13 @@ class ModifyFortranFile():
   def modify_variables(self,dict_variables):
     from string import whitespace
     import re
+    remove_next_line = False
     # create dictionary of compiled regular expression and substitution strings
-    dict_compiled_regex = dict((key,re.compile(re.escape(key)+\
+    regex_return_break = re.compile(r"^.*?;")
+    regex_double_return = re.compile(r"^.*,&$")
+    dict_compiled_regex_return = dict((key,re.compile(re.escape(key)+\
+    "(\s+|)=.*&$")) for key in dict_variables.keys())
+    dict_compiled_regex_sub    = dict((key,re.compile(re.escape(key)+\
     "(\s+|)=.*?(;|$)")) for key in dict_variables.keys())
     dict_substitute_str = dict((key,"".join([key,'=',self.convert_variable_fortran_string(\
     variable),';'])) for key,variable in dict_variables.items()) 
@@ -87,8 +128,9 @@ class ModifyFortranFile():
     with self.source_path.open(mode='r') as source:
       with(self.dest_path.open(mode='w+')) as destination:
         for line in source:
-          line = self.modify_string_regex(line,dict_substitute_str,dict_compiled_regex)
-          destination.write(line)
+          line,remove_next_line = self.modify_string_regex(line,remove_next_line,\
+          dict_substitute_str,dict_compiled_regex_sub,dict_compiled_regex_return,\
+          regex_return_break,regex_double_return); destination.write(line)
 
 # Test main
 if __name__ == '__main__':
@@ -105,4 +147,5 @@ if __name__ == '__main__':
   fortran_modifier = ModifyFortranFile(source_file,source_dir=source_dir,\
   dest_dir=dest_dir,modify_name=modify_name,separator=separator)  
   print(fortran_modifier)
+  fortran_modifier.modify_variables(dict_variables)
   del fortran_modifier 
