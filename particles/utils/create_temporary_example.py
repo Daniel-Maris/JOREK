b9b320f0997c7172b86536f35b158e481524e32a
Cristian Sommariva <cristian.sommariva@epfl.ch>
2023-11-21 15:14:57 +0100

    baseline for create_temporary_example

--- particles/utils/create_temporary_example.py
+++ particles/utils/create_temporary_example.py
@@ -16,25 +16,51 @@ def insert_packages_paths(package_paths,position=0):
   for package_path in package_paths:
     sys.path.insert(position,os.path.abspath(package_path))
 
+# Modify the particle example file an write it in a new file
+# inputs:
+#   example_file:          (string) name of the example file
+#   example_path:          (string) path to the example directory
+#   dest_path:             (string) destination directory of 
+#                          the new example file
+#   name_modifier:         (string) modifier of the example name
+#   separator:             (string) path separator
+#   dict_params_to_change: (dict) dictionary containing the
+#                          parameter to change
+def create_modified_example(example_file,example_path,\
+  dest_path,name_modifier,separator,dict_params_to_change):
+  from modify_fortran_file import ModifyFortranFile
+  file_modifier = ModifyFortranFile(example_file,\
+  source_dir=example_path,dest_dir=dest_path,\
+  modify_name=name_modifier,separator=separator)
+  file_modifier.modify_variables(dict_params_to_change)
+  del file_modifier
+
 # Argument parser -------------------------------
 # outputs:
 #   parser: (namespace) namespace containing the 
 #           inputs as attributes
-def generate_argument_parser(dict_path='./util/python_utils'):
+def generate_argument_parser():
   from argparse import ArgumentParser
-  # add dictionary path
-  insert_packages_paths([dict_path])
   from dictionary_parser_class import ParseDictionary
   # define parser
   parser = ArgumentParser(description=\
   "Automatically generate Fast camera example files with user's inputs")
-  parser.add_argument('--example_dir','-ed',type=str,required=False,\
+  parser.add_argument('--example-dir','-ed',type=str,required=False,\
   action='store',dest='example_dir',default='./particles/postprocessors/examples',\
   help='Directory path to the examples, default: ./particles/postprocessors/examples')
-  parser.add_argument('--example_name','-efn',type=str,required=False,\
+  parser.add_argument('--example-name','-efn',type=str,required=False,\
   action='store',dest='example_name',default='camera_RE_gyroaverage_synchrotron_example',\
   help='Filename of the example to be modified, default: camera_RE_gyroaverage_synchrotron_example')
-  parser.add_argument('-dptc','--dict-params-to-change',dest='param_to_change_dict',\
+  parser.add_argument('--destination-dir','-dd',type=str,required=False,\
+  action='store',dest='dest_dir',default='.',\
+  help='Destination directory of the modified example file, default: .')
+  parser.add_argument('--name-modifier','-nm',type=str,required=False,\
+  action='store',dest='name_modifier',default='.',\
+  help='Characters recognising a modified example, default: _modified')
+  parser.add_argument('--separator','-sep',type=str,required=False,\
+  action='store',dest='separator',default='/',\
+  help='Path separator, default: /')
+  parser.add_argument('-dptc','--dict-params-to-change',dest='params_to_change_dict',\
   action=ParseDictionary,required=None,default={'n_frames':1,'n_times':1,\
   'fields_filename':'jorek_restart','image_filename':'result_intensities',\
   'n_groups':1,'n_spectra':1,'n_wavelengths':40,'n_int_camera_param':5,\
@@ -47,6 +73,11 @@ def generate_argument_parser(dict_path='./util/python_utils'):
 
 # Execute script --------------------------------
 if __name__ == "__main__":
+  # data
+  package_paths = ['./util/python_utils']
+  # execute script
+  insert_packages_paths(package_paths)
   args = generate_argument_parser()
-  print(args.param_to_change_dict,args.example_name,args.example_dir)
+  create_modified_example(args.example_name,args.example_dir,args.dest_dir,\
+  args.name_modifier,args.separator,args.params_to_change_dict)
 # -----------------------------------------------
