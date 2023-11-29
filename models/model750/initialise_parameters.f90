2d2a42591439d58a0f6c18ded9044ed8595343db
John Morris <john.morris@ukaea.uk>
2023-11-29 15:45:41 +0000

    bugfix/model7xx_grid_paras - do not remove eta_coul_log_dep from develop branch

--- models/model750/initialise_parameters.f90
+++ models/model750/initialise_parameters.f90
@@ -165,7 +165,7 @@ namelist /in1/  tstep, nstep, tstep_n, nstep_n,                     &
                 vms_coeff_rho, vms_coeff_Ti, vms_coeff_Te,          &
                 vms_coeff_T, vms_coeff_rhon, vms_coeff_rhoimp,      &
                 vacuum_min, strumpack_matching,                     &
-                forceSDN, SDN_threshold
+                forceSDN, SDN_threshold, eta_coul_log_dep
 
 
 if (my_id .eq. 0) then
