aed8c20a3fafc3912d9399f7d97d0ae86d63e069
Cristian Sommariva <cristian.sommariva@epfl.ch>
2024-10-29 21:29:19 +0100

    changed order of send/recv buffer for native-hdf5 gatherv 3D

--- diagnostics/tests/mod_hdf5_io_module_mpi_test.f90
+++ diagnostics/tests/mod_hdf5_io_module_mpi_test.f90
@@ -770,12 +770,12 @@ subroutine test_HDF5_array3D_saving_r8()
   call HDF5_close(file_id); call remove_file(filename);
   call assert_equals_extended(n_elements(1),n_elements(2),n_elements(3),test_array,&
   result_array,tol_r8,"Error test HDF5 I/O 3D double posix: test and result array mismatch!")
-  filename = trim(filename_base)//trim(extension); elements_all = n_elements(1);
+  filename = trim(filename_base)//trim(extension); elements_all = n_elements(3);
   displs = 0; do ii=2,n_tasks_loc; displs(ii)=sum(elements_all(1:ii-1)); enddo;
-  offset = [rank_loc*n_elements(1),0,0]; test_array = 0; 
+  offset = [0,0,rank_loc*n_elements(3)]; test_array = 0; 
   if(rank_loc.eq.master_rank) call HDF5_open_or_create(trim(filename),file_id,ierr=ifail_loc) 
-  call HDF5_array3D_saving_native_or_gatherv(file_id,result_array,sum(elements_all),&
-  n_elements(2),n_elements(3),datasetname,.true.,dim1_all_tasks=elements_all,&
+  call HDF5_array3D_saving_native_or_gatherv(file_id,result_array,n_elements(1),n_elements(2),&
+  sum(elements_all),datasetname,.true.,dim3_all_tasks=elements_all,&
   displs=displs,mpi_rank=rank_loc,n_cpu=n_tasks_loc,mpi_comm_loc=mpi_comm_loc)
   if(rank_loc.eq.master_rank) call HDF5_close(file_id); call MPI_Barrier(mpi_comm_loc,ifail_loc);
   call HDF5_open(filename,file_id,ifail_loc,create_access_plist_in=access_hdf5_parallel,&
