2af2277ed3462e903d62933c2c9835dffe1446eb
Daniel Maris <danielmaris@iter.org>
2024-12-10 15:21:40 +0100

    Fixed find_edge_elements and added vpar to calc_NeTe
    The latter is necessary for calculating Gamma_d correctly but was
    forgotten in the previous commit.

--- particles/mod_fields.f90
+++ particles/mod_fields.f90
@@ -227,8 +227,8 @@ pure subroutine calc_F_profile(fields,i_elm,s,t,phi,Fprof)
 
 end subroutine calc_F_profile
 
-pure subroutine calc_NeTe(fields, time, i_elm, st, phi, n_e, T_e, grad_T_e)
-  use phys_module, only: central_density
+pure subroutine calc_NeTe(fields, time, i_elm, st, phi, n_e, T_e, grad_T_e, vpar)
+  use phys_module, only: central_density, central_mass
   use constants
   class(fields_base), intent(in)                    :: fields
   integer, intent(in)                               :: i_elm
@@ -236,17 +236,19 @@ pure subroutine calc_NeTe(fields, time, i_elm, st, phi, n_e, T_e, grad_T_e)
   real*8, intent(out)                               :: n_e !< electron density [m^-3]
   real*8, intent(out)                               :: T_e !< electron temperature [K]
   real*8, intent(out), optional, dimension(3)       :: grad_T_e !< gradient of electron temperature [K/m]
+  real*8, intent(out), optional                     :: vpar !< parallel velocity [m/s]
 
-  real*8, dimension(2) :: P, P_s, P_t, P_phi, P_time
+  real*8, dimension(3) :: P, P_s, P_t, P_phi, P_time
   real*8               :: R, R_s, R_t, Z, Z_s, Z_t, xjac
-  real*8 :: T_norm !< temperature normalisation
+  real*8               :: T_norm !< temperature normalisation
+  real*8               :: v_norm !< vpar normalisation
 
 #if (JOREK_MODEL == 400)
-! electron temperature
-  call fields%interp_PRZ(time,i_elm,[5,8],2,st(1),st(2),phi,P,P_s,P_t,P_phi,P_time,R,R_s,R_t,Z,Z_s,Z_t)
+  ! electron temperature
+  call fields%interp_PRZ(time,i_elm,[5,8,7],3,st(1),st(2),phi,P,P_s,P_t,P_phi,P_time,R,R_s,R_t,Z,Z_s,Z_t)
 #else
-! electron temperature + ion temperature (assumed equal)
-  call fields%interp_PRZ(time,i_elm,[5,6],2,st(1),st(2),phi,P,P_s,P_t,P_phi,P_time,R,R_s,R_t,Z,Z_s,Z_t)
+  ! electron temperature + ion temperature (assumed equal)
+  call fields%interp_PRZ(time,i_elm,[5,6,7],3,st(1),st(2),phi,P,P_s,P_t,P_phi,P_time,R,R_s,R_t,Z,Z_s,Z_t)
 #endif
 
   n_e = max(central_density * P(1) * 1d20,1d16)                           ! plasma density [1/m^3], capped against negative
@@ -262,6 +264,11 @@ pure subroutine calc_NeTe(fields, time, i_elm, st, phi, n_e, T_e, grad_T_e)
     grad_T_e = T_norm*[(  P_s(2) * Z_t - P_t(2) * Z_s)/ xjac, &
                      (- P_s(2) * R_t + P_t(2) * R_s)/ xjac, &
                      P_phi(2)/R]
+  end if
+
+  if (present(vpar)) then
+    v_norm = 1.d0/sqrt(MU_ZERO*central_mass*central_density*1.d20*MASS_PROTON)
+    vpar = P(3)*v_norm !note that it should still be multiplied by the norm of the B field to be si
   end if
 end subroutine calc_NeTe
 
