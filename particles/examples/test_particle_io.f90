a41d073ffe013ac17654b6297d8432d9992c01d6
Cristian Sommariva <cristian.sommariva@epfl.ch>
2024-12-10 19:46:14 +0100

    adapted test_particle_io.f90 to new standard and added wtime for timing both writing and reading routines

--- particles/examples/test_particle_io.f90
+++ particles/examples/test_particle_io.f90
@@ -6,6 +6,7 @@ program test_particle_io
   use hdf5,                           only: H5F_ACC_TRUNC_F
   use mod_gnu_rng,                    only: set_seed_sys_time
   use mod_gnu_rng,                    only: gnu_rng_interval
+  use mod_mpi_tools,                  only: get_mpi_wtime
   use mod_particle_types,             only: particle_fieldline_id,particle_fieldline
   use mod_particle_types,             only: particle_gc_id,particle_gc_vpar_id
   use mod_particle_types,             only: particle_kinetic_id,particle_kinetic_leapfrog_id
@@ -27,11 +28,15 @@ program test_particle_io
   integer                          :: ii,jj,ifail
   integer                          :: n_groups,fill_particle_type
   integer                          :: mpi_comm_io,mpi_info_io
+  integer                          :: my_id,n_tasks
   integer                          :: file_access_write
   integer,dimension(2)             :: Z_interval,seed_interval
-  integer,dimension(:),allocatable :: p_types,n_particles
+  integer,dimension(:),allocatable :: p_types,n_particles,Z_values
+  real*8                           :: start_time,end_time
+  real*8                           :: write_time,read_time
   real*8,dimension(2)              :: mass_interval
-  logical                          :: do_write,do_read
+  real*8,dimension(:),allocatable  :: mass_values,write_times,read_times
+  logical                          :: do_write,do_read,do_test
   logical                          :: use_hdf5_access_properties
   logical                          :: use_native_hdf5_mpio
   logical                          :: mpio_collective
@@ -41,6 +46,7 @@ program test_particle_io
   filename           = "particle_restart_test.h5"
   do_write           = .true.
   do_read            = .true.
+  do_test            = .true.
   ifail              = 0 
   n_groups           = 7  !< number of groups = number of particle types
   allocate(n_particles(n_groups)); allocate(p_types(n_groups))
@@ -70,13 +76,23 @@ program test_particle_io
     call fill_particles(n_groups,sim_write%groups,fill_particle_in=fill_particle_type,&
     rank_in=sim_write%my_id)
     call set_seed_sys_time(seed_interval)
+    if(allocated(mass_values)) deallocate(mass_values); allocate(mass_values(n_groups));
+    if(allocated(Z_values))    deallocate(Z_values);    allocate(Z_values(n_groups));
+    if(sim_write%my_id.eq.master_task) then
+      do ii=1,n_groups
+        call gnu_rng_interval(mass_interval,mass_values(ii))
+        call gnu_rng_interval(Z_interval,Z_values(ii))
+      enddo
+    endif
+    call MPI_Bcast(mass_values,n_groups,MPI_REAL8,master_task,mpi_comm_io,ifail)
+    call MPI_Bcast(Z_values,n_groups,MPI_INTEGER,master_task,mpi_comm_io,ifail)
     do ii=1,n_groups
-      call gnu_rng_interval(mass_interval,sim_write%groups(ii)%mass)
-      call gnu_rng_interval(Z_interval,sim_write%groups(ii)%Z)
+      sim_write%groups(ii)%mass = mass_values(ii); sim_write%groups(ii)%Z = Z_values(ii);
     enddo
+    if(allocated(mass_values)) deallocate(mass_values); if(allocated(Z_values)) deallocate(Z_values);
   endif
   !> Read
-  read_particle  = read_action(filename=filename,mpi_comm_in=mpi_comm_io,&
+  read_particle = read_action(filename=filename,mpi_comm_in=mpi_comm_io,&
   mpi_info_in=mpi_info_io,use_hdf5_access_properties_in=use_hdf5_access_properties)
   if(do_read) then
     event_read = [event(read_particle)];
@@ -84,10 +100,22 @@ program test_particle_io
     n_cpu=sim_write%n_cpu,do_jorek_init_in=.false.)
   endif
   !> Read - write operations -------------------------------------
-  if(do_write) call with(sim_write,event_write)
-  if(do_read)  call with(sim_read,event_read)
+  if(do_write) then
+    start_time = 0d0; end_time = 0d0; write_time = 0d0;
+    start_time = get_mpi_wtime()
+    call with(sim_write,event_write)
+    end_time   = get_mpi_wtime()
+    write_time = end_time - start_time
+  endif
+  if(do_read) then
+    start_time = 0d0; end_time = 0d0; read_time = 0d0;
+    start_time = get_mpi_wtime()
+    call with(sim_read,event_read)
+    end_time   = get_mpi_wtime()
+    read_time = end_time - start_time
+  endif
   !> Check that files are equals ---------------------------------
-  if(do_write.and.do_read) then
+  if(do_write.and.do_read.and.do_test) then
     do ii=2,n_groups
       select type (p=>sim_write%groups(ii)%particles)
       type is (particle_fieldline)
@@ -113,9 +141,22 @@ program test_particle_io
       call fruit_finalize_mpi(sim_write%n_cpu,sim_write%my_id)
     enddo
   endif
+  !> Log data ---------------------------------------------------- 
+  call MPI_comm_rank(mpi_comm_io,my_id,ifail)
+  call MPI_comm_size(mpi_comm_io,n_tasks,ifail)
+  if(allocated(write_times)) deallocate(write_times); allocate(write_times(n_tasks))
+  if(allocated(read_times))  deallocate(read_times);  allocate(read_times(n_tasks));
+  call MPI_Gather(write_time,1,MPI_REAL8,write_times,1,MPI_REAL8,master_task,mpi_comm_io,ifail)
+  call MPI_Gather(read_time,1,MPI_REAL8,read_times,1,MPI_REAL8,master_task,mpi_comm_io,ifail)
+  if(my_id.eq.master_task) then
+    write(*,*) "Time for writing data [s]: ",maxval(write_times)
+    write(*,*) "Time for reading data [s]: ",maxval(read_times)
+  endif
   !> Clean-up ---------------------------------------------------- 
-  if(allocated(p_types))     deallocate(p_types); 
-  if(allocated(n_particles)) deallocate(n_particles);
+  if(allocated(p_types))     deallocate(p_types)
+  if(allocated(n_particles)) deallocate(n_particles)
+  if(allocated(write_times)) deallocate(write_times)
+  if(allocated(read_times))  deallocate(read_times)
   if((sim_write%my_id.eq.master_task).and.remove_file) then
     inquire(file=filename,exist=file_exist)
     if(file_exist) call system("rm -rf "//trim(filename))
