24250a05416a682dd552ffd3f014285b51d6fdb3
George Su <george.su@ipp.mpg.de>
2025-01-10 14:53:04 +0100

    removed accidental changes

--- models/model303/initialise_parameters.f90
+++ models/model303/initialise_parameters.f90
@@ -133,15 +133,15 @@ namelist /in1/  tstep, nstep, tstep_n, nstep_n,                     &
                 mode_families_modes, n_mode_families,               &
                 weights_per_family, autodistribute_ranks,           &
                 ranks_per_family, treat_axis,                       &
-                tstep_particles, nstep_particles,      &
+                n_particles, tstep_particles, nstep_particles,      &
                 nsubstep_particles,                                 &
                 filter_perp,    filter_hyper,    filter_par,        &
                 filter_perp_n0, filter_hyper_n0, filter_par_n0,     &
+                use_kn_cx, use_kn_sputtering, use_kn_ionisation,             &
                 use_ncs, use_pcs, use_ccs, cte_current_FB_fact,     &
                 CARIDDI_mode, use_newton, maxNewton, gamma_Newton,  &
                 alpha_Newton, vacuum_min, strumpack_matching,       &
                 export_aux_node_list, xpoint_search_tries,          &
-                use_manual_random_seed, manual_seed,                &
                 bgf_rpolar, bgf_tht
 
 if (my_id .eq. 0) then
@@ -210,7 +210,6 @@ if (my_id .eq. 0) then
 
   if (sum(nstep_n) .gt. 0) then
     nstep = sum(nstep_n)
-    tstep = tstep_n(1)
   else
     tstep_n    = 0.d0
     tstep_n(1) = tstep
