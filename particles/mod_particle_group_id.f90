b54d265c02e69aa9fe78d6277bbba6e00fafc8e5
George Su <george.su@ipp.mpg.de>
2025-02-13 18:37:59 +0100

    changed the default value of matching_part_config_indices from 1 to n_part_groups_max for easier debugging

--- particles/mod_particle_group_id.f90
+++ particles/mod_particle_group_id.f90
@@ -11,7 +11,7 @@ integer :: id_counter = 0  !< how many ids have been automatically generated. Us
 !> the ith element of this array stores the matching part_group_configs index
 !> for ith group in part_groups_array (i.e converts part_groups_in_use index to 
 !> corresponding part_group_configs index)
-integer, dimension(n_part_groups_max) :: matching_part_config_indices = 1 ! only the first n_part_groups value of this should be accessed
+integer, dimension(n_part_groups_max) :: matching_part_config_indices = n_part_groups_max ! only the first n_part_groups value of this should be accessed
 
 public matching_part_config_indices, match_part_groups_and_configs, generate_part_groups_in_use
 private generate_part_group_id
