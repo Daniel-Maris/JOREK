89def6b2761ec550f7863a160043a78b7901d2c6
Daniel Maris <d.maris@differ.nl>
2025-06-12 16:57:27 +0200

    after succesful fied weight verification
    added diagnostics, e.g. for momentum loss fraction
    use = assignment
    small corrections (for integer N_try and nonzeros)

--- particles/mod_particle_collision.f90
+++ particles/mod_particle_collision.f90
@@ -77,7 +77,7 @@ subroutine neutral_self_collision(this, sim, dt)
   real*8 :: sigma_T !< [m^2] total collisional cross section of the collision pair
 
   integer :: pa_in_elm !< number of particles in the element under consideration
-  integer, parameter :: aim_pa_per_bin = 50 !< wanted amount of super particles in one collisional bin (element is split up to satisfy this)
+  integer, parameter :: aim_pa_per_bin = 50 !< wanted amount of super particles in one collisional bin (element is split up to satisfy this). Bigger means better statistics of particle density, but also loss of locality. Much smaller than 50 (like 2-10 or so) is not advised because the momenta of the particles in the bin might get correlated over time
   integer, dimension(:),       allocatable :: i_pa_elm     !< global particle indices of the particles in this element
   integer, dimension(:,:,:),   allocatable :: n_pa_bin_arr !< number of particles in each collisional bin of this element (s bin, t bin, phi bin)
   integer, dimension(:,:,:),   allocatable :: i_loc_bin    !< local index of particles in each collisional bin of this element (s bin, t bin, phi bin)
@@ -104,7 +104,7 @@ subroutine neutral_self_collision(this, sim, dt)
   real*8 :: v2f(3) !< final velocity of particle 2
   
   !global diagnostics
-  integer, parameter :: n_diag=7, i_P_av=1, i_tau=2, i_sigma_av=3, i_n_pairs=4, i_n_col=5, i_w_col=6, i_Pgt1=7
+  integer, parameter :: n_diag=10, i_P_av=1, i_tau=2, i_sigma_av=3, i_n_pairs=4, i_n_col=5, i_w_col=6, i_Pgt1=7, i_V=8, i_d_av=9, i_angle_av=10
   real*8 :: global_diag(n_diag) !< global diagnostics
   real*8 :: reduced_global_diag(n_diag) !< MPI reduced global_diag
   character(len=100) :: FMT !< writing format for diagnostics
@@ -212,7 +212,7 @@ subroutine neutral_self_collision(this, sim, dt)
         
         pa_in_elm = size(i_pa_elm,1)        
         
-        max_n_pa(1) = pa_in_elm
+        if(pa_in_elm > max_n_pa(1)) max_n_pa(1) = pa_in_elm
 
         !> skip collisions if there are nearly no particles
         if(pa_in_elm .lt. 2*n_phi) cycle
@@ -315,16 +315,22 @@ subroutine neutral_self_collision(this, sim, dt)
               ls = sqrt(R_s**2 + Z_s**2)/real(ns)
               lt = sqrt(R_t**2 + Z_t**2)/real(nt)
               V_c = ls*lt*(2.d0*PI*R/real(n_period*n_phi))
-                            
+
+              global_diag(i_V) = global_diag(i_V) + V_c !< sanity check on the total volume
+              
               ! prepare for drawing random particles
               call i_random%reset(n_pa_bin)
 
               ! determine the number of pairs to be tried for collision
               N_try = nint(0.5d0*n_pa_bin*this%P_max_elm(i_elm))
-              ! if N_try = 0 that's fine, it'll just skip the i_pair do loop
-
-              if(N_try > n_pa_bin/2) write(*,*) "ERROR: some particles will be used twice (N_try,n_pa_bin/2)",N_try,n_pa_bin/2
-
+              if(N_try < 1) N_try = 1 ! to make sure that for low collisional chance the number of collisions are not rounded off to 0
+              
+              if(N_try > n_pa_bin/2) then
+                !$omp critical
+                write(*,*) "ERROR: some particles will be used twice (N_try,n_pa_bin/2)",N_try,n_pa_bin/2
+                !$omp end critical
+              end if
+              
               ! loop over to-be-tried collisional pairs for this bin
               do i_pair = 1,N_try !< dummy variable for loop
                 !generate random number for the pair
@@ -334,9 +340,9 @@ subroutine neutral_self_collision(this, sim, dt)
                 i_pa1 = i_random%next(RN(1))
                 i_pa2 = i_random%next(RN(2))
 
-                !copy from MPI pa array
-                call copy_particle_kinetic_leapfrog(pa(i_pa_bin(is,it,i_phi)%pa_ind(i_pa1)),pa1) 
-                call copy_particle_kinetic_leapfrog(pa(i_pa_bin(is,it,i_phi)%pa_ind(i_pa2)),pa2) 
+                !copy from MPI pa array (with generic assignment =)
+                pa1 = pa(i_pa_bin(is,it,i_phi)%pa_ind(i_pa1))
+                pa2 = pa(i_pa_bin(is,it,i_phi)%pa_ind(i_pa2))
 
                 ! calculate P for this collision pair
                 w1 = pa1%weight
@@ -355,11 +361,13 @@ subroutine neutral_self_collision(this, sim, dt)
 
                 !diagnostics
                 global_diag(i_sigma_av) = global_diag(i_sigma_av) + sigma_T
+                global_diag(i_d_av)     = global_diag(i_d_av)     + sqrt(sigma_T/PI)
                 global_diag(i_P_av)     = global_diag(i_P_av)     + P_real
                 global_diag(i_n_pairs)  = global_diag(i_n_pairs)  + 1
                 if (P_real > P_max_mpi) P_max_mpi = P_real
                 
-                P_try = P_real / this%P_max_elm(i_elm)
+                ! rescale with N_try/(0.5*n_pa_bin) rather than with this%P_max_elm(i_elm) directly to correct for integer effects of N_try
+                P_try = P_real * (0.5d0*n_pa_bin)/N_try
 
                 if (P_try > 1.d0) then
                   global_diag(i_Pgt1) = global_diag(i_Pgt1) + 1
@@ -378,6 +386,8 @@ subroutine neutral_self_collision(this, sim, dt)
                   ! updates velocities of colliding part of super particles
                   call binary_elastic_collision(m1,m2,pa1%v,pa2%v,Theta,alpha,v1f,v2f)
 
+                  global_diag(i_angle_av) = global_diag(i_angle_av) + angle(pa1%v,v1f) + angle(pa2%v,v2f)
+                  
                   !correct new velocities for non-colliding component in heaviest particle
                   if(w1 .gt. w2) then
                     pa1%v = (w_s/w1) * v1f + (1-w_s/w1) * pa1%v
@@ -392,9 +402,9 @@ subroutine neutral_self_collision(this, sim, dt)
                   !calculating the average collision time tau of colliding particles
                   global_diag(i_n_col) = global_diag(i_n_col) + 2
                   
-                  ! copy back into MPI pa array
-                  call copy_particle_kinetic_leapfrog(pa1, pa(i_pa_bin(is,it,i_phi)%pa_ind(i_pa1))) 
-                  call copy_particle_kinetic_leapfrog(pa2, pa(i_pa_bin(is,it,i_phi)%pa_ind(i_pa2))) 
+                  ! copy back into MPI pa array (with generic assignment =)
+                  pa(i_pa_bin(is,it,i_phi)%pa_ind(i_pa1)) = pa1
+                  pa(i_pa_bin(is,it,i_phi)%pa_ind(i_pa2)) = pa2
 
                 end if !collision happens
 
@@ -448,11 +458,14 @@ subroutine neutral_self_collision(this, sim, dt)
     if(reduced_global_diag(i_Pgt1) > 0.5) write(*,"(A,I10.0,A,I10.0,A)") "WARNING: P_try>1 for ",nint(reduced_global_diag(i_Pgt1))," out of ",nint(reduced_global_diag(i_n_pairs))," collision attempts during this timestep"
 
     !getting the averages by dividing the sum by number of pairs
-    reduced_global_diag(i_P_av)     = reduced_global_diag(i_P_av)     / max(reduced_global_diag(i_n_pairs), 1.d-10)
-    reduced_global_diag(i_sigma_av) = reduced_global_diag(i_sigma_av) / max(reduced_global_diag(i_n_pairs), 1.d-10)
-    reduced_global_diag(i_tau)      = dt/reduced_global_diag(i_P_av)
+    reduced_global_diag(i_P_av)     = reduced_global_diag(i_P_av)     / nonzero(reduced_global_diag(i_n_pairs))
+    reduced_global_diag(i_sigma_av) = reduced_global_diag(i_sigma_av) / nonzero(reduced_global_diag(i_n_pairs))
+    reduced_global_diag(i_d_av)     = reduced_global_diag(i_d_av)     / nonzero(reduced_global_diag(i_n_pairs))
+    reduced_global_diag(i_angle_av) = reduced_global_diag(i_angle_av) / (nonzero(reduced_global_diag(i_n_col)) * (PI/2))
+    reduced_global_diag(i_tau)      = dt/nonzero(reduced_global_diag(i_P_av))
+    reduced_global_diag(i_V)        = reduced_global_diag(i_V)        / sim%n_mpi
     write(FMT,"(A,I2,A)") "(A,",n_diag,"es15.5)"  
-    write(*,trim(FMT)) "diagnostics (P average/tau average/sigma average/pairs tried/pairs collided/weight collided/occurence P>1) =",reduced_global_diag
+    write(*,trim(FMT)) "diagnostics (P av/tau av/sigma av/pairs tried/pairs coll/weight coll/# P>1/sum V_c/d av/av mom loss frac) =",reduced_global_diag
   end if
   !$ w(2) = omp_get_wtime()
   !$ mmm = mpi_minmeanmax(w(2)-w(1))
@@ -523,7 +536,7 @@ function calc_sigma_T(v_r, m1, m2) result(sigma_T)
   m_r   = m1*m2/(m1+m2)
   m_r   = m_r * ATOMIC_MASS_UNIT
 
-  d = d_ref*sqrt(((2*K_BOLTZ*T_ref/(m_r*v_r**2))**(omega-0.5d0) )/ gamma(2.5d0-omega)) !< gamma here is the gamma function, not phys_module gamma
+  d = d_ref*sqrt(((2*K_BOLTZ*T_ref/max(1.d-40,m_r*v_r**2))**(omega-0.5d0) )/ gamma(2.5d0-omega)) !< gamma here is the gamma function, not phys_module gamma
   sigma_T = PI*d**2
 
 end function calc_sigma_T
@@ -567,7 +580,7 @@ subroutine binary_elastic_collision(m1,m2,v1i,v2i,Theta,alpha,v1f,v2f)
   theta_1 = atan(sin(Theta)/nonzero(m1/m2 + cos(Theta)))
 
   !calculate the velocities in scattering plane
-  scalar_v1p = scalar_v1 * sqrt(1.d0/(1 + (m1/m2)*(sin(theta_1)/sin(theta_2))**2)) !< from energy balance, substituting scalar_v2p (see next line)
+  scalar_v1p = scalar_v1 * sqrt(1.d0/nonzero(1 + (m1/m2)*(sin(theta_1)/nonzero(sin(theta_2)))**2)) !< from energy balance, substituting scalar_v2p (see next line)
   scalar_v2p = -(m1/m2) * (sin(theta_1) / nonzero(sin(theta_2))) * scalar_v1p               !< from perpendicular momentum balance
 
   v2p = scalar_v2p*[cos(theta_2), sin(theta_2)]
@@ -717,5 +730,17 @@ pure function nonzero(in) result(out)
   out = in
   if (abs(in) < tol) out = sign(tol, in)
 end function nonzero
+
+! find the angle between two vectors [0,pi]
+pure function angle(v1,v2) result(alpha)
+  implicit none
+
+  real*8, intent(in) :: v1(3)
+  real*8, intent(in) :: v2(3)
+  real*8             :: alpha
+
+  alpha = acos(dot_product(v1,v2)/nonzero(norm2(v1)*norm2(v2)))
+
+end function angle
 
 end module mod_particle_collision
