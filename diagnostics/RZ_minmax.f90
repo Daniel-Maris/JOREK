b4b86b88692b6a8f4e2f7ac97cc028a76ea14c8e
Luca Venerando Greco <luca.venerando-greco@ipp.mpg.de>
2025-06-23 15:20:13 +0000

    added RZP

--- diagnostics/RZ_minmax.f90
+++ diagnostics/RZ_minmax.f90
@@ -1,17 +1,73 @@
-subroutine RZ_minmax(node_list,element_list,i_elm,Rmin,Rmax,Zmin,Zmax,i_plane_query_in)
-use mod_parameters, only: n_coord_tor
+subroutine RZ_minmax(node_list,element_list,i_elm,Rmin,Rmax,Zmin,Zmax)
+  use phys_module, only: i_plane_rtree
+  use basis_at_gaussian, only: HZ_coord
+  use data_structure
+
+  implicit none
+
+  ! interface
+  !   subroutine RZ_minmax_general (node_list,element_list,i_elm,Rmin,Rmax,Zmin,Zmax,HZ_coord_used,i_slice_query)
+  !     use data_structure
+
+  !     type (type_node_list), intent(in)    :: node_list
+  !     type (type_element_list), intent(in) :: element_list
+  !     integer, intent(in) :: i_elm, i_slice_query
+  !     real*8,  intent(in) :: HZ_coord_used(:,:)
+  !     real*8, intent(out) :: Rmin, Rmax, Zmin, Zmax
+  !   end subroutine RZ_minmax_general
+  ! end interface
+
+  type (type_node_list), intent(in)    :: node_list
+  type (type_element_list), intent(in) :: element_list
+  integer, intent(in) :: i_elm
+
+  real*8, intent(out) :: Rmin, Rmax, Zmin, Zmax
+
+  call RZ_minmax_general(node_list,element_list,i_elm,Rmin,Rmax,Zmin,Zmax,HZ_coord,i_plane_rtree)
+end subroutine RZ_minmax
+
+subroutine RZP_minmax(node_list,element_list,i_elm,Rmin,Rmax,Zmin,Zmax,HZ_coord_used,i_slice_query)
+  use data_structure
+  use mod_element_rtree, only: tree_slices
+  use mod_parameters, only: n_coord_tor
+
+  implicit none
+
+  ! interface
+  !   subroutine RZ_minmax_general (node_list,element_list,i_elm,Rmin,Rmax,Zmin,Zmax,HZ_coord_used,i_slice_query)
+  !     use data_structure
+
+  !     type (type_node_list), intent(in)    :: node_list
+  !     type (type_element_list), intent(in) :: element_list
+  !     integer, intent(in) :: i_elm, i_slice_query
+  !     real*8,  intent(in) :: HZ_coord_used(:,:)
+  !     real*8, intent(out) :: Rmin, Rmax, Zmin, Zmax
+  !   end subroutine RZ_minmax_general
+  ! end interface
+
+  type (type_node_list), intent(in)    :: node_list
+  type (type_element_list), intent(in) :: element_list
+  integer, intent(in) :: i_elm, i_slice_query
+  real*8,  intent(in) :: HZ_coord_used(n_coord_tor,tree_slices)
+  real*8, intent(out) :: Rmin, Rmax, Zmin, Zmax
+
+  call RZ_minmax_general(node_list,element_list,i_elm,Rmin,Rmax,Zmin,Zmax,HZ_coord_used, mod(i_slice_query - 1, tree_slices) + 1)
+end subroutine RZP_minmax
+
+subroutine RZ_minmax_general (node_list,element_list,i_elm,Rmin,Rmax,Zmin,Zmax,HZ_coord_used,i_slice_query)
 use data_structure
-use basis_at_gaussian, only: HZ_coord
-use phys_module, only: i_plane_rtree
 use mod_newton_methods
-use mod_parameters, only: n_order, n_plane
+use mod_parameters, only: n_order
+use mod_element_rtree, only: tree_slices
+use mod_parameters, only: n_coord_tor
 
 implicit none
 
 type (type_node_list), intent(in)    :: node_list
 type (type_element_list), intent(in) :: element_list
-integer, intent(in) :: i_elm
-integer, intent(in), optional :: i_plane_query_in
+integer, intent(in) :: i_elm, i_slice_query
+real*8,  intent(in) :: HZ_coord_used(n_coord_tor,tree_slices)              !< Basis functions of grid representation in toroidal direction
+
 real*8, intent(out) :: Rmin, Rmax, Zmin, Zmax
 
 real*8  :: psimin, psimax, psma, psmi, psmima, psim, psimr, psip, psipr
@@ -19,13 +75,7 @@ real*8  :: aa, bb, cc, det, r, dummy
 real*8,external :: root
 integer :: iv, n, im, n1, n2, i_tor
 real*8  :: s,t,P,P_s,P_t,P_st,P_ss,P_tt
-integer :: k, i_plane_query
-
-if (present(i_plane_query_in)) then
-i_plane_query = mod(i_plane_query_in - 1, n_plane) + 1
-else
-i_plane_query = i_plane_rtree
-endif
+integer :: k
 
 ! --- For n_order>3, we need to use Newton methods (not exactly true, should implement quartic root finder) 
 ! --- Could be important/faster for particles module!!!
@@ -53,17 +103,17 @@ do k=1,2
 
     if ((iv .eq. 1) .or. (iv .eq. 3)) THEN
       do i_tor=1,n_coord_tor
-        PSIM  = PSIM  + node_list%node(n1)%x(i_tor,1,k)*element_list%element(i_elm)%size(iv,1)*HZ_coord(i_tor,i_plane_query)
-        PSIMR = PSIMR + node_list%node(n1)%x(i_tor,2,k)*element_list%element(i_elm)%size(iv,2)*HZ_coord(i_tor,i_plane_query)*3.d0/2.d0
-        PSIP  = PSIP  + node_list%node(n2)%x(i_tor,1,k)*element_list%element(i_elm)%size(im,1)*HZ_coord(i_tor,i_plane_query)
-        PSIPR = PSIPR - node_list%node(n2)%x(i_tor,2,k)*element_list%element(i_elm)%size(im,2)*HZ_coord(i_tor,i_plane_query)*3.d0/2.d0
+        PSIM  = PSIM  + node_list%node(n1)%x(i_tor,1,k)*element_list%element(i_elm)%size(iv,1)*HZ_coord_used(i_tor,i_slice_query)
+        PSIMR = PSIMR + node_list%node(n1)%x(i_tor,2,k)*element_list%element(i_elm)%size(iv,2)*HZ_coord_used(i_tor,i_slice_query)*3.d0/2.d0
+        PSIP  = PSIP  + node_list%node(n2)%x(i_tor,1,k)*element_list%element(i_elm)%size(im,1)*HZ_coord_used(i_tor,i_slice_query)
+        PSIPR = PSIPR - node_list%node(n2)%x(i_tor,2,k)*element_list%element(i_elm)%size(im,2)*HZ_coord_used(i_tor,i_slice_query)*3.d0/2.d0
       end do
     elseif ((iv .eq. 2) .or. (iv .eq. 4)) then
       do i_tor=1,n_coord_tor
-        PSIM  = PSIM  + node_list%node(n1)%x(i_tor,1,k)*element_list%element(i_elm)%size(iv,1)*HZ_coord(i_tor,i_plane_query)
-        PSIMR = PSIMR + node_list%node(n1)%x(i_tor,3,k)*element_list%element(i_elm)%size(iv,3)*HZ_coord(i_tor,i_plane_query)*3.d0/2.d0
-        PSIP  = PSIP  + node_list%node(n2)%x(i_tor,1,k)*element_list%element(i_elm)%size(im,1)*HZ_coord(i_tor,i_plane_query)
-        PSIPR = PSIPR - node_list%node(n2)%x(i_tor,3,k)*element_list%element(i_elm)%size(im,3)*HZ_coord(i_tor,i_plane_query)*3.d0/2.d0
+        PSIM  = PSIM  + node_list%node(n1)%x(i_tor,1,k)*element_list%element(i_elm)%size(iv,1)*HZ_coord_used(i_tor,i_slice_query)
+        PSIMR = PSIMR + node_list%node(n1)%x(i_tor,3,k)*element_list%element(i_elm)%size(iv,3)*HZ_coord_used(i_tor,i_slice_query)*3.d0/2.d0
+        PSIP  = PSIP  + node_list%node(n2)%x(i_tor,1,k)*element_list%element(i_elm)%size(im,1)*HZ_coord_used(i_tor,i_slice_query)
+        PSIPR = PSIPR - node_list%node(n2)%x(i_tor,3,k)*element_list%element(i_elm)%size(im,3)*HZ_coord_used(i_tor,i_slice_query)*3.d0/2.d0
       end do
     endif
 
@@ -106,7 +156,7 @@ do k=1,2
     Zmax = psimax
   endif
 
-ENDDO
+enddo
 
-RETURN
-END
+return
+end subroutine RZ_minmax_general
