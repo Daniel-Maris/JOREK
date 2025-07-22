2cdd4738155bfb0f69ef8459211472552f165a0f
szmate <szmate@toki11.ipp.mpg.de>
2025-07-22 13:24:06 +0200

    added homma2020

--- particles/mod_particle_evolution.f90
+++ particles/mod_particle_evolution.f90
@@ -401,6 +401,10 @@ contains
               !> Homma use temperature in [J] (kb [j/K]* T_e [K] or e [J/eV] * Te_eV [eV])
               q = q_homma2013(kTb, grad_T_e*K_BOLTZ, B, n_b, m_b, q_b) !EL_CHG/K_BOLTZ
 
+              !> Alternatively, use collisionality dependent heat flux
+              !> for more information see S. Q. Korving et al 2024
+              !q = q_homma2020(kTb, grad_T_e*K_BOLTZ, B, n_b, m_b, q_b, 1.) !EL_CHG/K_BOLTZ
+
               !> Calculate coulomb logarithm and limit it to reasonable values
               coulomb_log = coulomb_logarithm(kTb, n_b, particle_tmp%q, q_b, sim%groups(group_num)%mass, m_b)
               coulomb_log = max(10.d0, coulomb_log)
