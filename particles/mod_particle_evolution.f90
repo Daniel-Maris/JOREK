98bc43a53df52d91c9f2affab39cdfc2ba117687
szmate <szmate@toki11.ipp.mpg.de>
2025-09-01 18:26:44 +0200

    testing combined calc_NeTiTe in particle evolution episode 2

--- particles/mod_particle_evolution.f90
+++ particles/mod_particle_evolution.f90
@@ -255,7 +255,7 @@ contains
           limits_coll = T_i_raw * K_BOLTZ / EL_CHG < 0.d0 !< limits for collisions
 #else
           ! Note to self: if with_TiTe this will get Te
-          call sim%fields%calc_NeTe(t, particle_tmp%i_elm, particle_tmp%st, particle_tmp%x(3), n_i, T_e, n_e_raw, T_e_raw, grad_T_i)
+          call sim%fields%calc_NeTiTe2(t, particle_tmp%i_elm, particle_tmp%st, particle_tmp%x(3), n_e=n_i, T_e=T_e, n_e_raw=n_e_raw, T_e_raw=T_e_raw, grad_T_e=grad_T_i)
           limits_coll = T_e_raw * K_BOLTZ / EL_CHG < 0.d0 !< limits for collisions
 #endif
 
