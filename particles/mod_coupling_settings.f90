f390e8294cba97df4a51522473bbf0f58a9ddf05
James Carpenter <zhpw96@durham.ac.uk>
2026-04-01 15:28:06 +0200

    Corrected typo in check_compatibility_epf error message

--- particles/mod_coupling_settings.f90
+++ particles/mod_coupling_settings.f90
@@ -133,7 +133,7 @@ subroutine check_compatibility_epf(group_num)
   if (trim(part_group_configs(group_num)%type) /= 'particle_kinetic_leapfrog') then
     write(*,*) "ERROR: incompatible setting enabled for group '", part_group_configs(group_num)%id, "': "
     write(*,*) "  Currently only type = 'particle_kinetic_leapfrog' is supported for"
-    write(*,*) "  groups with coupling scheme 'ncs'"
+    write(*,*) "  groups with coupling scheme 'epf'"
     stop
   endif
 
