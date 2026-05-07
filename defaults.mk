e4e2ee691fcbc2466c34fe564de0129b0194c8a7
Elias Waagaard <elias.waagaard@ipp.mpg.de>
2026-05-07 13:47:37 +0200

    USE_EXT_FIELD: add flag defaults to defaults.mk

--- defaults.mk
+++ defaults.mk
@@ -172,9 +172,15 @@ ifeq (model750, $(MODEL))
 endif
 
 CGDEP= generate_code                         # Pre-compute analytic expressions from mod_equations for performance
-USE_DOMM ?= 1
+USE_DOMM ?= 0
 ifeq ($(USE_DOMM), 1)
   DEFINES := $(DEFINES) -DUSE_DOMM              # Use Dommaschk potentials, without FE correction of n.B on boundary 
+endif
+USE_EXT_FIELD ?= 0
+ifeq ($(USE_EXT_FIELD), 1)
+  ifeq ($(USE_DOMM), 0)                         # Should only be set if USE_DOMM=0
+    DEFINES := $(DEFINES) -DUSE_EXT_FIELD       # Use external magnetic field from gvec2jorek file
+  endif
 endif
 ifeq (model180, $(MODEL))
   DEFINES := $(DEFINES) -DSEMIANALYTICAL -DSTELLARATOR_MODEL
