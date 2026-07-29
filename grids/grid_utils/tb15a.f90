e8730f2cd272e74710e566ac294fb12c5a147924
Nina Schwarz <nina.schwarz@cea.fr>
2026-07-29 12:47:13 +0200

    cleanup and comments

--- grids/grid_utils/tb15a.f90
+++ grids/grid_utils/tb15a.f90
@@ -1,28 +1,43 @@
+! Todo: make into module
 subroutine tb15a(n,x,f,d,w,lp)
-
+  !-------------------------------
+  ! Calculate periodic spline parameters
+  !-------------------------------
   implicit none
-  
-  integer,intent(in) :: n,lp
-  real(8),intent(in) :: x(n),f(n)
-  real(8),intent(out):: d(n)
-  real(8),intent(inout):: w(*)
-  
+
+  integer,intent(in) :: n,lp      ! Number of points, unit number 
+  real(8),intent(in) :: x(n),f(n) ! evaluation points and function values
+  real(8),intent(out):: d(n)      ! Result: Derivatives at knots
+  real(8),intent(inout):: w(*)    ! Workspace for comptability reasons
+
   real(8) :: a(n-1),b(n-1),c(n-1),rhs(n-1)
   real(8) :: sol(n-1)
-  
+
   real(8) :: h1,h2,f1,f2
   integer :: i,m
-  
-  
+
+
+
+  if (f(n) .ne. f(1)) then
+     write(*,*) "Function values must be periodic", f(n), f(1)
+     stop
+  end if
+
   if (n < 4) then
-     write(lp,*) "n too small"
-     return
+     write(*,*) "More than 4 points needed for spline calculation"
+     stop
   endif
-  
-  
+
+  do i = 2, n
+     if (x(i) .le. x(i-1)) then
+        write(*,*) "Coordinate points must be increasing"
+        stop
+     end if
+  end do
+
   m=n-1
-  
-  
+
+
   !------------------------------------------------------
   ! Build equations for unknowns:
   !
@@ -31,38 +46,34 @@ subroutine tb15a(n,x,f,d,w,lp)
   ! sol(n-1)=d(n)
   !
   !------------------------------------------------------
-  
- do i=2,n
 
-    if(i==n) then
+  do i=2,n
+
+     if(i==n) then
         h2 = 1.d0/(x(2)-x(1))
         f2 = f(2)
-    else
+     else
         h2 = 1.d0/(x(i+1)-x(i))
         f2 = f(i+1)
-    endif
+     endif
 
 
-    h1 = 1.d0/(x(i)-x(i-1))
+     h1 = 1.d0/(x(i)-x(i-1))
+     f1=f(i-1)
 
-    f1=f(i-1)
-
-
-    a(i-1)=h1
-
-    b(i-1)=2.d0*(h1+h2)
-
-    c(i-1)=h2
+     a(i-1)=h1             ! Subdiagonal
+     b(i-1)=2.d0*(h1+h2)   ! Diagonal
+     c(i-1)=h2             ! Supdiagonal
 
 
-    rhs(i-1)=3.d0*( &
+     rhs(i-1)=3.d0*( &
           f2*h2*h2 &
-        + f(i)*(h1*h1-h2*h2) &
-        - f1*h1*h1 )
+          + f(i)*(h1*h1-h2*h2) &
+          - f1*h1*h1 )
 
-enddo
-  
-  
+  enddo
+
+
   !------------------------------------------------------
   ! Cyclic corner terms
   !
@@ -73,105 +84,93 @@ enddo
   ! alpha*x1+a_m*x_{m-1}+b_m*xm
   !
   !------------------------------------------------------
-  
+
   call cyclic_solve(m,a,b,c,a(1),c(m),rhs,sol)
-  
-  
+
+
   do i=2,n
-      d(i)=sol(i-1)
+     d(i)=sol(i-1)
   enddo
   
   d(1)=d(n)
-  
-  
+
+
 end subroutine tb15a
 
-  subroutine cyclic_solve(n,a,b,c,alpha,beta,r,x)
+subroutine cyclic_solve(n,a,b,c,alpha,beta,r,x)
 
-    implicit none
-    
-    integer,intent(in)::n
-    
-    real(8),intent(in)::a(n),b(n),c(n)
-    real(8),intent(in)::alpha,beta
-    real(8),intent(in)::r(n)
-    
-    real(8),intent(out)::x(n)
-    
-    real(8)::bb(n),u(n),z(n)
-    real(8)::gamma,factor
-    integer::i
-    
-    
-    gamma=-b(1)
-    
-    
-    bb=b
-    bb(1)=b(1)-gamma
-    bb(n)=b(n)-alpha*beta/gamma
-    
-    
-    call tridag(n,a,bb,c,r,x)
-    
-    
-    u=0.d0
-    u(1)=gamma
-    u(n)=alpha
-    
-    
-    call tridag(n,a,bb,c,u,z)
-    
-    
-    factor=(x(1)+beta*x(n)/gamma) / &
-           (1.d0+z(1)+beta*z(n)/gamma)
-    
-    
-    do i=1,n
-        x(i)=x(i)-factor*z(i)
-    enddo
-    
-    
-    end subroutine cyclic_solve
-    subroutine tridag(n,a,b,c,r,u)
+  implicit none
 
-implicit none
-
-integer,intent(in)::n
-
-real(8),intent(in)::a(n),b(n),c(n),r(n)
-real(8),intent(out)::u(n)
-
-real(8)::gam(n)
-real(8)::bet
-integer::i
+  integer,intent(in)::n
+  real(8),intent(in)::a(n),b(n),c(n)
+  real(8),intent(in)::alpha,beta
+  real(8),intent(in)::r(n)
+  real(8),intent(out)::x(n)
+  
+  real(8)::bb(n),u(n),z(n)
+  real(8)::gamma,factor
+  integer::i
 
 
-bet=b(1)
+  gamma=-b(1)
 
-if(bet==0.d0) stop "zero pivot"
+  bb=b
+  bb(1)=b(1)-gamma
+  bb(n)=b(n)-alpha*beta/gamma
 
+  call tridag(n,a,bb,c,r,x)
 
-u(1)=r(1)/bet
-
-
-do i=2,n
-
-    gam(i)=c(i-1)/bet
-
-    bet=b(i)-a(i)*gam(i)
-
-    if(bet==0.d0) stop "zero pivot"
-
-    u(i)=(r(i)-a(i)*u(i-1))/bet
-
-enddo
+  u=0.d0
+  u(1)=gamma
+  u(n)=alpha
 
 
-do i=n-1,1,-1
+  call tridag(n,a,bb,c,u,z)
 
-    u(i)=u(i)-gam(i+1)*u(i+1)
+  factor=(x(1)+beta*x(n)/gamma) / &
+       (1.d0+z(1)+beta*z(n)/gamma)
 
-enddo
+
+  do i=1,n
+     x(i)=x(i)-factor*z(i)
+  enddo
+
+
+end subroutine cyclic_solve
+
+
+subroutine tridag(n,a,b,c,r,u)
+
+  implicit none
+
+  integer,intent(in)::n
+
+  real(8),intent(in)::a(n),b(n),c(n),r(n)
+  real(8),intent(out)::u(n)
+
+  real(8)::gam(n)
+  real(8)::bet
+  integer::i
+
+
+  bet=b(1)
+
+  if(bet==0.d0) stop "zero pivot"
+
+  u(1)=r(1)/bet
+
+
+  do i=2,n
+     gam(i)=c(i-1)/bet
+     bet=b(i)-a(i)*gam(i)
+     if(bet==0.d0) stop "zero pivot"
+     u(i)=(r(i)-a(i)*u(i-1))/bet
+  enddo
+
+
+  do i=n-1,1,-1
+     u(i)=u(i)-gam(i+1)*u(i+1)
+  enddo
 
 
 end subroutine tridag
--- grids/grid_utils/tg02a.f90
+++ grids/grid_utils/tg02a.f90
@@ -1,47 +1,53 @@
-subroutine tg02a(ix,n,u,s,d,x,v)
+subroutine tg02a(ix,n,x0,s,d,x,v)
+  
   implicit none
+  !------------------------------------------------------------------
+  ! Routine for interpolation of splines and their derivative
   !------------------------------------------------------------------
   !    n  : number of points
   !    ix : negative 0 -> no initial guess for where xi is
   !        positive -> gues for index close to value x
-  !   u   : the coordinates of the spline points
-  !   s   : the function values of the spline points
+  !   x0  : the coordinates of the spline points
+  !   f   : the function values of the spline points
   !   d   : the derivatives on the spline points
   !   x   : the coordinate where the output is wanted
   !   v(1-4) : value and derivatives of the spline interpolation
   !------------------------------------------------------------------
+
   real*8, intent(in)   :: x
   integer, intent(in)  :: ix,n
-  real*8, intent(inout)   :: d(*),s(*),u(*),v(4)
+  real*8, intent(inout)   :: d(*),s(*),x0(*),v(4)
 
+  ! Local variables
   integer :: j
   logical :: found
-  real*8  :: h00, h10, h01, h11, theta, theta2, theta3, t, eps,h
+  real*8  :: h00, h10, h01, h11, theta, theta2, theta3, t, eps, dx
+
   found = .false.
   eps = 1.d-30
-  if (x .le. u(1)) then
-     if (dabs(x-u(1)) .lt. eps*dabs(u(n)-u(1))) then
+
+  if (x .le. x0(1)) then
+     if (dabs(x-x0(1)) .lt. eps*dabs(x0(n)-x0(1))) then
         j = 1
      else
         v(:) = 0.d0
         return
      end if
-
-  else if (x .ge. u(n)) then
-     if (dabs(x-u(n)) .lt. eps * dabs(u(n)-u(1))) then
+  else if (x .ge. x0(n)) then
+     if (dabs(x-x0(n)) .lt. eps * dabs(x0(n)-x0(1))) then
         j = n-1
      else
         v(:) = 0.d0
         return
      end if
   else 
-     if (ix .gt. 0.d0) j = max(min(ix,n-1),1)
-     if (ix .lt. 0.d0) j = (x-u(1))/(u(n)-u(1))*(n-1)+1 ! initial guess of value
-
+     if (ix .gt. 0) j = max(min(ix,n-1),1)
+     if (ix .le. 0) j = (x-x0(1))/(x0(n)-x0(1))*(n-1)+1 ! initial guess of value
+     
      do while  (.not. found)
-        if (x .gt. u(j+1)) then
+        if (x .gt. x0(j+1)) then
            j = j + 1
-        else if (x .ge. u(j)) then
+        else if (x .ge. x0(j)) then
            found = .true.
         else
            j = j - 1
@@ -49,8 +55,8 @@ subroutine tg02a(ix,n,u,s,d,x,v)
      end do
   end if
 
-  h = u(j+1) - u(j)
-  theta = (x-u(j))/h
+  dx = x0(j+1) - x0(j)
+  theta = (x-x0(j))/dx
   theta2 = theta*theta
   theta3 = theta2*theta
 
@@ -61,12 +67,12 @@ subroutine tg02a(ix,n,u,s,d,x,v)
   h11 = theta3 - theta2
 
   ! Calculate spline values and their derivative
-  v(1) = h00*s(j) + h10*h*d(j) + h01*s(j+1) + h11*h*d(j+1)
-  v(2) = 6.d0*(theta2 - theta)*s(j) + (3.d0*theta2 - 4.d0*theta + 1)*h*d(j) + 6.d0*(-theta2 + theta)*s(j+1) + (3.d0*theta2 - 2.d0*theta)*h*d(j+1)
-  v(2) = v(2)/h
-  v(3) = (12.d0*theta - 6.d0)*s(j) + (6.d0*theta - 4.d0)*h*d(j) + (-12.d0*theta + 6.d0)*s(j+1) + (6.d0*theta - 2.d0)*h*d(j+1)
-  v(3) = v(3)/(h**2)
-  v(4) = 6.d0*(2*s(j) - 2.d0*s(j+1) + h*d(j) + h*d(j+1))/(h**3)
+  v(1) = h00*s(j) + h10*dx*d(j) + h01*s(j+1) + h11*dx*d(j+1)
+  v(2) = 6.d0*(theta2 - theta)*s(j) + (3.d0*theta2 - 4.d0*theta + 1)*dx*d(j) + 6.d0*(-theta2 + theta)*s(j+1) + (3.d0*theta2 - 2.d0*theta)*dx*d(j+1)
+  v(2) = v(2)/dx
+  v(3) = (12.d0*theta - 6.d0)*s(j) + (6.d0*theta - 4.d0)*dx*d(j) + (-12.d0*theta + 6.d0)*s(j+1) + (6.d0*theta - 2.d0)*dx*d(j+1)
+  v(3) = v(3)/(dx**2)
+  v(4) = 6.d0*(2*s(j) - 2.d0*s(j+1) + dx*d(j) + dx*d(j+1))/(dx**3)
 
 
 end subroutine tg02a
